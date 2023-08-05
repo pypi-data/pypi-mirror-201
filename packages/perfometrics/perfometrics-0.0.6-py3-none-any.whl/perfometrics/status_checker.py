17d3c4cfb4978586a072e6460f20440024593b30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:55 +0000

    Add package perfometrics/0.0.6/perfometrics-0.0.6-py3-none-any.whl

---
+++ packages/perfometrics/perfometrics-0.0.6-py3-none-any.whl/perfometrics/status_checker.py
@@ -0,0 +1,18 @@
+from perfometrics.domain_checker import DomainChecker
+from perfometrics.decorators import *
+
+class StatusChecker:
+
+    def __init__(self, url) -> None:
+        self.url = url
+    
+    @time_of_execution
+    def check_status(self):
+        self.checker = DomainChecker(self.url).send_request()
+        self.status = self.checker.status_code
+        return self.validation()
+    
+    def validation(self):
+        if self.status == 200:
+            return True
+        return False
