5d809b2b23a896daa8bd9a9f5904d7ce69a0c5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:58 +0000

    Add package umep-reqs/0.1/umep-reqs-0.1.tar.gz

---
+++ packages/umep-reqs/umep-reqs-0.1.tar.gz/umep-reqs-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="umep-reqs",
+  version="0.1",
+  packages=find_packages(),
+  install_requires=[
+      "supy==2022.9.22", # Replace with actual dependency and version number.
+    #   "dependency2==y.y.y" # Replace with actual dependency and version number.
+  ],
+  author="UMEP dev team",
+)
