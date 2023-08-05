67a727fba74437581da8c72c968e8909470c9918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:06 +0000

    Add package otrs-somconnexio/0.4.45/otrs_somconnexio-0.4.45-py2.py3-none-any.whl

---
+++ packages/otrs-somconnexio/otrs_somconnexio-0.4.45-py2.py3-none-any.whl/otrs_somconnexio/otrs_models/configurations/provision/router_4G_ticket.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+class Router4GTicketConfiguration:
+    process_id = "Process-4b7130980e3fb4fcf3c57f00c7a2e903"
+    activity_id = "Activity-f42ad58ca503cdf4f3e708cf7b503990"
+    type = "Petición"
+    queue = "Serveis de banda ampla::Provisió Router 4G"
+    state = "new"
+    priority = "3 normal"
