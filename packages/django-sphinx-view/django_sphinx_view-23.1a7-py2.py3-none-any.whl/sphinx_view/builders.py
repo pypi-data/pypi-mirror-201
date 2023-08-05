0aa00b9781b86a88172969b6f72c3801c85dc1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:32 +0000

    Add package django-sphinx-view/23.1a7/django_sphinx_view-23.1a7-py2.py3-none-any.whl

---
+++ packages/django-sphinx-view/django_sphinx_view-23.1a7-py2.py3-none-any.whl/sphinx_view/builders.py
@@ -0,0 +1,13 @@
+from sphinxcontrib.serializinghtml import JSONHTMLBuilder
+from sphinx.environment.adapters.toctree import TocTree
+
+
+class SphinxViewBuilder(JSONHTMLBuilder):
+
+    name = 'json'  # Overriding the default
+
+    def get_doc_context(self, docname, body, metatags):
+        doc = super().get_doc_context(docname, body, metatags)
+        self_toctree = TocTree(self.env).get_toctree_for(docname, self, True)
+        doc['toctree'] = lambda **kwargs: self.render_partial(self_toctree)['fragment']
+        return doc
