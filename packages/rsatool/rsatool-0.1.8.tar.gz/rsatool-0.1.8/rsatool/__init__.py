8f0ff4c8d47574de42175d46522c8586f71e1316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:03 +0000

    Add package rsatool/0.1.8/rsatool-0.1.8.tar.gz

---
+++ packages/rsatool/rsatool-0.1.8.tar.gz/rsatool-0.1.8/rsatool/__init__.py
@@ -0,0 +1,15 @@
+"""
+RSA_tool module
+
+eng: Module for fast calculating large primes, and RSA encryption and decryption of data of any size.
+
+rus: Модуль сделан для быстрого вычисления простых чисел, и шифрования/расшифрования данных любого размера по алгоритму RSA
+
+
+"""
+from .RSA_tool import RSA
+
+
+__author__ = "Nikita Grigorev, Andrey Ignatov"
+__date__ = "2023-03-22"
+__version__ = "1.1"
