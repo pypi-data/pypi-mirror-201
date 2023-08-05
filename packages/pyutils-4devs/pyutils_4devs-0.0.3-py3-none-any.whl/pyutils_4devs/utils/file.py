abcf72e31036dfa95ccc6c9dbe715902e366aa2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:58 +0000

    Add package pyutils-4devs/0.0.3/pyutils_4devs-0.0.3-py3-none-any.whl

---
+++ packages/pyutils-4devs/pyutils_4devs-0.0.3-py3-none-any.whl/pyutils_4devs/utils/file.py
@@ -0,0 +1,14 @@
+from pyutils_4devs.decorators.file import handle_file_exists
+
+
+class File:
+    @classmethod
+    def save_file(cls, filepath, content):
+        with open(filepath, 'wb') as file:
+            file.write(content)
+    
+    @classmethod
+    @handle_file_exists
+    def read_file(cls, filepath):
+        with open(filepath, 'rb') as file:
+            return file.read()
