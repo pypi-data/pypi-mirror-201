22dff9aec0f148f7ada7403584e26a95bad3cd33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:04 +0000

    Add package hybrid-slug/0.1.0/hybrid-slug-0.1.0.tar.gz

---
+++ packages/hybrid-slug/hybrid-slug-0.1.0.tar.gz/hybrid-slug-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hybrid_slug', 'hybrid_slug.api']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.28.2,<3.0.0']
+
+setup_kwargs = {
+    'name': 'hybrid-slug',
+    'version': '0.1.0',
+    'description': 'SDK for The One API',
+    'long_description': None,
+    'author': None,
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
