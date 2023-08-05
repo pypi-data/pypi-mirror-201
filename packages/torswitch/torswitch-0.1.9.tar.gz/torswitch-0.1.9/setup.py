71554baf5107a12692302ddf161f856fc8ee564d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:17 +0000

    Add package torswitch/0.1.9/torswitch-0.1.9.tar.gz

---
+++ packages/torswitch/torswitch-0.1.9.tar.gz/torswitch-0.1.9/setup.py
@@ -0,0 +1,42 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+with codecs.open(os.path.join(current_dir, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+def read_requirements():
+    with open('requirements.txt') as req:
+        content=req.read()
+        requirements=content.split('\n')
+
+    return requirements   
+
+def get_version():
+    with open("version.txt") as ver:
+        Version=ver.read()
+    return Version    
+
+DESCRIPTION = 'A basic package for using tor proxy with python.'
+
+# Setting up
+setup(
+    name="torswitch",
+    version=get_version(),
+    author="Alex Deathway",
+    author_email="<alexdeathway@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    keywords=['python', 'tor', 'pythontor', 'iprotation','ipaddress'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ]
+)
