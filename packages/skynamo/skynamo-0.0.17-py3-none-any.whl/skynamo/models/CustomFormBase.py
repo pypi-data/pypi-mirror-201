1abc5cdf5bf40b634df511f6c492e09000c08b1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:47 +0000

    Add package skynamo/0.0.17/skynamo-0.0.17-py3-none-any.whl

---
+++ packages/skynamo/skynamo-0.0.17-py3-none-any.whl/skynamo/models/CustomFormBase.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from skynamo.shared.helpers import getDateTimeObjectFromSkynamoDateTimeStr
+
+class CustomFormBase:
+	def __init__(self,json:dict):
+		self.id:int=json['id']
+		self.date:datetime=getDateTimeObjectFromSkynamoDateTimeStr(json['date'])
+		self.customer_id:int=json['customer_id']
+		self.customer_code:str=json['customer_code']
+		self.customer_name:str=json['customer_name']
+		self.user_id:int=json['user_id']
+		self.user_name:str=json['user_name']
+		self.interaction_id:int=json['interaction_id']
+		self.form_id:int=json['form_id']
+		self.form_name:str=json['form_name']
