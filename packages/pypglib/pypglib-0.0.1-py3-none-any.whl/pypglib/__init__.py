0a3fef3d3cd757a3223073a2f51db40cbf488a4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:45 +0000

    Add package pypglib/0.0.1/pypglib-0.0.1-py3-none-any.whl

---
+++ packages/pypglib/pypglib-0.0.1-py3-none-any.whl/pypglib/__init__.py
@@ -0,0 +1,19 @@
+import os
+
+PATH_PYPGLIB = os.path.dirname(os.path.abspath(__file__))
+
+
+def __getattr__(filename):
+    """Automatically creates a string to case path."""
+
+    # eplace extension
+    root, ext = os.path.splitext(filename)
+    filename = root + '.m'
+
+    for root, _, files in os.walk(PATH_PYPGLIB):
+        for name in files:
+            if name == filename:
+                return os.path.join(root, name)
+
+    msg = f"No file with name {filename}!"
+    raise FileNotFoundError(msg)
