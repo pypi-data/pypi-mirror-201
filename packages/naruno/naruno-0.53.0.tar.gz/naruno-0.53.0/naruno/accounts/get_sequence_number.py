c476c116d24ded4af18b48f624b507ad6a5a179f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:05 +0000

    Add package naruno/0.53.0/naruno-0.53.0.tar.gz

---
+++ packages/naruno/naruno-0.53.0.tar.gz/naruno-0.53.0/naruno/accounts/get_sequence_number.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+from naruno.accounts.get_accounts import GetAccounts
+from naruno.wallet.wallet_import import Address
+
+
+def GetSequanceNumber(user, account_list=None, dont_convert=False):
+    user = Address(user) if not dont_convert else user
+    sequence_number = 0
+    the_account_list = GetAccounts() if account_list is None else account_list
+    the_account_list.execute(
+        f"SELECT * FROM account_list WHERE address = '{user}'")
+    for Accounts in the_account_list.fetchall():
+        sequence_number = Accounts[1]
+        return sequence_number
+    return sequence_number
