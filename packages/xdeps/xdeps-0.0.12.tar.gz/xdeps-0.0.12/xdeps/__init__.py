c04bc28ff2af9833f83780678dfba8399407c38a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:49 +0000

    Add package xdeps/0.0.12/xdeps-0.0.12.tar.gz

---
+++ packages/xdeps/xdeps-0.0.12.tar.gz/xdeps-0.0.12/xdeps/__init__.py
@@ -0,0 +1,9 @@
+# copyright ############################### #
+# This file is part of the Xdeps Package.   #
+# Copyright (c) CERN, 2021.                 #
+# ######################################### #
+
+from .tasks import Manager
+from . import madxutils
+
+from ._version import __version__
