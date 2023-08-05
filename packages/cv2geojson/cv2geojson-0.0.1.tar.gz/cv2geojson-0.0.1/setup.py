98f6f8f210e6e3a6e8c737079d5fb16e0fe0ab47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:41 +0000

    Add package cv2geojson/0.0.1/cv2geojson-0.0.1.tar.gz

---
+++ packages/cv2geojson/cv2geojson-0.0.1.tar.gz/cv2geojson-0.0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Export contour annotations as geojson formatted data'
+
+# Setting up
+setup(
+    name="cv2geojson",
+    version=VERSION,
+    author="Mohsen Farzi",
+    author_email="<mhnfarzi@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'numpy', 'geojson'],
+    keywords=['python', 'geojson', 'opencv', 'contours', 'polygons'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ]
+)
