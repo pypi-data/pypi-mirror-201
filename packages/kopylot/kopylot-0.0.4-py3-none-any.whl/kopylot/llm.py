bcf22a5931e746cf55b130d2c557d94346f91926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:58 +0000

    Add package kopylot/0.0.4/kopylot-0.0.4-py3-none-any.whl

---
+++ packages/kopylot/kopylot-0.0.4-py3-none-any.whl/kopylot/llm.py
@@ -0,0 +1,20 @@
+import os
+
+import openai
+
+openai.api_key = os.getenv("KOPYLOT_AUTH_TOKEN", "")
+
+
+def validate_token() -> bool:
+    return openai.api_key != ""
+
+
+def ask_llm(prompt: str, temperature: float = 0.7, max_tokens: int = 2048) -> str:
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=temperature,
+        max_tokens=max_tokens,
+    )
+
+    return str(response.choices[0].text.strip())
