a3b4d417b1fbf23833642b8d32e2a11c6a64bfae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:15 +0000

    Add package mecord-cli/0.1.31/mecord_cli-0.1.31-py3-none-any.whl

---
+++ packages/mecord-cli/mecord_cli-0.1.31-py3-none-any.whl/mecord/widget_template/config.json.py
@@ -0,0 +1,13 @@
+{
+  "name": "Widget-Demo",
+  "version": "1.0",
+  "parent_widget_id": "",
+  "widget_id": "",
+  "group_id": "",
+  "cmd": "",
+  "data": {
+    "fn_name": [
+      "txt2image"
+    ]
+  }
+}
