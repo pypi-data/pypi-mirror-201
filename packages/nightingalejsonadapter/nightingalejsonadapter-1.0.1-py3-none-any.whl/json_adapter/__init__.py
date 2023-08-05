35c3e6c9a16c248b43978db76882ec552d8e7178
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:04 +0000

    Add package nightingalejsonadapter/1.0.1/nightingalejsonadapter-1.0.1-py3-none-any.whl

---
+++ packages/nightingalejsonadapter/nightingalejsonadapter-1.0.1-py3-none-any.whl/json_adapter/__init__.py
@@ -0,0 +1,6 @@
+from patient_info_adapter import Model
+from vitals_adapter import Model
+from vitals_response import Model 
+from vital_kafka_adapter import Model
+
+# 'method' is a function that is present in a file called 'file.py'
