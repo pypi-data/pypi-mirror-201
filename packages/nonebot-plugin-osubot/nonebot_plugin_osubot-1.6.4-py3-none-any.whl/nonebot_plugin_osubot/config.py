92142b3c085cd85f2946217f56ec7ae4d34b4836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:57 +0000

    Add package nonebot-plugin-osubot/1.6.4/nonebot_plugin_osubot-1.6.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-osubot/nonebot_plugin_osubot-1.6.4-py3-none-any.whl/nonebot_plugin_osubot/config.py
@@ -0,0 +1,12 @@
+from pydantic import Extra, BaseSettings
+from typing import Optional
+
+
+class Config(BaseSettings):
+    osu_client: Optional[int] = None
+    osu_key: Optional[str] = None
+    osz_mirror: Optional[int] = 0
+
+    class Config:
+        extra = Extra.ignore
+        case_sensitive = False
