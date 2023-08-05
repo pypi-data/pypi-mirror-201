f85b362dc348157db23e937c8b17db7b7266225b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:29 +0000

    Add package pypreql/0.0.1rc22/pypreql-0.0.1rc22-py3-none-any.whl

---
+++ packages/pypreql/pypreql-0.0.1rc22-py3-none-any.whl/preql/dialect/common.py
@@ -0,0 +1,12 @@
+from preql.core.models import Join
+
+
+def render_join(join: Join, quote_character: str = '"') -> str:
+    # {% for key in join.joinkeys %}{{ key.inner }} = {{ key.outer}}{% endfor %}
+    joinkeys = " AND ".join(
+        [
+            f"{join.left_cte.name}.{quote_character}{key.concept.safe_address}{quote_character} = {join.right_cte.name}.{quote_character}{key.concept.safe_address}{quote_character}"
+            for key in join.joinkeys
+        ]
+    )
+    return f"{join.jointype.value.upper()} JOIN {join.right_cte.name} on {joinkeys}"
