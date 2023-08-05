a7ea3c062b66c76ac7b1797e525e98d21bf4445e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:18 +0000

    Add package pytabs/0.1.0/pytabs-0.1.0-py3-none-any.whl

---
+++ packages/pytabs/pytabs-0.1.0-py3-none-any.whl/pytabs/point_obj.py
@@ -0,0 +1,46 @@
+# pyTABS - ETABS .NET API python wrapper
+# PointObj - cPointObj
+__all__ = ['PointObj']
+
+# import ETABS namespace and pyTABS error handler
+from pytabs.etabs_config import *
+from pytabs.error_handle import *
+
+# import custom enumerations
+
+
+# import typing
+
+
+class PointObj:
+    """PointObj interface"""
+    def __init__(self, sap_model : etabs.cSapModel) -> None:
+        # link of SapModel interface
+        self.sap_model = sap_model
+        # create PointObj interface
+        self.point_obj = etabs.cPointObj(sap_model.PointObj)
+        
+        # relate relevant ETABS enumerations
+        self.eDiaphragmOption = etabs.eDiaphragmOption
+        """EtabsModel `DiaphragmOption` enumeration"""
+        
+
+        # relate custom enumerations
+        
+        
+        
+    def get_name_from_label (self, label : str, story : str) -> str:
+        """Retrieves the unique name of a point object, given the label and story level.
+
+        :param label: The point object label 
+        :type label: str
+        :param story: The point object story level  
+        :type label: str
+        :return point_name: The unique name of the point object
+        :rtype: str
+        """
+
+        point_name = str()
+        [ret, point_name] = self.point_obj.GetNameFromLabel(label, story, point_name) 
+        handle(ret)
+        return point_name
