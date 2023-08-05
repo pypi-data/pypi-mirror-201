9fdc0c4054a70c5df5b9f950e11b365b2dde3286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:20 +0000

    Add package usb-monitor/1.12/usb-monitor-1.12.tar.gz

---
+++ packages/usb-monitor/usb-monitor-1.12.tar.gz/usb-monitor-1.12/usbmonitor/__platform_specific_detectors/_constants.py
@@ -0,0 +1,39 @@
+"""
+This file contains constant values and mappings used in the platform-specific USB device detection implementations.
+It includes constants for attributes, attribute separators, and regex patterns for specific Operating Systems.
+Additionally, it defines the default time interval for checking USB device changes.
+
+Author: Eric-Canas
+Date: 28-03-2023
+Email: eric@ericcanas.com
+Github: https://github.com/Eric-Canas
+"""
+
+from ..attributes import ID_MODEL_ID, ID_VENDOR, ID_MODEL, ID_VENDOR_FROM_DATABASE, ID_MODEL_FROM_DATABASE, \
+    DEVNAME, ID_USB_CLASS_FROM_DATABASE, ID_USB_INTERFACES, DEVTYPE, ID_VENDOR_ID
+
+_SECONDS_BETWEEN_CHECKS = 0.5
+
+_DEVICE_ID, _PNP_DEVICE_ID = 'DeviceID', 'PNPDeviceID'
+
+_LINUX_TO_WINDOWS_ATTRIBUTES = {
+    ID_MODEL_ID: 'HardwareID',
+    ID_VENDOR_ID: 'HardwareID',
+    ID_VENDOR: 'Name',
+    ID_MODEL: 'Name',
+    ID_VENDOR_FROM_DATABASE: 'Manufacturer',#'Description',
+    ID_MODEL_FROM_DATABASE: 'Caption',
+    DEVNAME: _DEVICE_ID,
+    ID_USB_CLASS_FROM_DATABASE: 'PNPClass',
+    ID_USB_INTERFACES: 'CompatibleID',
+    DEVTYPE: _PNP_DEVICE_ID,
+}
+
+_LINUX_TUPLE_ATTRIBUTES_SEPARATORS = {ID_USB_INTERFACES: ':'}
+
+_WINDOWS_REGEX_ATTRIBUTES = {ID_MODEL_ID: r'PID_([0-9A-Fa-f]{4})', ID_VENDOR_ID: r'VID_([0-9A-Fa-f]{4})',
+                             DEVTYPE: r'^(.+?)\\'}
+_WINDOWS_TO_LOWERCASE_ATTRIBUTES = (ID_MODEL_ID, ID_VENDOR_ID)
+_WINDOWS_NON_USB_DEVICES_IDS = ("ROOT_HUB20", "ROOT_HUB30")
+_WINDOWS_USB_QUERY = f"SELECT {', '.join(set(_LINUX_TO_WINDOWS_ATTRIBUTES.values()))} FROM Win32_PnPEntity " \
+                          f"WHERE {_PNP_DEVICE_ID} LIKE 'USB%'"
