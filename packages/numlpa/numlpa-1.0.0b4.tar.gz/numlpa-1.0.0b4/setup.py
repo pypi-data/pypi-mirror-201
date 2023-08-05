8fb0527d43289af7cf2d718b32833b2b15ae29d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package numlpa/1.0.0b4/numlpa-1.0.0b4.tar.gz

---
+++ packages/numlpa/numlpa-1.0.0b4.tar.gz/numlpa-1.0.0b4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension(
+            name="numlpa.kits.diffractometers.cpu.displacement",
+            sources=["src/numlpa/kits/diffractometers/cpu/displacementmodule.c"],
+        ),
+        Extension(
+            name="numlpa.kits.analyzers.cpu.interaction",
+            sources=["src/numlpa/kits/analyzers/cpu/interactionmodule.c"],
+        ),
+    ]
+)
