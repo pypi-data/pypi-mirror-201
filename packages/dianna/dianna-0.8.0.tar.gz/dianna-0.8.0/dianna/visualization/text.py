626a82b4c54b34dcbd6832e699f0d35a06f22ab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:12 +0000

    Add package dianna/0.8.0/dianna-0.8.0.tar.gz

---
+++ packages/dianna/dianna-0.8.0.tar.gz/dianna-0.8.0/dianna/visualization/text.py
@@ -0,0 +1,58 @@
+from IPython.display import HTML
+from IPython.display import display
+
+
+def highlight_text(explanation,
+                   input_tokens,
+                   show_plot=True,
+                   output_html_filename=None,
+                   max_opacity=.8):
+    """Highlights a given text based on values in a given explanation object.
+
+    Args:
+        explanation: list of tuples of (word, index of word in original data, importance)
+        input_tokens: list of all tokens (including those without importance)
+        show_plot: Shows plot if true (for testing or writing plots to disk instead)
+        output_html_filename: Name of the file to save the plot to (optional).
+        max_opacity: Maximum opacity (0-1)
+
+    Returns:
+        None
+    """
+    output = _create_html(input_tokens, explanation, max_opacity)
+
+    if output_html_filename:
+        with open(output_html_filename, 'w', encoding='utf-8') as output_html_file:
+            print(output, file=output_html_file)
+
+    if show_plot:
+        display(HTML(output))
+
+
+def _create_html(input_tokens, explanation, max_opacity):
+    max_importance = max(abs(item[2]) for item in explanation)
+    explained_indices = [index for _, index, _ in explanation]
+    highlighted_words = []
+    for index, word in enumerate(input_tokens):
+        # if word has an explanation, highlight based on that, otherwise
+        # make it grey
+        try:
+            explained_index = explained_indices.index(index)
+            importance = explanation[explained_index][2]
+            highlighted_words.append(
+                _highlight_word(word, importance, max_importance, max_opacity)
+                )
+        except ValueError:
+            highlighted_words.append(f'<span style="background:rgba(128, 128, 128, 0.3)">{word}</span>')
+
+    return '<html><body>' + ' '.join(highlighted_words) + '</body></html>'
+
+
+def _highlight_word(word, importance, max_importance, max_opacity):
+    opacity = max_opacity * abs(importance) / max_importance
+    if importance > 0:
+        color = f'rgba(255, 0, 0, {opacity:.2f})'
+    else:
+        color = f'rgba(0, 0, 255, {opacity:2f})'
+    highlighted_word = f'<span style="background:{color}">{word}</span>'
+    return highlighted_word
---
+++ packages/dianna/dianna-0.8.0.tar.gz/dianna-0.8.0/tests/test_text_visualization.py
@@ -0,0 +1,96 @@
+import os
+import shutil
+import unittest
+from pathlib import Path
+import spacy
+from dianna.utils.tokenizers import SpacyTokenizer
+from dianna.visualization.text import highlight_text
+
+
+spacy.cli.download('en_core_web_sm')
+
+
+class TextExample:
+    """Text and explanation for running visualizing tests."""
+    original_text = 'Doloremque aliquam totam ut. Aspernatur repellendus autem quia deleniti. Natus accusamus ' \
+                    'doloribus et in quam officiis veniam et. '
+    tokenizer = SpacyTokenizer()
+    tokens = tokenizer.tokenize(original_text)
+    explanation = [
+                    ('aliquam', 1, 0.6450221),
+                    ('et', 19, 0.63976675),
+                    ('in', 15, 0.6397511),
+                    ('Aspernatur', 5, 0.6329795),
+                    ('totam', 2, 0.6242337),
+                    ('quia', 8, 0.61881626),
+                    ('quam', 16, 0.6173148),
+                    ('Natus', 11, 0.60527676),
+                    ('ut', 3, 0.60203224)
+                    ]
+
+
+class TextExampleWithExpectedHtml:
+    """Short text and explanation and its expected html output after visualizing."""
+    expected_html = '<html><body><span style="background:rgba(255, 0, 0, 0.59)">Such</span> ' \
+                    '<span style="background:rgba(255, 0, 0, 0.62)">a</span> ' \
+                    '<span style="background:rgba(255, 0, 0, 0.80)">bad</span> ' \
+                    '<span style="background:rgba(255, 0, 0, 0.63)">movie</span> ' \
+                    '<span style="background:rgba(128, 128, 128, 0.3)">.</span></body></html>\n'
+
+    original_text = 'Such a bad movie.'
+    tokenizer = SpacyTokenizer()
+    tokens = tokenizer.tokenize(original_text)
+    explanation = [
+                    ('bad', 2, 0.9959058),
+                    ('movie', 3, 0.78263557),
+                    ('a', 1, 0.7753202),
+                    ('Such', 0, 0.73788315)
+                    ]
+
+
+class TextVisualizationTestCase(unittest.TestCase):
+    """Suite of tests for visualizing text given text and explanation data."""
+    temp_folder = 'temp_text_visualization_test'
+    html_file_path = str(Path(temp_folder) / 'output.html')
+
+    def test_text_visualization_html_output_exists(self):
+        """Test if any output is generated at all."""
+        highlight_text(TextExample.explanation, TextExample.tokens,
+                       output_html_filename=self.html_file_path)
+
+        assert Path(self.html_file_path).exists()
+
+    def test_text_visualization_html_output_contains_text(self):
+        """Test if all words in the input are present in the output html."""
+        highlight_text(TextExample.explanation, TextExample.tokens,
+                       output_html_filename=self.html_file_path)
+
+        assert Path(self.html_file_path).exists()
+        with open(self.html_file_path, encoding='utf-8') as result_file:
+            result = result_file.read()
+        for word in TextExample.tokens:
+            assert word in result
+
+    def test_text_visualization_html_output_is_correct(self):
+        """Test if exact html output of visualization is correct."""
+        highlight_text(TextExampleWithExpectedHtml.explanation, TextExampleWithExpectedHtml.tokens,
+                       output_html_filename=self.html_file_path)
+
+        assert Path(self.html_file_path).exists()
+
+        with open(self.html_file_path, encoding='utf-8') as result_file:
+            result = result_file.read()
+        assert result == TextExampleWithExpectedHtml.expected_html
+
+    def test_text_visualization_show_plot(self):
+        """Test if it runs while showing the plot."""
+        highlight_text(TextExample.explanation, TextExample.tokens,
+                       show_plot=True)
+
+    def setUp(self) -> None:
+        """Setup."""
+        os.mkdir(self.temp_folder)
+
+    def tearDown(self) -> None:
+        """Tear down."""
+        shutil.rmtree(self.temp_folder, ignore_errors=True)
