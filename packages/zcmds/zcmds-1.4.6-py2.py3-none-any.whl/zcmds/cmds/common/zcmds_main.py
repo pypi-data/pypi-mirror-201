7d32a639fc3f71686530c49520867775793e3365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:23 +0000

    Add package zcmds/1.4.6/zcmds-1.4.6-py2.py3-none-any.whl

---
+++ packages/zcmds/zcmds-1.4.6-py2.py3-none-any.whl/zcmds/cmds/common/zcmds_main.py
@@ -0,0 +1,65 @@
+"""
+    Normalizes the audio of a video file.
+"""
+import sys
+
+COMMON = [
+    "audnorm",
+    "diskaudit",
+    "img2vid",
+    "obs_organize",
+    "pdf2png",
+    "search_and_replace",
+    "search_in_files",
+    "sharedir",
+    "stereo2mono",
+    "vidmute",
+    "vidinfo",
+    "vid2gif",
+    "vid2jpg",
+    "vid2mp3",
+    "vid2mp4",
+    "vid2webm",
+    "vidclip",
+    "viddur",
+    "vidspeed",
+    "vidvol",
+    "vidshrink",
+    "vidmatrix",
+    "vidwebmaster",
+    "vidhero",
+    "vidlist",
+    "test_net_connection",
+    "ytclip",
+]
+
+WIN32 = [
+    "cat",
+    "cp",
+    "du",
+    "grep",
+    "home",
+    "ls",
+    "mv",
+    "open",
+    "rm",
+    "which",
+    "git-bash",
+    "touch",
+    "fixvmmem",
+    "sudo",
+]
+
+
+def main():
+    cmds = COMMON
+    if sys.platform == "win32":
+        cmds.extend(WIN32)
+    cmds = sorted(cmds)
+    print("zcmds:")
+    for cmd in cmds:
+        print(f"  {cmd}")
+
+
+if __name__ == "__main__":
+    main()
