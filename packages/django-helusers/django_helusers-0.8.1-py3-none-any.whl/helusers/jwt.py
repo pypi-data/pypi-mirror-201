070d4ffd102ed4ce77f7d39db8c6dd6a7d996f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:48 +0000

    Add package django-helusers/0.8.1/django_helusers-0.8.1-py3-none-any.whl

---
+++ packages/django-helusers/django_helusers-0.8.1-py3-none-any.whl/helusers/jwt.py
@@ -0,0 +1,91 @@
+try:
+    from ._rest_framework_jwt_impl import (JWTAuthentication,
+                                           get_user_id_from_payload_handler,
+                                           patch_jwt_settings)
+except ImportError:
+    pass
+
+from django.utils.functional import cached_property
+from jose import jwt
+
+from .settings import api_token_auth_settings
+
+
+_NOT_PROVIDED = object()
+
+
+class ValidationError(Exception):
+    pass
+
+
+class JWT:
+    def __init__(self, encoded_jwt):
+        """The constructor checks that a JWT can be extracted from the
+        provided input but it doesn't validate it in any way. If the
+        input is invalid, an exception is raised."""
+        self._encoded_jwt = encoded_jwt
+        self._claims = jwt.get_unverified_claims(encoded_jwt)
+
+    def validate(self, keys, audience, required_claims=_NOT_PROVIDED):
+        """Verifies the JWT's signature using the provided keys,
+        and validates the claims, raising an exception if anything fails.
+        Required claims can be specified using the required_claims argument
+        and it defaults to ["aud", "exp"]."""
+
+        if required_claims is _NOT_PROVIDED:
+            required_claims = ["aud", "exp"]
+
+        options = {
+            "verify_aud": False,
+        }
+
+        require_aud = "aud" in required_claims
+        required_claims.remove("aud")
+
+        for required_claim in required_claims:
+            options[f"require_{required_claim}"] = True
+
+        jwt.decode(self._encoded_jwt, keys, options=options)
+
+        claims = self.claims
+        if require_aud and "aud" not in claims:
+            raise ValidationError("Missing required 'aud' claim.")
+
+        if "aud" in claims:
+            claim_audiences = claims["aud"]
+            if isinstance(claim_audiences, str):
+                claim_audiences = {claim_audiences}
+            if isinstance(audience, str):
+                audience = {audience}
+            if len(set(audience).intersection(claim_audiences)) == 0:
+                raise ValidationError("Invalid audience.")
+
+    @property
+    def issuer(self):
+        """Returns the "iss" claim value."""
+        return self.claims["iss"]
+
+    @property
+    def claims(self):
+        """Returns all the claims of the JWT as a dictionary."""
+        return self._claims
+
+    def has_api_scope_with_prefix(self, prefix):
+        """Checks if there is an API scope with the given prefix.
+        The name of the claims field where API scopes are looked for is
+        determined by the OIDC_API_TOKEN_AUTH['API_AUTHORIZATION_FIELD']
+        setting."""
+        return any(
+            x == prefix or x.startswith(prefix + ".")
+            for x in self._authorized_api_scopes
+        )
+
+    @cached_property
+    def _authorized_api_scopes(self):
+        def is_list_of_non_empty_strings(value):
+            return isinstance(value, list) and all(
+                isinstance(x, str) and x for x in value
+            )
+
+        api_scopes = self.claims.get(api_token_auth_settings.API_AUTHORIZATION_FIELD)
+        return set(api_scopes) if is_list_of_non_empty_strings(api_scopes) else set()
---
+++ packages/django-helusers/django_helusers-0.8.1-py3-none-any.whl/helusers/oidc.py
@@ -0,0 +1,154 @@
+try:
+    from ._oidc_auth_impl import ApiTokenAuthentication, resolve_user
+except ImportError:
+    pass
+
+
+import requests
+from cachetools.func import ttl_cache
+from django.core.exceptions import ImproperlyConfigured
+from django.core.signals import setting_changed
+from django.dispatch import receiver
+from django.utils.functional import cached_property
+
+from .authz import UserAuthorization
+from .jwt import JWT
+from .models import OIDCBackChannelLogoutEvent
+from .settings import api_token_auth_settings
+from .user_utils import get_or_create_user
+
+
+class OIDCConfig:
+    def __init__(self, issuer):
+        self._issuer = issuer
+
+    @ttl_cache(ttl=api_token_auth_settings.OIDC_CONFIG_EXPIRATION_TIME)
+    def keys(self):
+        config_url = self._issuer + "/.well-known/openid-configuration"
+        config = requests.get(config_url).json()
+
+        keys_url = config["jwks_uri"]
+        return requests.get(keys_url).json()
+
+
+def _build_defaults():
+    class _Defaults:
+        @cached_property
+        def audience(self):
+            if not api_token_auth_settings.AUDIENCE:
+                raise ImproperlyConfigured(
+                    "You must set OIDC_API_TOKEN_AUTH['AUDIENCE'] setting to the accepted JWT audience."
+                )
+            return api_token_auth_settings.AUDIENCE
+
+        @cached_property
+        def issuers(self):
+            issuers = api_token_auth_settings.ISSUER
+            if not issuers:
+                raise ImproperlyConfigured(
+                    "You must set OIDC_API_TOKEN_AUTH['ISSUER'] setting to one or to a list of accepted JWT issuers."
+                )
+            if isinstance(issuers, str):
+                issuers = [issuers]
+            return issuers
+
+        @cached_property
+        def configs(self):
+            configs = dict()
+            for issuer in self.issuers:
+                configs[issuer] = OIDCConfig(issuer)
+            return configs
+
+        @cached_property
+        def key_provider(self):
+            confs = self.configs
+
+            def _key_provider(issuer):
+                return confs[issuer].keys()
+
+            return _key_provider
+
+    return _Defaults()
+
+
+_defaults = _build_defaults()
+
+
+@receiver(setting_changed)
+def _reload_settings(setting, **kwargs):
+    if setting == "OIDC_API_TOKEN_AUTH":
+        global _defaults
+        _defaults = _build_defaults()
+
+
+def get_keys(issuer):
+    return _defaults.key_provider(issuer)
+
+
+def accepted_audience():
+    return _defaults.audience
+
+
+class AuthenticationError(Exception):
+    pass
+
+
+_NOT_PROVIDED = object()
+
+
+class RequestJWTAuthentication:
+    def __init__(self, key_provider=_NOT_PROVIDED):
+        if key_provider is not _NOT_PROVIDED:
+            import warnings
+
+            warnings.warn(
+                "The 'key_provider' argument to RequestJWTAuthentication is obsolete. "
+                "Providing one has no effect any more.",
+                DeprecationWarning,
+            )
+
+    def authenticate(self, request):
+        """Looks for a JWT from the request's "Authorization" header. If the header
+        is not found, or it doesn't contain a JWT, returns None.
+
+        If the header is found and contains a JWT then the JWT gets verified.
+        If verification passes, takes a user's id from the JWT's "sub" claim.
+        Creates a User if it doesn't already exist. On success returns a
+        UserAuthorization object. Raises an AuthenticationError on authentication
+        failure."""
+        try:
+            auth_header = request.headers["Authorization"]
+            auth_scheme, jwt_value = auth_header.split()
+            if auth_scheme.lower() != "bearer":
+                return None
+            jwt = JWT(jwt_value)
+        except Exception:
+            return None
+
+        try:
+            issuer = jwt.issuer
+        except KeyError:
+            raise AuthenticationError('Required "iss" claim is missing.')
+
+        if issuer not in _defaults.issuers:
+            raise AuthenticationError("Unknown JWT issuer {}.".format(issuer))
+
+        keys = _defaults.key_provider(issuer)
+        try:
+            jwt.validate(keys, _defaults.audience)
+        except Exception:
+            raise AuthenticationError("JWT verification failed.")
+
+        if api_token_auth_settings.REQUIRE_API_SCOPE_FOR_AUTHENTICATION:
+            api_scope = api_token_auth_settings.API_SCOPE_PREFIX
+            if not jwt.has_api_scope_with_prefix(api_scope):
+                raise AuthenticationError(
+                    'Not authorized for API scope "{}"'.format(api_scope)
+                )
+
+        if OIDCBackChannelLogoutEvent.objects.is_session_terminated_for_token(jwt):
+            raise AuthenticationError("Session has been terminated.")
+
+        claims = jwt.claims
+        user = get_or_create_user(claims, oidc=True)
+        return UserAuthorization(user, claims)
