676920cc40797acdb5b34195b5fc8f209474c60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:43 +0000

    Add package codx/0.1.2/codx-0.1.2.tar.gz

---
+++ packages/codx/codx-0.1.2.tar.gz/codx-0.1.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['codx']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['biopython>=1.81,<2.0',
+ 'click>=8.1.3,<9.0.0',
+ 'pandas>=1.5.3,<2.0.0',
+ 'uniprotparser>=1.0.9,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['codx = codx.cli:main']}
+
+setup_kwargs = {
+    'name': 'codx',
+    'version': '0.1.2',
+    'description': 'A package used to retrieve exon for protein sequences from RefSeqGene database',
+    'long_description': '# CODX\n---\n\n`codx` is a python package that allow retrieval of exons data from NCBI RefSeqGene database.\n\n## Installation\n\n```bash\npip install codx\n```\n\n## Usage Python Package\n---\nThe package uses gene id in order to retrieve exons data from NCBI RefSeqGene database. The gene id can be obtained from the Uniprot database using the accession id of the gene. The `get_geneids_from_uniprot` function can be used to obtain the gene id from RefSeqGene database of NCBI.\n\n\n```python\n# if you only have accession id, you must first use the get_geneids_from_uniprot function to get the gene id from Uniprot\nfrom codx.components import get_geneids_from_uniprot\n\ngene_ids = get_geneids_from_uniprot(["P35568", "P05019", "Q99490", "Q8NEJ0", "Q13322", "Q15323"])\n# the result will be a set of gene ids that can be obtained from the Uniprot database using the list of Uniprot accession above\n```\n\n\n\n```python\n# Import the create_db function to create a sqlite3 database with gene and exon data from NCBI\nfrom codx.components import create_db\n\n\n# 120892 is the gene id for LRRK2 gene\ndb = create_db(["120892"], entrez_email="your@email.com") # You need to provide an email address to use the NCBI API\n\n# From the database object, you can retrieve a gene object using its gene name\ngene = db.get_gene("LRRK2")\n\n# From the gene objects you can retrieve exons data from the blocks attribute each exon object has its start and end location as well as the associated sequence\nfor exon in gene.blocks:\n    print(exon.start, exon.end, exon.sequence)\n\n# Using the gene object it is also possible to create all possible ordered combinations of exons\n# This will be a generator object that yield a SeqRecord object for each combination\n# There however may be a lot of combinations so depending on the gene, you may not want to use this with a very large gene unless there are no other options\nfor exon_combination in gene.shuffle_blocks():\n    print(exon_combination)\n\n# To create six frame translation of any sequence, you can use the three_frame_translation function twice, one with and one without the reverse complement option enable\n# Each output is a dictionary with the translatable sequence as value and the frame as key\nfrom codx.components import three_frame_translation\nfor exon_combination in gene.shuffle_blocks():\n    three_frame = three_frame_translation(exon_combination.seq, only_start_at_atg=True)\n    three_frame_complement = three_frame_translation(exon_combination.seq, only_start_at_atg=True, reverse_complement=True)\n\n```\n\n## Usage Command Line\n---\nIn addition to the python API, installation also provide a cli interface that can be used for the same purpose\n\n\n```bash\nUsage: codx [OPTIONS] IDS\n\nOptions:\n  -o, --output TEXT              Output file\n  -i, --include-intron           Include intron\n  -u, --uniprot                  Input is Uniprot accession ids\n  -t, --translate                Translate to protein\n  -3, --three-frame-translation  Translate to protein in 3 frames\n  -6, --six-frame-translation    Translate to protein in 6 frames (3 forward\n                                 and 3 reverse complement)\n  --help                         Show this message and exit.\n```\n\nHere IDS positional argument are a list of gene ids or uniprot accession ids delimited by `,`.\n\nExample usage can be seen below\n\n```bash\ncodx -o output.fasta -u P35568,P05019,Q99490,Q8NEJ0,Q13322,Q15323\n```\n\n```bash\ncodx -o output.fasta 1190,120892\n```',
+    'author': 'Toan Phung',
+    'author_email': 'toan.phungkhoiquoctoan@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
