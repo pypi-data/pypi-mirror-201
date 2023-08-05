0d5b7b7ce552c65ba38b54a59beeb7ccd135d2b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:13 +0000

    Add package spawn-lia/0.1.20/spawn_lia-0.1.20-py3-none-any.whl

---
+++ packages/spawn-lia/spawn_lia-0.1.20-py3-none-any.whl/lia/emojis.py
@@ -0,0 +1,16 @@
+"""
+This is an emoji map for usage in shell outputs
+
+:author: Julian M. Kleber
+"""
+from emoji import emojize
+
+
+angered_face = emojize(":enraged_face:")
+face_blowing_kiss = emojize(":face_blowing_a_kiss:")
+fearful_face = emojize(":fearful_face:")
+face_screaming_in_fear = emojize(":face_screaming_in_fear:")
+magic_wand = emojize(":magic_wand:")
+pensive_face = emojize(":pensive_face:")
+kissing_cat = emojize(":kissing_cat:")
+face_with_rolling_eyes = emojize(":face_with_rolling_eyes:")
