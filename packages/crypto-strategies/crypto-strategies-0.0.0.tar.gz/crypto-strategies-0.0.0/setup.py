2e2fa9d4ba4b541bb87ce04d758150e6c0d768e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:46 +0000

    Add package crypto-strategies/0.0.0/crypto-strategies-0.0.0.tar.gz

---
+++ packages/crypto-strategies/crypto-strategies-0.0.0.tar.gz/crypto-strategies-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='crypto-strategies',
+      version='0.0.0',
+      description='Foo',
+      url='https://squadrone@bitbucket.org/bar/foo.git',
+      author='Squadrone',
+      author_email='foo@bar.com',
+      packages=['foo'],
+      zip_safe=False)
