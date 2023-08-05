4082c706b194c4144d2a41f23202d1409759ab1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:54 +0000

    Add package zq-pt-albert/0.1.1/zq_pt_albert-0.1.1.tar.gz

---
+++ packages/zq-pt-albert/zq_pt_albert-0.1.1.tar.gz/zq_pt_albert-0.1.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zq_pt_albert',
+    version='0.1.1',
+    description=(
+        'google small zh albert tf'
+    ),
+    author='zhangqi',
+    author_email='zhangqi23@163.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+)
