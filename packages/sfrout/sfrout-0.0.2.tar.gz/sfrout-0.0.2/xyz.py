c3bdbbd30acba3f1891a92ea875e5aebab6e6944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:14 +0000

    Add package sfrout/0.0.2/sfrout-0.0.2.tar.gz

---
+++ packages/sfrout/sfrout-0.0.2.tar.gz/sfrout-0.0.2/xyz.py
@@ -0,0 +1,5 @@
+from src.sfrout import run
+
+run("https://hp.my.salesforce.com/", 
+    "C:\\Users\\hoszowsl\\Hewlett Packard Enterprise\\WW SSP Tools and Processes - SSP Data Library\\Projects\\input\\reports.csv",
+    summary_filepath="C:\\Users\\hoszowsl\\Hewlett Packard Enterprise\\WW SSP Tools and Processes - SSP Data Library\\SFR\\summary.csv")
