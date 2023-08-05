775c414f5079d925e1105b9ffd9c50ce7121e1e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:11 +0000

    Add package distributed-transcoder-common/0.2.2/distributed-transcoder-common-0.2.2.tar.gz

---
+++ packages/distributed-transcoder-common/distributed-transcoder-common-0.2.2.tar.gz/distributed-transcoder-common-0.2.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['distributed_transcoder_common']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'distributed-transcoder-common',
+    'version': '0.2.2',
+    'description': 'A common library for for the distributed transcoder project',
+    'long_description': None,
+    'author': 'Eric Volpert',
+    'author_email': 'ericvolp12@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
