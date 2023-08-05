edb40ca3eb2bc4af2868e1c2b074d930e40e0399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:31 +0000

    Add package cygsubprocess/0.11/cygsubprocess-0.11.tar.gz

---
+++ packages/cygsubprocess/cygsubprocess-0.11.tar.gz/cygsubprocess-0.11/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+#change to dict
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(os.path.abspath(os.path.dirname(__file__)),'README.md'), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.11'
+DESCRIPTION = "Executes Cygwin bash scripts/commands in Python, captures and prints the output"
+
+# Setting up
+setup(
+    name="cygsubprocess",
+    version=VERSION,
+    license='MIT',
+    url = 'https://github.com/hansalemaos/cygsubprocess',
+    author="Johannes Fischer",
+    author_email="<aulasparticularesdealemaosp@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    #packages=['add2winpath', 'requests'],
+    keywords=['bash', 'cygwin', 'sh', 'linux'],
+    classifiers=['Development Status :: 4 - Beta', 'Programming Language :: Python :: 3 :: Only', 'Programming Language :: Python :: 3.10', 'Topic :: Scientific/Engineering :: Visualization', 'Topic :: Software Development :: Libraries :: Python Modules', 'Topic :: Utilities'],
+    install_requires=['add2winpath', 'requests'],
+    include_package_data=True
+)
+#python setup.py sdist bdist_wheel
+#twine upload dist/*
