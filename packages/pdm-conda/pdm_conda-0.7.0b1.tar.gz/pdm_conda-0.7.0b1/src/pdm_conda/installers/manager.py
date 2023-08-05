e8bc0c1e8b5f4e6b084d1476799793837ec0a56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:19 +0000

    Add package pdm-conda/0.7.0b1/pdm_conda-0.7.0b1.tar.gz

---
+++ packages/pdm-conda/pdm_conda-0.7.0b1.tar.gz/pdm_conda-0.7.0b1/src/pdm_conda/installers/manager.py
@@ -0,0 +1,43 @@
+from importlib.metadata import Distribution
+from typing import cast
+
+from installer.exceptions import InstallerError
+from pdm.exceptions import RequirementError, UninstallError
+from pdm.installers import InstallManager
+
+from pdm_conda.conda import conda_install, conda_uninstall
+from pdm_conda.models.candidates import Candidate, CondaCandidate
+from pdm_conda.models.environment import CondaEnvironment, Environment
+from pdm_conda.models.setup import CondaSetupDistribution
+
+
+class CondaInstallManager(InstallManager):
+    def __init__(self, environment: Environment, *, use_install_cache: bool = False) -> None:
+        super().__init__(environment, use_install_cache=use_install_cache)
+        self.environment = cast(CondaEnvironment, environment)
+
+    def install(self, candidate: Candidate) -> None:
+        """
+        Install candidate, use conda if conda package else default installer
+        :param candidate: candidate to install
+        """
+        if isinstance(candidate, CondaCandidate):
+            try:
+                conda_install(self.environment.project, candidate.link.url, no_deps=True)
+            except (RequirementError, ValueError) as e:
+                raise InstallerError(e) from e
+        else:
+            super().install(candidate)
+
+    def uninstall(self, dist: Distribution) -> None:
+        """
+        Uninstall distribution, use conda if conda package else default uninstaller
+        :param dist: distribution to uninstall
+        """
+        if isinstance(dist, CondaSetupDistribution):
+            try:
+                conda_uninstall(self.environment.project, dist.name, no_deps=True)
+            except RequirementError as e:
+                raise UninstallError(e) from e
+        else:
+            super().uninstall(dist)
