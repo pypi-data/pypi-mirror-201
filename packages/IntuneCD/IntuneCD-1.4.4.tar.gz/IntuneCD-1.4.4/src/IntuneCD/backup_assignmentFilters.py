d068ca114e449ca9a4e78f9476ee021be45dec25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package IntuneCD/1.4.4/IntuneCD-1.4.4.tar.gz

---
+++ packages/IntuneCD/IntuneCD-1.4.4.tar.gz/IntuneCD-1.4.4/src/IntuneCD/backup_assignmentFilters.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+This module backs up all Filters in Intune.
+"""
+
+from .clean_filename import clean_filename
+from .graph_request import makeapirequest
+from .save_output import save_output
+from .remove_keys import remove_keys
+
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceManagement/assignmentFilters"
+
+
+# Get all Filters and save them in specified path
+def savebackup(path, output, token):
+    """
+    Saves all Filter in Intune to a JSON or YAML file.
+
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = path + "/" + "Filters/"
+    data = makeapirequest(ENDPOINT, token)
+
+    if data:
+        for assign_filter in data["value"]:
+            config_count += 1
+            assign_filter = remove_keys(assign_filter)
+            print("Backing up Filter: " + assign_filter["displayName"])
+
+            # Get filename without illegal characters
+            fname = clean_filename(assign_filter["displayName"])
+            # Save Filters as JSON or YAML depending on configured value in
+            # "-o"
+            save_output(output, configpath, fname, assign_filter)
+
+    return config_count
---
+++ packages/IntuneCD/IntuneCD-1.4.4.tar.gz/IntuneCD-1.4.4/src/IntuneCD/backup_vppTokens.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+"""
+This module backs up all VPP tokens in Intune.
+"""
+
+from .clean_filename import clean_filename
+from .graph_request import makeapirequest
+from .save_output import save_output
+from .remove_keys import remove_keys
+
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceAppManagement/vppTokens"
+
+
+# Get all VPP tokens and save them in specified path
+def savebackup(path, output, token):
+    """
+    Save all VPP tokens in Intune to a JSON or YAML file.
+
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = f"{path}/Apple VPP Tokens/"
+    data = makeapirequest(ENDPOINT, token)
+
+    for vpp_token in data["value"]:
+        config_count += 1
+        token_name = vpp_token["displayName"]
+        vpp_token = remove_keys(vpp_token)
+
+        print(f"Backing up VPP token: {token_name}")
+
+        # Get filename without illegal characters
+        fname = clean_filename(token_name)
+
+        # Save token as JSON or YAML depending on configured value in "-o"
+        save_output(output, configpath, fname, vpp_token)
+
+    return config_count
