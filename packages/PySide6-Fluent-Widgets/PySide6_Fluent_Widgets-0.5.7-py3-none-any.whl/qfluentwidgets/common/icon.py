530752d6ddfb84880b681c71db1ee1b1687a3898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:16 +0000

    Add package PySide6-Fluent-Widgets/0.5.7/PySide6_Fluent_Widgets-0.5.7-py3-none-any.whl

---
+++ packages/PySide6-Fluent-Widgets/PySide6_Fluent_Widgets-0.5.7-py3-none-any.whl/qfluentwidgets/common/icon.py
@@ -0,0 +1,264 @@
+# coding:utf-8
+from enum import Enum
+
+from PySide6.QtXml import QDomDocument
+from PySide6.QtCore import QPoint, QRect, QRectF, Qt, QFile
+from PySide6.QtGui import QIcon, QIconEngine, QImage, QPainter, QPixmap, QColor
+from PySide6.QtSvg import QSvgRenderer
+
+from .config import isDarkTheme, Theme
+
+
+class IconEngine(QIconEngine):
+    """ Icon engine """
+
+    def __init__(self, iconPath):
+        self.iconPath = iconPath
+        super().__init__()
+
+    def paint(self, painter, rect, mode, state):
+        painter.setRenderHints(QPainter.Antialiasing |
+                               QPainter.SmoothPixmapTransform)
+        if not self.iconPath.lower().endswith('svg'):
+            painter.drawImage(rect, QImage(self.iconPath))
+        else:
+            drawSvgIcon(self.iconPath, painter, rect)
+
+    def pixmap(self, size, mode, state):
+        pixmap = QPixmap(size)
+        pixmap.fill(Qt.transparent)
+        self.paint(QPainter(pixmap), QRect(QPoint(0, 0), size), mode, state)
+        return pixmap
+
+
+class Icon(QIcon):
+
+    def __init__(self, iconPath):
+        self.iconPath = iconPath
+        super().__init__(IconEngine(iconPath))
+
+
+class MenuIconEngine(QIconEngine):
+
+    def __init__(self, icon):
+        super().__init__()
+        self.icon = icon
+
+    def paint(self, painter, rect, mode, state):
+        self.icon.paint(painter, rect, Qt.AlignHCenter, QIcon.Normal, state)
+
+
+def getIconColor():
+    """ get the color of icon based on theme """
+    return "white" if isDarkTheme() else 'black'
+
+
+def drawSvgIcon(icon, painter, rect):
+    """ draw svg icon
+
+    Parameters
+    ----------
+    icon: str | bytes | QByteArray
+        the path or code of svg icon
+
+    painter: QPainter
+        painter
+
+    rect: QRect | QRectF
+        the rect to render icon
+    """
+    renderer = QSvgRenderer(icon)
+    renderer.render(painter, QRectF(rect))
+
+
+def writeSvg(iconPath: str, indexes=None, **attributes):
+    """ write svg with specified attributes
+
+    Parameters
+    ----------
+    iconPath: str
+        svg icon path
+
+    indexes: List[int]
+        the path to be filled
+
+    **attributes:
+        the attributes of path
+
+    Returns
+    -------
+    svg: str
+        svg code
+    """
+    if not iconPath.lower().endswith('.svg'):
+        return ""
+
+    f = QFile(iconPath)
+    f.open(QFile.ReadOnly)
+
+    dom = QDomDocument()
+    dom.setContent(f.readAll())
+
+    f.close()
+
+    # change the color of each path
+    pathNodes = dom.elementsByTagName('path')
+    indexes = range(pathNodes.length()) if not indexes else indexes
+    for i in indexes:
+        element = pathNodes.at(i).toElement()
+
+        for k, v in attributes.items():
+            element.setAttribute(k, v)
+
+    return dom.toString()
+
+
+def drawIcon(icon, painter, rect):
+    """ draw icon
+
+    Parameters
+    ----------
+    icon: str | QIcon | FluentIconBaseBase
+        the icon to be drawn
+
+    painter: QPainter
+        painter
+
+    rect: QRect | QRectF
+        the rect to render icon
+    """
+    if isinstance(icon, FluentIconBase):
+        icon.render(painter, rect)
+    else:
+        icon = QIcon(icon)
+        rect = QRectF(rect).toRect()
+        image = icon.pixmap(rect.width(), rect.height())
+        painter.drawPixmap(rect, image)
+
+
+class FluentIconBase:
+    """ Fluent icon base class """
+
+    def path(self, theme=Theme.AUTO):
+        """ get the path of icon
+
+        Parameters
+        ----------
+        theme: Theme
+            the theme of icon
+            * `Theme.Light`: black icon
+            * `Theme.DARK`: white icon
+            * `Theme.AUTO`: icon color depends on `config.theme`
+        """
+        raise NotImplementedError
+
+    def icon(self, theme=Theme.AUTO):
+        """ create an fluent icon
+
+        Parameters
+        ----------
+        theme: Theme
+            the theme of icon
+            * `Theme.Light`: black icon
+            * `Theme.DARK`: white icon
+            * `Theme.AUTO`: icon color depends on `config.theme`
+        """
+        return QIcon(self.path(theme))
+
+    def render(self, painter, rect, theme=Theme.AUTO):
+        """ draw svg icon
+
+        Parameters
+        ----------
+        painter: QPainter
+            painter
+
+        rect: QRect | QRectF
+            the rect to render icon
+
+        theme: Theme
+            the theme of icon
+            * `Theme.Light`: black icon
+            * `Theme.DARK`: white icon
+            * `Theme.AUTO`: icon color depends on `config.theme`
+        """
+        drawSvgIcon(self.path(theme), painter, rect)
+
+
+class FluentIcon(FluentIconBase, Enum):
+    """ Fluent icon """
+
+    ADD = "Add"
+    CUT = "Cut"
+    PIN = "Pin"
+    TAG = "Tag"
+    WEB = "Web"
+    CHAT = "Chat"
+    COPY = "Copy"
+    CODE = "Code"
+    EDIT = "Edit"
+    FONT = "Font"
+    HELP = "Help"
+    HOME = "Home"
+    INFO = "Info"
+    LINK = "Link"
+    MAIL = "Mail"
+    MENU = "Menu"
+    MORE = "More"
+    SEND = "Send"
+    SYNC = "Sync"
+    ZOOM = "Zoom"
+    ALBUM = "Album"
+    BRUSH = "Brush"
+    CLOSE = "Close"
+    EMBED = "Embed"
+    HEART = "Heart"
+    MOVIE = "Movie"
+    MUSIC = "Music"
+    PHOTO = "Photo"
+    PHONE = "Phone"
+    VIDEO = "Video"
+    PASTE = "Paste"
+    CAMERA = "Camera"
+    CANCEL = "Cancel"
+    FOLDER = "Folder"
+    SCROLL = "Scroll"
+    LAYOUT = "Layout"
+    GITHUB = "GitHub"
+    SEARCH = "Search"
+    UPDATE = "Update"
+    RETURN = "Return"
+    ZOOM_IN = "ZoomIn"
+    SETTING = "Setting"
+    PALETTE = "Palette"
+    MESSAGE = "Message"
+    ZOOM_OUT = "ZoomOut"
+    FEEDBACK = "Feedback"
+    MINIMIZE = "Minimize"
+    CHECKBOX = "CheckBox"
+    DOCUMENT = "Document"
+    LANGUAGE = "Language"
+    DOWNLOAD = "Download"
+    QUESTION = "Question"
+    SEND_FILL = "SendFill"
+    COMPLETED = "Completed"
+    CONSTRACT = "Constract"
+    ALIGNMENT = "Alignment"
+    BOOK_SHELF = "BookShelf"
+    PENCIL_INK = "PencilInk"
+    FOLDER_ADD = "FolderAdd"
+    MICROPHONE = "Microphone"
+    ARROW_DOWN = "ChevronDown"
+    TRANSPARENT = "Transparent"
+    MUSIC_FOLDER = "MusicFolder"
+    CHEVRON_RIGHT = "ChevronRight"
+    BACKGROUND_FILL = "BackgroundColor"
+    FLUORESCENT_PEN = "FluorescentPen"
+
+    def path(self, theme=Theme.AUTO):
+        if theme == Theme.AUTO:
+            c = getIconColor()
+        else:
+            c = "white" if theme == Theme.DARK else "black"
+
+        return f':/qfluentwidgets/images/icons/{self.value}_{c}.svg'
---
+++ packages/PySide6-Fluent-Widgets/PySide6_Fluent_Widgets-0.5.7-py3-none-any.whl/qfluentwidgets/components/widgets/scroll_area.py
@@ -0,0 +1,141 @@
+# coding:utf-8
+from PySide6.QtCore import QEasingCurve, Qt, Signal, QPropertyAnimation, QEvent
+from PySide6.QtWidgets import QScrollArea, QScrollBar
+
+from ...common.smooth_scroll import SmoothScroll
+
+
+class ScrollArea(QScrollArea):
+    """ A scroll area which can scroll smoothly """
+
+    def __init__(self, parent=None, orient=Qt.Vertical):
+        """
+        Parameters
+        ----------
+        parent: QWidget
+            parent widget
+
+        orient: Orientation
+            scroll orientation
+        """
+        super().__init__(parent)
+        self.smoothScroll = SmoothScroll(self, orient)
+        self.viewport().installEventFilter(self)
+
+    def setSmoothMode(self, mode):
+        """ set smooth mode
+
+        Parameters
+        ----------
+        mode: SmoothMode
+            smooth scroll mode
+        """
+        self.smoothScroll.setSmoothMode(mode)
+
+    def eventFilter(self, obj, e):
+        if obj is self.viewport():
+            if e.type() == QEvent.Wheel:
+                self.smoothScroll.wheelEvent(e)
+
+                # stop event propagation
+                e.setAccepted(True)
+                return True
+
+        return super().eventFilter(obj, e)
+
+
+class SmoothScrollBar(QScrollBar):
+    """ Smooth scroll bar """
+
+    scrollFinished = Signal()
+
+    def __init__(self, parent=None):
+        QScrollBar.__init__(self, parent)
+        self.ani = QPropertyAnimation()
+        self.ani.setTargetObject(self)
+        self.ani.setPropertyName(b"value")
+        self.ani.setEasingCurve(QEasingCurve.OutCubic)
+        self.ani.setDuration(500)
+        self.__value = self.value()
+        self.ani.finished.connect(self.scrollFinished)
+
+    def setValue(self, value):
+        if value == self.value():
+            return
+
+        # stop running animation
+        self.ani.stop()
+        self.scrollFinished.emit()
+
+        self.ani.setStartValue(self.value())
+        self.ani.setEndValue(value)
+        self.ani.start()
+
+    def scrollValue(self, value):
+        """ scroll the specified distance """
+        self.__value += value
+        self.__value = max(self.minimum(), self.__value)
+        self.__value = min(self.maximum(), self.__value)
+        self.setValue(self.__value)
+
+    def scrollTo(self, value):
+        """ scroll to the specified position """
+        self.__value = value
+        self.__value = max(self.minimum(), self.__value)
+        self.__value = min(self.maximum(), self.__value)
+        self.setValue(self.__value)
+
+    def resetValue(self, value):
+        self.__value = value
+
+    def mousePressEvent(self, e):
+        self.ani.stop()
+        super().mousePressEvent(e)
+        self.__value = self.value()
+
+    def mouseReleaseEvent(self, e):
+        self.ani.stop()
+        super().mouseReleaseEvent(e)
+        self.__value = self.value()
+
+    def mouseMoveEvent(self, e):
+        self.ani.stop()
+        super().mouseMoveEvent(e)
+        self.__value = self.value()
+
+
+class SmoothScrollArea(QScrollArea):
+    """ Smooth scroll area """
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.hScrollBar = SmoothScrollBar()
+        self.vScrollBar = SmoothScrollBar()
+        self.hScrollBar.setOrientation(Qt.Horizontal)
+        self.vScrollBar.setOrientation(Qt.Vertical)
+        self.setVerticalScrollBar(self.vScrollBar)
+        self.setHorizontalScrollBar(self.hScrollBar)
+
+    def setScrollAnimation(self, orient, duration, easing=QEasingCurve.OutCubic):
+        """ set scroll animation
+
+        Parameters
+        ----------
+        orient: Orient
+            scroll orientation
+
+        duration: int
+            scroll duration
+
+        easing: QEasingCurve
+            animation type
+        """
+        bar = self.hScrollBar if orient == Qt.Horizontal else self.vScrollBar
+        bar.ani.setDuration(duration)
+        bar.ani.setEasingCurve(easing)
+
+    def wheelEvent(self, e):
+        if e.modifiers() == Qt.NoModifier:
+            self.vScrollBar.scrollValue(-e.angleDelta().y())
+        else:
+            self.hScrollBar.scrollValue(-e.angleDelta().x())
