087861dc3aa78902e8c6cd9c13bc447063277dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:52 +0000

    Add package skaiciuotuvas/0.1/skaiciuotuvas-0.1.tar.gz

---
+++ packages/skaiciuotuvas/skaiciuotuvas-0.1.tar.gz/skaiciuotuvas-0.1/README.md
@@ -0,0 +1,2 @@
+# skaiciuotuvas
+calculator
---
+++ packages/skaiciuotuvas/skaiciuotuvas-0.1.tar.gz/skaiciuotuvas-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skaiciuotuvas',
+    version='0.1',
+    description='Calculator',
+    packages=find_packages(),
+)
