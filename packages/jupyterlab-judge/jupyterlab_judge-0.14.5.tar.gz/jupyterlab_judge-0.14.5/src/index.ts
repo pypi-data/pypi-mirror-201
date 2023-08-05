7e8695ff04d4c2a2342aabbd75c61208660908c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:12 +0000

    Add package jupyterlab-judge/0.14.5/jupyterlab_judge-0.14.5.tar.gz

---
+++ packages/jupyterlab-judge/jupyterlab_judge-0.14.5.tar.gz/jupyterlab_judge-0.14.5/src/index.ts
@@ -0,0 +1,307 @@
+import {
+  ILayoutRestorer,
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { ICommandPalette, WidgetTracker } from '@jupyterlab/apputils';
+import { ITranslator } from '@jupyterlab/translation';
+import {
+  JudgeDocument,
+  JudgeDocumentFactory,
+  JudgePanel
+} from './widgets/JudgePanel';
+import { CodeEditor, IEditorServices } from '@jupyterlab/codeeditor';
+import { addCommands, addMenuItems, CommandIDs } from './commands';
+import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
+import { IDocumentManager } from '@jupyterlab/docmanager';
+import { IFileBrowserFactory } from '@jupyterlab/filebrowser';
+import { IMainMenu } from '@jupyterlab/mainmenu';
+import {
+  BROWSER_NAME,
+  DRIVE_NAME,
+  PLUGIN_ID,
+  TRANSLATOR_DOMAIN
+} from './constants';
+import { Drive } from '@jupyterlab/services';
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { JSONObject } from '@lumino/coreutils';
+import { JudgeModel } from './model';
+import {
+  IJudgePanelFactoryRegistry,
+  IJudgeSignal,
+  IJudgeSubmissionAreaFactoryRegistry,
+  IJudgeTerminalFactoryRegistry,
+  IProblemProvider,
+  IProblemProviderRegistry,
+  ISubmissionListFactoryRegistry
+} from './tokens';
+import { HardCodedProblemProvider } from './problemProvider/HardCodedProblemProvider';
+import { ProblemProvider } from './problemProvider/problemProvider';
+import { Signal } from '@lumino/signaling';
+import { JudgeSubmissionArea } from './widgets/JudgeSubmissionArea';
+import { Widget } from '@lumino/widgets';
+import { JudgeTerminal } from './widgets/JudgeTerminal';
+import {
+  SubmissionList,
+  SubmissionListImpl
+} from './components/SubmissionList';
+
+/**
+ * A signal that emits whenever a submission is submitted.
+ */
+const submitted = new Signal<
+  any,
+  {
+    widget: JudgePanel;
+    problem: ProblemProvider.IProblem;
+    submission: ProblemProvider.ISubmission;
+  }
+>({});
+
+const signal: JupyterFrontEndPlugin<IJudgeSignal> = {
+  id: `${PLUGIN_ID}:IJudgeSignal`,
+  provides: IJudgeSignal,
+  activate: (_app: JupyterFrontEnd): IJudgeSignal => {
+    return {
+      get submitted() {
+        return submitted;
+      }
+    };
+  },
+  autoStart: true
+};
+
+let problemProvider: IProblemProvider = new HardCodedProblemProvider();
+const problemProviderRegistry: JupyterFrontEndPlugin<IProblemProviderRegistry> =
+  {
+    id: `${PLUGIN_ID}:IProblemProviderRegistry`,
+    provides: IProblemProviderRegistry,
+    activate: (_app: JupyterFrontEnd): IProblemProviderRegistry => {
+      return {
+        register: (provider: IProblemProvider) => {
+          problemProvider = provider;
+        }
+      };
+    },
+    autoStart: true
+  };
+
+let judgePanelFactory = (options: JudgePanel.IOptions) =>
+  new JudgePanel(options);
+const judgePanelFactoryRegistry: JupyterFrontEndPlugin<IJudgePanelFactoryRegistry> =
+  {
+    id: `${PLUGIN_ID}:IJudgePanelFactoryRegistry`,
+    provides: IJudgePanelFactoryRegistry,
+    activate: (_app: JupyterFrontEnd): IJudgePanelFactoryRegistry => {
+      return {
+        register: (factory: (options: JudgePanel.IOptions) => JudgePanel) => {
+          judgePanelFactory = factory;
+        }
+      };
+    },
+    autoStart: true
+  };
+
+let judgeSubmissionAreaFactory: (
+  options: JudgeSubmissionArea.IOptions
+) => Widget = (options: JudgeSubmissionArea.IOptions) =>
+  new JudgeSubmissionArea(options);
+const judgeSubmissionAreaFactoryRegistry: JupyterFrontEndPlugin<IJudgeSubmissionAreaFactoryRegistry> =
+  {
+    id: `${PLUGIN_ID}:IJudgeSubmissionAreaFactoryRegistry`,
+    provides: IJudgeSubmissionAreaFactoryRegistry,
+    activate: (_app: JupyterFrontEnd): IJudgeSubmissionAreaFactoryRegistry => {
+      return {
+        register: (
+          factory: (options: JudgeSubmissionArea.IOptions) => Widget
+        ) => {
+          judgeSubmissionAreaFactory = factory;
+        }
+      };
+    },
+    autoStart: true
+  };
+
+let judgeTerminalFactory: (
+  options: JudgeTerminal.IOptions
+) => JudgeTerminal.IJudgeTerminal = (options: JudgeTerminal.IOptions) =>
+  new JudgeTerminal(options);
+const judgeTerminalFactoryRegistry: JupyterFrontEndPlugin<IJudgeTerminalFactoryRegistry> =
+  {
+    id: `${PLUGIN_ID}:IJudgeTerminalFactoryRegistry`,
+    provides: IJudgeTerminalFactoryRegistry,
+    activate: (_app: JupyterFrontEnd): IJudgeTerminalFactoryRegistry => {
+      return {
+        register: (
+          factory: (
+            options: JudgeTerminal.IOptions
+          ) => JudgeTerminal.IJudgeTerminal
+        ) => {
+          judgeTerminalFactory = factory;
+        }
+      };
+    },
+    autoStart: true
+  };
+
+let submissionListFactory: (options: SubmissionList.IOptions) => JSX.Element =
+  SubmissionListImpl;
+const submissionListFactoryRegistry: JupyterFrontEndPlugin<ISubmissionListFactoryRegistry> =
+  {
+    id: `${PLUGIN_ID}:ISubmissionListFactoryRegistry`,
+    provides: ISubmissionListFactoryRegistry,
+    activate: (_app: JupyterFrontEnd): ISubmissionListFactoryRegistry => {
+      return {
+        register: (
+          factory: (options: SubmissionList.IOptions) => JSX.Element
+        ) => {
+          submissionListFactory = factory;
+        }
+      };
+    },
+    autoStart: true
+  };
+
+/**
+ * Initialization data for the jupyterlab_judge extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: `${PLUGIN_ID}:plugin`,
+  autoStart: true,
+  requires: [
+    ITranslator,
+    IEditorServices,
+    IRenderMimeRegistry,
+    IDocumentManager,
+    IFileBrowserFactory,
+    IMainMenu,
+    ICommandPalette
+  ],
+  optional: [ISettingRegistry, ILayoutRestorer],
+  activate: async (
+    app: JupyterFrontEnd,
+    translator: ITranslator,
+    editorService: IEditorServices,
+    rendermime: IRenderMimeRegistry,
+    docManager: IDocumentManager,
+    browserFactory: IFileBrowserFactory,
+    menu: IMainMenu,
+    palette: ICommandPalette,
+    settingRegistry: ISettingRegistry | null,
+    restorer: ILayoutRestorer | null
+  ) => {
+    const trans = translator.load(TRANSLATOR_DOMAIN);
+    const namespace = 'judge';
+    const tracker = new WidgetTracker<JudgeDocument>({
+      namespace
+    });
+    const judgeDocumentFactoryName = trans.__('Judge');
+
+    // 사용자가 노트북에 설정한 코드 셀 컨피그를 가져와 저지에 적용합니다.
+    let editorConfig: Partial<CodeEditor.IConfig> | null = null;
+    if (settingRegistry) {
+      const settings = await settingRegistry.load(
+        '@jupyterlab/notebook-extension:tracker'
+      );
+      const codeConfig = settings.get('codeCellConfig').composite as JSONObject;
+      editorConfig = codeConfig;
+    } else {
+      editorConfig = {};
+    }
+
+    const widgetFactory = new JudgeDocumentFactory({
+      editorServices: editorService,
+      rendermime: rendermime,
+      commands: app.commands,
+      editorConfig: editorConfig,
+      judgePanelFactory,
+      judgeSubmissionAreaFactory,
+      judgeTerminalFactory,
+      submissionListFactory,
+      submitted,
+      factoryOptions: {
+        name: judgeDocumentFactoryName,
+        modelName: 'judge-model',
+        fileTypes: ['judge'],
+        defaultFor: ['judge'],
+        preferKernel: true,
+        canStartKernel: true,
+        shutdownOnClose: true,
+        translator: translator
+      }
+    });
+
+    widgetFactory.widgetCreated.connect((sender, widget) => {
+      // Notify the widget tracker if restore data needs to update.
+      widget.context.pathChanged.connect(() => {
+        void tracker.save(widget);
+      });
+      void tracker.add(widget);
+    });
+    app.docRegistry.addWidgetFactory(widgetFactory);
+
+    app.docRegistry.addModelFactory(
+      new JudgeModel.JudgeModelFactory({
+        problemProviderFactory: () => problemProvider
+      })
+    );
+    app.docRegistry.addFileType({
+      name: 'judge',
+      contentType: 'file',
+      fileFormat: 'text',
+      displayName: trans.__('Judge File'),
+      extensions: ['.judge'],
+      mimeTypes: ['text/json', 'application/json']
+    });
+
+    addCommands(app.commands, trans, docManager, tracker, problemProvider);
+    addMenuItems(menu, tracker, trans);
+
+    palette.addItem({
+      command: CommandIDs.openOrCreateFromId,
+      category: 'Judge',
+      args: { problemId: '1' }
+    });
+
+    if (restorer) {
+      void restorer.restore(tracker, {
+        command: CommandIDs.open,
+        args: widget => ({ path: widget.context.path }),
+        name: widget => widget.context.path
+      });
+    }
+
+    // 독립적인 Drive와 File Browser 를 생성합니다.
+    // 이 Drive 경로로 접근되면 File Browser 에 영향을 주지 않습니다.
+    // 기존 Drive 를 사용하는 경우 Judge 파일을 클릭하면 숨김 폴더가 열리는 문제가 있습니다.
+    // https://www.notion.so/team-mono/UX-d07ce16254c64434b13712db5d042a4c
+    app.serviceManager.contents.addDrive(new Drive({ name: DRIVE_NAME }));
+    browserFactory.createFileBrowser(BROWSER_NAME, {
+      driveName: DRIVE_NAME
+    });
+  }
+};
+
+/**
+ * Export the plugins as default.
+ */
+const plugins: JupyterFrontEndPlugin<any>[] = [
+  plugin,
+  problemProviderRegistry,
+  judgePanelFactoryRegistry,
+  judgeSubmissionAreaFactoryRegistry,
+  judgeTerminalFactoryRegistry,
+  submissionListFactoryRegistry,
+  signal
+];
+
+export default plugins;
+
+export * from './tokens';
+
+export { openOrCreateFromId } from './commands';
+
+export * from './widgets';
+export * from './components';
+export * from './problemProvider';
---
+++ packages/jupyterlab-judge/jupyterlab_judge-0.14.5.tar.gz/jupyterlab_judge-0.14.5/src/tokens.ts
@@ -0,0 +1,85 @@
+import { Token } from '@lumino/coreutils';
+import { ISignal } from '@lumino/signaling';
+import { Widget } from '@lumino/widgets';
+import { SubmissionList } from './components/SubmissionList';
+import { PLUGIN_ID } from './constants';
+import { ProblemProvider } from './problemProvider/problemProvider';
+import { JudgePanel } from './widgets/JudgePanel';
+import { JudgeSubmissionArea } from './widgets/JudgeSubmissionArea';
+import { JudgeTerminal } from './widgets/JudgeTerminal';
+
+/**
+ * The Problem Provider token.
+ */
+export const IProblemProviderRegistry = new Token<IProblemProviderRegistry>(
+  `${PLUGIN_ID}:IProblemProviderRegistry`
+);
+
+export interface IProblemProviderRegistry {
+  register(provider: IProblemProvider): void;
+}
+
+export interface IProblemProvider {
+  getProblem(id: string): Promise<ProblemProvider.IProblem | null>;
+  getTestCases(id: string): Promise<string[]>;
+  validate(
+    id: string,
+    outputs: string[]
+  ): Promise<ProblemProvider.IValidateResult>;
+  getSubmissions(id: string): Promise<ProblemProvider.ISubmission[]>;
+  submit(
+    request: ProblemProvider.ISubmissionRequest
+  ): Promise<ProblemProvider.ISubmission>;
+}
+
+export const IJudgePanelFactoryRegistry = new Token<IJudgePanelFactoryRegistry>(
+  `${PLUGIN_ID}:IJudgePanelFactoryRegistry`
+);
+
+export interface IJudgePanelFactoryRegistry {
+  register(factory: (options: JudgePanel.IOptions) => JudgePanel): void;
+}
+
+export const IJudgeSubmissionAreaFactoryRegistry =
+  new Token<IJudgeSubmissionAreaFactoryRegistry>(
+    `${PLUGIN_ID}:IJudgeSubmissionAreaFactoryRegistry`
+  );
+
+export interface IJudgeSubmissionAreaFactoryRegistry {
+  register(factory: (options: JudgeSubmissionArea.IOptions) => Widget): void;
+}
+
+export const IJudgeTerminalFactoryRegistry =
+  new Token<IJudgeTerminalFactoryRegistry>(
+    `${PLUGIN_ID}:IJudgeTerminalFactoryRegistry`
+  );
+
+export interface IJudgeTerminalFactoryRegistry {
+  register(
+    factory: (options: JudgeTerminal.IOptions) => JudgeTerminal.IJudgeTerminal
+  ): void;
+}
+
+export const ISubmissionListFactoryRegistry =
+  new Token<ISubmissionListFactoryRegistry>(
+    `${PLUGIN_ID}:ISubmissionListFactoryRegistry`
+  );
+
+export interface ISubmissionListFactoryRegistry {
+  register(factory: (options: SubmissionList.IOptions) => JSX.Element): void;
+}
+
+export const IJudgeSignal = new Token<IJudgeSignal>(
+  `${PLUGIN_ID}:IJudgeSignal`
+);
+
+export interface IJudgeSignal {
+  readonly submitted: ISignal<
+    any,
+    {
+      widget: JudgePanel;
+      problem: ProblemProvider.IProblem;
+      submission: ProblemProvider.ISubmission;
+    }
+  >;
+}
