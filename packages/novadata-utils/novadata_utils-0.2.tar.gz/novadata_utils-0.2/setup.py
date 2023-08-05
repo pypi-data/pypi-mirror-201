2380bbeb626fc5cc6b466ef893fd53541a05a463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:46 +0000

    Add package novadata-utils/0.2/novadata_utils-0.2.tar.gz

---
+++ packages/novadata-utils/novadata_utils-0.2.tar.gz/novadata_utils-0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setup(
+    name="novadata_utils",
+    version="0.2",
+    url="https://github.com/TimeNovaData/novadata_utils/",
+    license="MIT License",
+    author="Flávio Silva",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="flavio.nogueira.profissional@gmail.com",
+    keywords="Django, utils, ndt, novadata, nova data, nova, data",
+    description="novadata utils",
+    packages=find_packages(),
+    install_requires=[
+        "django",
+        "djangorestframework",
+        "django-object-actions",
+        "django-import-export",
+    ],
+    project_urls={
+        "GitHub": "https://github.com/TimeNovaData/novadata_utils/",
+    },
+)
