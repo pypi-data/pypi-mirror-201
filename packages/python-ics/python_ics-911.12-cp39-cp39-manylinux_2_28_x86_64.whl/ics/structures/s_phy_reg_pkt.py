189a52a013b55b357f3e4dbd6eba881c48703c6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:07 +0000

    Add package python-ics/911.12/python_ics-911.12-cp39-cp39-manylinux_2_28_x86_64.whl

---
+++ packages/python-ics/python_ics-911.12-cp39-cp39-manylinux_2_28_x86_64.whl/ics/structures/s_phy_reg_pkt.py
@@ -0,0 +1,49 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+from ics.structures.s_phy_reg_pkt_clause22_mess import *
+from ics.structures.s_phy_reg_pkt_clause45_mess import *
+
+
+class Nameless3555(ctypes.Structure):
+    _fields_ = [
+        ('Enabled', ctypes.c_uint16, 1),
+        ('WriteEnable', ctypes.c_uint16, 1),
+        ('Clause45Enable', ctypes.c_uint16, 1),
+        ('status', ctypes.c_uint16, 3),
+        ('reserved', ctypes.c_uint16, 2),
+        ('BusIndex', ctypes.c_uint16, 4),
+        ('version', ctypes.c_uint16, 4),
+    ]
+
+
+
+class Nameless45381(ctypes.Union):
+    _anonymous_  = ('Nameless3555',)
+    _fields_ = [
+        ('Nameless3555', Nameless3555),
+        ('flags', ctypes.c_uint16),
+    ]
+
+
+
+class Nameless14534(ctypes.Union):
+    _fields_ = [
+        ('clause22', PhyRegPktClause22Mess_t),
+        ('clause45', PhyRegPktClause45Mess_t),
+    ]
+
+
+
+class s_phy_reg_pkt(ctypes.Structure):
+    _anonymous_  = ('Nameless45381', 'Nameless14534')
+    _fields_ = [
+        ('Nameless45381', Nameless45381),
+        ('Nameless14534', Nameless14534),
+    ]
+
+
+SPhyRegPkt = s_phy_reg_pkt
+PhyRegPkt_t = s_phy_reg_pkt
+
