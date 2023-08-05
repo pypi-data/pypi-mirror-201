efa8f8866a85c6e7c20eb3db10c4eee77fdb93cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:22 +0000

    Add package codx/0.1.1/codx-0.1.1-py3-none-any.whl

---
+++ packages/codx/codx-0.1.1-py3-none-any.whl/codx/test_cli.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+
+from codx.cli import main
+
+
+class TestCli:
+    def test_main_gene_ids(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ["11047"])
+        assert result.exit_code == 0
+
+    def test_main_uniprot(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ["P04637", "--uniprot"])
+        assert result.exit_code == 0
+
+    def test_main_translate(self):
+        runner = CliRunner()
+        result = runner.invoke(main, ["11047", "--translate"])
+        assert result.exit_code == 0
