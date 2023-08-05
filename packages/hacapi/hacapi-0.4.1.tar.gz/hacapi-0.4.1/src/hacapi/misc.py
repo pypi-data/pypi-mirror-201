957fe55dd1f6aeec1c18e197595cc92c5e646292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:31 +0000

    Add package hacapi/0.4.1/hacapi-0.4.1.tar.gz

---
+++ packages/hacapi/hacapi-0.4.1.tar.gz/hacapi-0.4.1/src/hacapi/misc.py
@@ -0,0 +1,19 @@
+import pytz
+from datetime import datetime
+
+# Checks if the list of grades has changed
+
+def isUpdated(original, new):
+    matching = True
+
+    try:
+        return_value = new.index([x for x in new if x not in original][0])
+    except IndexError:
+        matching = False
+
+    return matching, return_value
+
+
+def getTime():
+    my_date = datetime.now(pytz.timezone('US/Central'))
+    return my_date.strftime("%m/%d/%Y %I:%M:%S %p")
