6456a68db64571493907ba3d359fc332909fe61b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:41 +0000

    Add package meltano/2.17.1/meltano-2.17.1-py3-none-any.whl

---
+++ packages/meltano/meltano-2.17.1-py3-none-any.whl/meltano/cli/__init__.py
@@ -0,0 +1,121 @@
+"""Main entry point for the meltano CLI."""
+
+from __future__ import annotations
+
+import logging
+import os
+import sys
+import typing as t
+
+from meltano.cli.utils import CliError
+from meltano.core.error import MeltanoError, ProjectReadonly
+from meltano.core.logging import setup_logging
+
+# TODO: Importing the cli.cli module breaks other cli module imports
+# This suggests a cyclic dependency or a poorly structured interface.
+# This should be investigated and resolved to avoid implicit behavior
+# based solely on import order.
+from meltano.cli.cli import cli  # isort:skip
+from meltano.cli import (  # isort:skip # noqa: WPS235
+    add,
+    compile,
+    config,
+    discovery,
+    dragon,
+    elt,
+    environment,
+    initialize,
+    install,
+    invoke,
+    lock,
+    remove,
+    repl,
+    schedule,
+    schema,
+    select,
+    state,
+    ui,
+    upgrade,
+    user,
+    run,
+    validate,
+    job,
+)
+
+if t.TYPE_CHECKING:
+    from meltano.core.tracking.tracker import Tracker
+
+
+# Holds the exit code for error reporting during process exiting. In
+# particular, a function registered by the `atexit` module uses this value.
+exit_code: None | int = None
+
+atexit_handler_registered = False
+exit_code_reported = False
+exit_event_tracker: Tracker | None = None
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
+
+troubleshooting_message = """\
+Need help fixing this problem? Visit http://melta.no/ for troubleshooting steps, or to
+join our friendly Slack community.
+"""
+
+
+def handle_meltano_error(error: MeltanoError) -> t.NoReturn:
+    """Handle a MeltanoError.
+
+    Args:
+        error: The error to handle.
+
+    Raises:
+        CliError: always.
+    """
+    raise CliError(str(error)) from error
+
+
+def _run_cli():
+    """Run the Meltano CLI.
+
+    Raises:
+        KeyboardInterrupt: if caught.
+    """
+    try:
+        try:  # noqa: WPS225, WPS505
+            cli(obj={"project": None})
+        except ProjectReadonly as err:
+            raise CliError(
+                f"The requested action could not be completed: {err}",
+            ) from err
+        except KeyboardInterrupt:  # noqa: WPS329
+            raise
+        except MeltanoError as err:
+            handle_meltano_error(err)
+        except Exception as err:
+            raise CliError(f"{troubleshooting_message}\n{err}") from err
+    except CliError as cli_error:
+        cli_error.print()
+        sys.exit(1)
+
+
+def main():
+    """Entry point for the meltano CLI."""
+    # Mark the current process as executed via the CLI
+    os.environ["MELTANO_JOB_TRIGGER"] = os.getenv("MELTANO_JOB_TRIGGER", "cli")
+    try:
+        _run_cli()
+    finally:
+        global exit_code
+        ex = sys.exc_info()[1]
+        if ex is None:
+            exit_code = 0  # noqa: WPS442
+        elif isinstance(ex, SystemExit):
+            exit_code = 0 if ex.code is None else ex.code  # noqa: WPS442
+        else:
+            exit_code = 1  # noqa: WPS442
+        # Track the exit event now to provide more details via the exception context.
+        # We assume the process will exit practically immediately after `main` returns.
+        if exit_event_tracker is not None:
+            exit_event_tracker.track_exit_event()
---
+++ packages/meltano/meltano-2.17.1-py3-none-any.whl/meltano/cli/schema.py
@@ -0,0 +1,29 @@
+"""Schema creation CLI."""
+
+from __future__ import annotations
+
+import click
+
+from meltano.core.db import ensure_schema_exists, project_engine
+
+from . import cli
+from .params import pass_project
+from .utils import InstrumentedCmd, InstrumentedGroup
+
+
+@cli.group(cls=InstrumentedGroup, short_help="Manage system DB schema.")
+def schema():
+    """Manage system DB schema."""
+
+
+@schema.command(
+    cls=InstrumentedCmd,
+    short_help="Create system DB schema, if not exists.",
+)
+@click.argument("schema")
+@click.argument("roles", nargs=-1, required=True)
+@pass_project()
+def create(project, schema_name, roles):
+    """Create system DB schema, if not exists."""
+    engine, _ = project_engine(project)
+    ensure_schema_exists(engine, schema_name, grant_roles=roles)
