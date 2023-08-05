676f259b8e6bec723f83431a4dd2d3b214ea272c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:04 +0000

    Add package slackfin/0.1/slackfin-0.1-py2.py3-none-any.whl

---
+++ packages/slackfin/slackfin-0.1-py2.py3-none-any.whl/slackfin/formatter.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+
+class SlackFormatter:
+    """
+    SlackFormatter
+    
+    Slack formats a number of things in a special way. This class provides
+    methods to format those things.
+    """
+    
+    def datetime(
+        self,
+        timestamp: datetime = None,
+        date_format: str = "{date_num} {time}",
+        url: str = None,
+    ):
+        """Format a datetime object into a Slack date string."""
+
+        if not timestamp:
+            timestamp = datetime.now()
+        formatted_string = f"<!date^{int(timestamp.timestamp())}^{date_format}"
+        if url:
+            formatted_string += f"^{url}"
+        formatted_string += f"|{str(timestamp)}>"
+        return formatted_string
