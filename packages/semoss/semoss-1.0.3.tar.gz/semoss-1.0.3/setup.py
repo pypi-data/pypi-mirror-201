86f2a8453cfae34c6b9b214d93c34f623c8e2b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:25 +0000

    Add package semoss/1.0.3/semoss-1.0.3.tar.gz

---
+++ packages/semoss/semoss-1.0.3.tar.gz/semoss-1.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setuptools,find_packages
+with open('README.md','r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="semoss",                     # This is the name of the package
+    version="1.0.3",
+    packages=find_packages(),
+    install_requires=['pandas','requests'], 
+    author="Thomas Trankle",                     # Full name of the author
+    description="Utility package to connect to SEMOSS instances and query data",
+    license="MIT",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown'
+)
