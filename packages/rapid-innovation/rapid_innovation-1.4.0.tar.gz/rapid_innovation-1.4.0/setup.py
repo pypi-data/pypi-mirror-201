eb2350263cd651b5444e4b01aad637bb1587fd41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:18 +0000

    Add package rapid-innovation/1.4.0/rapid_innovation-1.4.0.tar.gz

---
+++ packages/rapid-innovation/rapid_innovation-1.4.0.tar.gz/rapid_innovation-1.4.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rapid_innovation',
+    version='1.4.0',
+    description='Rapid Innovation package',
+    url='https://github.com/Rapid-Python/python-package-rapid',
+    author='Abhishek Negi',
+    author_email='abhisheknegi@rapidinnovation.dev',
+    license='MIT',
+    python_requires='>=3.7',
+    packages=['rapid', 'rapid.app'],
+    install_requires=[],
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
