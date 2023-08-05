e42ebe9d486a0cd85d9cc93c73bd76c877003d7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:31 +0000

    Add package hacapi/0.4.2/hacapi-0.4.2.tar.gz

---
+++ packages/hacapi/hacapi-0.4.2.tar.gz/hacapi-0.4.2/src/hacapi/transcript.py
@@ -0,0 +1,31 @@
+from . import session
+import pandas as pd
+import re
+
+session_requests = session.session_requests
+urls = "https://hac.friscoisd.org/HomeAccess/Content/Student/Transcript.aspx"
+
+result = session_requests.get(urls, headers=dict(referer=urls))
+
+df = pd.read_html(result.text)
+
+print(df)
+
+gpa_text = df[0].loc[3, 0]
+
+def returnWeightedGPA():
+     # Define a regular expression pattern to match the Weighted GPA
+    pattern = r'Weighted GPA\s+(\d+\.\d+)'
+    # Use re.search() to find the first occurrence of the pattern in the string
+    match = re.search(pattern, gpa_text)
+    # If a match was found, return the Weighted GPA as a float; otherwise, return None
+    return float(match.group(1)) if match else None
+
+
+def returnCollegeGPA():
+    pattern = r'College GPA\s+(\d+\.\d+)'
+    match = re.search(pattern, gpa_text)
+    if match:
+        return float(match.group(1))
+    else:
+        return None
