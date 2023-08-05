f80e9587c63293c235d33d72425e22b9d2b2e694
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package anonfiles-directlink/1.2/anonfiles_directlink-1.2-py3-none-any.whl

---
+++ packages/anonfiles-directlink/anonfiles_directlink-1.2-py3-none-any.whl/anonfiles_directlink/__init__.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+def direct_link(p):
+    soup = BeautifulSoup(requests.get(p).text, 'html.parser')
+    for l in soup.find_all(id='download-url'):
+        print(l['href'])
+        return l['href']
