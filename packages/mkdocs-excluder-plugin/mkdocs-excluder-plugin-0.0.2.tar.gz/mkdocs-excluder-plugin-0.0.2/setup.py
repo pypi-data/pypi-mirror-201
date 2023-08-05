2465d7c273075f1b6a522450fa4ab6dac4b196e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:20 +0000

    Add package mkdocs-excluder-plugin/0.0.2/mkdocs-excluder-plugin-0.0.2.tar.gz

---
+++ packages/mkdocs-excluder-plugin/mkdocs-excluder-plugin-0.0.2.tar.gz/mkdocs-excluder-plugin-0.0.2/setup.py
@@ -0,0 +1,32 @@
+# copyright (c) 2018- polygoniq xyz s.r.o.
+
+import os.path
+import setuptools
+
+
+def read(name):
+    mydir = os.path.abspath(os.path.dirname(__file__))
+    return open(os.path.join(mydir, name)).read()
+
+
+setuptools.setup(
+    name='mkdocs-excluder-plugin',
+    version='0.0.2',
+    packages=['mkdocs_excluder'],
+    license='Apache',
+    author='polygoniq',
+    author_email='zdeno@polygoniq.com',
+    description='A mkdocs plugin that lets you exclude files or trees and removes navigation entries.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    install_requires=['mkdocs'],
+
+    # The following rows are important to register your plugin.
+    # The format is "(plugin name) = (plugin folder):(class name)"
+    # Without them, mkdocs will not be able to recognize it.
+    entry_points={
+        'mkdocs.plugins': [
+            'excluder = mkdocs_excluder:Exclude',
+        ]
+    },
+)
