89fec26cf91993175e02faef90d6d9d0a2de96d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:48 +0000

    Add package mayalabs/0.0.6/mayalabs-0.0.6.tar.gz

---
+++ packages/mayalabs/mayalabs-0.0.6.tar.gz/mayalabs-0.0.6/mayalabs/__init__.py
@@ -0,0 +1,10 @@
+from .session import Session
+from .worker import Worker
+from .worker import WorkerClient
+from .session import SessionClient
+from .function import Function
+from .mayalabs import authenticate
+import os
+
+api_key = os.environ.get("MAYA_API_KEY")
+api_base = os.environ.get("MAYA_API_BASE", "https://api.mayalabs.io")
