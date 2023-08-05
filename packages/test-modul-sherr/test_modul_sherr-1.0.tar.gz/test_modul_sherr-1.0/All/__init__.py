e91d2d29dbd7b3d83a81fa7434f25b09c3ccb514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:23 +0000

    Add package test-modul-sherr/1.0/test_modul_sherr-1.0.tar.gz

---
+++ packages/test-modul-sherr/test_modul_sherr-1.0.tar.gz/test_modul_sherr-1.0/All/__init__.py
@@ -0,0 +1,5 @@
+import hello
+
+if __name__ == "__main__":
+    for i in range(10):
+        hello.hi()
---
+++ packages/test-modul-sherr/test_modul_sherr-1.0.tar.gz/test_modul_sherr-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='test_modul_sherr',
+      version='1.0',
+      description='test',
+      packages=['All'],
+      author_email='serfertor@mail.ru',
+      zip_safe=False)
