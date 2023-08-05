da0c3dd89ec35bff89fe8dd6f8490b4fc91033a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:32 +0000

    Add package gusty/0.19.0/gusty-0.19.0.tar.gz

---
+++ packages/gusty/gusty-0.19.0.tar.gz/gusty-0.19.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gusty",
+    version="0.19.0",
+    author="Chris Cardillo, Michael Chow, David Robinson",
+    author_email="cfcardillo23@gmail.com",
+    description="Making DAG construction easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/chriscardillo/gusty",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "ABSQL",
+        "apache-airflow",
+        "inflection",
+        "jupytext",
+        "nbformat",
+        "pendulum",
+        "PyYaml",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
