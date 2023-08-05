3b9aa38d8e3a3f64de1465cad3341156acb3680c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:33 +0000

    Add package pad-asset-manager/1.0.2/pad_asset_manager-1.0.2.tar.gz

---
+++ packages/pad-asset-manager/pad_asset_manager-1.0.2.tar.gz/pad_asset_manager-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pad_asset_manager",
+    version="1.0.2",
+    author="Aradia Megido#2552 and Cody Watts",
+    author_email="chasehult@gmail.com",
+    license="MIT",
+    description="An asset and extra manager for Puzzle and Dragons",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TsubakiBotPad/pad_asset_manager",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
