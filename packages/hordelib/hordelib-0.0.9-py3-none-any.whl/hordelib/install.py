431cebda8585f61cb20568f7edaee8adb5d16523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:23 +0000

    Add package hordelib/0.0.9/hordelib-0.0.9-py3-none-any.whl

---
+++ packages/hordelib/hordelib-0.0.9-py3-none-any.whl/hordelib/install.py
@@ -0,0 +1,67 @@
+# install.py
+# Fetch a specific version of the upstream ComfyUI project
+import os
+import subprocess
+from loguru import logger
+
+
+class Installer:
+    def __init__(self):
+        self.ourdir = os.path.dirname(os.path.realpath(__file__))
+
+    def get_commit_hash(self):
+        head_file = os.path.join(self.ourdir, "ComfyUI", ".git", "HEAD")
+        if not os.path.exists(head_file):
+            return "NOT FOUND"
+        try:
+            with open(head_file, "r") as f:
+                head_contents = f.read().strip()
+
+            if not head_contents.startswith("ref:"):
+                return head_contents
+
+            ref_path = os.path.join(".git", *head_contents[5:].split("/"))
+
+            with open(ref_path, "r") as f:
+                commit_hash = f.read().strip()
+
+            return commit_hash
+        except Exception:
+            return ""
+
+    def _run(self, command, subdir=None):
+        if not subdir:
+            directory = self.ourdir
+        else:
+            directory = os.path.join(self.ourdir, subdir)
+        try:
+            result = subprocess.run(
+                command, shell=True, text=True, capture_output=True, cwd=directory
+            )
+        except Exception as Ex:
+            logger.error(Ex)
+            return
+        if result.returncode:
+            logger.error(result.stderr)
+            return
+        return (True, result.stdout)
+
+    def install(self, comfy_version):
+
+        # Install if ComfyUI is missing completely
+        if not os.path.exists(f"{self.ourdir}/ComfyUI"):
+            self._run("git clone https://github.com/comfyanonymous/ComfyUI.git")
+            self._run(f"git checkout {comfy_version}", "ComfyUI")
+            return
+
+        # If it's installed, is it up to date?
+        version = self.get_commit_hash()
+        if version == comfy_version:
+            # Yes, all done
+            return
+
+        # Update comfyui
+        logger.info(
+            f"Current ComfyUI version {version[:8]} requires {comfy_version[:8]}"
+        )
+        self._run(f"git checkout {comfy_version}", "ComfyUI")
---
+++ packages/hordelib/hordelib-0.0.9-py3-none-any.whl/tests/test_inference.py
@@ -0,0 +1,68 @@
+# test_inference.py
+import pytest
+from hordelib.comfy import Comfy
+from PIL import Image
+
+
+class TestInference:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        self.comfy = Comfy()
+        yield
+        self.comfy = None
+
+    def test_unknown_pipeline(self):
+        result = self.comfy.run_pipeline("non-existent-pipeline", {})
+        assert result is None
+
+    def test_stable_diffusion_pipeline(self):
+        params = {
+            "sampler.sampler_name": "dpmpp_2m",
+            "sampler.cfg": 7.5,
+            "sampler.denoise": 1.0,
+            "sampler.seed": 12345,
+            "empty_latent_image.width": 768,
+            "empty_latent_image.height": 768,
+            "empty_latent_image.batch_size": 1,
+            "sampler.scheduler": "karras",
+            "sampler.steps": 25,
+            "prompt.text": "a closeup photo of a confused dog",
+            "negative_prompt.text": "cat, black and white, deformed",
+            "model_loader.ckpt_name": "model.ckpt",
+        }
+        images = self.comfy.run_image_pipeline("stable_diffusion", params)
+
+        image = Image.open(images[0]["imagedata"])
+        image.save("pipeline_stable_diffusion.png")
+
+    def test_stable_diffusion_hires_fix_pipeline(self):
+        params = {
+            "sampler.seed": 12345,
+            "sampler.cfg": 7.5,
+            "sampler.scheduler": "normal",
+            "sampler.sampler_name": "dpmpp_sde",
+            "sampler.denoise": 1.0,
+            "sampler.steps": 12,
+            "prompt.text": (
+                "(masterpiece) HDR victorian portrait painting of (girl), "
+                "blonde hair, mountain nature, blue sky"
+            ),
+            "negative_prompt.text": "bad hands, text, watermark",
+            "model_loader.ckpt_name": "model.ckpt",
+            "empty_latent_image.width": 768,
+            "empty_latent_image.height": 768,
+            "latent_upscale.width": 1216,
+            "latent_upscale.height": 1216,
+            "latent_upscale.crop": "disabled",
+            "latent_upscale.upscale_method": "nearest-exact",
+            "upscale_sampler.seed": 45678,
+            "upscale_sampler.steps": 15,
+            "upscale_sampler.cfg": 8.0,
+            "upscale_sampler.sampler_name": "dpmpp_2m",
+            "upscale_sampler.scheduler": "simple",
+            "upscale_sampler.denoise": 0.5,
+        }
+        images = self.comfy.run_image_pipeline("stable_diffusion_hires_fix", params)
+
+        image = Image.open(images[0]["imagedata"])
+        image.save("pipeline_stable_diffusion_hires_fix.png")
