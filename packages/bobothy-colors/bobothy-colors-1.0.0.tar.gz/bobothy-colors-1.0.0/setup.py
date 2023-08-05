79d8738e615c958b6206b1a64c8af66acaf1e478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:50 +0000

    Add package bobothy-colors/1.0.0/bobothy-colors-1.0.0.tar.gz

---
+++ packages/bobothy-colors/bobothy-colors-1.0.0.tar.gz/bobothy-colors-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="bobothy-colors",
+    version="1.0.0",
+    description="library of rgb colors",
+    py_modules=["colors"],
+    python_requires=">=2.0, >=3.0",
+    author="Bob-the-Bobothy",
+    url="https://github.com/Bob-the-Bobothy/colors",
+    install_requires=["random"],
+)
