b9e9b52323ddd2456a054d15d03cb938f14621a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:59 +0000

    Add package clevercsv/0.7.7/clevercsv-0.7.7-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/clevercsv/clevercsv-0.7.7-cp311-cp311-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/clevercsv/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from csv import QUOTE_ALL
+from csv import QUOTE_MINIMAL
+from csv import QUOTE_NONE
+from csv import QUOTE_NONNUMERIC
+
+from .__version__ import __version__
+from .cparser_util import field_size_limit
+from .detect import Detector
+from .detect import Detector as Sniffer
+from .dialect import excel
+from .dialect import excel_tab
+from .dialect import unix_dialect
+from .dict_read_write import DictReader
+from .dict_read_write import DictWriter
+from .exceptions import Error
+from .read import reader
+from .wrappers import detect_dialect
+from .wrappers import read_dataframe
+from .wrappers import read_dicts
+from .wrappers import read_table
+from .wrappers import stream_dicts
+from .wrappers import stream_table
+from .wrappers import write_table
+from .write import writer
