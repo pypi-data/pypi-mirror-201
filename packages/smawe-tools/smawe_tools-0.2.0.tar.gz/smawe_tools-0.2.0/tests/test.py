fcbc97024d7fb7d508fc5700313f24e587dca388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:05 +0000

    Add package smawe-tools/0.2.0/smawe_tools-0.2.0.tar.gz

---
+++ packages/smawe-tools/smawe_tools-0.2.0.tar.gz/smawe_tools-0.2.0/tests/test.py
@@ -0,0 +1,17 @@
+"""
+字体颜色设置
+    url: https://www.jianshu.com/p/a6791ba66e3d
+"""
+# print("\033[1;34ml love you\033[0m")
+# try:
+#     from retrying import retry
+# except ModuleNotFoundError as e:
+#     logging.warning(e)
+#     logging.warning("\033[1;34mInstalling dependent libraries: retrying\033[0m")
+#     completed_process = subprocess.run("{} -m pip install retrying".format(sys.executable))
+#     if completed_process.returncode:
+#         logging.info("Using a temporary solution.\n\033[1;34mPlease manually install the retrying library if "
+#                      "necessary\033[0m")
+#         from smawe_tools import retry
+#     else:
+#         from retrying import retry
