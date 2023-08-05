f6c85ccb6c01d5c786ca29268c3486ed12cf0330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:04 +0000

    Add package oidn/0.3a0/oidn-0.3a0-py3-none-win_amd64.whl

---
+++ packages/oidn/oidn-0.3a0-py3-none-win_amd64.whl/oidn/__init__.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import ctypes
+import oidn.capi as capi
+from oidn.capi import *
+
+oidn_version = '1.4.3'
+oidn_py_version = '0.3alpha'
+
+def __load_lib():
+    cur_path = os.path.dirname(__file__)
+    if sys.platform == 'linux':
+        return ctypes.CDLL(os.path.join(cur_path, f"lib.linux.x64/libOpenImageDenoise.so.{oidn_version}"))
+    elif sys.platform == 'darwin':
+        return ctypes.CDLL(os.path.join(cur_path, f"lib.macos.aarch64/libOpenImageDenoise.{oidn_version}.dylib"))
+    elif sys.platform == 'win32':
+        return ctypes.CDLL(os.path.join(cur_path, f"lib.win.x64/OpenImageDenoise.dll"))
+    else:
+        raise RuntimeError("Unsupported platform")
+__lib_oidn = __load_lib()
+
+capi.__init_by_lib(__lib_oidn)
+
+    
+
+    
+
+    
+
+    
