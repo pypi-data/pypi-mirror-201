40e77de8ef1f946c5ce57c8ed2d95da44af8b1a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:50 +0000

    Add package goby-sdk/1.0.1/goby_sdk-1.0.1.tar.gz

---
+++ packages/goby-sdk/goby_sdk-1.0.1.tar.gz/goby_sdk-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='goby_sdk',
+    version='1.0.1',
+    description='A Python SDK for Goby API',
+    author='siberia',
+    author_email='siberiah0h@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+    ],
+)
