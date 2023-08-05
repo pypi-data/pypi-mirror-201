23f9962cc6358c9446e445b8bfc2eb2ec0c60ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:28 +0000

    Add package MtxDrawer/0.0.11/MtxDrawer-0.0.11.tar.gz

---
+++ packages/MtxDrawer/MtxDrawer-0.0.11.tar.gz/MtxDrawer-0.0.11/MtxDrawer/__init__.py
@@ -0,0 +1,13 @@
+from QuickProject import QproDefaultConsole as console
+from QuickProject import QproInfoString as info_string
+from QuickProject import QproWarnString as warn_string
+from QuickProject import QproErrorString as erro_string
+from QuickProject import QproDefaultStatus as status
+from decimal import getcontext, Decimal, ROUND_HALF_EVEN
+
+name = 'MtxDrawer'
+getcontext().rounding = ROUND_HALF_EVEN
+
+
+def my_round(x: float) -> int:
+    return int(Decimal(x).quantize(Decimal('1')))
