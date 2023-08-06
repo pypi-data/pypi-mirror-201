a9bacf7ad4c97e3e37c643295910c4eb57832cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:00 +0000

    Add package jugalbandi/0.1/jugalbandi-0.1.tar.gz

---
+++ packages/jugalbandi/jugalbandi-0.1.tar.gz/jugalbandi-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='jugalbandi',
+    version='0.1',
+    description='Client side SDK to build applications using Jugalbandi',
+    author='Saurabh Kumar Karn',
+    author_email='saurabh@agami.in',
+    packages=['jugalbandi'],
+    install_requires=['requests'],
+)
+
