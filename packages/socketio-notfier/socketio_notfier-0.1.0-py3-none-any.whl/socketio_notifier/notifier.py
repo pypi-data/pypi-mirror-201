c40fd068ad846b44e4b27b161be3a9c496fae1fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:10 +0000

    Add package socketio-notfier/0.1.0/socketio_notfier-0.1.0-py3-none-any.whl

---
+++ packages/socketio-notfier/socketio_notfier-0.1.0-py3-none-any.whl/socketio_notifier/notifier.py
@@ -0,0 +1,25 @@
+import asyncio
+import socketio
+
+async def enviar_mensagem(message, server_address, port, event_name):
+    sio = socketio.AsyncClient()
+
+    @sio.event
+    async def connect():
+        print('Conexão estabelecida')
+
+    @sio.event
+    async def my_message():
+        print('Mensagem recebida')
+        await sio.emit(event_name, message)
+
+    @sio.event
+    async def disconnect():
+        print('Desconectado do servidor')
+
+    await sio.connect(f'http://{server_address}:{port}')
+    await my_message()
+    await sio.wait()
+
+def notify(message, server_address, port, event_name):
+    asyncio.run(enviar_mensagem(message, server_address, port, event_name))
