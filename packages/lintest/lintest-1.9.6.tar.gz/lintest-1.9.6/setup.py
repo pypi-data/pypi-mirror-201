6e0f95e70440b54631012c6861451e90ce0a0217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:43 +0000

    Add package lintest/1.9.6/lintest-1.9.6.tar.gz

---
+++ packages/lintest/lintest-1.9.6.tar.gz/lintest-1.9.6/setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+setup(
+    name='lintest',
+    version='1.9.6',
+    author='Wang Lin',
+    author_email='think_wl@163.com',
+    packages=['lintest'],
+    install_requires=[
+    	"psutil",
+    	"requests",
+        "curlify",
+    	"selenium",
+    	"selenium-wire",
+    	"setuptools",
+    	"urllib3",
+    	"PyMySQL",
+        "jsoncomparison",
+    	"chromedriver_autoinstaller"
+    ],
+)
+
+# python setup.py sdist --formats=gztar,zip
