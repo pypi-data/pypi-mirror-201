4daba15a69ac5a7e2f52649a9f543b981ccff62f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:32 +0000

    Add package flgo/0.0.8/flgo-0.0.8-py3-none-any.whl

---
+++ packages/flgo/flgo-0.0.8-py3-none-any.whl/flgo/benchmark/svhn_classification/__init__.py
@@ -0,0 +1,5 @@
+import flgo.benchmark.toolkits.visualization
+from flgo.benchmark.svhn_classification.model import cnn
+
+default_model = cnn
+visualize = flgo.benchmark.toolkits.visualization.visualize_by_class
