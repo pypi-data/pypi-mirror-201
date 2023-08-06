4d0aaf004ef1fab543d4f87184b3589f03ac9f6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:34 +0000

    Add package Geode-ImplicitModeling/1.16.1/Geode_ImplicitModeling-1.16.1-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.16.1-cp39-cp39-win_amd64.whl/geode_implicitmodeling/geode_implicitmodeling_insertion.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+from geode_conversion import model
+from geode_explicitmodeling import explicit_modeling2d
+
+from geode_implicitmodeling_py_insertion import *
+ImplicitModelingInsertion.initialize()
