a642f2fe54ae7aab77de62100fc35f10c7af26a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:19 +0000

    Add package gmap/1.5.2/gmap-1.5.2.tar.gz

---
+++ packages/gmap/gmap-1.5.2.tar.gz/gmap-1.5.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gmap",
+    version="1.5.2",
+    author="Tahsin",
+    author_email="your_email@example.com",
+    description="A package that prints 'Hello, World!'",
+    packages=find_packages(
+      include=['gmap', 'gmap.*']
+    ),
+    entry_points={
+        'console_scripts': [
+            'gmap = gmap.app:main'
+        ]
+    }
+)
