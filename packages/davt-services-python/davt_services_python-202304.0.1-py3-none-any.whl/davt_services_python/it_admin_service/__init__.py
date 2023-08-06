a141ad7df6d084aaa32b166e9f51390ee48790c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:47 +0000

    Add package davt-services-python/202304.0.1/davt_services_python-202304.0.1-py3-none-any.whl

---
+++ packages/davt-services-python/davt_services_python-202304.0.1-py3-none-any.whl/davt_services_python/it_admin_service/__init__.py
@@ -0,0 +1,20 @@
+"""Initialize the it_admin_service subpackage of davt_services_python package"""
+
+
+import sys  # don't remove required for error handling
+import os
+
+# Import from sibling directory ..\developer_service
+OS_NAME = os.name
+
+sys.path.append("..")
+if OS_NAME.lower() == "nt":
+    print("windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "\\..\\..\\..")))
+else:
+    print("non windows")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../..")))
+    sys.path.append(os.path.dirname(os.path.abspath(__file__ + "/../../..")))
