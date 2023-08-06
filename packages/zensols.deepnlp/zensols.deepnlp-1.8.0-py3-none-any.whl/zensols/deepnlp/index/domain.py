6d578f2b2b826b273ce03475db1babc8a2626b38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:49 +0000

    Add package zensols.deepnlp/1.8.0/zensols.deepnlp-1.8.0-py3-none-any.whl

---
+++ packages/zensols.deepnlp/zensols.deepnlp-1.8.0-py3-none-any.whl/zensols/deepnlp/index/domain.py
@@ -0,0 +1,122 @@
+"""Contains a base class for vectorizers for indexing document.
+
+"""
+__author__ = 'Paul Landes'
+
+from typing import Tuple, Iterable, Any
+from abc import ABC, ABCMeta, abstractmethod
+from dataclasses import dataclass, field
+import logging
+from itertools import chain
+from pathlib import Path
+from zensols.util import time
+from zensols.persist import (
+    persisted,
+    PersistedWork,
+    PersistableContainer,
+    Primeable
+)
+from zensols.nlp import FeatureDocument
+from zensols.deepnlp.vectorize import FeatureDocumentVectorizer
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class IndexedDocumentFactory(ABC):
+    """Creates training documents used to generate indexed features (i.e. latent
+    dirichlet allocation, latent semantic indexing etc).
+
+    :see: :class:`.DocumentIndexVectorizer`
+
+    """
+    @abstractmethod
+    def create_training_docs(self) -> Tuple[FeatureDocument]:
+        """Create the documents used to index in the model during training.
+
+        """
+        pass
+
+
+@dataclass
+class DocumentIndexVectorizer(FeatureDocumentVectorizer,
+                              PersistableContainer, Primeable,
+                              metaclass=ABCMeta):
+    """A vectorizer that generates vectorized features based on the index documents
+    of the training set.  For example, latent dirichlet allocation maybe be
+    used to generated a distrubiton of likelihood a document belongs to a
+    topic.
+
+    Subclasses of this abstract class are both vectorizers and models.  The
+    model created once, and then cached.  To clear the cache and force it to be
+    retrained, use :meth:`clear`.
+
+    The method :meth:`_create_model` must be implemented.
+
+    :see: :class:`.TopicModelDocumentIndexerVectorizer`
+
+    .. document private functions
+    .. automethod:: _create_model
+
+    """
+    doc_factory: IndexedDocumentFactory = field()
+    """The document factor used to create training documents for the model
+    vectorizer.
+
+    """
+    index_path: Path = field()
+    """The path to the pickeled cache file of the trained model.
+
+    """
+    def __post_init__(self):
+        PersistableContainer.__init__(self)
+        self.index_path.parent.mkdir(parents=True, exist_ok=True)
+        self._model_pw = PersistedWork(self.index_path, self)
+
+    @staticmethod
+    def feat_to_tokens(docs: Tuple[FeatureDocument]) -> Tuple[str]:
+        """Create a tuple of string tokens from a set of documents suitable for
+        document indexing.  The strings are the lemmas of the tokens.
+
+        **Important**: this method must remain static since the LSI instance of
+        this class uses it as a factory function in the a vectorizer.
+
+        """
+        toks = map(lambda d: d.lemma.lower(),
+                   filter(lambda d: not d.is_stop and not d.is_punctuation,
+                          chain.from_iterable(
+                              map(lambda d: d.tokens, docs))))
+        return tuple(toks)
+
+    @abstractmethod
+    def _create_model(self, docs: Iterable[FeatureDocument]) -> Any:
+        """Create the model for this indexer.  The model is implementation specific.
+        The model must be pickelabel and is cached in as :obj:`model`.
+
+        """
+        pass
+
+    @property
+    @persisted('_model_pw')
+    def model(self):
+        """Return the trained model for this vectorizer.  See the class docs on how it
+        is cached and cleared.
+
+        """
+        with time('created training documents'):
+            docs = self.doc_factory.create_training_docs()
+        with time('trained model'):
+            if logger.isEnabledFor(logging.INFO):
+                logger.info(f'creating model at {self.index_path}')
+            return self._create_model(docs)
+
+    def __getstate__(self):
+        return self.__dict__
+
+    def prime(self):
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'priming {self}')
+        self.model
+
+    def clear(self):
+        self._model.clear()
---
+++ packages/zensols.deepnlp/zensols.deepnlp-1.8.0-py3-none-any.whl/zensols/deepnlp/index/lsi.py
@@ -0,0 +1,90 @@
+from typing import Tuple, Iterable, Any
+from dataclasses import dataclass, field
+import logging
+import numpy as np
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.preprocessing import Normalizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from zensols.util import time
+from zensols.nlp import FeatureDocument, TokenContainer
+from zensols.deeplearn.vectorize import FeatureContext, TensorFeatureContext
+from zensols.deepnlp.vectorize import TextFeatureType
+from . import DocumentIndexVectorizer
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class LatentSemanticDocumentIndexerVectorizer(DocumentIndexVectorizer):
+    """Train a latent semantic indexing (LSI, aka LSA) model.
+
+    Citation:
+
+    Deerwester, S., Dumais, S.T., Furnas, G.W., Landauer, T.K., and Harshman,
+    R. 1990.  Indexing by Latent Semantic Analysis. Journal of the American
+    Society for Information Science; New York, N.Y. 41, 6, 391–407.
+
+    :shape: ``(1,)``
+
+    :see: :class:`sklearn.decomposition.TruncatedSVD`
+
+    """
+    DESCRIPTION = 'latent semantic indexing'
+    FEATURE_TYPE = TextFeatureType.DOCUMENT
+
+    components: int = field(default=100)
+    """The number of components for the output."""
+
+    iterations: int = field(default=10)
+    """Number of iterations for randomized SVD solver."""
+
+    def _get_shape(self) -> Tuple[int, int]:
+        return 1,
+
+    def _create_model(self, docs: Iterable[FeatureDocument]) -> Any:
+        """Train using a singular value decomposition, then truncate to get the most
+        salient terms in a document/term matrics.
+
+        """
+        vectorizer = TfidfVectorizer(
+            lowercase=False,
+            tokenizer=self.feat_to_tokens
+        )
+        with time('TF/IDF vectorized {X_train_tfidf.shape[0]} documents'):
+            X_train_tfidf = vectorizer.fit_transform(docs)
+        if logger.isEnabledFor(logging.DEBUG):
+            logger.debug(f'tfidf shape: {X_train_tfidf.shape}')
+        svd = TruncatedSVD(self.components, n_iter=self.iterations)
+        lsa: Pipeline = make_pipeline(svd, Normalizer(copy=False))
+        with time('SVD complete'):
+            X_train_lsa = lsa.fit_transform(X_train_tfidf)
+        if logger.isEnabledFor(logging.INFO):
+            logger.info(f'created model with {self.components} components, ' +
+                        f'over {self.iterations} iterations with ' +
+                        f'TF/IDF matrix shape: {X_train_tfidf.shape}, ' +
+                        f'SVD matrix shape: {X_train_lsa.shape}')
+        return {'vectorizer': vectorizer,
+                'lsa': lsa}
+
+    def _transform_doc(self, doc: FeatureDocument, vectorizer: TfidfVectorizer,
+                       lsa: Pipeline) -> np.ndarray:
+        X_test_tfidf = vectorizer.transform([doc])
+        X_test_lsa = lsa.transform(X_test_tfidf)
+        return X_test_lsa
+
+    def similarity(self, a: FeatureDocument, b: FeatureDocument) -> float:
+        """Return the semantic similarity between two documents.
+
+        """
+        model = self.model
+        vectorizer = model['vectorizer']
+        lsa = model['lsa']
+        emb_a = self._transform_doc(a, vectorizer, lsa)
+        emb_b = self._transform_doc(b, vectorizer, lsa)
+        return np.dot(emb_a, emb_b.T)[0][0]
+
+    def _encode(self, containers: Tuple[TokenContainer]) -> FeatureContext:
+        measure = self.similarity(*containers)
+        arr = self.torch_config.singleton([measure])
+        return TensorFeatureContext(self.feature_id, arr)
