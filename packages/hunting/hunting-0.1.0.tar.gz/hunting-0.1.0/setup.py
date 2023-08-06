9ce6f47a3aff2f2dc8186feb9c9c8b3b332b7dfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:55 +0000

    Add package hunting/0.1.0/hunting-0.1.0.tar.gz

---
+++ packages/hunting/hunting-0.1.0.tar.gz/hunting-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['temp']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'hunting',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': '',
+    'author': 'Factly Labs',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.11,<4.0',
+}
+
+
+setup(**setup_kwargs)
