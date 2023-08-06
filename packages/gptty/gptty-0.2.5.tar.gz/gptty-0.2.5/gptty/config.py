d7b45e332a865e587be129ecebc64cc5841da334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:44 +0000

    Add package gptty/0.2.5/gptty-0.2.5.tar.gz

---
+++ packages/gptty/gptty-0.2.5.tar.gz/gptty-0.2.5/gptty/config.py
@@ -0,0 +1,49 @@
+__name__ = "gptty.config"
+__author__ = "Sig Janoska-Bedi"
+__credits__ = ["Sig Janoska-Bedi"]
+__version__ = "0.2.5"
+__license__ = "MIT"
+__maintainer__ = "Sig Janoska-Bedi"
+__email__ = "signe@atreeus.com"
+
+import configparser
+
+# parse config data
+def get_config_data(config_file='gptty.ini'):
+    # create a configuration object
+    config = configparser.ConfigParser()
+
+    config['DEFAULT'] = {
+        'api_key': "",
+        'your_name': 'question',
+        'gpt_name': 'response',
+        'output_file': 'output.txt',
+        'model': 'text-davinci-003',
+        'temperature': 0.0,
+        'max_tokens': 250,
+        'max_context_length': 150,
+        'context_keywords_only': True,
+        'preserve_new_lines': False,
+        'verify_internet_endpoint': 'google.com',
+    }
+
+    # read the configuration file (if it exists)
+    config.read(config_file)
+
+    parsed_data = {
+        'api_key': config.get('main', 'api_key', fallback="",),
+        'your_name': config.get('main', 'your_name', fallback='question'),
+        'gpt_name': config.get('main', 'gpt_name', fallback='response'),
+        'output_file': config.get('main', 'output_file', fallback='output.txt'),
+        'model': config.get('main', 'model', fallback='text-davinci-003'),
+        'temperature': config.getfloat('main', 'temperature', fallback=0.0),
+        'max_tokens': config.getint('main', 'max_tokens', fallback=25),
+        'max_context_length': config.getint('main', 'max_context_length', fallback=150),
+        'context_keywords_only': config.getboolean('main', 'context_keywords_only', fallback=True),
+        'preserve_new_lines': config.getboolean('main', 'preserve_new_lines', fallback=False),
+        'verify_internet_endpoint': config.get('main', 'verify_internet_endpoint', fallback='google.com'),
+	}
+
+   
+
+    return parsed_data
