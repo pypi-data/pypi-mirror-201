08c41f9601a6886f91f64801fd5769f038b94931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:00 +0000

    Add package pasta-eln/2.3.11/pasta_eln-2.3.11-py3-none-any.whl

---
+++ packages/pasta-eln/pasta_eln-2.3.11-py3-none-any.whl/pasta_eln/tempStrings.py
@@ -0,0 +1,42 @@
+""" ##Automatically created file: do not update manually """
+todoString = """
+
+<h3>Very important show stopper</h3>
+<ul>
+</ul>
+
+<h3>To be verified/identified</h3>
+<ul>
+<li> create project within project (unable to recreate)
+</ul>
+
+<h3>Improvement to convenience</h3>
+<ul>
+<li> convenience: allow scroll in sidebar
+<li> convenience: selection does not scroll; one cannot select a row
+<li> drag&drop: external files
+<li> invert filter: not 'Sur' in name => '^((?!Sur).)*$' in name
+<li> rerunExtractors: as batch
+</ul>
+
+<h3>Bigger things implemented soon</h3>
+<ul>
+<li> change procedure: on disk / database -> conflicts
+<li> create list of unaccessible files: linked with accessible files
+<li> finish ontology dialog
+<li> question? what is the difference to TextButton?
+<li> tableHeaderChange: requires view to change to views, not ontology
+</ul>
+
+<h3>Things worthwile remembering/uncritical</h3>
+<ul>
+<li> Aug 2023: remove
+<li> addToConfig
+<li> addToConfig
+<li> addToConfig
+<li> if successful in Aug2023: remove
+<li> if successful in Aug2023: remove "echo....read"
+<li> moreChecksDB: if parent has corresponding show
+<li> serverConfiguration: this should become a GUI and CLI and separate into three-files: functions, CLI, GUI
+</ul>
+"""
