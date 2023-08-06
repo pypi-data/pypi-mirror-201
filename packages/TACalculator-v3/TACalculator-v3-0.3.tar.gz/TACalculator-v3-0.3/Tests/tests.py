dc544b50a5ee342ba4db2f2fbb09f34907a36a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:40 +0000

    Add package TACalculator-v3/0.3/TACalculator-v3-0.3.tar.gz

---
+++ packages/TACalculator-v3/TACalculator-v3-0.3.tar.gz/TACalculator-v3-0.3/Tests/tests.py
@@ -0,0 +1,47 @@
+import unittest
+
+from file.calculator import Calculator
+
+
+class TestCalculator(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.memory, 0)
+        self.assertEqual(self.calculator.add(5), 5)
+        self.assertEqual(self.calculator.add(5), 10)
+        self.assertEqual(self.calculator.add(6), 16)
+        self.assertEqual(self.calculator.add('asbs'), "Invalid argument type. Only int or float values are allowed.")
+        self.assertEqual(self.calculator.memory, 16)
+        self.assertEqual(self.calculator.reset(), 0)
+
+    def test_substract(self):
+        self.assertEqual(self.calculator.memory, 0)
+        self.assertEqual(self.calculator.substract(5), -5)
+        self.assertEqual(self.calculator.substract(3), -8)
+        self.assertEqual(self.calculator.memory, -8)
+        self.assertEqual(self.calculator.reset(), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.memory, 0)
+        self.assertEqual(self.calculator.add(2), 2)
+        self.assertEqual(self.calculator.multiply(4), 8)
+        self.assertEqual(self.calculator.memory, 8)
+        self.assertEqual(self.calculator.reset(), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.memory, 0)
+        self.assertEqual(self.calculator.add(4), 4)
+        self.assertEqual(self.calculator.divide(2), 2)
+        self.assertEqual(self.calculator.memory, 2)
+        self.assertEqual(self.calculator.reset(), 0)
+
+    def test_root(self):
+        self.assertEqual(self.calculator.memory, 0)
+        self.assertEqual(self.calculator.root(2), 0)
+        self.assertEqual(self.calculator.add(4), 4)
+        self.assertEqual(self.calculator.root(2), 2)
+        self.assertEqual(self.calculator.memory, 2)
+        self.assertEqual(self.calculator.reset(), 0)
