aa3ef155ef85a40f5a64518d2aa1f5879bb29ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:36 +0000

    Add package zliPkg/11.31/zliPkg-11.31.tar.gz

---
+++ packages/zliPkg/zliPkg-11.31.tar.gz/zliPkg-11.31/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+from pathlib import Path
+
+def get_install_requires():
+    """Returns requirements.txt parsed to a list"""
+    fname = Path(__file__).parent / 'requirements.txt'
+    targets = []
+    if fname.exists():
+        with open(fname, 'r') as f:
+            targets = f.read().splitlines()
+    return targets
+
+setup(
+    name='zliPkg',
+    version='11.31',
+    license='MIT',
+    author='zli',
+    packages=['zliPkg'],
+    package_dir={'zliPkg': 'zliPkg'},
+    install_requires=get_install_requires()
+)
