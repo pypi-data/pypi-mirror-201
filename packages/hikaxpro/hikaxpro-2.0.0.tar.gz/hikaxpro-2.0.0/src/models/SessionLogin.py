25bfcfac2704851dbc74fc82a1dad88b2aef0b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:43 +0000

    Add package hikaxpro/2.0.0/hikaxpro-2.0.0.tar.gz

---
+++ packages/hikaxpro/hikaxpro-2.0.0.tar.gz/hikaxpro-2.0.0/src/models/SessionLogin.py
@@ -0,0 +1,6 @@
+class SessionLogin:
+    def __init__(self, session_id, username, password):
+        self.session_id = session_id
+        self.username = username
+        self.password = password
+        self.session_id_version = "2.1"
