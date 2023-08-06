d2d9f8e7f869a1de095d43511d3a7bae3ccc4ea2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:58 +0000

    Add package neosekai-api/0.0.2/neosekai_api-0.0.2-py3-none-any.whl

---
+++ packages/neosekai-api/neosekai_api-0.0.2-py3-none-any.whl/neosekai_api/__init__.py
@@ -0,0 +1,7 @@
+from .chapter import NovelChapter
+from .novel import Novel
+
+__author__ = "John Erinjery"
+__version__ = "0.0.2"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2023 John Erinjery"
---
+++ packages/neosekai-api/neosekai_api-0.0.2-py3-none-any.whl/neosekai_api/chapter.py
@@ -0,0 +1,47 @@
+from neosekai_api import Novel
+import requests
+from bs4 import BeautifulSoup
+
+
+class NovelChapter:
+
+    def __init__(self, _url) -> None:
+        self.url = self.__urlformatter(_url)
+        self.__response_object = requests.get(self.url, timeout=10)
+        self.details = self.chapter_details()
+        self.volume = self.details['volume']
+        self.name = self.details['chapter_name']
+        self.release_date = self.details['release_date']
+
+    def __urlformatter(self, _url):
+        __url = ''
+        if 'https://' not in _url:
+            _url == 'https://' + _url
+        else:
+            return _url
+
+    def chapter_details(self):
+        novel_url = self.url[:self.url.index('/', 43)]
+        novel = Novel(novel_url)
+        index_page = novel.get_index_page()
+        for i in index_page:
+            if index_page[i]['url'] == self.url:
+                return index_page[i]
+
+    def get_chapter_content(self):
+        soup = BeautifulSoup(self.__response_object.text, 'lxml')
+        div = soup.find('div', attrs={'class': 'text-left'})
+        paras = div.find_all('p')
+        content = {}
+        n = 1
+        for i in paras:
+            if i.span != None:
+                content[str(n)] = {'type': 'text',
+                                   'content': i.span.text.strip()}
+                n += 1
+            elif i.img != None:
+                content[str(n)] = {'type': 'img', 'content': i.img['src']}
+                n += 1
+            else:
+                continue
+        return content
