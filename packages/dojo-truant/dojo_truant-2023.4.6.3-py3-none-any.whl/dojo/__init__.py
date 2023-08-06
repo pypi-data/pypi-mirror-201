bb34159c6782b856c41b18b4c8cc9e746a0e7161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:02 +0000

    Add package dojo-truant/2023.4.6.3/dojo_truant-2023.4.6.3-py3-none-any.whl

---
+++ packages/dojo-truant/dojo_truant-2023.4.6.3-py3-none-any.whl/dojo/__init__.py
@@ -0,0 +1,12 @@
+
+from .api import DAPI
+
+from .product_type import Product_Type
+from .product import Product
+from .dojo_group import Dojo_Group
+
+from .write_chain import write_chain
+
+
+ID_LOOKUPS = {"prod_type": Product_Type,
+              "group": Dojo_Group}
