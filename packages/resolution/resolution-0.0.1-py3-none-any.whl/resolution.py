93a95fe66fd85761016e0d4d28b8cc7862d9a248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:53 +0000

    Add package resolution/0.0.1/resolution-0.0.1-py3-none-any.whl

---
+++ packages/resolution/resolution-0.0.1-py3-none-any.whl/resolution.py
@@ -0,0 +1,16 @@
+"""
+Resolution
+~~~~~~~~~~
+"""
+
+def main():
+    """Main Function
+
+    The Main Function, also the entry point for the console script, or
+    CLI (Command Line Interface)
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
