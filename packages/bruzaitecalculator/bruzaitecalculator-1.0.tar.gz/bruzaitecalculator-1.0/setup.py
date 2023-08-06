a55cd377e80b8c71800514e42f0a6661a51cd103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:13 +0000

    Add package bruzaitecalculator/1.0/bruzaitecalculator-1.0.tar.gz

---
+++ packages/bruzaitecalculator/bruzaitecalculator-1.0.tar.gz/bruzaitecalculator-1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bruzaitecalculator',
+    version='1.0',
+    description='A simple calculator package',
+    author='Ausra Bruzaite',
+    author_email='bruzaite.ausra@gmail.com',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
+    ]
+)
+
