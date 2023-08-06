09cea76dd5d1f1872a35faf526684c61dd149810
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:09 +0000

    Add package aqueduct-sdk/0.2.9/aqueduct_sdk-0.2.9-py3-none-any.whl

---
+++ packages/aqueduct-sdk/aqueduct_sdk-0.2.9-py3-none-any.whl/aqueduct/models/artifact.py
@@ -0,0 +1,17 @@
+import uuid
+
+from aqueduct.constants.enums import ArtifactType
+from pydantic import BaseModel
+
+
+class ArtifactMetadata(BaseModel):
+    id: uuid.UUID
+    name: str
+    type: ArtifactType
+    # Whether this artifact was given a name explicitly by the user.
+    # If true, this artifact name is expected to be unique in the DAG.
+    explicitly_named: bool = False
+    from_local_data: bool = False
+
+    class Config:
+        fields = {"explicitly_named": {"exclude": ...}, "from_local_data": {"exclude": ...}}
