3227b4837562ce58f84f3103126629a6dd1f97e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:41 +0000

    Add package create-tox-app/1.0.0/create_tox_app-1.0.0-py3-none-any.whl

---
+++ packages/create-tox-app/create_tox_app-1.0.0-py3-none-any.whl/create_tox_app/__main__.py
@@ -0,0 +1,44 @@
+import sys
+import argparse
+import os
+from create_tox_app.generator import Generator
+from create_tox_app.exit_codes import SUCCESS, FAIL
+
+
+def create_argparser():
+    parser = argparse.ArgumentParser(
+        description="A wrapper that creates a tox env with"
+        " the minimal setup for a cli python app.",
+        prog="create-tox-app",
+        add_help=True,
+    )
+    parser.add_argument("appdir", nargs=1)
+
+    return parser
+
+
+def get_arguments():
+    argparser = create_argparser()
+    return argparser.parse_args()
+
+
+def main():
+    try:
+        args = get_arguments()
+        application_directory = args.appdir[0]
+
+        if not os.path.isdir(application_directory):
+            generator = Generator(application_directory)
+            generator.create_application()
+        else:
+            print(f"Directory {application_directory} exists!")
+            return FAIL
+
+    except Exception as exception:
+        print(exception)
+        return FAIL
+    return SUCCESS
+
+
+if __name__ == "__main__":
+    sys.exit(main())
