081eb525f4d142d065ae64b51e7e628ee00b0cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:40 +0000

    Add package typing-filter/0.1.1/typing_filter-0.1.1-py3-none-any.whl

---
+++ packages/typing-filter/typing_filter-0.1.1-py3-none-any.whl/typing_filter/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Top-level init file to make package available for import.
+
+**Author: Jonathan Delgado**
+
+Handles importing and setting project-wide fields.
+
+"""
+#------------- Fields -------------#
+__author__ = 'Jonathan Delgado'
+__version__ = '0.0.1.1'
+
+#------------- Imports -------------#
+from .typing_filter import *
