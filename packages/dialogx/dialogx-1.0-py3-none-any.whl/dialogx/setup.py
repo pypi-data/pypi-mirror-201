d0aff1723ca76fcbc571ad9d07e4cefcbafadcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:23 +0000

    Add package dialogx/1.0/dialogx-1.0-py3-none-any.whl

---
+++ packages/dialogx/dialogx-1.0-py3-none-any.whl/dialogx/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name="dialogx",
+	version="1.0",
+	author="MySoft",
+	author_email="rixthetyrunt@gmail.com",
+	description="A lightweight Python library for creating customizable and interactive dialogs and windows with Pygame.",
+	packages=["dialogx"],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: Other/Proprietary License",
+		"Operating System :: Microsoft :: Windows",
+	],
+	install_requires=[
+		"pygame"
+	]
+)
