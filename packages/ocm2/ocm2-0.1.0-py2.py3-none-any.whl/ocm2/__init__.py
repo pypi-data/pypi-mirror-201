801bbbdfcfd1dc89c037d58dca49c21b0a16d4fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:47 +0000

    Add package ocm2/0.1.0/ocm2-0.1.0-py2.py3-none-any.whl

---
+++ packages/ocm2/ocm2-0.1.0-py2.py3-none-any.whl/ocm2/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for ocm2."""
+
+__author__ = """Akhilesh Kumar"""
+__email__ = 'akhiraj9661@gmail.com'
+__version__ = '0.1.0'
+
+from .ocm2 import ExportSubdatasets, metaInfo, GetExtent, do_georef
