4ec1cde476696b057793b5a2ccf5480b987a5092
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:58 +0000

    Add package pyexception-notifier/0.0.2/pyexception-notifier-0.0.2.tar.gz

---
+++ packages/pyexception-notifier/pyexception-notifier-0.0.2.tar.gz/pyexception-notifier-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open('Readme.md') as f:
+    long_description = f.read()
+
+
+setup(
+    name='pyexception-notifier',
+    packages=find_packages(),
+    version='0.0.2',
+    description='Exception notifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='r.islyamgali',
+    license='MIT',
+    install_requires=[
+        "pydantic",
+        "uvicorn",
+        "requests(>=2.27,<3.0)",
+        "python-dotenv",
+        "gunicorn[standard]",
+    ],
+    python_requires=">=3.8"
+)
