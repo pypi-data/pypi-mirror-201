9e613ad50d48d66e588567c2f1bfc566818be85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:45 +0000

    Add package wiu/0.0.1/wiu-0.0.1.tar.gz

---
+++ packages/wiu/wiu-0.0.1.tar.gz/wiu-0.0.1/wiu/calculator.py
@@ -0,0 +1,30 @@
+class Calculator:
+    
+    # basic operations
+    def add      (num1: int|float, num2: int|float) -> int|float : return (num1 + num2)
+    def subtract (num1: int|float, num2: int|float) -> int|float : return (num1 - num2)
+    def multiply (num1: int|float, num2: int|float) -> int|float : return (num1 * num2)
+    def divide   (num1: int|float, num2: int|float) -> int|float : return (num1 / num2)
+
+    # product of a list of numbers
+    def product(array: list) -> int|float:
+        result = array[0]
+
+        for index in range(len(array)):
+            if (index + 1) == len(array):
+                break
+
+            else:
+                result *= array[index + 1]
+
+        return result
+
+    # factorial
+    def factorial(number: int) -> int:
+        return Calculator.product(range(1, number+1))
+
+
+
+if __name__ == '__main__':
+    prod = Calculator.factorial(4)
+    print(prod)
