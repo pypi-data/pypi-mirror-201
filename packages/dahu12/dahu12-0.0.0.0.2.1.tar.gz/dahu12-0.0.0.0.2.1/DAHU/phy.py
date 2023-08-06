e4e3b52994568701a58575e718b373096dee39cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:35 +0000

    Add package dahu12/0.0.0.0.2.1/dahu12-0.0.0.0.2.1.tar.gz

---
+++ packages/dahu12/dahu12-0.0.0.0.2.1.tar.gz/dahu12-0.0.0.0.2.1/DAHU/phy.py
@@ -0,0 +1,15 @@
+## Module : Partie Physique ##
+
+import main
+import erreurs as er
+
+## Plusieurs branches :
+# -elec
+# -thermo
+# ...##
+
+def extractp(chemin):
+    df = main.importdata(chemin)
+
+def diagenthal() :
+    pass
