3a076ae89b4b2e7f046c3910a649d52490ed6ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:00:28 +0000

    Add package Nondimensionalization-UssopSniperKing/0.0.1/Nondimensionalization_UssopSniperKing-0.0.1.tar.gz

---
+++ packages/Nondimensionalization-UssopSniperKing/Nondimensionalization_UssopSniperKing-0.0.1.tar.gz/Nondimensionalization_UssopSniperKing-0.0.1/src/Nondimensionalization_UssopSniperKing/numbers.py
@@ -0,0 +1,51 @@
+
+def Reynolds(velocity, length, viscosity):
+	"""Compute the Reynolds number
+
+	Parameters
+	----------
+	velocity : float
+ 		velocity of the fluid [m s^-1]
+	length : float
+  		characteristic length of the system [m]
+  	viscosity : float
+  		cinematic viscosity of the fluid [m^2 s^-1]
+   	
+    """
+	return (velocity * length)/viscosity
+	
+
+def Prandtl():
+	"""Compute the Prandtl number
+
+	Parameters
+	----------
+	"""
+	pass
+
+
+def Nusselt():
+	"""Compute the Nusselt number
+
+	Parameters
+	----------
+	"""
+	pass
+
+
+def Grashof():
+	"""Compute the Grashof number
+
+	Parameters
+	----------
+	"""
+	pass
+
+
+def Schmidt():
+	"""Compute the Schmidt number
+
+	Parameters
+	----------
+	"""
+	pass
