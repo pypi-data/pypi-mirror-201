fa037d1a393412452b5a6d7535a5f50a1a3a2a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:30 +0000

    Add package security-scan-invoker/1.0.4/security_scan_invoker-1.0.4-py3-none-any.whl

---
+++ packages/security-scan-invoker/security_scan_invoker-1.0.4-py3-none-any.whl/security_scan_invoker/arg_parser_wrapper.py
@@ -0,0 +1,27 @@
+# Copyright 2020-2023 Deere & Company.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from .iface_arg_parser import IArgParser
+
+
+class ArgParserWrapper(IArgParser):
+    def __init__(self):
+        self.arg_parser: argparse.ArgumentParser = argparse.ArgumentParser()
+
+    def add_argument(self, arg_name, **kwargs):
+        self.arg_parser.add_argument(arg_name, **kwargs)
+
+    def parse_args(self):
+        return self.arg_parser.parse_args()
