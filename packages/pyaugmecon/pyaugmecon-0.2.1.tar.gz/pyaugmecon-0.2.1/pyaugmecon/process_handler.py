68e584284ab3f5f0df767d743d5b87b8f784d146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:56:18 +0000

    Add package pyaugmecon/0.2.1/pyaugmecon-0.2.1.tar.gz

---
+++ packages/pyaugmecon/pyaugmecon-0.2.1.tar.gz/pyaugmecon-0.2.1/pyaugmecon/process_handler.py
@@ -0,0 +1,55 @@
+import logging
+import time
+from threading import Thread
+
+from pyaugmecon.flag import Flag
+from pyaugmecon.helper import Timer
+from pyaugmecon.model import Model
+from pyaugmecon.options import Options
+from pyaugmecon.queue_handler import QueueHandler
+from pyaugmecon.solver_process import SolverProcess
+
+
+class ProcessHandler(object):
+    def __init__(self, opts: Options, model: Model, queues: QueueHandler):
+        self.opts = opts
+        self.model = model
+        self.queues = queues
+        self.logger = logging.getLogger(opts.log_name)
+        self.flag = Flag(self.opts)
+
+        if self.opts.process_timeout:
+            self.timeout = Thread(target=self.check_timeout)
+
+        self.procs = [
+            SolverProcess(p_num, self.opts, self.model, self.queues, self.flag)
+            for p_num in range(self.queues.proc_count)
+        ]
+
+    def start(self):
+        self.runtime = Timer()
+        self.logger.info(f"Starting {self.queues.proc_count} worker process(es)")
+
+        for p in self.procs:
+            p.start()
+
+        if self.opts.process_timeout:
+            self.timeout.start()
+
+    def check_timeout(self):
+        while self.runtime.get() <= self.opts.process_timeout:
+            if not any(p.is_alive() for p in self.procs):
+                break
+            time.sleep(0.5)
+        else:
+            self.logger.info("Timed out, gracefully stopping all worker proces(es)")
+            self.queues.empty_job_qs()
+
+    def join(self):
+        self.logger.info(f"Joining {self.queues.proc_count} worker process(es)")
+
+        if self.opts.process_timeout:
+            self.timeout.join()
+
+        for p in self.procs:
+            p.join()
---
+++ packages/pyaugmecon/pyaugmecon-0.2.1.tar.gz/pyaugmecon-0.2.1/pyaugmecon/queue_handler.py
@@ -0,0 +1,73 @@
+import logging
+import queue
+from multiprocessing import Queue
+
+import numpy as np
+
+from pyaugmecon.options import Options
+
+
+class QueueHandler(object):
+    def __init__(self, work, opts: Options):
+        self.work = work
+        self.opts = opts
+        self.logger = logging.getLogger(opts.log_name)
+        self.result_q = Queue()
+
+    def get_longest_q(self):
+        q_length = [q.qsize() for q in self.job_qs]
+        if all(q == 0 for q in q_length):
+            return None
+        else:
+            return q_length.index(max(q_length))
+
+    def get_work(self, i):
+        try:
+            return self.job_qs[i].get_nowait()
+        except queue.Empty:
+            if self.opts.redivide_work and self.get_longest_q():
+                return self.get_work(self.get_longest_q())
+            else:
+                if self.opts.process_logging:
+                    self.logger.info(f"PID: {i} exited")
+
+                self.result_q.put("STOP")
+                return None
+
+    def put_result(self, result):
+        self.result_q.put(result)
+
+    def get_result(self):
+        results = []
+
+        for _ in range(self.proc_count):
+            while True:
+                result = self.result_q.get()
+                if result == "STOP":
+                    break
+                else:
+                    results.append(result)
+
+        return results
+
+    def empty_job_qs(self):
+        for i in range(self.proc_count):
+            while True:
+                try:
+                    self.job_qs[i].get_nowait()
+                except queue.Empty:
+                    break
+
+    def split_work(self):
+        blocks = [self.work[i : i + self.opts.gp] for i in range(0, len(self.work), self.opts.gp)]
+        blocks = np.array_split(np.array(blocks), self.opts.cpu_count)
+        blocks = [x for x in blocks if x.size > 0]  # Remove empty blocks
+        self.proc_count = len(blocks)
+
+        self.logger.info(f"Dividing grid over {self.proc_count} process(es)")
+        self.job_qs = [Queue() for _ in range(self.proc_count)]
+
+        for i, b in enumerate(blocks):
+            for item in b:
+                item = [tuple(x) for x in item.tolist()]
+                self.job_qs[i].put_nowait(item)
