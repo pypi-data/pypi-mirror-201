84c1f8ad37fb2a9fb6b41dbb725d1218d5982dfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:43 +0000

    Add package juice-core-uplink-api-client/0.2.1/juice_core_uplink_api_client-0.2.1-py3-none-any.whl

---
+++ packages/juice-core-uplink-api-client/juice_core_uplink_api_client-0.2.1-py3-none-any.whl/juice_core/__init__.py
@@ -0,0 +1,8 @@
+"""
+This module provides the core functionality of the Juice Core Uplink API Client
+in a more convenient way than the generated code.
+
+May functionalities are not yet implemented, but some are.
+"""
+
+from .SHTRestInterface import SHTRestInterface
