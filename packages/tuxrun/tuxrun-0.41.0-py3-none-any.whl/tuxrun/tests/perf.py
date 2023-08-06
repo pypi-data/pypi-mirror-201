574912da05153be2e0a1580ef62a68a119c364b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:17 +0000

    Add package tuxrun/0.41.0/tuxrun-0.41.0-py3-none-any.whl

---
+++ packages/tuxrun/tuxrun-0.41.0-py3-none-any.whl/tuxrun/tests/perf.py
@@ -0,0 +1,20 @@
+# vim: set ts=4
+#
+# Copyright 2022-present Linaro Limited
+#
+# SPDX-License-Identifier: MIT
+
+from tuxrun.tests import Test
+
+
+class Perf(Test):
+    devices = ["qemu-*", "fvp-aemva"]
+    name = "perf"
+    timeout = 30
+    need_test_definition = True
+
+    def render(self, **kwargs):
+        kwargs["name"] = self.name
+        kwargs["timeout"] = self.timeout
+
+        return self._render("perf.yaml.jinja2", **kwargs)
