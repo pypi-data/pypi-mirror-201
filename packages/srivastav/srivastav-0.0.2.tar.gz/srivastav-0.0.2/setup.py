84c388a9f3f90e773aa0e28d05aa0e6d2149ad7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:51 +0000

    Add package srivastav/0.0.2/srivastav-0.0.2.tar.gz

---
+++ packages/srivastav/srivastav-0.0.2.tar.gz/srivastav-0.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='srivastav',
+    version='0.0.2',    
+    description='Downalerts django Middleware ',
+    url='https://github.com/downalerts/django-logger',
+    author='Aman Srivastav',
+    author_email='amansrivastav.bytequests@gmail.com',
+    license='BSD 2-clause',
+    packages=['srivastav'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.5',
+    ],
+)
