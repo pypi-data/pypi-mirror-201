e1b812e3cde8b5b5f421f97102cb02328c487dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:34 +0000

    Add package fairscape-models/0.1.2/fairscape_models-0.1.2-py3-none-any.whl

---
+++ packages/fairscape-models/fairscape_models-0.1.2-py3-none-any.whl/fairscape_models/__init__.py
@@ -0,0 +1,11 @@
+from fairscape_models.dataset import Dataset
+from fairscape_models.computation import Computation
+from fairscape_models.software import Software
+from fairscape_models.ro_crate import ROCrate
+
+__all__ = [
+    'dataset',
+    'computation',
+    'software',
+    'ROCrate'
+]
