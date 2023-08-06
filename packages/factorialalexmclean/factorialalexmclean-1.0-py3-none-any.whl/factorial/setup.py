e7f9010c3f01e0506405a02dbba52dbb82f33335
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:22 +0000

    Add package factorialalexmclean/1.0/factorialalexmclean-1.0-py3-none-any.whl

---
+++ packages/factorialalexmclean/factorialalexmclean-1.0-py3-none-any.whl/factorial/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='factorialalexmclean',
+    version='1.0',
+    description='A Singly Linked List Data structure',
+    author='Alex McLean',
+    author_email='alexander.mclean@ucalgary.ca,',
+    packages=['factorial'],
+    install_requires=[],
+    )
