29be6ccebf26131fa2a461e854c88a50eebe990d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:52 +0000

    Add package luau/0.1.0/luau-0.1.0.tar.gz

---
+++ packages/luau/luau-0.1.0.tar.gz/luau-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="luau",
+    version="0.1.0",
+    author="nightcycle",
+    author_email="coyer@nightcycle.us",
+    description="a basic python package for writing luau scripts",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+    install_requires=["re", "typing"],
+)
