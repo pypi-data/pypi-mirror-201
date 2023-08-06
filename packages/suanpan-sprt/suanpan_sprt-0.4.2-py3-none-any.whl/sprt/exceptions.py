674f89ac8e4214177734d1c73bc8634b4e00f46b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:42 +0000

    Add package suanpan-sprt/0.4.2/suanpan_sprt-0.4.2-py3-none-any.whl

---
+++ packages/suanpan-sprt/suanpan_sprt-0.4.2-py3-none-any.whl/sprt/exceptions.py
@@ -0,0 +1,14 @@
+class ArgumentRequiredError(Exception):
+    ...
+
+
+class ArgumentError(Exception):
+    ...
+
+
+class RequestSchemaValidationError(Exception):
+    ...
+
+
+class ParamsArgError(Exception):
+    ...
