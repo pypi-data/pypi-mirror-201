7afdf6e35b777f8e69f90f04f390e9475c0617b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:06 +0000

    Add package sentential/0.10.0/sentential-0.10.0-py3-none-any.whl

---
+++ packages/sentential/sentential-0.10.0-py3-none-any.whl/sentential/templates/shapes.py
@@ -0,0 +1,31 @@
+from sentential.support.shaper import Shaper, List, Field
+
+"""
+Uncomment and modify the below classes if needed
+- Env contractualizes `sntl envs`, all made available in lambda environment.
+- Arg contractualizes `sntl args`, all made available during build.
+- Tags applies key/values to all created resources
+
+Examples:
+
+    class Env(Shaper):
+        db_host: str
+        db_port: int
+        db_user: str
+        db_pass: str
+
+    class Arg(Shaper):
+        build_arg: str
+
+    class Tags(Shaper):
+        env: str
+"""
+
+# class Envs(Shaper):
+#   ...
+
+# class Args(Shaper):
+#   ...
+
+# class Tags(Shaper):
+#   ...
