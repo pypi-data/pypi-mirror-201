60028351c50d01ba0770bb73ae5272da4550f710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:27 +0000

    Add package runtimepy/1.0.1/runtimepy-1.0.1.tar.gz

---
+++ packages/runtimepy/runtimepy-1.0.1.tar.gz/runtimepy-1.0.1/runtimepy/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.0
+# hash=a869e3ab405cc0055ec5bb7a73182b8b
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "1.0.1"
