9198c24433225119ced00bdc08bc8e96e2874f2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:51 +0000

    Add package portabletab/0.2.0/portabletab-0.2.0.tar.gz

---
+++ packages/portabletab/portabletab-0.2.0.tar.gz/portabletab-0.2.0/PortableTab/__init__.py
@@ -0,0 +1,11 @@
+from .base_table import BaseTable
+from .capnp_manager import CapnpManager
+from .capnp_table import CapnpTable
+
+__version__ = '0.2.0'
+
+__all__ = [
+    "BaseTable",
+    "CapnpManager",
+    "CapnpTable",
+]
