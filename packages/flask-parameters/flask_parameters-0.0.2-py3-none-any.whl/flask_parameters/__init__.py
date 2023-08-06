d2e14102b31db092f8d46eb9f496f5a27b83a908
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:26 +0000

    Add package flask-parameters/0.0.2/flask_parameters-0.0.2-py3-none-any.whl

---
+++ packages/flask-parameters/flask_parameters-0.0.2-py3-none-any.whl/flask_parameters/__init__.py
@@ -0,0 +1,3 @@
+from flask_parameters.flask_helpers import inject_query_params
+from flask_parameters.flask_helpers import inject_and_validate_query_params
+from flask_parameters.exceptions import register_error_handlers
