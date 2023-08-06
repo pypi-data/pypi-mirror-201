612e1f9331b185e4da4cbd2c41d7c31238d521e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:32 +0000

    Add package messaging-app/0.1.7/messaging_app-0.1.7.tar.gz

---
+++ packages/messaging-app/messaging_app-0.1.7.tar.gz/messaging_app-0.1.7/messaging_app/func/encrypt.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from messaging_app.lib.keys_system import the_keys, save_keys
+
+import os
+
+def encrypt_text(text_data,key):
+    temp_keys = the_keys()[key]
+    n = temp_keys["n"]                               #declare n
+    e = temp_keys["e"]                               #declare e
+
+
+    text_data = [text_data[i:i+477] for i in range(0, len(text_data), 477)]
+
+    total = ""
+    for i in text_data:
+        encrypted_mess = ""     #create 
+
+        #turn text data to bytes
+        i = bytes(i, "utf-8")
+
+        #turn bytes to int
+        i = int.from_bytes(i, byteorder='big')
+
+        encrypted_mess = str(encryption(i,e,n))
+        total += encrypted_mess+"\n"
+
+
+
+
+    return(total)
+
+#encyrption funciton. m^e % n
+def encryption(m,e,n):
+    x = pow(m,e,n)
+    return x
+
+ 
---
+++ packages/messaging-app/messaging_app-0.1.7.tar.gz/messaging_app-0.1.7/messaging_app/web/chat.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from flask import Flask, render_template, request, redirect
+
+
+import os
+import sys
+
+from waitress import serve
+
+from messaging_app.lib.keys_system import the_keys
+from messaging_app.lib.messages_system import  the_message
+from messaging_app.func.send import add_new_user_request, send_new_message
+
+from messaging_app.func.encrypt import encrypt_text
+
+
+from messaging_app.func.save_new_message import save_new_message
+
+
+from naruno.apps.remote_app import Integration
+
+from messaging_app.func.get import get_thread
+
+import messaging_app
+
+app = Flask(__name__)
+from messaging_app.func.integration import the_integration
+
+@app.route('/')
+def main_page():
+    return render_template('home.html')
+@app.route('/user-list')
+def user_list():
+    users = []
+
+    temp_keys = the_keys()
+
+    for element in temp_keys:
+        if "name" in temp_keys[element]:
+            users.append(temp_keys[element])
+    
+
+    return render_template('user-list.html', users=users)
+
+@app.route('/messages/<name>')
+def messages(name):
+
+    temp_keys = the_keys()
+
+    for element in temp_keys:
+        if "name" in temp_keys[element]:
+          if temp_keys[element]["name"] == name:
+            return render_template('messages.html', user_id=element)
+
+@app.route('/get-messages/<id>')
+def get_messages(id):
+
+    temp_keys = the_keys()
+
+    if str(id) in temp_keys:
+        messages = the_message(str(id))
+        return render_template('messages-col.html', user=temp_keys[str(id)], messages=messages)
+
+@app.route("/add-new-user", methods =['POST','GET'])
+def add_new_users():
+    if request.method == 'POST':
+ 
+        publickey = request.form.get('publickey').replace("\n","")
+        add_new_user_request(publickey)
+ 
+        return redirect(request.referrer)
+
+@app.route("/new-message", methods =['POST','GET'])
+def send_message():
+    if request.method == 'POST':
+ 
+        user = request.form.get('user')
+
+        text = request.form.get('text')
+
+
+        temp_keys = the_keys()[user]
+
+        encrypted = encrypt_text(text, user)
+
+
+
+        pubkey = temp_keys["fromUser"]
+
+
+
+
+        send_new_message(encrypted, pubkey)
+
+        message = {"from":"me","message":text}
+
+        
+        
+
+        save_new_message(message,temp_keys["n"],temp_keys["e"])
+ 
+        return redirect(request.referrer)
+
+def start_messaging_app(port=81, integration_port=8000, password="123"):
+    messaging_app.func.integration.the_integration = Integration("messaging_app", port=integration_port, password=password, sended=True)
+    from threading import Thread
+    t = Thread(target=get_thread)
+    t.start()
+    serve(app, host="0.0.0.0", port=port)
+
+
