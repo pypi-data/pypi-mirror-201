4579c076bedde337b7687a7423d2924a3ce4ea82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:18 +0000

    Add package statscend/0.1.3/statscend-0.1.3.tar.gz

---
+++ packages/statscend/statscend-0.1.3.tar.gz/statscend-0.1.3/statscend/__init__.py
@@ -0,0 +1,13 @@
+from .datasets import data
+from .bn_logistic_regression import bn_logistic_regression
+from .mn_logistic_regression import mn_logistic_regression
+from .ordinal_regression import ordinal_regression
+
+
+__all__ = [
+    'data',
+    'linear_regression',
+    'bn_logistic_regression',
+    'mn_logistic_regression',
+    'ordinal_regression',
+]
