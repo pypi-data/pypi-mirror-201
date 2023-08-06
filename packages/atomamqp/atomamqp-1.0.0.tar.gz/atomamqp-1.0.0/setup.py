01f97f928dfd92bf7e547ddafea644326eb68283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:32 +0000

    Add package atomamqp/1.0.0/atomamqp-1.0.0.tar.gz

---
+++ packages/atomamqp/atomamqp-1.0.0.tar.gz/atomamqp-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='atomamqp',
+    version='1.0.0',
+    author='Phuong Do',
+    author_email='phdo@energidanmark.dk',
+    description='A Python library for sending and receiving messages using RabbitMQ',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'pika==1.3.1',
+    ],
+)
+
