4272cec971b1fbc4071db44e3a2250586ee7b27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:12 +0000

    Add package bitcode/1.4.0/bitcode-1.4.0.tar.gz

---
+++ packages/bitcode/bitcode-1.4.0.tar.gz/bitcode-1.4.0/bitcode/console.py
@@ -0,0 +1,24 @@
+"""\
+Reference: 123web.uk/bitpy/docs/console.html
+"""
+
+import os
+
+def write(*text, end="", split="", flush=True):
+	"""Print the given text to stdout"""
+	print(*text, end=end, sep=split, flush=flush)
+
+def writeln(*text, split="", flush=True):
+	"""Print the given text to stdout, and appends a new line"""
+	write(*text, end="\n", split=split, flush=flush)
+
+def beep():
+	"""Play the \\a 'beep' sound in the console"""
+	write("\a")
+
+def clear():
+	"""Clear the console"""
+	if os.name == "nt":
+		os.system("cls")
+	else:
+		os.system("clear")
