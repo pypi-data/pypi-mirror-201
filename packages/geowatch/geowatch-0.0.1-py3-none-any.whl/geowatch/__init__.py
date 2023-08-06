4ccf896d24b1882e8331fa2e96bd0d6505eae0f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:30 +0000

    Add package geowatch/0.0.1/geowatch-0.0.1-py3-none-any.whl

---
+++ packages/geowatch/geowatch-0.0.1-py3-none-any.whl/geowatch/__init__.py
@@ -0,0 +1,11 @@
+"""
+Basic
+"""
+__version__ = '0.0.1'
+__author__ = 'Jon Crall'
+__author_email__ = 'erotemic@gmail.com'
+__url__ = 'None'
+
+__mkinit__ = """
+mkinit /home/joncrall/code/geowatch/geowatch/__init__.py
+"""
