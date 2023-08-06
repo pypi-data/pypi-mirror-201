85587028f4984b1af7f31b9d31bce3d1cc2297cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:16 +0000

    Add package pyexhange-r3ne/0.0.7/pyexhange_r3ne-0.0.7.tar.gz

---
+++ packages/pyexhange-r3ne/pyexhange_r3ne-0.0.7.tar.gz/pyexhange_r3ne-0.0.7/test.py
@@ -0,0 +1,10 @@
+import pyexhange
+
+#Buy 1 Ethereum
+print(pyexhange.handle_command("trade 1 1 eth"))
+
+#Sell 1 Ethereum
+print(pyexhange.handle_command("trade 1 -1 eth"))
+
+#As user 1 send 100 dollars to user 2
+print(pyexhange.handle_command("send 1 2 100 USD There_you_go"))
