ca71dc3dc378984f3c4c637316fbb443de4293fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:27 +0000

    Add package proxy6api/0.1.2/proxy6api-0.1.2.tar.gz

---
+++ packages/proxy6api/proxy6api-0.1.2.tar.gz/proxy6api-0.1.2/README.md
@@ -0,0 +1,2 @@
+# Proxy6API
+This is unofficial software for using api with proxy 6.net
---
+++ packages/proxy6api/proxy6api-0.1.2.tar.gz/proxy6api-0.1.2/proxy6api/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "Proxy_6_API_URL",
+    "Proxy_6_Client",
+    "CODES_OF_ERRORS",
+    "COUNTRIES_HUMAN_NAME_KEYS",
+    "COUNTRIES_ISO2_KEYS",
+]
+
+from .client import Proxy_6_API_URL, Proxy_6_Client
+from .settings import CODES_OF_ERRORS, COUNTRIES_HUMAN_NAME_KEYS, COUNTRIES_ISO2_KEYS
+
+name = "Proxy6 API"
+__author__ = "Konstantin Raikhert"
