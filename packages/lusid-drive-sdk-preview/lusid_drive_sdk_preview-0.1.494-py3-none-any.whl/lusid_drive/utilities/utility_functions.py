6022a8b5fd3f04173604d35be48285aa27510603
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:12 +0000

    Add package lusid-drive-sdk-preview/0.1.494/lusid_drive_sdk_preview-0.1.494-py3-none-any.whl

---
+++ packages/lusid-drive-sdk-preview/lusid_drive_sdk_preview-0.1.494-py3-none-any.whl/lusid_drive/utilities/utility_functions.py
@@ -0,0 +1,37 @@
+import lusid_drive
+import lusid_drive.models as models
+import json
+import logging
+
+
+logger = logging.getLogger("drive-logger")
+logger.setLevel("INFO")
+
+def name_to_id(item_list, target_item):
+    item_id = [obj.id for obj in item_list.values if obj.name == target_item]
+
+    if len(item_id) != 1:
+        # TODO: raise an exception due to no matching item name, or multiple matches
+        pass
+
+    else:
+        return item_id[0]
+
+
+# a path to id function would be useful to build here...
+
+
+def get_folder_id(api_factory, folder_name):
+    folders_api = api_factory.build(lusid_drive.api.FoldersApi)
+    response = folders_api.get_root_folder(filter=f"name eq '{folder_name}'")
+    folder_id = name_to_id(response, folder_name)
+
+    return folder_id
+
+
+def get_file_id(api_factory, file_name, folder_id):
+    folders_api = api_factory.build(lusid_drive.api.FoldersApi)
+    response = folders_api.get_folder_contents(folder_id, filter=f"name eq '{file_name}'")
+    file_id = name_to_id(response, file_name)
+
+    return file_id
