8ff40f6c11faf2028824309d43161a4721b3f931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:21 +0000

    Add package awscli-mate/0.3.1/awscli_mate-0.3.1.tar.gz

---
+++ packages/awscli-mate/awscli_mate-0.3.1.tar.gz/awscli_mate-0.3.1/awscli_mate/api.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+Public API of awscli_mate
+"""
+
+from .awscli import AWSCliConfig
+from .constants import (
+    SectionTypeEnum,
+    ConfigKeyEnum,
+    CredentialKeyEnum,
+)
