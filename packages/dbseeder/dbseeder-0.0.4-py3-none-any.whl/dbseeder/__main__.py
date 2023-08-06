59ef458ef018d212dfe22362e290a8e3377d10d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:45 +0000

    Add package dbseeder/0.0.4/dbseeder-0.0.4-py3-none-any.whl

---
+++ packages/dbseeder/dbseeder-0.0.4-py3-none-any.whl/dbseeder/__main__.py
@@ -0,0 +1,9 @@
+from dbseeder import database
+
+def main():
+    db = database.Database(host="localhost", user="root", password="", database="seed")
+    # db.makeSeed(rows_num=100)
+    db.clearAndMakeSeed(rows_num=100)
+
+if __name__ == "__main__":
+    main()
