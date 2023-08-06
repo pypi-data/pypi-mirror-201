c673654c3d46a5fb77357c9ae259aa7bd440bbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:16 +0000

    Add package AIFunction/1.0.8/AIFunction-1.0.8.tar.gz

---
+++ packages/AIFunction/AIFunction-1.0.8.tar.gz/AIFunction-1.0.8/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "AIFunction",
+    version = "1.0.8",
+    author = "nekumelon",
+    author_email = "nekumelon@gmail.com",
+    description = "Generate realtime AI Functions!",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/nekumelon/AIFunction",
+    project_urls = {
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = (['AIFunction', 'AIFunction/modules', 'AIFunction/modules/web', 'AIFunction/modules/web/OpenAI'] ),
+    python_requires = ">=3.6"
+)
