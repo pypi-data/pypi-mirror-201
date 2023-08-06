d4b523c7170eaf6421ba9f38a76ef01dd75874a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:43 +0000

    Add package codenames/1.1.3/codenames-1.1.3.tar.gz

---
+++ packages/codenames/codenames-1.1.3.tar.gz/codenames-1.1.3/tests/player_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from codenames.game import CardColor, Player, TeamColor
+
+
+def test_player_team_card_color():
+    p1 = Player("Player 1")
+    p1.team_color = TeamColor.RED
+    p2 = Player("Player 2")
+    p2.team_color = TeamColor.BLUE
+    p3 = Player("Player 3")
+    p3.team_color = None
+
+    assert p1.team_card_color == CardColor.RED
+    assert p2.team_card_color == CardColor.BLUE
+
+    with pytest.raises(ValueError):
+        _ = p3.team_card_color
