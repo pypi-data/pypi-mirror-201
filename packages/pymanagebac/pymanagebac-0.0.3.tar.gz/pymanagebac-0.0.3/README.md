485c0735dc3b6ad2042797fc0a29871c2bc2f01b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:34 +0000

    Add package pymanagebac/0.0.3/pymanagebac-0.0.3.tar.gz

---
+++ packages/pymanagebac/pymanagebac-0.0.3.tar.gz/pymanagebac-0.0.3/README.md
@@ -0,0 +1,17 @@
+# mangbac-scraper
+a managebac scraper in python using selenium
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Installation
+to install this module run the following command:
+
+``pip install pymangebac``
+
+After that make sure you install the [geckodriver](https://github.com/mozilla/geckodriver/releases) for your OS to PATH
+
+## examples / usage
+look at the example directory for examples.
+
+
+if there are anu problems, please report them!
---
+++ packages/pymanagebac/pymanagebac-0.0.3.tar.gz/pymanagebac-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = 'A python module for interacting with Managebac using selenium.'
+LONG_DESCRIPTION = """
+pymanagebac is a python module for interacting with Managebac using selenium.
+
+this is a scraper that you can login using your student credentials.
+you can get your soon tasks from the managebac website.
+you can get your grades from the managebac website.
+
+this module is made by a student for students.
+please if something is not working contact me! and provide the issue with the full error message.
+"""
+
+setup(name='pymanagebac',
+      version=VERSION,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3',
+      ],
+      license='gplv3',
+      keywords=['managebac', 'selenium', 'api', 'scraping'],
+      url='https://github.com/supermikea/pymanagebac',
+      author='Mike Wiegman Avila',
+      author_email='mike12wiegman@gmail.com',
+      install_requires=['selenium', 'bs4'],
+      packages=find_packages()
+      )
