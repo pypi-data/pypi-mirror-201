ce1332f261c53a9c8238005eeee0638cd26a23d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:01 +0000

    Add package inferelator-velocity/0.2.0/inferelator_velocity-0.2.0-py3-none-any.whl

---
+++ packages/inferelator-velocity/inferelator_velocity-0.2.0-py3-none-any.whl/inferelator_velocity/utils/keys.py
@@ -0,0 +1,32 @@
+OBS_TIME_KEY = "program_{prog}_time"
+OBSM_PCA_KEY ="program_{prog}_pca"
+
+OBSP_DIST_KEY = "program_{prog}_distances"
+UNS_GRAPH_SUBKEY = "program_{prog}_graph"
+
+PROGRAM_KEY = "programs"
+
+N_BINS = 10
+
+NOISE2SELF_KEY = 'noise2self'
+NOISE2SELF_DIST_KEY = 'noise2self_distance_graph'
+
+METRIC_SUBKEY = 'metric'
+METRIC_GENE_SUBKEY = 'metric_genes'
+METRIC_DIST_SUBKEY = '{metric}_distance'
+MCV_LOSS_SUBKEY = 'molecular_cv_loss'
+LEIDEN_CORR_SUBKEY = 'leiden_correlation'
+PROGRAM_CLUST_SUBKEY = 'cluster_program_map'
+N_COMP_SUBKEY = 'n_comps'
+N_PROG_SUBKEY = 'n_programs'
+PROG_NAMES_SUBKEY = 'program_names'
+
+CENTROID_SUBKEY = 'centroids'
+SHORTEST_PATH_SUBKEY = 'shortest_path'
+CLOSEST_ASSIGNMENT_SUBKEY = 'closest_path_assignment'
+ASSIGNMENT_NAME_SUBKEY = 'assignment_names'
+ASSIGNMENT_CENTROID_SUBKEY = 'assignment_centroids'
+ASSIGNMENT_PATH_SUBKEY = 'assignment_path'
+
+CLUSTER_ORDER_SUBKEY = 'cluster_order'
+CLUSTER_TIME_SUBKEY = 'cluster_times'
