3180c95fd1df8c28c2258b852bcb61f0bb9f97be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:00 +0000

    Add package extotype/0.2/extotype-0.2.tar.gz

---
+++ packages/extotype/extotype-0.2.tar.gz/extotype-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='extotype',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=['sphinx'],
+    classifiers=[],
+    include_package_data=True,
+)
