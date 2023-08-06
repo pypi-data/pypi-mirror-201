cba8cc7803eee3cf557e38f6b7ea39392a324ccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:41 +0000

    Add package create-tox-app/1.0.0/create-tox-app-1.0.0.tar.gz

---
+++ packages/create-tox-app/create-tox-app-1.0.0.tar.gz/create-tox-app-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="create-tox-app",
+    version="1.0.0",
+    url="https://github.com/wozniakpl/create-tox-app.git",
+    author="Bartosz Woźniak",
+    author_email="bwozniakdev@protonmail.com",
+    description="A wrapper that creates a tox env with the minimal setup for a Python app.",
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={"console_scripts": ["create-tox-app = create_tox_app.__main__:main"]},
+)
