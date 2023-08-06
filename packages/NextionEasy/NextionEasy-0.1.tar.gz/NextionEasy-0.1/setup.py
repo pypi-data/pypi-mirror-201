139f5f76dfa4e805cd689b3f28635db412601a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:35 +0000

    Add package NextionEasy/0.1/NextionEasy-0.1.tar.gz

---
+++ packages/NextionEasy/NextionEasy-0.1.tar.gz/NextionEasy-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='NextionEasy',
+      version='0.1',
+      description='Библиотека для получения и отправки данных на дисплей nextion',
+      packages=['nextion_easy'],
+      author_email='testerlzt@mail.ru',
+      zip_safe=False)
