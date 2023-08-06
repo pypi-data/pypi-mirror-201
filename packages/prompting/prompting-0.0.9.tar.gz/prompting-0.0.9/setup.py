d0951131f1da822c74fb52dbd2f687cf7abb96f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:51 +0000

    Add package prompting/0.0.9/prompting-0.0.9.tar.gz

---
+++ packages/prompting/prompting-0.0.9.tar.gz/prompting-0.0.9/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='prompting',
+      version='0.0.9',
+      description='This module provides iPython integration and magics that allow prompting.',
+      readme = "README.md",
+      url='https://gitlab.com/mcaledonensis/magickey/-/tree/prompting',
+      author='Merlinus Caledonensis',
+      author_email='merlin@roundtable.game',
+      license='Apache 2.0',
+      packages=['prompting', 'prompting/prompts'],
+      package_data={'': ['prompts/meta_prompt.txt', 'prompts/system_prompt.txt']},
+      include_package_data=True,
+      install_requires=['openai', 'parsimonious'],
+      zip_safe=False)
