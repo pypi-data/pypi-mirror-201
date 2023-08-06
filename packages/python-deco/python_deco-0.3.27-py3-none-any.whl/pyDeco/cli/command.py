a4755d7fe1f265fff0b75fabe228c1d1bda51eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:34 +0000

    Add package python-deco/0.3.27/python_deco-0.3.27-py3-none-any.whl

---
+++ packages/python-deco/python_deco-0.3.27-py3-none-any.whl/pyDeco/cli/command.py
@@ -0,0 +1,89 @@
+import argparse
+import json
+import sys
+import inspect
+
+import yaml
+
+import pyDeco.utils as util
+
+sys.path.insert(0, "../../")
+
+logger = util.init_logger(__name__)
+
+
+# Override argparse.ArgumentParser
+class ArgumentParser(argparse.ArgumentParser):
+    def error(self, message):
+        logger.error(
+            f"\n{util.bcolors.FAIL}Traceback: {message}.\n    See below for help.{util.bcolors.ENDC}\n\n[USAGE]"
+        )
+        self.print_help()
+        print()
+        self.exit(0)
+
+
+# add --json to argparse for cli
+def add_json_arguments(parser):
+    parser.add_argument("--json", type=str, required=True, help="/path/to/file.json")
+
+
+# read cli input parameters from json file
+def cli_json(arg_parser_func: callable = add_json_arguments):
+    parser = ArgumentParser()
+    arg_parser_func(parser)
+    parsed_args = parser.parse_args()
+    json_path = parsed_args.json
+
+    def read_json(func: callable, json_path: str = json_path):
+        def wrapper(*args, **kwargs):
+            with open(json_path, "rb") as f:
+                params = json.load(f)
+
+            sig = inspect.signature(func)
+            func_args = sig.parameters
+
+            if len(func_args) != len(params.keys()):
+                logger.error(
+                    f"\n{util.bcolors.FAIL}Traceback:\n    File '{inspect.stack()[1][1]}', line {inspect.stack()[1][2]}, in {func.__name__}: \nParameters do not match. Expected {[arg for arg in func_args.keys()]} but got {[arg for arg in params.keys()]}{util.bcolors.ENDC}"
+                )
+                exit()
+
+            return func(**params)
+
+        return wrapper
+
+    return read_json
+
+
+# add --yaml to argparse for cli
+def add_yaml_arguments(parser):
+    parser.add_argument("--yaml", type=str, required=True, help="/path/to/file.yaml")
+
+
+# read cli input parameters from yaml file
+def cli_yaml(arg_parser_func: callable = add_yaml_arguments):
+    parser = ArgumentParser()
+    arg_parser_func(parser)
+    parsed_args = parser.parse_args()
+    json_path = parsed_args.yaml
+
+    def read_yaml(func: callable, json_path: str = json_path):
+        def wrapper(*args, **kwargs):
+            with open(json_path, "r") as f:
+                params = yaml.safe_load(f)
+
+            sig = inspect.signature(func)
+            func_args = sig.parameters
+
+            if list(func_args.keys()) != list(params.keys()):
+                logger.error(
+                    f"\n{util.bcolors.FAIL}Traceback:\n    File '{inspect.stack()[1][1]}', line {inspect.stack()[1][2]}, in {func.__name__}: \nParameters do not match. Expected {[arg for arg in func_args.keys()]} but got {[arg for arg in params.keys()]}{util.bcolors.ENDC}"
+                )
+                exit()
+
+            return func(**params)
+
+        return wrapper
+
+    return read_yaml
---
+++ packages/python-deco/python_deco-0.3.27-py3-none-any.whl/pyDeco/debug/debug/inactive.py
@@ -0,0 +1,18 @@
+import sys
+
+sys.path.insert(0, "../../")
+from pyDeco.utils import init_logger
+
+logger = init_logger(__name__)
+
+
+def inactive(func: callable):
+    def inner(*args, **kwargs):
+        logger.debug(
+            f"Function {func.__name__}() decorated with @inactive. Skipping..."
+        )
+
+        if len(args) > 0:
+            return [None] * len(args)
+
+    return inner
