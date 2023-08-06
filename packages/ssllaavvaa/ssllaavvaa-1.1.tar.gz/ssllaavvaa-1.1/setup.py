f79c78c5987a6e6f91cf52f8d3dfabdcb021ab7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:16 +0000

    Add package ssllaavvaa/1.1/ssllaavvaa-1.1.tar.gz

---
+++ packages/ssllaavvaa/ssllaavvaa-1.1.tar.gz/ssllaavvaa-1.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ssllaavvaa',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_alx'],
+      author_email='sv14.04@mail.ru',
+      zip_safe=False)
