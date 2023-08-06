479c4e593bb3ac2a2695a8933fa789f8164e96f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:27 +0000

    Add package pure-checker/2.1/pure-checker-2.1.tar.gz

---
+++ packages/pure-checker/pure-checker-2.1.tar.gz/pure-checker-2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, Extension
+
+
+def main():
+    setup(
+        name="pure-checker",
+        description="A static analysis file format checker.",
+        ext_modules=[Extension("pure_checker", ["pure_checker.c"], libraries=["z"])],
+        include_package_data=True,
+        packages=[],
+    )
+
+
+if __name__ == "__main__":
+    main()
