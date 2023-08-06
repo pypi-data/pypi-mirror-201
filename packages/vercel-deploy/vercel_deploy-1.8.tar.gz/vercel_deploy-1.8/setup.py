074a7faec5e64529734abe19b81b146c6141130a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:09 +0000

    Add package vercel-deploy/1.8/vercel_deploy-1.8.tar.gz

---
+++ packages/vercel-deploy/vercel_deploy-1.8.tar.gz/vercel_deploy-1.8/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+# Read requirements.txt
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="vercel_deploy",
+    version="1.8",
+    packages=find_packages(),
+    py_modules=[ 'git_clone', 'vercel_deploy'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'git_clone = git_clone:main,vercel_deploy = vercel_deploy:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
