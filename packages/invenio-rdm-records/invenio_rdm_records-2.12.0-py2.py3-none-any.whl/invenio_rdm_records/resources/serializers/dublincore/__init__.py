eb77f4ba1ae92d4f85bcf3015ebec8114bd1f925
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:03 +0000

    Add package invenio-rdm-records/2.12.0/invenio_rdm_records-2.12.0-py2.py3-none-any.whl

---
+++ packages/invenio-rdm-records/invenio_rdm_records-2.12.0-py2.py3-none-any.whl/invenio_rdm_records/resources/serializers/dublincore/__init__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Dublin Core Serializers for Invenio RDM Records."""
+
+from dcxml import simpledc
+from flask_resources import BaseListSchema, MarshmallowSerializer
+from flask_resources.serializers import JSONSerializer, SimpleSerializer
+
+from .schema import DublinCoreSchema
+
+
+class DublinCoreJSONSerializer(MarshmallowSerializer):
+    """Marshmallow based Dublin Core serializer for records."""
+
+    def __init__(self, **options):
+        """Constructor."""
+        super().__init__(
+            format_serializer_cls=JSONSerializer,
+            object_schema_cls=DublinCoreSchema,
+            list_schema_cls=BaseListSchema,
+            **options,
+        )
+
+
+class DublinCoreXMLSerializer(MarshmallowSerializer):
+    """Marshmallow based Dublin Core serializer for records.
+
+    Note: This serializer is not suitable for serializing large number of
+    records.
+    """
+
+    def __init__(self, **options):
+        """Constructor."""
+        super().__init__(
+            format_serializer_cls=SimpleSerializer,
+            object_schema_cls=DublinCoreSchema,
+            list_schema_cls=BaseListSchema,
+            encoder=simpledc.tostring,
+            **options,
+        )
