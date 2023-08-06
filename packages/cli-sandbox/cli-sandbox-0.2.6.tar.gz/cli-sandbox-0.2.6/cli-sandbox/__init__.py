f074f774368e2b42f4148670ad17275b81b75817
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:27 +0000

    Add package cli-sandbox/0.2.6/cli-sandbox-0.2.6.tar.gz

---
+++ packages/cli-sandbox/cli-sandbox-0.2.6.tar.gz/cli-sandbox-0.2.6/cli-sandbox/__init__.py
@@ -0,0 +1,3 @@
+# your_package/__init__.py
+
+__version__ = '0.2.6'  # replace with the actual version number of your package
---
+++ packages/cli-sandbox/cli-sandbox-0.2.6.tar.gz/cli-sandbox-0.2.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cli-sandbox',
+    version='0.2.6',
+    description='cli experiments',
+    author='Your Name',
+    author_email='hughes036@gmail.com',
+    url='https://github.com/AllenCell/cli-sandbox',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='your keywords here',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scikit-learn',
+    ],
+)
