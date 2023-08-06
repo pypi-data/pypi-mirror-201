cdb98100bb29319c1400464341d90ff3581e3c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:59 +0000

    Add package rustylib/1.8.9/rustylib-1.8.9-py3-none-any.whl

---
+++ packages/rustylib/rustylib-1.8.9-py3-none-any.whl/rustylib/Rmath.py
@@ -0,0 +1,134 @@
+def init():
+    rand = Math()
+
+class Math:
+    def getVector(coordsA, coordsB):
+        if isinstance(coordsA, list) == False or isinstance(coordsB, list) == False:
+            print("RustyLib Error - coordsA or coordsB isn't a list. (Maybe you meant 'coordsA = [x, y]' or 'coordsB = [x, y]')")
+        else:
+            Vxab = coordsB[0] - coordsA[0]
+            Vyab = coordsB[1] - coordsA[1]
+            VectorAB = [Vxab, Vyab]
+            return VectorAB
+
+    def getVectorNorm(vector):
+        if isinstance(vector, list) == False:
+            print("RustyLib Error - Vector isn't a list. (Maybe you meant 'vector = [x, y]')")
+        else:
+            distance = Math.sqrt(vector[0]**2 + vector[1]**2)
+            return distance
+    
+    def getPrime(LIST_NAME):
+        Rprime = []
+        for i in LIST_NAME:
+            index = 0
+            for j in range(1, i):
+                if i%j == 0:
+                    index += 1
+            if index == 1:
+                Rprime.append(i)
+        return Rprime
+
+    def getAllPrime(limit:int=100) -> int:
+        numbers = []
+        for i in range(limit):
+            numbers.append(i)
+        RallPrime = Math.getPrime(numbers)
+        return RallPrime
+    
+    def sqrt(num) -> float:
+        root = num**(1/2)
+        return root
+
+    def cubeRoot(num) -> float:
+        root = num**(1/3)
+        return root
+    
+    def powerRoot(num, power) -> float:
+        root = num**(1/power)
+        return root
+    
+    def rad(angle) -> float:
+        rad = angle/180 * Math.pi
+        return rad
+
+    def angle(rad) -> float:
+        angle = rad/Math.pi * 180
+        return angle
+    
+    def round(number, decimalAllowed=0):
+        mul = 10 ** decimalAllowed
+        return int(number * mul + 0.5) / mul
+    
+    def factorial(number):
+        increment = number-1
+        num = number
+        while increment > 0:
+            num *= increment
+            increment-=1
+        if number == 0:
+            return 1
+        return num
+    
+    def sumi(seq, i=0):
+        total = 0
+        if isinstance(seq, (list, tuple)):
+            for num in seq[i:]:
+                total += num
+            return total
+        else:
+            print("Rusty Error - seq can be either a tuple or a list.")
+
+        
+    pi = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821480865132823066470938446095505822317253594081284811174502841027019385211055596446229489549303819644288109756659334461284756482337867831652712019091456485669234603486104543266482133936072602491412737245870066063155881748815209209628292540917153643678925903600113305305488204665213841469519415116094330572703657595919530921861173819326117931051185480744623799627495673518857527248912279381830119491298336733624406566430860213949463952247371907021798609437027705392171762931767523846748184676694051320005681271452635608277857713427577896091736371787214684409012249534301465495853710507922796892589235420199561121290219608640344181598136297747713099605187072113499999983729780499510597317328160963185950244594553469083026425223082533446850352619311881710100031378387528865875332083814206171776691473035982534904287554687311595628638823537875937519577818577805321712268066130019278766111959092164201989
+
+class Object():
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
+class Point(Object):
+    def __init__(self, x, y, z=None):
+        super().__init__()
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __str__(self):
+        if self.z is not None:
+            return f"({self.x}, {self.y}, {self.z})"
+        else:
+            return f"({self.x}, {self.y})"
+
+    def distance(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        if self.z is not None and other.z is not None:
+            dz = self.z - other.z
+            return (dx ** 2 + dy ** 2 + dz ** 2) ** 0.5
+        else:
+            return (dx ** 2 + dy ** 2) ** 0.5
+
+    def render(points):
+        min_x = min(point.x for point in points)
+        max_x = max(point.x for point in points)
+        min_y = min(point.y for point in points)
+        max_y = max(point.y for point in points)
+        min_z = min(point.z for point in points)
+        max_z = max(point.z for point in points)
+
+        grid_width = max_y - min_y + 1
+        grid_height = max_z - min_z + 1
+        grid = [[' ' for y in range(grid_width)] for z in range(grid_height)]
+
+        for point in points:
+            y = point.y - min_y
+            z = point.z - min_z
+            grid[z][y] = 'x'
+
+        output = ''
+        for row in grid:
+            output += ''.join(row) + '\n'
+
+        return output
---
+++ packages/rustylib/rustylib-1.8.9-py3-none-any.whl/rustylib/Rrandom.py
@@ -0,0 +1,80 @@
+import time
+from .Rmath import Math
+from .Rusty import Rusty
+"""
+initialize some basics 
+"""
+"""
+Random Module
+"""
+class Random:
+    def init():
+        rand = Random()
+        rand.seed()
+        return rand
+
+    def seed(self, seed=None):
+        if seed is None:
+            seed = int(time.time() * 1000)
+        self.seed_value = seed
+
+    def rint(self, IntA, IntB) -> int:
+        self.seed_value = (1103515245 * self.seed_value + 12345) & 0xFFFFFFFF
+        return IntA + (self.seed_value % (IntB - IntA + 1))
+    
+    def rfloat(self, FloatA:float, FloatB:float) -> float:
+        self.seed_value = (1103515245 * self.seed_value + 12345) & 0xFFFFFFFF
+        return FloatA + (FloatB - FloatA) * self.seed_value / 0xFFFFFFFF
+    
+    def choice(self, LIST_NAME):
+        rand = Random()
+        rand.seed()
+        POSITION = rand.rint(0, len(LIST_NAME)-1)
+        return LIST_NAME[POSITION]
+
+    def shuffle(self, LIST_NAME):
+        rand = Random()
+        rand.seed()
+        for _ in range(1):
+            for items in LIST_NAME:
+                POSITION = rand.rint(0, len(LIST_NAME)-1)
+                NEW_POSITION = rand.rint(0, len(LIST_NAME)-1)
+                print(POSITION, NEW_POSITION)
+
+                SAVED_ITEM = LIST_NAME.pop(POSITION)
+                LIST_NAME.insert(NEW_POSITION, SAVED_ITEM)
+        return LIST_NAME
+    
+    def strio(self, limit, fullCap=False, probability:int=50) -> str:
+        rand = Random()
+        rand.seed()
+        STR_C = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        FINAL_STR_C_RETURN = []
+        for _ in range(limit):
+            POSITION = rand.rint(0, len(STR_C)-1)
+            if fullCap == True:
+                getRandomFloat = (self.rfloat(0, 1))*100
+                if getRandomFloat <= probability:
+                    STR_C[POSITION] = STR_C[POSITION].upper()
+            FINAL_STR_C_RETURN.append(STR_C[POSITION])
+        FINAL_STR_C_RETURN = ''.join(FINAL_STR_C_RETURN)
+        return FINAL_STR_C_RETURN
+    
+    def probability(self, max:float=100.0):
+        return Math.round(self.rfloat(0, 1) * 100, 2)
+    
+
+    def getrandbits(self, intN: int) -> int:
+        """
+        Returns a non-negative integer with k random bits.
+        """
+        rand = Random()
+        rand.seed()
+        if intN <= 0:
+            raise ValueError("number of bits must be positive")    
+        num_ints = (intN + 31) // 32   
+        result = 0
+        for i in range(num_ints):
+            result = (result << 32) + rand.rint(0, (1 << 32) - 1)
+        
+        return result >> (num_ints * 32 - intN )
