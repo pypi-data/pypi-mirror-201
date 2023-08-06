9f57a692a86a2844a6a89014608bbaed04abc11e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:05 +0000

    Add package eulith-web3/0.0.18/eulith_web3-0.0.18.tar.gz

---
+++ packages/eulith-web3/eulith_web3-0.0.18.tar.gz/eulith_web3-0.0.18/src/eulith_web3/contract_bindings/pendle/token_helper.py
@@ -0,0 +1,23 @@
+from typing import Optional, Union, List, TypedDict
+from eth_typing import Address, ChecksumAddress
+from web3 import Web3
+from web3.types import TxParams
+
+
+class ContractAddressNotSet(Exception):
+    pass
+
+
+class TokenHelper:
+    def __init__(self, web3: Web3, contract_address: Optional[Union[Address, ChecksumAddress]] = None):
+        self.address: Optional[Union[Address, ChecksumAddress]] = contract_address
+        self.abi = []
+        self.bytecode = ''
+        self.w3 = web3
+            
+    def deploy(self):
+        contract = self.w3.eth.contract(abi=self.abi, bytecode=self.bytecode)
+        tx_hash = contract.constructor().transact()
+        tx_receipt = self.w3.eth.wait_for_transaction_receipt(tx_hash)
+        self.address = tx_receipt.contractAddress
+        
