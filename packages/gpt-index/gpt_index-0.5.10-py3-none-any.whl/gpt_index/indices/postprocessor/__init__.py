71e5778cd65413d19f241b3e3ecdad6188620330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:29 +0000

    Add package gpt-index/0.5.10/gpt_index-0.5.10-py3-none-any.whl

---
+++ packages/gpt-index/gpt_index-0.5.10-py3-none-any.whl/gpt_index/indices/postprocessor/__init__.py
@@ -0,0 +1,18 @@
+"""Node PostProcessor module."""
+
+
+from gpt_index.indices.postprocessor.base import BasePostprocessor
+from gpt_index.indices.postprocessor.node import (
+    SimilarityPostprocessor,
+    KeywordNodePostprocessor,
+    PrevNextNodePostprocessor,
+    AutoPrevNextNodePostprocessor,
+)
+
+__all__ = [
+    "BasePostprocessor",
+    "SimilarityPostprocessor",
+    "KeywordNodePostprocessor",
+    "PrevNextNodePostprocessor",
+    "AutoPrevNextNodePostprocessor",
+]
