d387b8fc83db7dc27d837e8780845d6fdf6052d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:07 +0000

    Add package 58348538794578345789/0.0.13/58348538794578345789-0.0.13.tar.gz

---
+++ packages/58348538794578345789/58348538794578345789-0.0.13.tar.gz/58348538794578345789-0.0.13/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.13'
+DESCRIPTION = 'Streaming video data via networks'
+LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+
+# Setting up
+setup(
+    name="58348538794578345789",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+)
