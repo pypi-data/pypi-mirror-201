66297fd07705634fc10406e2dccc341a5ff5a56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:22 +0000

    Add package RP-DataBase/1.1.0/RP-DataBase-1.1.0.tar.gz

---
+++ packages/RP-DataBase/RP-DataBase-1.1.0.tar.gz/RP-DataBase-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RP-DataBase',
+    version='1.1.0',
+    packages=['RPDB'],
+    url='https://github.com/hsn8086/RPDB',
+    license='MIT license',
+    author='hsn8086',
+    author_email='hsn1919810@gmail.com',
+    description='a database'
+)
