c11a1b434f23574a0eb808f096045ad3f4bea12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:02:02 +0000

    Add package awscli-mate/0.1.1/awscli_mate-0.1.1.tar.gz

---
+++ packages/awscli-mate/awscli_mate-0.1.1.tar.gz/awscli_mate-0.1.1/awscli_mate/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+AWS CLI improvement.
+"""
+
+
+from ._version import __version__
+
+__short_description__ = "AWS CLI improvement."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
+__author_email__ = "husanhe@gmail.com"
+__github_username__ = "MacHu-GWU"
