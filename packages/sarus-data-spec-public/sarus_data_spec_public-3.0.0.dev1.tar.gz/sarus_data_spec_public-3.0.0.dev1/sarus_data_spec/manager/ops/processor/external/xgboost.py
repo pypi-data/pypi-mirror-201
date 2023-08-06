40af054db3cb173fee71acd6b9707f6e12027283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:55 +0000

    Add package sarus-data-spec-public/3.0.0.dev1/sarus_data_spec_public-3.0.0.dev1.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-3.0.0.dev1.tar.gz/sarus_data_spec_public-3.0.0.dev1/sarus_data_spec/manager/ops/processor/external/xgboost.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+
+try:
+    import xgboost  # type: ignore[import]
+except ModuleNotFoundError:
+    pass  # error message in typing.py
+
+
+async def xgb_fit(
+    model: xgboost.XGBClassifier, *args: Any, **kwargs: Any
+) -> xgboost.XGBClassifier:
+    fitted_model = model.fit(*args, **kwargs)
+    return fitted_model
+
+
+async def xgb_predict(
+    model: xgboost.XGBClassifier,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    y_pred = model.predict(*args, **kwargs)
+    return y_pred
+
+
+async def xgb_predict_proba(
+    model: xgboost.XGBClassifier,
+    *args: Any,
+    **kwargs: Any,
+) -> Any:
+    y_pred = model.predict_proba(*args, **kwargs)
+    return y_pred
