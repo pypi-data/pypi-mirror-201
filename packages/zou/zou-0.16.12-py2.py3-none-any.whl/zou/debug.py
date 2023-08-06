efc83f4dece5c4526c0e29e39594cbaf9fc6075e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:30 +0000

    Add package zou/0.16.12/zou-0.16.12-py2.py3-none-any.whl

---
+++ packages/zou/zou-0.16.12-py2.py3-none-any.whl/zou/debug.py
@@ -0,0 +1,17 @@
+import os
+from gevent import monkey
+
+monkey.patch_all()
+import logging
+from flask_socketio import SocketIO
+from zou.app import app, config
+
+FORMAT = "%(message)s"
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+socketio = SocketIO(app, cors_allowed_origins=[], cors_credentials=False)
+
+if __name__ == "__main__":
+    print(
+        "The Kitsu API server is listening on port %s..." % config.DEBUG_PORT
+    )
+    socketio.run(app, port=config.DEBUG_PORT, debug=True)
