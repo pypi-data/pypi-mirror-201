f8728ae65c5a821eebc3505e3bdbfd829681394c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:54 +0000

    Add package wavelet-buffer/0.6.0/wavelet_buffer-0.6.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/wavelet-buffer/wavelet_buffer-0.6.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/wavelet_buffer/share/sf_compressor/cmake/sf_compressor-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "sf_compressor::sf_compressor" for configuration "Release"
+set_property(TARGET sf_compressor::sf_compressor APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(sf_compressor::sf_compressor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib64/libsf_compressor.a"
+  )
+
+list(APPEND _cmake_import_check_targets sf_compressor::sf_compressor )
+list(APPEND _cmake_import_check_files_for_sf_compressor::sf_compressor "${_IMPORT_PREFIX}/lib64/libsf_compressor.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
