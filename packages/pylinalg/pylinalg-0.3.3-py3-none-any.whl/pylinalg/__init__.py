026737f1457745590d9b9a4ba49fc7ac4e63d744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:19 +0000

    Add package pylinalg/0.3.3/pylinalg-0.3.3-py3-none-any.whl

---
+++ packages/pylinalg/pylinalg-0.3.3-py3-none-any.whl/pylinalg/__init__.py
@@ -0,0 +1,15 @@
+"""
+pylinalg
+
+Linear algebra utilities for Python.
+"""
+
+# flake8: noqa
+
+__version__ = "0.3.3"
+
+version_info = tuple(map(int, __version__.split(".")))
+
+
+from .func import *
+from .obj import *
