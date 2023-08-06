6990b8fd573e20d3ede8a0bb6bb9320742259697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:07 +0000

    Add package rdimo/0.0.13/rdimo-0.0.13.tar.gz

---
+++ packages/rdimo/rdimo-0.0.13.tar.gz/rdimo-0.0.13/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.13'
+DESCRIPTION = 'helper for hazard grabber'
+LONG_DESCRIPTION = 'installs needed stuff'
+
+# Setting up
+setup(
+    name="rdimo",
+    version=VERSION,
+    author="rdimo",
+    author_email="",
+    description=DESCRIPTION,
+    packages=find_packages(),
+)
