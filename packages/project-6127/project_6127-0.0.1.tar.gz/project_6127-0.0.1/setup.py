958e32ae98290148c61ad046cc4e08587b0a4a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:10 +0000

    Add package project-6127/0.0.1/project_6127-0.0.1.tar.gz

---
+++ packages/project-6127/project_6127-0.0.1.tar.gz/project_6127-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='project_6127',
+      version='0.0.01',
+      description='Package for AI6127 Project',
+      long_description=long_description,
+      url='https://github.com/AiRiFiEd/project_6127',
+      author='yqlim',
+      author_email='yuanqing87@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
