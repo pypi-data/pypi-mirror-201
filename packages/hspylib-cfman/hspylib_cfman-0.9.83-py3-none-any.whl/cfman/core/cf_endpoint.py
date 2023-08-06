82ea127e21d6f31171f67dd59562d8ff7ebb4ab5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:02:01 +0000

    Add package hspylib-cfman/0.9.83/hspylib_cfman-0.9.83-py3-none-any.whl

---
+++ packages/hspylib-cfman/hspylib_cfman-0.9.83-py3-none-any.whl/cfman/core/cf_endpoint.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   @project: HSPyLib-CFMan
+   @package: cfman.core
+      @file: cf_endpoint.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+
+from typing import Tuple
+
+
+class CFEndpoint:
+    """Represent a cf API endpoint entry.
+    """
+
+    def __init__(self, attrs: Tuple[str]):
+        self.alias = attrs[0]
+        self.host = attrs[1]
+        self.protected = attrs[2]
+
+    def __str__(self) -> str:
+        return f"{self.alias}  {self.host}"
+
+    def __repr__(self):
+        return str(self)
