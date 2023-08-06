7b7a2e104cd14a1eecd00e477e2c41bbb62f7528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:09 +0000

    Add package horsetalk/0.3.0/horsetalk-0.3.0.tar.gz

---
+++ packages/horsetalk/horsetalk-0.3.0.tar.gz/horsetalk-0.3.0/horsetalk/__init__.py
@@ -0,0 +1,25 @@
+from .aw_going_description import AWGoingDescription
+from .breed import Breed
+from .coat_colour import CoatColour
+from .dirt_going_description import DirtGoingDescription
+from .disaster import Disaster
+from .gender import Gender
+from .obstacle import Obstacle
+from .racing_code import RacingCode
+from .sex import Sex
+from .surface import Surface
+from .turf_going_description import TurfGoingDescription
+
+__all__ = [
+    "AWGoingDescription",
+    "Breed",
+    "CoatColour",
+    "DirtGoingDescription",
+    "Disaster",
+    "Gender",
+    "Obstacle",
+    "RacingCode",
+    "Sex",
+    "Surface",
+    "TurfGoingDescription",
+]
