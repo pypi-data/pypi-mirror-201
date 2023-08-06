304ce66cd44f5181e8188df757affb89d004be7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:54 +0000

    Add package wavelet-buffer/0.6.0/wavelet_buffer-0.6.0-cp310-cp310-win_amd64.whl

---
+++ packages/wavelet-buffer/wavelet_buffer-0.6.0-cp310-cp310-win_amd64.whl/wavelet_buffer/share/matrix_compressor/cmake/matrix_compressor-targets-release.cmake
@@ -0,0 +1,29 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "matrix_compressor::matrix_compressor" for configuration "Release"
+set_property(TARGET matrix_compressor::matrix_compressor APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(matrix_compressor::matrix_compressor PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/matrix_compressor.lib"
+  )
+
+list(APPEND _cmake_import_check_targets matrix_compressor::matrix_compressor )
+list(APPEND _cmake_import_check_files_for_matrix_compressor::matrix_compressor "${_IMPORT_PREFIX}/lib/matrix_compressor.lib" )
+
+# Import target "matrix_compressor::streamvbyte" for configuration "Release"
+set_property(TARGET matrix_compressor::streamvbyte APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(matrix_compressor::streamvbyte PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/streamvbyte.lib"
+  )
+
+list(APPEND _cmake_import_check_targets matrix_compressor::streamvbyte )
+list(APPEND _cmake_import_check_files_for_matrix_compressor::streamvbyte "${_IMPORT_PREFIX}/lib/streamvbyte.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
