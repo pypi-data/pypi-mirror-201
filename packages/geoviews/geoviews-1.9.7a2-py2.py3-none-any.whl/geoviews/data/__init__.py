f3bafaf765203d8210d114f605528534da054cbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:32 +0000

    Add package geoviews/1.9.7a2/geoviews-1.9.7a2-py2.py3-none-any.whl

---
+++ packages/geoviews/geoviews-1.9.7a2-py2.py3-none-any.whl/geoviews/data/__init__.py
@@ -0,0 +1,5 @@
+from __future__ import absolute_import
+
+from .geom_dict import GeomDictInterface # noqa (API import)
+from .geopandas import GeoPandasInterface # noqa (API import)
+from .iris import CubeInterface # noqa (API import)
