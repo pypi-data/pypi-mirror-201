66da01f949d344ae62a15f3cb3e1d0ecd6297f8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:54 +0000

    Add package pyisv/1.0.0/pyisv-1.0.0.tar.gz

---
+++ packages/pyisv/pyisv-1.0.0.tar.gz/pyisv-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyisv",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="Rama Sesha Sridhar Mantripragada",
+    author_email="ram.sridhar4@gmail.com",
+    description="A Machine Learning Technique to Extract the Intra Seasonal Variability (ISV) from the Geophysical Data.",
+    url="https://github.com/ram-sridhar/pyisv",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
