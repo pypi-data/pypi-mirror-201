96d6926512dd472276067f3c56ed21acd016efbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:10 +0000

    Add package horsetalk/0.6.1/horsetalk-0.6.1.tar.gz

---
+++ packages/horsetalk/horsetalk-0.6.1.tar.gz/horsetalk-0.6.1/horsetalk/finishing_position.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class FinishingPosition(Enum):
+    UNPLACED = 0
+    FIRST = 1
+    SECOND = 2
+    THIRD = 3
+    FOURTH = 4
+    FIFTH = 5
+    SIXTH = 6
+    SEVENTH = 7
+    EIGHTH = 8
+    NINTH = 9
+
+    def __str__(self):
+        return str(self.value)
