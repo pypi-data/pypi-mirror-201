dd956e5969f8ec3509efe6cb6d7108cf2ad593ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:26 +0000

    Add package clip-bbox/0.2.0/clip_bbox-0.2.0-py3-none-any.whl

---
+++ packages/clip-bbox/clip_bbox-0.2.0-py3-none-any.whl/clip_bbox/tests/test_all.py
@@ -0,0 +1,22 @@
+import clip_bbox.clipbbox as cb
+
+# from unittest.mock import patch
+import pathlib as pl
+
+# INTEGRATION TESTS
+
+
+# UNIT TESTS
+def test_run_clip_bbox():
+    # TODO: create "example inputs" folder with skimage and descriptions
+    cb.run_clip_bbox(input_res=(720, 1280))
+    root = "../"
+    for i in range(8):
+        path = pl.Path(root + "img_{}_bbox.png".format(i))
+        assert path.isfile()
+
+
+# @patch('builtins.print')
+# def test_print_hello(mock_print):
+#     print_hello()
+#     assert mock_print.call_args.args == ("Hello, world!",)
