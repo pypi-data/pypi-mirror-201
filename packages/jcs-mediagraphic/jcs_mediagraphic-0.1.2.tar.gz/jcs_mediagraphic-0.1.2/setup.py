02c00535104c0c713b1c200e45e31c4e757dcd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:24 +0000

    Add package jcs-mediagraphic/0.1.2/jcs_mediagraphic-0.1.2.tar.gz

---
+++ packages/jcs-mediagraphic/jcs_mediagraphic-0.1.2.tar.gz/jcs_mediagraphic-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+import jcs_mediagraphic
+
+
+install_requires = [
+    "pip",
+    "fpdf2",
+    "PIL"
+]
+
+
+
+
+def long_description():
+    with open('README.md', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='jcs_mediagraphic',
+    version=jcs_mediagraphic.__version__,
+    description='Python library for MediaGraphic Group usages',
+    author=jcs_mediagraphic.__author__,
+    license=jcs_mediagraphic.__license__,
+    packages=find_packages(include=['jcs_mediagraphic']),
+    install_requires=install_requires,
+)
