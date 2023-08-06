3a0e9a47e6ddb734f1cc9ed2714c93272ae8e056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:52 +0000

    Add package ratzethellopkg01/0.0.1/ratzethellopkg01-0.0.1-py3-none-any.whl

---
+++ packages/ratzethellopkg01/ratzethellopkg01-0.0.1-py3-none-any.whl/libname/hello.py
@@ -0,0 +1,8 @@
+def sayhello(name):
+    #name=(input("What is your name"))
+    print(f"Hello {name}, How are you doing?")
+
+if __name__ == '__main__':
+    #name=input("What is your name \n")
+    sayhello(input())
+
