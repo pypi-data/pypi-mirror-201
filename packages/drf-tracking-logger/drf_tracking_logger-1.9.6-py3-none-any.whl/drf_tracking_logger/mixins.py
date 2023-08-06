ef6c476d95ab23459a15ed428bb849828f083aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:00 +0000

    Add package drf-tracking-logger/1.9.6/drf_tracking_logger-1.9.6-py3-none-any.whl

---
+++ packages/drf-tracking-logger/drf_tracking_logger-1.9.6-py3-none-any.whl/drf_tracking_logger/mixins.py
@@ -0,0 +1,25 @@
+from .base_mixins import BaseLoggingMixin
+import logging
+import os
+
+logger = logging.getLogger(os.getenv('TRACKING_LOGGER'))
+
+
+class LoggingMixin(BaseLoggingMixin):
+    def handle_log(self):
+        """
+        Hook to define what happens with the log.
+        """
+        if self.log['status_code'] < 400:
+            logger.info(self.log)
+        else:
+            logger.error(self.log)
+
+
+class LoggingErrorsMixin(LoggingMixin):
+    """
+    Log only errors
+    """
+
+    def should_log(self, request, response):
+        return response.status_code >= 400
