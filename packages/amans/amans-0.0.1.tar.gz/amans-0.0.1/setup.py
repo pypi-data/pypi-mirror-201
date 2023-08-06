71c76ab344441c387cc30694a38ad995ee80f22b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:03 +0000

    Add package amans/0.0.1/amans-0.0.1.tar.gz

---
+++ packages/amans/amans-0.0.1.tar.gz/amans-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='amans',
+    version='0.0.1',    
+    description='Print date time exmaple',
+    url='https://github.com/amansrivastav-bytequests/system-information',
+    author='Aman Srivastav',
+    author_email='amansrivastav.bytequests@gmail.com',
+    license='BSD 2-clause',
+    packages=['amans'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.5',
+    ],
+)
