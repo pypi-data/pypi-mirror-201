77b9f75e313dea28dc2e18cc948c76981dbfa36f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:21 +0000

    Add package eboekhouden-python/0.1.6/eboekhouden_python-0.1.6.tar.gz

---
+++ packages/eboekhouden-python/eboekhouden_python-0.1.6.tar.gz/eboekhouden_python-0.1.6/eboekhouden_python/__init__.py
@@ -0,0 +1,16 @@
+"""Main init for eboekhouden_python."""
+from .eboekhouden_client import EboekhoudenClient
+from .constants import (
+    BtwCode,
+    InExBTW,
+    MutatieSoort,
+)
+
+
+__version__ = "0.1.6"
+__all__ = [
+    BtwCode,
+    EboekhoudenClient,
+    InExBTW,
+    MutatieSoort,
+]
