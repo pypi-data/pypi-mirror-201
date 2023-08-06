1d669a1a3cbedeb36fab55cd07dac69d1e787ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:42 +0000

    Add package mantellum/1.0.0/mantellum-1.0.0.tar.gz

---
+++ packages/mantellum/mantellum-1.0.0.tar.gz/mantellum-1.0.0/src/mantellum/date_and_time_utils.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def get_utc_timestamp(with_decimal: bool=False):
+    epoch = datetime(1970,1,1,0,0,0)
+    now = datetime.utcnow()
+    timestamp = (now - epoch).total_seconds()
+    if with_decimal:
+        return timestamp
+    return int(timestamp)
