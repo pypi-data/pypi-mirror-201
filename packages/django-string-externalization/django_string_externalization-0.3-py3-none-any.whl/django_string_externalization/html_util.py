9edaf2b2390070124e54f29f3c19594108db5668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:14 +0000

    Add package django-string-externalization/0.3/django_string_externalization-0.3-py3-none-any.whl

---
+++ packages/django-string-externalization/django_string_externalization-0.3-py3-none-any.whl/django_string_externalization/html_util.py
@@ -0,0 +1,32 @@
+from django.utils.safestring import mark_safe
+
+import bleach
+
+
+ALLOWED_TAGS = ["a", "b", "em", "i", "li", "ol", "p", "strong", "ul", "br", "div"]
+
+
+def sanitize_html(html_str, allow_weird_characters=False):
+    with_terrible_replacements = bleach.clean(html_str, tags=ALLOWED_TAGS)
+    # see https://github.com/mozilla/bleach/issues/192
+    if allow_weird_characters:
+        return with_terrible_replacements.replace("&amp;", "&")
+    else:
+        return with_terrible_replacements
+
+
+def is_md_valid(md_str):
+    import mistune    # pylint: disable="import-outside-toplevel relative-beyond-top-level"
+    markdown_from_src = mistune.create_markdown()
+    unsanitized = markdown_from_src(str(md_str))
+    sanitized = sanitize_html(unsanitized)
+    return unsanitized == sanitized
+
+
+def markdown(md_str):
+    """
+    mark_safe being run through bleach
+    """
+    import mistune    # pylint: disable="import-outside-toplevel relative-beyond-top-level"
+    markdown = mistune.create_markdown()
+    return mark_safe(sanitize_html(markdown(str(md_str))))
