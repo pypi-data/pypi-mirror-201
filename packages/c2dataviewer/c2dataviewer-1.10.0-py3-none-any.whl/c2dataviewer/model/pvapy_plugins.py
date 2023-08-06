0de68461fa392f8b2433e26eecab0ff24a219e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:25 +0000

    Add package c2dataviewer/1.10.0/c2dataviewer-1.10.0-py3-none-any.whl

---
+++ packages/c2dataviewer/c2dataviewer-1.10.0-py3-none-any.whl/c2dataviewer/model/pvapy_plugins.py
@@ -0,0 +1,409 @@
+# -*- coding: utf-8 -*-
+
+"""
+C2 DATA VIEWER is distributed subject to a Software License Agreement found
+in the file LICENSE that is included with this distribution.
+SPDX-License-Identifier: EPICS
+
+Copyright 2018 UChicago Argonne LLC
+ as operator of Argonne National Laboratory
+
+PVA object viewer utilities using pvaPy as pvAccess binding
+
+@author: Guobao Shen <gshen@anl.gov>
+"""
+import time
+from threading import Lock
+from collections import deque
+from statistics import mean
+import enum
+
+import pvaccess as pva
+from pvaccess import PvaException
+
+def make_protocol(proto):
+    if isinstance(proto, pva.ProviderType):
+        return proto
+    
+    proto = proto.lower()
+    if proto == 'ca':
+        return pva.ProviderType.CA
+    elif proto == 'pva':
+        return pva.ProviderType.PVA
+    else:
+        raise Error('Invalid protocol: ' + proto)
+
+def parse_pvname(pvname, default_proto):
+    proto = default_proto
+    if "://" in pvname:
+        proto, pvname = name.split('://')
+        proto = make_protocol(proto.lower().strip())
+
+    return pvname, proto
+
+class PollStrategy:
+    def __init__(self, context, timer):
+        self.ctx = context
+        self.timer = timer
+        self.timer.timeout.connect(self.poll)
+        
+    def _data_callback(self, data):        
+        self.ctx.data_callback_wrapper(data)
+
+    def _err_callback(self, msg):
+        self.ctx.notify_error(msg)
+        
+    def poll(self):
+        # try:
+        #     self.data = self.ctx.get()
+        # except PvaException as e:
+        #     self.stop()
+        # self._data_callback(self.data)
+
+        try:
+            self.ctx.channel.asyncGet(self._data_callback, self._err_callback, '')
+        except pva.PvaException:
+            #error because too many asyncGet calls at once.  Ignore
+            pass
+        
+    def start(self):
+        self.timer.start(1000/self.ctx.rate)
+
+    def stop(self):
+        self.timer.stop()
+    
+class MonitorStrategy:
+    def __init__(self, context):
+        self.ctx = context
+        
+    def _data_callback(self, data):
+        self.ctx.data_callback_wrapper(data)
+
+    def _connection_callback(self, is_connected):
+        if is_connected:
+            self.ctx.set_state(ConnectionState.CONNECTED)
+        elif self.ctx.is_running():
+            self.ctx.notify_error()            
+        
+    def start(self):
+        try:
+            self.ctx.channel.setConnectionCallback(self._connection_callback)
+            self.ctx.channel.subscribe('monitorCallback', self._data_callback)
+            self.ctx.channel.startMonitor('')
+        except PvaException as e:
+            self.ctx.notify_error(str(e))
+        
+    def stop(self):
+        try:
+            self.ctx.channel.setConnectionCallback(None)
+            self.ctx.channel.stopMonitor()
+            self.ctx.channel.unsubscribe('monitorCallback')
+        except PvaException:
+            pass
+
+
+class ConnectionState(enum.Enum):
+    CONNECTED = 1
+    CONNECTING = 2
+    DISCONNECTING = 3
+    DISCONNECTED = 4
+    FAILED_TO_CONNECT = 5
+
+    def __str__(self):
+        string_lookup = {
+            1 : 'Connected',
+            2 : 'Connecting',
+            3 : 'Disconnecting',
+            4 : 'Disconnected',
+            5 : 'Failed to connect'
+        }
+
+        return string_lookup[int(self.value)]
+
+class Channel:
+    def __init__(self, name, timer, provider=pva.PVA, status_callback=None):
+        self.channel = pva.Channel(name, provider)
+        self.provider = provider
+        self.name = name
+        self.rate = None
+        self.data_callback = None
+        self.data = None
+        self.monitor_strategy = MonitorStrategy(self)
+        self.poll_strategy = PollStrategy(self, timer) if timer else None
+        self.strategy = None
+        self.rate = None
+        self.status_callback = status_callback
+        self.state = ConnectionState.DISCONNECTED
+
+    def data_callback_wrapper(self, data):
+        if not self.is_running():
+            return
+        
+        self.set_state(ConnectionState.CONNECTED)
+        if self.data_callback:
+            self.data_callback(data)
+        else:
+            self.data = data.get()
+
+    def notify_error(self, msg=None):
+        self.set_state(ConnectionState.FAILED_TO_CONNECT, msg)
+        
+    def start(self, routine=None, rate=None, status_callback=None):
+        self.data_callback = routine
+        self.rate = rate
+        self.strategy = self.poll_strategy if self.rate else self.monitor_strategy
+        if not self.strategy:
+            raise Exception("Can't poll data unless DataSource timer is configured")
+        if status_callback:
+            self.status_callback = status_callback
+        self.set_state(ConnectionState.CONNECTING)
+        self.strategy.start()
+
+    def set_state(self, state, msg=None):
+        if state != self.state:
+            self.state = state
+            if self.status_callback:
+                self.status_callback(str(state), msg)
+                
+    def stop(self):
+        self.set_state(ConnectionState.DISCONNECTING)
+        if self.strategy:
+            self.strategy.stop()
+        self.set_state(ConnectionState.DISCONNECTED)
+
+    def is_running(self):
+        return self.state in [ConnectionState.CONNECTED, ConnectionState.CONNECTING]
+    
+    def get(self):
+        try:
+            return self.channel.get('')
+        except PvaException as e:
+            self.notify_error(str(e))
+            raise
+
+    def async_get(self, data_callback, error_callback):
+        try:
+            self.channel.asyncGet(data_callback, error_callback, '')
+        except pva.PvaException:
+            #error because too many asyncGet calls at once.  Ignore
+            pass
+        
+class DataSource:
+    def __init__(self, timer_factory=None, default=None):
+        """
+
+        :param default:
+        """
+        # local cache of latest data
+        self.data = None
+        # EPICS7 channel
+        self.channel = None
+        self.channel_cache = {}
+        self.fps = None
+        self.timer_factory = timer_factory if timer_factory else lambda: None
+            
+        self.data_callback = None
+        self.status_callback = None
+        
+        # Default PV name
+        self.device = None
+        if default is not None:
+            if type(default) in [list, tuple]:
+                self.__init_connection(default[0])
+            elif type(default) == dict:
+                self.__init_connection(list(default.values())[0])
+            elif type(default) == str:
+                self.__init_connection(default)
+            else:
+                raise RuntimeError("Unknown data type of default parameter: ", default)
+
+        # trigger support from external EPICS3/7 channel
+        self.trigger = None
+        self.trigger_chan = None
+
+    def create_connection(self, name, provider):
+        if name in self.channel_cache and self.channel_cache[name].provider == provider:
+            return self.channel_cache[name]
+        else:
+            chan = Channel(name, self.timer_factory(), provider=provider)
+            self.channel_cache[name] = chan
+            return chan
+    
+    def __init_connection(self, name):
+        """
+        Create initial channel connection with given PV name
+
+        :param name: EPICS7 PV name
+        :return:
+        """
+
+        name, proto = parse_pvname(name, pva.ProviderType.PVA)
+        self.device = name
+        
+        self.channel = Channel(self.device, self.timer_factory(), proto)
+        self.channel_cache[name] = self.channel
+            
+    def get(self):
+        """
+        Get data from current PV channel
+        :return:
+        """
+        if self.channel is None:
+            return None
+
+        if not self.channel.status_callback:
+            self.channel.status_callback = self.status_callback
+        
+        return self.channel.get()
+
+    def async_get(self, success_callback=None, error_callback=None):
+        """
+        Get data from current PV channel
+        :return:
+        """
+        if self.channel is None:
+            return
+
+        if not self.channel.status_callback:
+            self.channel.status_callback = self.status_callback
+
+        if not success_callback:
+            success_callback = lambda x: None
+
+        if not error_callback:
+            error_callback = lambda x: None
+            
+        self.channel.async_get(success_callback, error_callback)
+
+    def update_framerate(self, fps):
+        self.fps = fps
+        
+    def update_device(self, name, restart=False):
+        """
+        Update device, EPICS PV name, and test its connectivity
+
+        :param name: device name
+        :param restart: flag to restart or not
+        :return:
+        :raise PvaException: raise pvaccess exception when channel cannot be connected.
+        """
+        if self.channel is not None:
+            self.stop()
+
+        if name != "":
+            chan = None
+            if name in self.channel_cache:
+                chan = self.channel_cache[name]
+            else:
+                name, proto = parse_pvname(name, pva.ProviderType.PVA)
+                chan = Channel(name, self.timer_factory(), provider=proto, status_callback=self.status_callback)
+                self.channel_cache[name] = chan
+                
+            self.channel = chan
+            self.device = name
+
+            # test channel connectivity
+            chan.get()
+
+            if restart:
+                self.start()
+        else:
+            self.channel = None
+
+    def start(self, routine=None, status_callback=None):
+        """
+        Start a EPICS7 monitor for current device.
+
+        :return:
+        :raise PvaException: raise pvaccess exception when channel cannot be connected.
+        """
+        if self.channel is None:
+            return
+
+        if routine:
+            self.data_callback = routine
+
+        if status_callback:
+            self.status_callback = status_callback
+            
+        self.channel.start(routine=self.data_callback, rate=self.fps, status_callback=self.status_callback)
+
+    def stop(self):
+        """
+        Stop monitor EPICS7 channel for current device.
+
+        :return:
+        :raise PvaException: raise pvaccess exception when channel fails to disconnect.
+        """
+        if self.channel is None:
+            return
+
+        self.channel.stop()
+
+    def update_trigger(self, name, proto='ca'):
+        """
+        Update trigger PV name.  If failed to connect, trigger is set to None
+
+        :param name: PV name for external trigger
+        :param proto: protocol, should be 'pva' or 'ca'. 'ca' by default
+        :return: trigger fields
+        """
+        if self.trigger_chan is not None:
+            self.stop_trigger()
+
+        self.trigger = None
+        
+        if name == self.trigger:
+            # nothing change
+            return
+        
+        if proto.lower() == "ca":
+            # use channel access protocol, which is default
+            trigger_chan = pva.Channel(name, pva.CA)
+        elif proto.lower() == "pva":
+            # use pvAccess protocol
+            trigger_chan = pva.Channel(name)
+        else:
+            raise RuntimeError("Unknown EPICS communication protocol {}".format(proto))
+
+        # test connectivity, which might cause for example timeout exception
+        pv = trigger_chan.get("")
+
+        # update trigger name & channel
+        self.trigger = name
+        self.trigger_proto = proto.lower()
+        self.trigger_chan = trigger_chan
+        pv_structure = pv.getStructureDict()
+        keys =[ k for k,v in pv_structure.items() ]
+        
+        return keys
+
+    def start_trigger(self, routine):
+        """
+
+        :return:
+        """
+        try:
+            self.trigger_chan.subscribe('triggerMonitorCallback', routine)
+            req = ''
+            if self.trigger_proto == 'ca':
+                #need to explicitly ask for timeStamp 
+                req = 'field(timeStamp,value)'
+            self.trigger_chan.startMonitor(req)
+        except PvaException:
+            raise RuntimeError("Cannot connect to PV {}".format(self.trigger))
+
+    def stop_trigger(self):
+        """
+
+        :return:
+        """
+        if not self.trigger_chan:
+            return
+        
+        try:
+            self.trigger_chan.stopMonitor()
+            self.trigger_chan.unsubscribe('triggerMonitorCallback')
+        except PvaException:
+            # raise RuntimeError("Fail to disconnect")
+            pass
---
+++ packages/c2dataviewer/c2dataviewer-1.10.0-py3-none-any.whl/c2dataviewer/view/scope_config_base.py
@@ -0,0 +1,212 @@
+"""
+C2 DATA VIEWER is distributed subject to a Software License Agreement found
+in the file LICENSE that is included with this distribution.
+SPDX-License-Identifier: EPICS
+"""
+
+class ScopeConfigureBase:
+    def __init__(self, params, **kwargs):
+        self.params = params
+        self.default_trigger = kwargs.get("trigger", None)
+
+    def add_source_aquisition_props(self, children, section):
+        """
+        Add acquisition information specific to the application data source.  This
+        should be implemented by the child class
+        """
+        return children
+
+    def assemble_acquisition(self, section=None, buffer_unit='Samples'):
+        """
+        Assemble acquisition information
+
+        :param section:
+        :return:
+        """
+
+        children = []
+
+        buffer_size = None
+        try:
+            buffer_size = section["BUFFER"]
+        except:
+            pass
+        
+        children += [
+            {"name": "Freeze", "type": "bool", "value": False},
+            {"name": "Buffer (%s)" % buffer_unit, "type": "int", "value": buffer_size, "siPrefix": False, 'decimals': 20}
+        ]
+
+        acquisition = {"name": "Acquisition", "type":"group", "children": children}
+        
+        return acquisition
+
+    
+    def assemble_display(self, section=None):
+        """
+        Assemble display information
+
+        :param section:
+        :return:
+        """
+        if section is None:
+            display = {"name": "Display", "type": "group", "children": [
+                {"name": "Mode", "type": "list", "values": {
+                    "Normal": "normal",
+                    "FFT": "fft",
+                    "PSD": "psd",
+                    "Diff": "diff",
+                    "X vs Y": "xy",
+                }, "value": "Normal"},
+                {"name": "FFT filter", "type": "list", "values": {
+                    "None" : "none",
+                    "Hamming" : "hamming"
+                }, "value": "None"},
+                {"name": "Exp moving avg", "type": "int", "value": 1, "limits" : (1, 1e+10)},
+                {"name": "Autoscale", "type": "bool", "value": False},
+                {"name": "Single axis", "type": "bool", "value": True},
+                {"name": "Histogram", "type": "bool", "value": False},
+                {"name": "Num Bins", "type": "int", "value": 100},
+                {"name": "Refresh", "type": "float", "value": 0.1, "siPrefix": True, "suffix": "s"},
+            ]}
+        else:
+            display_mode = section.get("MODE", "normal").lower().strip()
+            if display_mode not in ["normal", "fft", "psd", "diff", "xy"]:
+                display_mode = "normal"
+
+            fft_filter = section.get("FFT_FILTER", None)
+            if fft_filter is not None:
+                if fft_filter.lower().strip() in ["none", "hamming"]:
+                    fft_filter = fft_filter.lower().strip().capitalize()
+                else:
+                    # Disable filter if config not valid
+                    fft_filter = "none"
+
+            try:
+                n_average = section.getint("AVERAGE", 1)
+                n_average = n_average if n_average > 0 else 1
+            except ValueError:
+                n_average = 1
+
+            autoscale = section.get("AUTOSCALE", None)
+            if autoscale is not None:
+                if autoscale.upper().strip() in ["TRUE"]:
+                    autoscale = True
+                else:
+                    # set auto scale value to None if a "None" string comes from configuration
+                    autoscale = False
+            else:
+                autoscale = False
+
+            single_axis = section.get("SINGLE_AXIS", None)
+            if single_axis is not None:
+                if single_axis.upper().strip() in ["FALSE"]:
+                    single_axis = False
+                else:
+                    single_axis = True
+            else:
+                single_axis = True
+
+            histogram = section.get("HISTOGRAM", None)
+            if histogram is not None:
+                if histogram.upper().strip() in ["TRUE"]:
+                    histogram = True
+                else:
+                    # set auto scale value to None if a "None" string comes from configuration
+                    histogram = False
+            else:
+                histogram = False
+
+            try:
+                n_binning = section.getint("N_BIN", 100)
+            except ValueError:
+                n_binning = 1
+
+            try:
+                refresh = section.getfloat("REFRESH", 100)
+            except ValueError:
+                refresh = 1
+
+            display = {"name": "Display", "type": "group", "children": [
+                {"name": "Mode", "type": "list", "values": {
+                    "Normal": "normal",
+                    "FFT": "fft",
+                    "PSD": "psd",
+                    "Diff": "diff",
+                    "X vs Y": "xy",
+                }, "value": display_mode},
+                {"name": "FFT filter", "type": "list", "values": {
+                    "None" : "none",
+                    "Hamming" : "hamming"
+                }, "value": fft_filter},
+                {"name": "Exp moving avg", "type": "int", "value": n_average, "limits" : (1, 1e+10)},
+                {"name": "Autoscale", "type": "bool", "value": autoscale},
+                {"name": "Single axis", "type": "bool", "value": single_axis},
+                {"name": "Histogram", "type": "bool", "value": histogram},
+                {"name": "Num Bins", "type": "int", "value": n_binning},
+                {"name": "Refresh", "type": "float", "value": refresh / 1000., "siPrefix": True, "suffix": "s"},
+            ]}
+
+        return display
+
+
+    def assemble_trigger(self, section=None):
+        trigger_pv = self.default_trigger
+        trigger_mode = 'none'
+
+        if section:
+            # Trigger PV name and protocol, which comes with format of proto://pv_name
+            # the protocol is either ca:// for channel access or pva:// for pvAccess
+            trigger_pv = section.get("TRIGGER", None)
+            if self.default_trigger is not None:
+                trigger_pv = self.default_trigger
+
+            if trigger_pv is not None and trigger_pv.upper().strip() == "NONE":
+                # set trigger PV value to None if a "None" string comes from configuration
+                trigger_pv = None
+            if trigger_pv is None:
+                trigger_mode = None
+            else:
+                trigger_mode = section.get("TRIGGER_MODE", None)
+            if trigger_mode is not None:
+                trigger_mode = trigger_mode.lower().strip()
+                if trigger_mode == 'off':
+                    trigger_mode = 'none'
+            else:
+                trigger_mode = 'none'
+        
+        cfg ={"name": "Trigger",
+              "type": "group",
+              "children" : [
+                  { "name" :  "Mode", "type": "list", "values": {
+                    "Off" : "none",
+                    "On change" : "onchange",
+                    "Greater than threshold" : "gtthreshold",
+                    "Lesser than threshold" : "ltthreshold"
+                    },
+                  "value" : trigger_mode
+                 },
+                  {"name": "PV", "type": "str", "value": trigger_pv},
+                  {"name": "Trig Status", "type": "str", "value": "", "readonly": True},
+                  {"name": "Trig Value", "type": "str", "value": "", "readonly": True},
+                  {"name": "Time Field", "type": "list", "values" :
+                   [ "None" ], "default" : "None", "visible" : False},
+                  {"name": "Data Time Field", "type": "list", "values" :["None"], "default" : "None" },
+                  {"name": "Autoscale Buffer", "type": "bool", "value" : True},
+                  {"name": "Threshold", "type": "float", "value": 0.0},
+              ]}
+        return cfg
+        
+    def assemble_statistics(self):
+        statistics = {"name": "Statistics", "type": "group", "children": [
+            {"name": "CPU", "type": "float", "value": 0, "readonly": True, "suffix": "%"},
+            {"name": "Lost Arrays", "type": "int", "value": 0, "readonly": True},
+            {"name": "Tot. Arrays", "type": "int", "value": 0, "readonly": True, "siPrefix": True},
+            {"name": "Arrays/Sec", "type": "float", "value": 0., "readonly": True, "siPrefix": True,
+             "suffix": "/sec"},
+            {"name": "Bytes/Sec", "type": "float", "value": 0., "readonly": True, "siPrefix": True,
+             "suffix": "/sec"},
+            {"name": "Rate", "type": "float", "value": 0., "readonly": True, "siPrefix": True,
+             "suffix": "Frames/sec"},
+        ]}
+        return statistics
