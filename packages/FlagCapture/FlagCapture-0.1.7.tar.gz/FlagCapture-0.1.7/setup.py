23304f8978a7467b854f06e5832c767d0f19c5a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:57:57 +0000

    Add package FlagCapture/0.1.7/FlagCapture-0.1.7.tar.gz

---
+++ packages/FlagCapture/FlagCapture-0.1.7.tar.gz/FlagCapture-0.1.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='FlagCapture',
+    packages=['FlagCapture'],
+    zip_safe=True, version='0.1.7',
+    author='Michael Caggeso',
+    author_email='laurencaggeso@gmail.com',
+    long_description='Made it so _game works and also Game instead of my old anouncment in 0.1.6 also made it so people can log out without crashing the server',
+    description='Made it so _game works and also Game instead of my old anouncment in 0.1.6 also made it so people can log out without crashing the server',
+    url="https://pypi.org/search/?q=FlagCapture&o=",
+    download_url="https://pypi.org/search/?q=FlagCapture&o=")
