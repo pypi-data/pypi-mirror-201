8713c266a0113fd97afed43883f565c3ea4bdbc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:07 +0000

    Add package startstop/1.0.4/startstop-1.0.4-py3-none-any.whl

---
+++ packages/startstop/startstop-1.0.4-py3-none-any.whl/startstop/__init__.py
@@ -0,0 +1,20 @@
+from pyinstrument import Profiler
+
+
+class StartStopProfiler:
+    def __init__(self, interval=0.001, async_mode="enabled"):
+        self.profiler = Profiler(interval, async_mode)
+        self.running = False
+
+    def __call__(self):
+        if not self.running:
+            self.profiler.start()
+            self.running = True
+        else:
+            self.profiler.stop()
+            self.running = False
+            self.profiler.open_in_browser()
+            self.profiler.reset()
+
+
+t = StartStopProfiler()
