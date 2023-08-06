8e444eb3f1fc3092f097d75691d73ba94405be9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:51 +0000

    Add package carwatch/0.1.0/carwatch-0.1.0.tar.gz

---
+++ packages/carwatch/carwatch-0.1.0.tar.gz/carwatch-0.1.0/carwatch/__init__.py
@@ -0,0 +1,8 @@
+"""CARWatch: A Python package for improving the accuracy of cortisol awakening response sampling."""
+
+__version__ = "0.1.0"
+
+from carwatch import examples  # noqa: F401
+from carwatch import labels  # noqa: F401
+
+__all__ = ["labels"]
