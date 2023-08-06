2a5eed999a7d6c1dea063e6667eee98b3745285e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:57 +0000

    Add package meowcorp/0.1.1/meowcorp-0.1.1.tar.gz

---
+++ packages/meowcorp/meowcorp-0.1.1.tar.gz/meowcorp-0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "python-dotenv~=0.21.0",
+    "sqlalchemy~=1.4.39",
+    "psycopg2~=2.9.5",
+    "aiogram~=2.23.1"
+]
+
+setup(
+    name="meowcorp",
+    version="0.1.1",
+    packages=find_packages(include=['meowcorp', 'meowcorp.*', 'meowcorp.conf.project_template.*']),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'meowcorp-admin = meowcorp.commands:execute_from_command_line',
+        ]
+    }
+)
