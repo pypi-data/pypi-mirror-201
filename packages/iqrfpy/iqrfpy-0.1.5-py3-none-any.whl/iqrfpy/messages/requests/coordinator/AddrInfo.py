f544bffb6f1399b9ee3c58e94d49dce3ef9103e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:08 +0000

    Add package iqrfpy/0.1.5/iqrfpy-0.1.5-py3-none-any.whl

---
+++ packages/iqrfpy/iqrfpy-0.1.5-py3-none-any.whl/iqrfpy/messages/requests/coordinator/AddrInfo.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+from typeguard import typechecked
+from uuid import uuid4
+from typing import Union
+from iqrfpy.enums.Commands import CoordinatorRequestCommands
+from iqrfpy.enums.MessageTypes import CoordinatorMessages
+from iqrfpy.enums.peripherals import EmbedPeripherals
+from iqrfpy.utils.common import Common
+from iqrfpy.messages.requests.IRequest import IRequest
+
+__all__ = ['AddrInfoRequest']
+
+
+@typechecked
+class AddrInfoRequest(IRequest):
+
+    def __init__(self, hwpid: int = Common.HWPID_MAX, msgid: str = str(uuid4())):
+        super().__init__(
+            nadr=0,
+            pnum=EmbedPeripherals.COORDINATOR,
+            pcmd=CoordinatorRequestCommands.ADDR_INFO,
+            m_type=CoordinatorMessages.ADDR_INFO,
+            hwpid=hwpid,
+            msgid=msgid
+        )
+
+    def to_dpa(self, mutable: bool = False) -> Union[bytes, bytearray]:
+        return super().to_dpa(mutable=mutable)
+
+    def to_json(self) -> dict:
+        return super().to_json()
