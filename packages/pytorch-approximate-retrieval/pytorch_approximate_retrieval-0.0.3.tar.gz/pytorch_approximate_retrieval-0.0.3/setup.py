c32a293c5ddff31285dfbcc8228398aefd7c6e86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:18 +0000

    Add package pytorch-approximate-retrieval/0.0.3/pytorch_approximate_retrieval-0.0.3.tar.gz

---
+++ packages/pytorch-approximate-retrieval/pytorch_approximate_retrieval-0.0.3.tar.gz/pytorch_approximate_retrieval-0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pytorch_approximate_retrieval",
+    version="0.0.3",
+    description="Approximate KNN retrieval for PyTorch",
+    url="http://github.com/itsdaniele/pytorch_approximate_retrieval",
+    long_description_content_type="text/markdown",
+    author="Daniele Paliotta",
+    author_email="daniele.paliotta@unige.ch",
+    license="MIT",
+    packages=find_packages(exclude=[]),
+    install_requires=[
+        "numpy",
+        "torch>=1.6",
+    ],
+)
