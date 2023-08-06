40a748a951cdd12b8b3b471c52224206a32f9a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:45 +0000

    Add package sym-sdk/0.32.0/sym_sdk-0.32.0.tar.gz

---
+++ packages/sym-sdk/sym_sdk-0.32.0.tar.gz/sym_sdk-0.32.0/sym/sdk/exceptions/__init__.py
@@ -0,0 +1,33 @@
+"""Exceptions that can be raised by the Sym Runtime."""
+
+__all__ = [
+    "AccessStrategyError",
+    "AptibleError",
+    "AWSError",
+    "AWSLambdaError",
+    "CouldNotSaveError",
+    "ExceptionWithHint",
+    "GitHubError",
+    "HTTPError",
+    "IdentityError",
+    "MissingArgument",
+    "OktaError",
+    "OneLoginError",
+    "PagerDutyError",
+    "SDKError",
+    "SlackError",
+    "SymException",
+]
+
+from .access_strategy import AccessStrategyError
+from .aptible import AptibleError
+from .aws import AWSError, AWSLambdaError
+from .github import GitHubError
+from .http import HTTPError
+from .identity import CouldNotSaveError, IdentityError
+from .okta import OktaError
+from .onelogin import OneLoginError
+from .pagerduty import PagerDutyError
+from .sdk import MissingArgument, SDKError
+from .slack import SlackError
+from .sym_exception import ExceptionWithHint, SymException
