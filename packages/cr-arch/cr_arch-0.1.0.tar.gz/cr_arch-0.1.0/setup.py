7e409302c77d51a518011b0b14ce8e08183ce200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:36 +0000

    Add package cr-arch/0.1.0/cr_arch-0.1.0.tar.gz

---
+++ packages/cr-arch/cr_arch-0.1.0.tar.gz/cr_arch-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cr_arch",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # NEED TO ADD DEPENDENCIES!
+    ],
+    author="Shiyang Lai",
+    author_email="shiyanglai@uchicago.edu",
+    description="A package for implementing multi-pipeline neural network architecture for conversation recognition."
+)
