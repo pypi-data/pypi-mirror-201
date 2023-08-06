fd8586a91af709ec87a301a349a7b6b80dcc828e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:37 +0000

    Add package torchmanager-nightly/1.2b1/torchmanager-nightly-1.2b1.tar.gz

---
+++ packages/torchmanager-nightly/torchmanager-nightly-1.2b1.tar.gz/torchmanager-nightly-1.2b1/lib/data/__init__.py
@@ -0,0 +1,2 @@
+from . import utils
+from .dataset import Dataset, DataLoader, batched
---
+++ packages/torchmanager-nightly/torchmanager-nightly-1.2b1.tar.gz/torchmanager-nightly-1.2b1/lib/train/__init__.py
@@ -0,0 +1,3 @@
+from torchmanager_core.protocols import Frequency as LrScheduleFreq
+from .checkpoint import Checkpoint, list_checkpoints, load
+from .learning_rate import update_lr
