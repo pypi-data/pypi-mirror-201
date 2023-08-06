b4bf091cf6b397972f119c388e60623856f902e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:31 +0000

    Add package tritondse/0.1.1/tritondse-0.1.1.tar.gz

---
+++ packages/tritondse/tritondse-0.1.1.tar.gz/tritondse-0.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Installation of the tritondse module."""
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="tritondse",
+    version="0.1.1",
+    description="A library of Dynamic Symbolic Exploration based the Triton library",
+    packages=find_packages(),
+    setup_requires=[],
+    install_requires=[
+        "triton-library",
+        "lief",
+        "pyQBDI",
+        "cle",
+        "quokka-project",
+        "enum_tools"
+    ],
+    tests_require=[],
+    license="qb",
+    author="Quarkslab",
+    classifiers=[
+        'Topic :: Security',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+    ],
+    test_suite="",
+    scripts=[]
+)
