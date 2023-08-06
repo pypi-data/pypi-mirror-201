b14f9cfec757c3f9e5a9f417e7e890a4e55dc119
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:19 +0000

    Add package web-utils-soft/0.4.1/web-utils-soft-0.4.1.tar.gz

---
+++ packages/web-utils-soft/web-utils-soft-0.4.1.tar.gz/web-utils-soft-0.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+__version__ = "0.4.1"
+
+setup(
+    name="web-utils-soft",
+    version=__version__,
+    description="Python Project Package Template",
+    author="Wilson Ramirez",
+    author_email="wil_ramirez02@hotmail.com",
+    license="MIT",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=["requests"],
+    include_package_data=True,
+    url="https://github.com/danielramirez30/python-project-template",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    # entry_points={
+    #     "console_scripts": [
+    #         "get-data=src.json_utils:get_json",
+    #         "get-image=src.image_utils:get_image",
+    #     ]
+    # },
+)
