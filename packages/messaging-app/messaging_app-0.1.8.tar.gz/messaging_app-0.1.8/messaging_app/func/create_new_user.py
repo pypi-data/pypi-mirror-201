0e8004b2bd042b7ef0e715667607c69d8e82b98a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:10 +0000

    Add package messaging-app/0.1.8/messaging_app-0.1.8.tar.gz

---
+++ packages/messaging-app/messaging_app-0.1.8.tar.gz/messaging_app-0.1.8/messaging_app/func/create_new_user.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+from messaging_app.func.gen_keys import gen_keys
+from messaging_app.lib.keys_system import the_keys, save_keys
+
+from naruno.wallet.wallet_import import Address, wallet_import
+
+def create_new_user(name,fromUser,n,e):
+
+    
+    temp_keys = the_keys()
+
+    in_list = False
+    if not fromUser == wallet_import(-1, 3):
+        for key in temp_keys:
+          if "fromUser" in temp_keys[key]:
+            if temp_keys[key]["fromUser"] == fromUser:
+                in_list = True
+                if temp_keys[key]["n"] == 0:
+                    temp_keys[key]["n"] = n
+                if temp_keys[key]["e"] == 0:
+                    temp_keys[key]["e"] = e                    
+                save_keys(temp_keys)
+                return(key)
+
+    if not in_list:
+        number = str(len(temp_keys) + 1)
+        
+        temp_keys[number] = {}
+        
+        temp_keys[number]["name"] = name
+
+        temp_keys[number]["fromUser"] = fromUser
+        temp_keys[number]["n"] = n
+        temp_keys[number]["e"] = e
+
+        save_keys(temp_keys)
+        
+        return(number)
