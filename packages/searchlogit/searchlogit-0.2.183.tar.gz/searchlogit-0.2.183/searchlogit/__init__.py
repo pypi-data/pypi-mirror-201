cfca8e5cd3910bbf9a34f7054f8c2a42c748f544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:14 +0000

    Add package searchlogit/0.2.183/searchlogit-0.2.183.tar.gz

---
+++ packages/searchlogit/searchlogit-0.2.183.tar.gz/searchlogit-0.2.183/searchlogit/__init__.py
@@ -0,0 +1,6 @@
+from .mixed_logit import MixedLogit
+from .multinomial_logit import MultinomialLogit
+from .latent_class_model import LatentClassModel
+from .latent_class_mixed_model import LatentClassMixedModel
+from .search import Search
+from ._device import device
