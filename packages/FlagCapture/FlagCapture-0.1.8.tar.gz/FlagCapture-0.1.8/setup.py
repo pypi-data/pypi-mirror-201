13a7540ae6062d5532ce787b53568a2989150cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:55 +0000

    Add package FlagCapture/0.1.8/FlagCapture-0.1.8.tar.gz

---
+++ packages/FlagCapture/FlagCapture-0.1.8.tar.gz/FlagCapture-0.1.8/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='FlagCapture',
+    packages=['FlagCapture'],
+    zip_safe=True, version='0.1.8',
+    author='Michael Caggeso',
+    author_email='laurencaggeso@gmail.com',
+    long_description='Made it so it doesent use numpy and so turbo works and dosent go off you screen.',
+    description='Made it so it doesent use numpy and so turbo works and dosent go off you screen',
+    url="https://pypi.org/search/?q=FlagCapture&o=",
+    download_url="https://pypi.org/search/?q=FlagCapture&o=")
