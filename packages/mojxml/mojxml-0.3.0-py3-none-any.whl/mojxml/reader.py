50bfd41033c3fe5633d8b6164f7b614ce52ebd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:46 +0000

    Add package mojxml/0.3.0/mojxml-0.3.0-py3-none-any.whl

---
+++ packages/mojxml/mojxml-0.3.0-py3-none-any.whl/mojxml/reader.py
@@ -0,0 +1,37 @@
+"""Handle XMLs and single/doule-zipped archives of MOJXML trnsparently"""
+
+from pathlib import Path
+from typing import Iterable, List
+from zipfile import ZipFile
+
+
+def iter_content_xmls(src_paths: List[Path]) -> Iterable[bytes]:
+    """Iterate XML contents from given zips and xmls"""
+    for src_path in src_paths:
+        src_path = Path(src_path)
+        if src_path.suffix == ".xml":
+            with open(src_path, "rb") as f:
+                yield f.read()
+        elif src_path.suffix == ".zip":
+            with MojXMLZipFile(src_path) as mzf:
+                yield from mzf.iter_xml_contents()
+        else:
+            raise ValueError(f"Unsupported file type: {src_path.suffix}")
+
+
+class MojXMLZipFile(ZipFile):
+    """Handles single/doule-zipped archives of MOJ XMLs transparently"""
+
+    def iter_xml_contents(self):
+        """Iterate XML contents from given zips"""
+        for name in self.namelist():
+            if name.endswith(".zip"):
+                yield self._extract_xml_content(name[:-4])
+            elif name.endswith(".xml"):
+                yield self.open(name).read()
+
+    def _extract_xml_content(self, internal_name: str) -> bytes:
+        with self.open(internal_name + ".zip") as f:
+            with ZipFile(f) as zf:
+                xml_content = zf.open(internal_name + ".xml").read()
+                return xml_content
