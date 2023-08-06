53c4f5784dbb80c0ecd4f59acfbc8a4955ce5470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:02 +0000

    Add package pipdu-sdk/1.0.0.dev4075953558/pipdu-sdk-1.0.0.dev4075953558.tar.gz

---
+++ packages/pipdu-sdk/pipdu-sdk-1.0.0.dev4075953558.tar.gz/pipdu-sdk-1.0.0.dev4075953558/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pipdu-sdk',
+    packages=['pipdu-sdk'],
+    version='1.0.0.dev4075953558',
+    license='MIT',
+    description='PiPDU python SDK',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Mircea-Pavel Anton',
+    author_email='contact@mirceanton.com',
+    url='https://gitlab.com/mirceanton/piPDU/-/tree/main/utils/python-sdk',
+    project_urls={
+        "Bug Tracker": "https://gitlab.com/mirceanton/piPDU/-/issues"
+    },
+    install_requires=['prometheus_client', 'requests', 'pyyaml'],
+    keywords=["pypi", "pipdu", "mirceanton"],
+)
