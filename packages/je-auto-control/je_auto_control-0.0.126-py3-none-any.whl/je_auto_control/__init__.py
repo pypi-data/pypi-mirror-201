55bd7bfd9655ea8538c31d9ce70f506090f4ea10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:49 +0000

    Add package je-auto-control/0.0.126/je_auto_control-0.0.126-py3-none-any.whl

---
+++ packages/je-auto-control/je_auto_control-0.0.126-py3-none-any.whl/je_auto_control/__init__.py
@@ -0,0 +1,101 @@
+"""
+import all wrapper function
+"""
+
+# Critical
+from je_auto_control.utils.critical_exit.critcal_exit import CriticalExit
+from je_auto_control.utils.exception.exceptions import AutoControlActionException
+from je_auto_control.utils.exception.exceptions import AutoControlActionNullException
+from je_auto_control.utils.exception.exceptions import AutoControlCantFindKeyException
+# Exception
+from je_auto_control.utils.exception.exceptions import AutoControlException
+from je_auto_control.utils.exception.exceptions import AutoControlJsonActionException
+from je_auto_control.utils.exception.exceptions import AutoControlKeyboardException
+from je_auto_control.utils.exception.exceptions import AutoControlMouseException
+from je_auto_control.utils.exception.exceptions import AutoControlRecordException
+from je_auto_control.utils.exception.exceptions import AutoControlScreenException
+from je_auto_control.utils.exception.exceptions import ImageNotFoundException
+from je_auto_control.utils.executor.action_executor import add_command_to_executor
+# executor
+from je_auto_control.utils.executor.action_executor import execute_action
+from je_auto_control.utils.executor.action_executor import execute_files
+from je_auto_control.utils.executor.action_executor import executor
+from je_auto_control.utils.file_process.create_project_structure import create_template_dir
+# file process
+from je_auto_control.utils.file_process.get_dir_file_list import get_dir_files_as_list
+# html report
+from je_auto_control.utils.generate_report.generate_html_report import generate_html
+from je_auto_control.utils.generate_report.generate_html_report import generate_html_report
+# utils image
+from je_auto_control.utils.image.screenshot import pil_screenshot
+# json
+from je_auto_control.utils.json.json_file import read_action_json
+from je_auto_control.utils.json.json_file import write_action_json
+from je_auto_control.utils.generate_report.generate_json_report import generate_json
+from je_auto_control.utils.generate_report.generate_json_report import generate_json_report
+# socket server
+from je_auto_control.utils.socket_server.auto_control_socket_server import start_autocontrol_socket_server
+# test record
+from je_auto_control.utils.test_record.record_test_class import test_record_instance
+# timeout
+from je_auto_control.utils.timeout.multiprocess_timeout import multiprocess_timeout
+# import image
+from je_auto_control.wrapper.auto_control_image import locate_all_image
+from je_auto_control.wrapper.auto_control_image import locate_and_click
+from je_auto_control.wrapper.auto_control_image import locate_image_center
+from je_auto_control.wrapper.auto_control_keyboard import check_key_is_press
+from je_auto_control.wrapper.auto_control_keyboard import hotkey
+# import keyboard
+from je_auto_control.wrapper.auto_control_keyboard import keys_table
+from je_auto_control.wrapper.auto_control_keyboard import press_key
+from je_auto_control.wrapper.auto_control_keyboard import release_key
+from je_auto_control.wrapper.auto_control_keyboard import type_key
+from je_auto_control.wrapper.auto_control_keyboard import write
+# import mouse
+from je_auto_control.wrapper.auto_control_mouse import click_mouse
+from je_auto_control.wrapper.auto_control_mouse import mouse_table
+from je_auto_control.wrapper.auto_control_mouse import position
+from je_auto_control.wrapper.auto_control_mouse import press_mouse
+from je_auto_control.wrapper.auto_control_mouse import release_mouse
+from je_auto_control.wrapper.auto_control_mouse import scroll
+from je_auto_control.wrapper.auto_control_mouse import set_position
+from je_auto_control.wrapper.auto_control_mouse import special_table
+# test_record
+from je_auto_control.wrapper.auto_control_record import record
+from je_auto_control.wrapper.auto_control_record import stop_record
+from je_auto_control.wrapper.auto_control_screen import screenshot
+# import screen
+from je_auto_control.wrapper.auto_control_screen import size
+# xml
+from je_auto_control.utils.generate_report.generate_xml_report import generate_xml
+from je_auto_control.utils.generate_report.generate_xml_report import generate_xml_report
+# callback
+from je_auto_control.utils.callback.callback_function_executor import callback_executor
+# package manager
+from je_auto_control.utils.package_manager.package_manager_class import package_manager
+
+__all__ = [
+    "click_mouse", "mouse_table", "position", "press_mouse", "release_mouse",
+    "scroll", "set_position", "special_table",
+    "keys_table", "press_key", "release_key", "type_key", "check_key_is_press",
+    "write", "hotkey",
+    "size", "screenshot",
+    "locate_all_image", "locate_image_center", "locate_and_click",
+    "CriticalExit",
+    "AutoControlException", "AutoControlKeyboardException",
+    "AutoControlMouseException", "AutoControlCantFindKeyException",
+    "AutoControlScreenException", "ImageNotFoundException",
+    "AutoControlJsonActionException", "AutoControlRecordException",
+    "AutoControlActionNullException", "AutoControlActionException",
+    "record", "stop_record",
+    "read_action_json", "write_action_json",
+    "execute_action", "execute_files", "executor", "add_command_to_executor",
+    "multiprocess_timeout", "test_record_instance",
+    "screenshot",
+    "pil_screenshot",
+    "generate_html", "generate_html_report",
+    "generate_json", "generate_json_report",
+    "generate_xml", "generate_xml_report",
+    "get_dir_files_as_list", "create_template_dir", "start_autocontrol_socket_server",
+    "callback_executor", "package_manager"
+]
