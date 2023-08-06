71cc05a89d73d9a30b277521e6ed469f2fe1c732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:51 +0000

    Add package UnleashClient/5.6.0/UnleashClient-5.6.0.tar.gz

---
+++ packages/UnleashClient/UnleashClient-5.6.0.tar.gz/UnleashClient-5.6.0/tests/unit_tests/strategies/test_defaultstrategy.py
@@ -0,0 +1,11 @@
+import pytest
+from UnleashClient.strategies import Default
+
+
+@pytest.fixture()
+def strategy():
+    yield Default()
+
+
+def test_defaultstrategy(strategy):
+    assert isinstance(strategy.execute(), bool)
