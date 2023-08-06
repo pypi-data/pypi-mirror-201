4e58620d7d1d9757ba79af16be496da2a65b094b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:30 +0000

    Add package Geode-SimplexRemesh/5.2.11/Geode_SimplexRemesh-5.2.11-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-5.2.11-cp310-cp310-win_amd64.whl/geode_simplexremesh/section.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+import geode_numerics
+
+from .geode_simplexremesh_py_section import *
+SimplexRemeshSection.initialize()
