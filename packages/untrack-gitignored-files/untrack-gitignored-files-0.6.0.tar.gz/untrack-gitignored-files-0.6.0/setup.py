4495ba137cdbea5a8f273d06db6f7167b97b6e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:27 +0000

    Add package untrack-gitignored-files/0.6.0/untrack-gitignored-files-0.6.0.tar.gz

---
+++ packages/untrack-gitignored-files/untrack-gitignored-files-0.6.0.tar.gz/untrack-gitignored-files-0.6.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="untrack-gitignored-files",
+    version='0.6.0',
+    description="A command-line tool to list and untrack gitignored files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Dmitry Mazin",
+    author_email="dm@cyberdemon.org",
+    url="https://github.com/dmazin/untrack-gitignored-files",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "untrack-gitignored-files=untrack_gitignored_files.core:main",
+        ],
+    },
+    install_requires=[
+        "gitpython",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
