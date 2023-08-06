acce198b676b2138f10e1179ba8ebab2608adfa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:15 +0000

    Add package lndb/0.40.0/lndb-0.40.0.tar.gz

---
+++ packages/lndb/lndb-0.40.0.tar.gz/lndb-0.40.0/lndb/_close.py
@@ -0,0 +1,15 @@
+import os
+
+from .dev._settings_store import current_instance_settings_file
+
+
+def close() -> None:
+    """Close existing instance.
+
+    Returns `None` if succeeds, otherwise an exception is raised.
+    """
+    try:
+        current_instance_settings_file().unlink()
+        os.environ["LAMINDB_INSTANCE_LOADED"] = "0"
+    except FileNotFoundError:
+        raise
