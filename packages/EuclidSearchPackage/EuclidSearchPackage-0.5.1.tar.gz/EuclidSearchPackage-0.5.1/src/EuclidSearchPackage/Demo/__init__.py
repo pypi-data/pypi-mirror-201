3b4c48e626e1bcff37a89c56ca75ad0e26757872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:58 +0000

    Add package EuclidSearchPackage/0.5.1/EuclidSearchPackage-0.5.1.tar.gz

---
+++ packages/EuclidSearchPackage/EuclidSearchPackage-0.5.1.tar.gz/EuclidSearchPackage-0.5.1/src/EuclidSearchPackage/Demo/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/3/28 16:35
+# @Author  : Euclid-Jie
+# @File    : __init__.py.py
+
+from .WeiboClassV2 import WeiboClassV2
+from .WeiboClassV3 import WeiboClassV3
