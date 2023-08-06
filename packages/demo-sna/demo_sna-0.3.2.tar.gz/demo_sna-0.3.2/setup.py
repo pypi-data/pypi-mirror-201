0817fdbd4252f058d61849649840440abac0a36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:10 +0000

    Add package demo-sna/0.3.2/demo_sna-0.3.2.tar.gz

---
+++ packages/demo-sna/demo_sna-0.3.2.tar.gz/demo_sna-0.3.2/setup.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="Alaa' Omar",
+    author_email='alaa.omer2009@gmail.com',
+    python_requires='>=3.10',
+    # classifiers=[
+    #     'Development Status :: 2 - Pre-Alpha',
+    #     'Intended Audience :: Developers',
+    #     'License :: OSI Approved :: MIT License',
+    #     'Natural Language :: English',
+    #     'Programming Language :: Python :: 3.10' ,
+    # ],
+    data_files=[('data', ['data/my_data.pickle'])],
+    CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: Arabic',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Text Processing',
+    'Topic :: Text Processing :: Linguistic',
+     ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    entry_points={
+        'console_scripts': [
+            'demo_sna=demo_sna.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='demo_sna',
+    name='demo_sna',
+    packages=find_packages(include=['demo_sna', 'demo_sna.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/eng-aomar/demo',
+    version='0.3.2',
+    zip_safe=False,    
+)
