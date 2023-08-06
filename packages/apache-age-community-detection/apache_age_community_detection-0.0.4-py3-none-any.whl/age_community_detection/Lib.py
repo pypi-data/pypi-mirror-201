f68115aa76ddf05008e8eb475af69a0dd8609dc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:56 +0000

    Add package apache-age-community-detection/0.0.4/apache_age_community_detection-0.0.4-py3-none-any.whl

---
+++ packages/apache-age-community-detection/apache_age_community_detection-0.0.4-py3-none-any.whl/age_community_detection/Lib.py
@@ -0,0 +1,35 @@
+import numpy as np
+import ctypes
+import os
+
+absolute_path = os.path.dirname(__file__)
+absolute_path += "/C_helper/library.so"
+
+# # Load the shared library
+lib = ctypes.CDLL(absolute_path)
+
+def get_community(nodes, edges):
+    # Create a 2D NumPy array
+    a = np.array(edges, dtype=np.int32)
+    nodeCount = len(nodes)
+
+    # Get the dimensions of the array
+    nrows, ncols = a.shape
+
+    # Create a 1D NumPy array to hold the output
+    b = np.zeros(nodeCount, dtype=np.int32)
+
+    # Pass the arrays to the C function
+    lib.get_community(
+        a.ctypes.data_as(ctypes.POINTER(ctypes.c_int)), # edge
+        ctypes.c_int(nrows), #edge_count
+        ctypes.c_int(nodeCount), #node_count
+        b.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+    )
+    b = [p for p in b]
+    return b
+
+
+
+
+
