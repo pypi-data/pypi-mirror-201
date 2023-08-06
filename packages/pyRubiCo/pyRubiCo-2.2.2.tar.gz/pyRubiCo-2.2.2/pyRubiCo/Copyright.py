db4e447bc761758908ddf7cdcb55178621ca1d19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:01:32 +0000

    Add package pyRubiCo/2.2.2/pyRubiCo-2.2.2.tar.gz

---
+++ packages/pyRubiCo/pyRubiCo-2.2.2.tar.gz/pyRubiCo-2.2.2/pyRubiCo/Copyright.py
@@ -0,0 +1,5 @@
+class copyright:
+	__Version__ = "0.0.1"
+	CopyRight = print(f"\n\npyRubiCo library version {__Version__} \n\n" + "pyRubiCo Copyright (C) 2022 Ali Eslami Team pyRubiCo\n\n\n\n")
+	print("........")
+	print(" ")
