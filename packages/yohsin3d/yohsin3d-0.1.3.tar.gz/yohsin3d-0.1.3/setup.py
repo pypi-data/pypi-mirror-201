687ac5ed935a2a7a44477b36d62f061df252658c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:12 +0000

    Add package yohsin3d/0.1.3/yohsin3d-0.1.3.tar.gz

---
+++ packages/yohsin3d/yohsin3d-0.1.3.tar.gz/yohsin3d-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yohsin3d',
+    version='0.1.3',
+    description='Program your own RoboCup 3D soccer playing agents in python',
+    author='Habib Shahzad, Abuzar Rasool, Faaz Abidi',
+    author_email='habibshehzad55@gmail.com, availabuzar@gmail.com, hasanfaaz10@gmail.com',
+    url='https://github.com/FC-Yohsin/yohisn3d',
+    packages=find_packages(),
+    install_requires=[],
+    python_requires='>=3.11',
+)
