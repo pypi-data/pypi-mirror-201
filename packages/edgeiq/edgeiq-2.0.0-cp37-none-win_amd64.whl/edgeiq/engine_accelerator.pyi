df836ac20c7f9abedf43d8a0fd5d07d17ff18b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:12 +0000

    Add package edgeiq/2.0.0/edgeiq-2.0.0-cp37-none-win_amd64.whl

---
+++ packages/edgeiq/edgeiq-2.0.0-cp37-none-win_amd64.whl/edgeiq/engine_accelerator.pyi
@@ -0,0 +1,19 @@
+from enum import Enum
+
+class Engine(str, Enum):
+    DNN: str
+    DNN_OPENVINO: str
+    DNN_CUDA: str
+    TENSOR_RT: str
+    HAILO_RT: str
+    QAIC_RT: str
+
+class Accelerator(str, Enum):
+    DEFAULT: str
+    CPU: str
+    GPU: str
+    MYRIAD: str
+    NVIDIA: str
+    NVIDIA_FP16: str
+    HAILO: str
+    QAIC: str
---
+++ packages/edgeiq/edgeiq-2.0.0-cp37-none-win_amd64.whl/edgeiq/processing/__init__.pyi
@@ -0,0 +1,7 @@
+from .image_classification import ClassificationProcessor as ClassificationProcessor
+from .instance_segmentation import InstanceSegmentationProcessor as InstanceSegmentationProcessor
+from .libhuman_pose import PoseEstimationProcessor as PoseEstimationProcessor
+from .object_detection import ObjectDetectionProcessor as ObjectDetectionProcessor
+from .processor import Processor as Processor, ProcessorT as ProcessorT
+from .semantic_segmentation import SemanticSegmentationProcessor as SemanticSegmentationProcessor
+from .trt_plugin import load_trt_plugin as load_trt_plugin
