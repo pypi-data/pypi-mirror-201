dbc7655e82f9af29afa143b284ae9bba2135acf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:59:28 +0000

    Add package ebs-linuxnode-core/3.1.3/ebs-linuxnode-core-3.1.3.tar.gz

---
+++ packages/ebs-linuxnode-core/ebs-linuxnode-core-3.1.3.tar.gz/ebs-linuxnode-core-3.1.3/ebs/linuxnode/core/shell.py
@@ -0,0 +1,21 @@
+
+
+from os import environ
+from twisted.internet.utils import getProcessOutput
+from .basemixin import BaseMixin
+from .config import ConfigMixin
+
+
+class BaseShellMixin(ConfigMixin, BaseMixin):
+    # def __init__(self, *args, **kwargs):
+    #     self._shell_processes = {}
+    #     super(BaseShellMixin, self).__init__(self, *args, **kwargs)
+
+    def _shell_execute(self, command, response_handler):
+        if len(command) > 1:
+            args = command[1:]
+        else:
+            args = []
+        d = getProcessOutput(command[0], args, env=environ)
+        d.addCallback(response_handler)
+        return d
