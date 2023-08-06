a4b6374848f2d4d0093cd00edd463cc13278e020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:36 +0000

    Add package easy-kline/0.1.1/easy_kline-0.1.1.tar.gz

---
+++ packages/easy-kline/easy_kline-0.1.1.tar.gz/easy_kline-0.1.1/tests/test.py
@@ -0,0 +1,15 @@
+import easy_klinee
+import pandas as pd
+
+
+def SMA(data: str, length: int, column: str):
+    return data[column].rolling(window=length).mean()
+
+kline = easy_klinee.bybit('BTCUSDT', '1m', '2023-04-6 14:01',auto_print = False )
+while True :
+    kline = easy_klinee.stream()    
+    
+    kline['sma'] = SMA(kline , 14 , 'Close')
+    print(kline)
+
+
