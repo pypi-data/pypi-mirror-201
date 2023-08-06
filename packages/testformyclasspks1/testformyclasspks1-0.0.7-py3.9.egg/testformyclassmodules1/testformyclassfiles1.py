4b1a8e6d843868a98baf0ecc26ce964003fdaa43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:48 +0000

    Add package testformyclasspks1/0.0.7/testformyclasspks1-0.0.7-py3.9.egg

---
+++ packages/testformyclasspks1/testformyclasspks1-0.0.7-py3.9.egg/testformyclassmodules1/testformyclassfiles1.py
@@ -0,0 +1,12 @@
+__author__='timenet2300'
+__author_email__='timenet2300@gmail.com'
+
+#Define your code here.
+class myclass1:
+    def myfun1(self,x):
+        return(x**2)
+class myclass2:
+    def myfun2(self,x):
+        return(x**3)
+# myo=myclass10()
+# myo.myfun1a(3)   
