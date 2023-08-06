1b635ca289c56193707e60c682c63bb030338470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:23 +0000

    Add package spacysee/0.0.4/spacysee-0.0.4.tar.gz

---
+++ packages/spacysee/spacysee-0.0.4.tar.gz/spacysee-0.0.4/README.md
@@ -0,0 +1,28 @@
+# SpacySee
+
+A project that helps you visualize your Spacy docs in Jupyter notebooks
+
+## Installation
+
+```bash
+pip install spacysee
+```
+
+## Usage
+
+```python
+from spacysee import render
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("This is a neat way to visualize your Spacy docs")
+
+render(doc, width="500", height="500")
+```
+
+## Example
+
+![Example](https://raw.githubusercontent.com/moxley01/spacysee/master/screenshot.png)
+
+## Notes
+
+Each of the dependency tags, POS tags and morphological features are clickable. Clicking on a tag will bring up the relevant documentation for that tag.
---
+++ packages/spacysee/spacysee-0.0.4.tar.gz/spacysee-0.0.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spacysee",
+    version="0.0.4",
+    author="Matthew Oxley",
+    description="Visualizer for spaCy NER models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=["client/build"],
+    package_data={'client/build': ['*.html']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["spacysee"],
+    package_dir={'':'src/spacysee'},
+    install_requires=[],
+)
