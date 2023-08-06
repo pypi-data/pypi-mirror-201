ac72ec614e601e8f9bb456b7434cebf6a3d3e17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:50 +0000

    Add package pyGuardPoint/0.7.2/pyGuardPoint-0.7.2.tar.gz

---
+++ packages/pyGuardPoint/pyGuardPoint-0.7.2.tar.gz/pyGuardPoint-0.7.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pyGuardPoint",
+      version="0.7.2",
+      description="Easy-to-use Python module implementing GuardPoint's WebAPI",
+      maintainer_email="sales@sensoraccess.co.uk",
+      install_requires=['validators', 'fuzzywuzzy', 'Levenshtein'],
+      packages=['pyGuardPoint'],
+      license_files=('LICENSE.txt',),
+      zip_safe=False)
