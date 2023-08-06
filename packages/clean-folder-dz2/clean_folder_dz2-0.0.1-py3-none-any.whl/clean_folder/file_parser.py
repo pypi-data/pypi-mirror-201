b4d675e4168c2ace9213c04fad431d15e5e0d518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:44 +0000

    Add package clean-folder-dz2/0.0.1/clean_folder_dz2-0.0.1-py3-none-any.whl

---
+++ packages/clean-folder-dz2/clean_folder_dz2-0.0.1-py3-none-any.whl/clean_folder/file_parser.py
@@ -0,0 +1,85 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+MY_OTHER = []
+ZIP_ARCHIVES = []
+GZ_ARCHIVES = []
+TAR_ARCHIVES = []
+AVI_VIDEO = []
+MOV_VIDEO = []
+MP4_VIDEO = []
+MKV_VIDEO = []
+DOC_DOCUMENT = []
+DOCX_DOCUMENT = []
+TXT_DOCUMENT = []
+PDF_DOCUMENT = []
+XLSX_DOCUMENT = []
+PPTX_DOCUMENT = []
+
+REGISTER_EXTENSIONS = {
+    'JPEG': JPEG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'SVG': SVG_IMAGES,
+    'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+    'ZIP': ZIP_ARCHIVES,
+    'GZ': GZ_ARCHIVES,
+    'TAR': TAR_ARCHIVES,
+    'AVI': AVI_VIDEO,
+    'MP4': MP4_VIDEO,
+    'MOV': MOV_VIDEO,
+    'MKV': MKV_VIDEO,
+    'DOC': DOC_DOCUMENT,
+    'DOCX': DOCX_DOCUMENT,
+    'TXT': TXT_DOCUMENT,
+    'PDF': PDF_DOCUMENT,
+    'XLSX': XLSX_DOCUMENT,
+    'PPTX': PPTX_DOCUMENT
+}
+
+FOLDERS = []
+EXTENSIONS = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        if item.is_dir():
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                scan(item)
+            continue
+
+        ext = get_extension(item.name)
+        fullname = folder / item.name
+        if not ext:
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSIONS[ext]
+                EXTENSIONS.add(ext)
+                container.append(fullname)
+            except KeyError:
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+
+
+if __name__ == '__main__':
+    folder_for_scan = sys.argv[1]
+    print(f'Start in folder {folder_for_scan}')
+    scan(Path(folder_for_scan))
