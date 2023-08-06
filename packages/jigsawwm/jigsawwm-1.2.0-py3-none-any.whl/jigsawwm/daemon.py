579b5de4defd6b376c194e227bb1748df5b39515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:52 +0000

    Add package jigsawwm/1.2.0/jigsawwm-1.2.0-py3-none-any.whl

---
+++ packages/jigsawwm/jigsawwm-1.2.0-py3-none-any.whl/jigsawwm/daemon.py
@@ -0,0 +1,230 @@
+import sys
+
+sys.coinit_flags = 0x0
+import io
+import os.path
+import time
+from ctypes.wintypes import DWORD, HWND, LONG
+from datetime import datetime
+from threading import Thread
+from tkinter import messagebox
+from traceback import print_exception
+from typing import Callable, Dict, List, Optional, Sequence, Union
+
+import pystray
+from PIL import Image
+
+from jigsawwm.hotkey import hotkey, keyboard_event_handler
+from jigsawwm.manager import WindowManager
+from jigsawwm.services import get_services
+from jigsawwm.smartstart import get_smartstarts
+from jigsawwm.w32.hook import Hook
+from jigsawwm.w32.vk import Vk
+from jigsawwm.w32.window import Window, is_app_window, is_window
+from jigsawwm.w32.winevent import WinEvent
+
+
+class Daemon:
+    """JigsawWM Daemon serivce, you must inherite this class and override the `setup` function
+    to configurate the Manager.
+    """
+
+    _hook: Hook
+    _timers: Dict[Callable, Thread] = {}
+    _timer_thread: Thread
+    _trayicon: pystray.Icon
+    _started: bool = False
+    _wm: WindowManager
+    menu_items: Sequence[pystray.MenuItem] = []
+
+    def __init__(self):
+        self._hook = Hook()
+        self._timers = {}
+        self._timer_thread = None
+        self._trayicon = None
+
+    def _error_handler(self, e: Exception):
+        file = io.StringIO()
+        # print_exception(e, e, None, file=file)
+        print_exception(*sys.exc_info(), file=file)
+        text = file.getvalue()
+        print(text, file=sys.stderr)
+        messagebox.showerror("JigsawWM", text)
+
+    def hotkey(
+        self,
+        combkeys: Union[Sequence[Vk], str],
+        target: Union[Callable, str],
+        swallow: bool = True,
+    ):
+        """Register a global hotkey
+
+        :param Union[Sequence[Vk], str] combkeys: key combination, i.e. ``[Vk.WIN, Vk.J]`` or ``Win + j``
+        :param Union[Callback, str] target: can be a callback function or another key combination
+        :param bool swallow: stop event propagation to prevent combkeys being processed by other programs
+        """
+        try:
+            hotkey(combkeys, target, swallow, self._error_handler)
+        except Exception as e:
+            self._error_handler(e)
+
+    def start_hooks(self):
+        """Start all hooks"""
+        self._hook.install_keyboard_hook(keyboard_event_handler)
+        self._hook.install_winevent_hook(
+            self._winevent_callback,
+            WinEvent.EVENT_OBJECT_SHOW,
+            WinEvent.EVENT_OBJECT_HIDE,
+        )
+        self._hook.install_winevent_hook(
+            self._winevent_callback,
+            WinEvent.EVENT_OBJECT_CLOAKED,
+            WinEvent.EVENT_OBJECT_UNCLOAKED,
+        )
+        self._hook.install_winevent_hook(
+            self._winevent_callback,
+            WinEvent.EVENT_SYSTEM_MINIMIZESTART,
+            WinEvent.EVENT_SYSTEM_MINIMIZEEND,
+        )
+        self._hook.install_winevent_hook(
+            self._winevent_callback, WinEvent.EVENT_SYSTEM_MOVESIZEEND
+        )
+        self._hook.start()
+
+    def _winevent_callback(
+        self,
+        event: WinEvent,
+        hwnd: HWND,
+        id_obj: LONG,
+        id_chd: LONG,
+        id_evt_thread: DWORD,
+        evt_time: DWORD,
+    ):
+        if (
+            id_obj
+            or id_chd
+            or not is_window(hwnd)
+            or not is_app_window(hwnd)
+            or self._wm.is_ignored(Window(hwnd))
+        ):
+            return
+        print(
+            "[{now}] {event:30s} {hwnd:8d} {title}".format(
+                now=datetime.now().strftime("%M:%S.%f"),
+                event=event.name,
+                hwnd=hwnd,
+                #  ido: {id_obj:6d} idc: {id_chd:6d}
+                # id_obj=id_obj,
+                # id_chd=id_chd,
+                title=Window(hwnd).title,
+            )
+        )
+        self._wm.sync(restrict=event == WinEvent.EVENT_SYSTEM_MOVESIZEEND)
+
+    def stop_hooks(self):
+        """Stop all hooks"""
+        self._hook.stop()
+
+    def timer(self, interval: float, callback: Callable, once: Optional[bool] = False):
+        """Run callback function with a fixed time interval repeatedly
+
+        :param float interval: interval between calls
+        :param Callable callback: function to be called
+        :param bool once: function would be called only once
+        """
+
+        # wrap func with in try-catch for safty
+        def run():
+            # global _timers
+            while callback in self._timers:
+                time.sleep(interval)
+                try:
+                    callback()
+                except Exception as e:
+                    self._error_handler(e)
+                if once:
+                    del self._timers[callback]
+
+        self._timers[callback] = Thread(target=run)
+
+    def start_timers(self):
+        """Start all timers"""
+        for timer_thread in self._timers.values():
+            timer_thread.start()
+
+    def stop_timers(self):
+        """Stop all timers"""
+        self._timers.clear()
+
+    def start_trayicon(self):
+        """Start trayicon"""
+        script_dir = os.path.dirname(__file__)
+        icon_path = os.path.join(script_dir, "assets", "logo.png")
+        icon = Image.open(icon_path)
+
+        def dynamic_menu() -> List[pystray.MenuItem]:
+            service_menu_items = [
+                pystray.MenuItem(
+                    f"[{service.status_text}] {service.name}",
+                    service.toggle,
+                )
+                for service in get_services()
+            ]
+            smartstart_menu_items = [
+                pystray.MenuItem(
+                    f"{smartstart.name}",
+                    smartstart.run_anyway,
+                )
+                for smartstart in get_smartstarts()
+            ]
+            return [
+                *self.menu_items,
+                *smartstart_menu_items,
+                pystray.Menu.SEPARATOR,
+                *service_menu_items,
+                pystray.Menu.SEPARATOR,
+                pystray.MenuItem("Exit", self.stop),
+            ]
+
+        tray_icon = pystray.Icon(
+            "JigsawWM",
+            icon=icon,
+            menu=pystray.Menu(dynamic_menu),
+        )
+        self._trayicon = tray_icon
+        self._trayicon.run_detached()
+
+    def stop_trayicon(self):
+        """Stop trayicon"""
+        self._trayicon.stop()
+        self._trayicon = None
+
+    def setup(self):
+        """To be overrided by the users to configure the Window Manager"""
+        pass
+
+    def start(self):
+        """Start daemon service"""
+        try:
+            self._wm = self.setup()
+        except Exception as e:
+            self._error_handler(e)
+            sys.exit(1)
+            return
+        self._started = True
+        self.start_trayicon()
+        self.start_timers()
+        self.start_hooks()
+        while self._started:
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                self.stop()
+                break
+
+    def stop(self):
+        """Stop daemon service"""
+        self.stop_hooks()
+        self.stop_timers()
+        self.stop_trayicon()
+        self._started = False
---
+++ packages/jigsawwm/jigsawwm-1.2.0-py3-none-any.whl/jigsawwm/w32/hook.py
@@ -0,0 +1,364 @@
+import enum
+import threading
+import time
+from ctypes import *
+from ctypes.wintypes import *
+from functools import partial
+from typing import Callable, Optional, Tuple
+
+from .winevent import HWINEVENTHOOK, WINEVENTHOOKPROC, WinEvent
+
+user32 = WinDLL("user32", use_last_error=True)
+
+HC_ACTION = 0
+# incoming message
+WM_QUIT = 0x0012
+
+
+# types for the hook including input parameter and return result
+
+ULONG_PTR = WPARAM
+LRESULT = LPARAM
+# LPMSG = POINTER(MSG)
+
+# Hook
+HOOKPROC = WINFUNCTYPE(LRESULT, c_int, WPARAM, LPARAM)
+
+
+# setup api
+
+
+def _errcheck_bool(result, func, args):
+    if not result:
+        raise WinError(get_last_error())
+    return args
+
+
+user32.SetWindowsHookExW.errcheck = _errcheck_bool
+user32.SetWindowsHookExW.restype = HHOOK
+user32.SetWindowsHookExW.argtypes = (
+    c_int,  # _In_ idHook
+    HOOKPROC,  # _In_ lpfn
+    HINSTANCE,  # _In_ hMod
+    DWORD,
+)  # _In_ dwThreadId
+
+user32.CallNextHookEx.restype = LRESULT
+user32.CallNextHookEx.argtypes = (
+    HHOOK,  # _In_opt_ hhk
+    c_int,  # _In_     nCode
+    WPARAM,  # _In_     wParam
+    LPARAM,
+)  # _In_     lParam
+
+user32.GetMessageW.argtypes = (
+    LPMSG,  # _Out_    lpMsg
+    HWND,  # _In_opt_ hWnd
+    UINT,  # _In_     wMsgFilterMin
+    UINT,
+)  # _In_     wMsgFilterMax
+
+user32.TranslateMessage.argtypes = (LPMSG,)
+user32.DispatchMessageW.argtypes = (LPMSG,)
+
+user32.SetWinEventHook.restype = HWINEVENTHOOK
+
+# keyboard hook definition
+
+
+class KBDLLHOOKMSGID(enum.IntEnum):
+    """Keyboard event msgid"""
+
+    WM_KEYDOWN = 0x0100
+    WM_KEYUP = 0x0101
+    WM_SYSKEYDOWN = 0x0104
+    WM_SYSKEYUP = 0x0105
+
+
+class KBDLLHOOKDATA(Structure):
+    """Keyboard event data"""
+
+    vkCode: int
+    scanCode: int
+    flags: int
+    time: int
+    dwExtraInfo: int
+
+    _fields_ = (
+        ("vkCode", DWORD),
+        ("scanCode", DWORD),
+        ("flags", DWORD),
+        ("time", DWORD),
+        ("dwExtraInfo", ULONG_PTR),
+    )
+
+
+# mouse hook definition
+
+
+class MSLLHOOKMSGID(enum.IntEnum):
+    """Mouse event msgid"""
+
+    WM_MOUSEMOVE = 0x0200
+    WM_LBUTTONDOWN = 0x0201
+    WM_LBUTTONUP = 0x0202
+    WM_RBUTTONDOWN = 0x0204
+    WM_RBUTTONUP = 0x0205
+    WM_MBUTTONDOWN = 0x0207
+    WM_MBUTTONUP = 0x0208
+    WM_MOUSEWHEEL = 0x020A
+    WM_MOUSEHWHEEL = 0x020E
+
+
+class MSLLHOOKDATA(Structure):
+    """Mouse event data"""
+
+    pt: POINT
+    mouseData: int
+    flags: int
+    time: int
+    dwExtraInfo: int
+
+    _fields_ = (
+        ("pt", POINT),
+        ("mouseData", DWORD),
+        ("flags", DWORD),
+        ("time", DWORD),
+        ("dwExtraInfo", ULONG_PTR),
+    )
+
+
+# wrap them all inside Hook class
+
+
+def keyboard(msgid: KBDLLHOOKMSGID, msg: KBDLLHOOKDATA) -> bool:
+    print(
+        "{:15s} {:15s}: vkCode {:3x} scanCode {:3x} flags: {:3d}, time: {} extra: {}".format(
+            Vk(msg.vkCode).name,
+            msgid.name,
+            msg.vkCode,
+            msg.scanCode,
+            msg.flags,
+            msg.time,
+            msg.dwExtraInfo,
+        )
+    )
+
+
+class Hook(threading.Thread):
+    """Hook low level keyboard/mouse event
+
+    Usage:
+
+    .. code-block:: python
+
+        def swallow_keyboard_a_key_event(msgid: KBDLLHOOKMSGID, data: KBDLLHOOKDATA) -> bool:
+            return data.vkCode == VirtualKey.VK_A:
+
+        def swallow_mouse_middle_btn_event(msgid: MSLLHOOKMSGID, data: MSLLHOOKDATA) -> bool:
+            return msgid == MSLLHOOKMSGID.WM_MBUTTONUP or msgid == MSLLHOOKMSGID.WM_MBUTTONDOWN:
+
+        def winevent(
+            event: WinEvent,
+            hwnd: HWND,
+            id_obj: LONG,
+            id_chd: LONG,
+            id_evt_thread: DWORD,
+            time: DWORD,
+        ):
+            pass
+
+
+        hook = Hook()
+        kbd_hook_id = hook.install_keyboard_hook(swallow_keyboard_a_key_event)
+        mouse_hook_id = hook.install_mouse_hook(swallow_mouse_middle_btn_event)
+        winevent_hook_id = hook.install_winevent_hook()
+        hook.start()
+
+    """
+
+    def __init__(self):
+        self._installed_hooks = {}
+        self._queue = []
+        super().__init__()
+
+    def _install_hook(
+        self, hook_id, wparam_type, lparam_type, handler
+    ) -> Tuple[HANDLE, Callable]:
+        # for the hooks to work, note that only low level keyboard/mouse work this way
+        # while others require DLL injection
+        @HOOKPROC
+        def proc(nCode, wParam, lParam):
+            if nCode == HC_ACTION:
+                wparam = wparam_type(wParam)
+                lparam = cast(lParam, POINTER(lparam_type))[0]
+                if handler(wparam, lparam):
+                    return 1
+            return user32.CallNextHookEx(None, nCode, wParam, lParam)
+
+        hook = user32.SetWindowsHookExW(hook_id, proc, None, 0)
+        return hook, proc
+
+    def _install_winevent_hook(
+        self,
+        event_min: WinEvent,
+        event_max: WinEvent,
+        cb: Callable[[WinEvent, HWND, LONG, LONG, DWORD, DWORD], None],
+    ) -> Tuple[HANDLE, Callable]:
+        @WINEVENTHOOKPROC
+        def proc(hhook, event, hwnd, id_obj, id_chd, id_evt_thread, dwms_evt_time):
+            cb(WinEvent(event), hwnd, id_obj, id_chd, id_evt_thread, dwms_evt_time)
+
+        hook = user32.SetWinEventHook(
+            DWORD(event_min.value),
+            DWORD(event_max.value),
+            HMODULE(None),
+            proc,
+            0,
+            0,
+            0,  # WINEVENT_OUTOFCONTEXT
+        )
+        return hook, proc
+
+    def install_keyboard_hook(
+        self, callback: Callable[[KBDLLHOOKMSGID, KBDLLHOOKDATA], bool]
+    ):
+        """Install keyboard hook
+
+        :param callback: function to be called when key press/release, return ``True`` to stop
+            propagation
+        """
+        self._queue.append(
+            partial(self._install_hook, 13, KBDLLHOOKMSGID, KBDLLHOOKDATA, callback)
+        )
+
+    def install_mouse_hook(
+        self, callback: Callable[[MSLLHOOKMSGID, MSLLHOOKDATA], bool]
+    ):
+        """Install mouse hook
+
+        :param callback: function to be called when mouse moved, bth pressed/release and scroll,
+ return ``True`` to stop
+            propagation
+        """
+        self._queue.append(
+            partial(self._install_hook, 14, MSLLHOOKMSGID, MSLLHOOKDATA, callback)
+        )
+
+    def install_winevent_hook(
+        self,
+        callback: Callable[[WinEvent, HWND, LONG, LONG, DWORD, DWORD], None],
+        event_min: WinEvent,
+        event_max: Optional[WinEvent] = None,
+    ):
+        """Install winevent hook
+
+        :param callback: function to be called when Windows event occurred
+        """
+        self._queue.append(
+            partial(
+                self._install_winevent_hook, event_min, event_max or event_min, callback
+            )
+        )
+
+    def run(self):
+        """Start hooking, all installed hooks would not actually work until
+        this method invoked. You may install new hooks thereafter"""
+        # IMPORTANT: the hook must be installed in the thread!
+        msg = MSG()
+        while True:
+            # install pending hooks
+            while self._queue:
+                install_hook = self._queue.pop()
+                hook, proc = install_hook()
+                self._installed_hooks[hook] = proc
+
+            bRet = user32.GetMessageW(byref(msg), None, 0, 0)
+            if not bRet:
+                break
+            if bRet == -1:
+                raise WinError(get_last_error())
+            user32.TranslateMessage(byref(msg))
+            user32.DispatchMessageW(byref(msg))
+
+    def stop(self):
+        """Stop all hooks"""
+        user32.PostThreadMessageW(self.ident, WM_QUIT, 0, 0)
+
+
+if __name__ == "__main__":
+    from datetime import datetime
+
+    from . import window
+    from .vk import Vk
+
+    def keyboard(msgid: KBDLLHOOKMSGID, msg: KBDLLHOOKDATA) -> bool:
+        print(
+            "{:15s} {:15s}: vkCode {:3x} scanCode {:3x} flags: {:3d}, time: {} extra: {}".format(
+                Vk(msg.vkCode).name,
+                msgid.name,
+                msg.vkCode,
+                msg.scanCode,
+                msg.flags,
+                msg.time,
+                msg.dwExtraInfo,
+            )
+        )
+
+    def mouse(msgid: MSLLHOOKMSGID, msg: MSLLHOOKDATA) -> bool:
+        msg = (
+            (msg.pt.x, msg.pt.y),
+            msg.mouseData,
+            msg.flags,
+            msg.time,
+            msg.dwExtraInfo,
+        )
+        print("{:15s}: {}".format(msgid.name, msg))
+
+    def winevent(
+        event: WinEvent,
+        hwnd: HWND,
+        id_obj: LONG,
+        id_chd: LONG,
+        id_evt_thread: DWORD,
+        time: DWORD,
+    ):
+        print("==================================")
+        # print(
+        #     hwnd is None,
+        #     id_obj is None,
+        #     id_chd is None,
+        #     window.Window(hwnd).title is None,
+        # )
+        print(
+            "[{now}] {event:30s} {hwnd:8d} ido: {id_obj:6d} idc: {id_chd:6d} {title}".format(
+                now=datetime.now().strftime("%M:%S.%f"),
+                event=event.name,
+                hwnd=hwnd or 0,
+                id_obj=id_obj,
+                id_chd=id_chd,
+                title=window.Window(hwnd).title,
+            )
+        )
+        window.inspect_window(hwnd)
+        print("==================================")
+
+    hook = Hook()
+    # hook.install_winevent_hook(winevent, WinEvent.EVENT_SYSTEM_MOVESIZEEND)
+    # hook.install_winevent_hook(winevent, WinEvent.EVENT_SYSTEM_MINIMIZESTART, WinEvent.EVENT_SYSTEM_MINIMIZEEND)
+    # hook.install_winevent_hook(
+    #     winevent, WinEvent.EVENT_OBJECT_SHOW, WinEvent.EVENT_OBJECT_HIDE
+    # )
+    # hook.install_winevent_hook(
+    #     winevent, WinEvent.EVENT_OBJECT_CREATE, WinEvent.EVENT_OBJECT_DESTROY
+    # )
+    hook.install_winevent_hook(winevent, WinEvent.EVENT_MIN, WinEvent.EVENT_MAX)
+    # hook.install_keyboard_hook(keyboard)
+    hook.start()
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            hook.stop()
+            break
