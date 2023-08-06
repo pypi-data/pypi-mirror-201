3a6ec3e624345951c6ae812cea194b0b8d7e340d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:46 +0000

    Add package Geode-Conversion/4.4.7/Geode_Conversion-4.4.7-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/Geode-Conversion/Geode_Conversion-4.4.7-cp310-cp310-manylinux2014_x86_64.whl/geode_conversion/model.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+
+from .geode_conversion_py_model import *
+ConversionModel.initialize()
