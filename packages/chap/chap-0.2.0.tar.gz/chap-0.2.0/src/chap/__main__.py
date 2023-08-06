c197b3db08625fa19e15755f0615376520343f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:22 +0000

    Add package chap/0.2.0/chap-0.2.0.tar.gz

---
+++ packages/chap/chap-0.2.0.tar.gz/chap-0.2.0/src/chap/__main__.py
@@ -0,0 +1,33 @@
+# SPDX-FileCopyrightText: 2023 Jeff Epler <jepler@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+import importlib
+import pkgutil
+
+import click
+
+from . import commands  # pylint: disable=no-name-in-module
+
+
+class MyCLI(click.MultiCommand):
+    def list_commands(self, ctx):
+        rv = []
+        for pi in pkgutil.walk_packages(commands.__path__):
+            name = pi.name
+            if not name.startswith("__"):
+                rv.append(name)
+        rv.sort()
+        return rv
+
+    def get_command(self, ctx, cmd_name):
+        try:
+            return importlib.import_module("." + cmd_name, commands.__name__).main
+        except ModuleNotFoundError as exc:
+            raise click.UsageError(f"Invalid subcommand {cmd_name!r}", ctx) from exc
+
+
+main = MyCLI(help="Commandline interface to ChatGPT")
+
+if __name__ == "__main__":
+    main()
