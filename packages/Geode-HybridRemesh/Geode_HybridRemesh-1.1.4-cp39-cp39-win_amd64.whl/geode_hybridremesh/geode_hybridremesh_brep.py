1e60c0f85e80ec569c4913481d9bd3a2c380c0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:53 +0000

    Add package Geode-HybridRemesh/1.1.4/Geode_HybridRemesh-1.1.4-cp39-cp39-win_amd64.whl

---
+++ packages/Geode-HybridRemesh/Geode_HybridRemesh-1.1.4-cp39-cp39-win_amd64.whl/geode_hybridremesh/geode_hybridremesh_brep.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core
+
+from geode_hybridremesh_py_brep import *
+HybridRemeshBRep.initialize()
