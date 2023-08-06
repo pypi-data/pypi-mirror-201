0f32d57fb8f9aaeef56598cfc9fbcad235c87b01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:12 +0000

    Add package piplibaryscrape/1.0.0/piplibaryscrape-1.0.0.tar.gz

---
+++ packages/piplibaryscrape/piplibaryscrape-1.0.0.tar.gz/piplibaryscrape-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+DESCRIPTION = "Package"
+LONG_DESCRIPTION = "Package"
+
+# Setting up
+setup(
+    name="piplibaryscrape",
+    version=VERSION,
+    author="NHJonas",
+    author_email="NHJonas@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
