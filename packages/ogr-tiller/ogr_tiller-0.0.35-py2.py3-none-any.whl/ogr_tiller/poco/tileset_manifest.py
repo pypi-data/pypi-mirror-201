b5ed100d7753a1218de90c586fdcd797b5a163b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:26 +0000

    Add package ogr-tiller/0.0.35/ogr_tiller-0.0.35-py2.py3-none-any.whl

---
+++ packages/ogr-tiller/ogr_tiller-0.0.35-py2.py3-none-any.whl/ogr_tiller/poco/tileset_manifest.py
@@ -0,0 +1,14 @@
+class TilesetManifest:
+    def __init__(self,
+                 name: str,
+                 minzoom: int,
+                 maxzoom: int,
+                 attribution: str,
+                 tile_buffer: int,
+                 simplify_tolerance: float):
+        self.name = name
+        self.minzoom = minzoom
+        self.maxzoom = maxzoom
+        self.attribution = attribution
+        self.tile_buffer = tile_buffer
+        self.simplify_tolerance = simplify_tolerance
---
+++ packages/ogr-tiller/ogr_tiller-0.0.35-py2.py3-none-any.whl/ogr_tiller/utils/tile_utils.py
@@ -0,0 +1,30 @@
+from typing import Any, List, Tuple
+import mapbox_vector_tile
+from ogr_tiller.utils.fast_api_utils import abort_after
+from ogr_tiller.utils.proj_utils import get_bbox_for_crs
+import morecantile
+
+tms = morecantile.tms.get("WebMercatorQuad")
+
+
+def get_tile_no_abort(layer_features: Tuple[str, List[Any]], x: int, y: int, z: int, srid: str):
+    bbox_bounds = tms.xy_bounds(morecantile.Tile(x, y, z))
+    bbox = (bbox_bounds.left, bbox_bounds.bottom, bbox_bounds.right, bbox_bounds.top)
+
+    if srid != 'EPSG:3857':
+        bbox = get_bbox_for_crs("EPSG:3857", srid, bbox)
+
+    result = b''
+    for layer_name, features in layer_features:
+        tile = mapbox_vector_tile.encode([
+            {
+                "name": layer_name,
+                "features": features
+            }
+        ], default_options={'quantize_bounds': bbox})
+        result += tile
+    return result
+
+@abort_after(3)
+def get_tile(layer_features: Tuple[str, List[Any]], x: int, y: int, z: int, srid: str):
+    return get_tile_no_abort(layer_features, x, y, z, srid)
