024fdb9a04b5db3eab0c2caa033ea4ca4d207fd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:43 +0000

    Add package sqlsy/0.0.3/sqlsy-0.0.3.tar.gz

---
+++ packages/sqlsy/sqlsy-0.0.3.tar.gz/sqlsy-0.0.3/src/sqlsy/utils/custom_callbacks.py
@@ -0,0 +1,37 @@
+import random
+from sqlsy.engine import Engine
+
+
+random.seed()
+
+
+# choose from sequence at random
+def random_choice(sequence: list):
+	return random.choice(sequence)
+
+
+# make multiple calls independent
+def sequential_choice():
+	state ={}
+	def internal(sequence:list):
+		nonlocal state
+		col = Engine.get_col()
+		state.setdefault(col, 0)
+		state[col] += 1
+		return sequence[state[col]-1]
+	return internal
+
+
+# to make calls to sequence
+# independent we need shared count {}
+def sequence():
+	state = {}
+	# generate sequence of numbers
+	def internal(n1:int, n2:int):
+		nonlocal state
+		col = Engine.get_col()   # get current column being filled
+		state.setdefault(col, n1)
+
+		state[col] += 1
+		return state[col]-1
+	return internal
