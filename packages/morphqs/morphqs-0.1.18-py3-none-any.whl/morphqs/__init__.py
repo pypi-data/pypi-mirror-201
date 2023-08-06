a994e6654751e80e715ee73f7f1e78da617cd725
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:50 +0000

    Add package morphqs/0.1.18/morphqs-0.1.18-py3-none-any.whl

---
+++ packages/morphqs/morphqs-0.1.18-py3-none-any.whl/morphqs/__init__.py
@@ -0,0 +1,3 @@
+from morphqs.components.integrations import (add_integration, add_integration_ansible)
+from morphqs.components.uchigheredmsp import (deploy_higher_ed_msp)
+from morphqs.components.usecasedeployment import (deploy_usecase_tools)
