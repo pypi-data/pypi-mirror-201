f408a9a12f22c6f120bb0176c27b43ae529e7c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:07 +0000

    Add package chatarena/0.1.6/chatarena-0.1.6.tar.gz

---
+++ packages/chatarena/chatarena-0.1.6.tar.gz/chatarena-0.1.6/chatarena/environments/__init__.py
@@ -0,0 +1,27 @@
+from .base import Environment, TimeStep
+from .conversation import Conversation, ModeratedConversation
+from .chameleon import Chameleon
+from .pettingzoo_chess import ChessEnvironment
+
+from ..config import EnvironmentConfig
+
+ALL_ENVIRONMENTS = [
+    Conversation,
+    ModeratedConversation,
+    Chameleon,
+    ChessEnvironment,
+]
+
+ENV_REGISTRY = {env.type_name: env for env in ALL_ENVIRONMENTS}
+
+
+# Load an environment from a config dictionary
+def load_environment(config: EnvironmentConfig):
+    try:
+        env_cls = ENV_REGISTRY[config["env_type"]]
+    except KeyError:
+        raise ValueError(f"Unknown environment type: {config['env_type']}")
+
+    print(config)
+    env = env_cls.from_config(config)
+    return env
