b5e18ad55e4ae5d58591b016a93aa3cd85c21bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:29 +0000

    Add package cofi/0.1.3.dev2/cofi-0.1.3.dev2-py3-none-any.whl

---
+++ packages/cofi/cofi-0.1.3.dev2-py3-none-any.whl/cofi/utils/__init__.py
@@ -0,0 +1,13 @@
+"""Utility classes and functions (e.g. to generate regularization terms and more)
+"""
+
+from ._regularization import (
+    BaseRegularization,
+    QuadraticReg,
+)
+
+
+__all__ = [
+    "BaseRegularization",
+    "QuadraticReg",
+]
