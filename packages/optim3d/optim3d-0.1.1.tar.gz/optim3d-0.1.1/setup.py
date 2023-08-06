ffc8942ba6a2e6e3455d7780b4915a6fca219e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:18 +0000

    Add package optim3d/0.1.1/optim3d-0.1.1.tar.gz

---
+++ packages/optim3d/optim3d-0.1.1.tar.gz/optim3d-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="optim3d",
+    version='0.1.1',
+    description="CLI application for efficient and optimized reconstruction of large-scale 3D building models",
+    author = 'Anass Yarroudh',
+    author_email = 'ayarroudh@uliege.be',
+    url = 'https://github.com/Yarroudh/Optim3D',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "optim3d=optim3d.main:cli"
+        ]
+    }
+)
