7824136cfe64008ed0af915a698bb5966f5f6997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:41 +0000

    Add package formant/1.118.69/formant-1.118.69.tar.gz

---
+++ packages/formant/formant-1.118.69.tar.gz/formant-1.118.69/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationFieldType, TagParameters, SheetParameters, AnnotationType, AnnotationTags, UserParametersRolesItem, AnnotationFieldValuesRequest, AnnotationTemplateListResponse, AnnotationField, UserParameters, AnnotationTemplate, AnnotationFieldValuesRequestTags, AnnotationTemplateTags, AnnotationFieldValue, AnnotationStreamType, Annotation, AnnotationFieldValuesResponse
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
