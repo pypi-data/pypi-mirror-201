d10e261869b2591cfd8f8ce09eaeea874eadbeaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:05 +0000

    Add package foggynight/2.0.0/foggynight-2.0.0.tar.gz

---
+++ packages/foggynight/foggynight-2.0.0.tar.gz/foggynight-2.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='foggynight',
+    packages=['foggynight'],
+    version='2.0.0',
+    description='Weather forecast data',
+    author='Justin Bartoldus',
+    author_email='jbxbox117@Hotmail.com',
+    keywords=['weather', 'forecast', 'openweather'],
+    install_requires=[
+        'requests',
+        ]
+)
