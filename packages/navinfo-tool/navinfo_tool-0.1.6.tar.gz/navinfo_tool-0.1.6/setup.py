07b08ee22177a93462ab427ea492843ff0ef36c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:40 +0000

    Add package navinfo-tool/0.1.6/navinfo_tool-0.1.6.tar.gz

---
+++ packages/navinfo-tool/navinfo_tool-0.1.6.tar.gz/navinfo_tool-0.1.6/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='navinfo_tool',
+    version='0.1.6',
+    description=(
+        'utils for nlp'
+    ),
+    author='navinfo_nlp',
+    author_email='navinfo_nlp@163.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'xlrd==1.2.0',
+        'jieba',
+        'xlwt',
+        'xlutils',
+        'matplotlib',
+        'sklearn',
+        'pandas',
+        'fairies',
+        'configparser',
+        'xpinyin==0.7.6',
+        'xlsxwriter==3.0.1',
+        'tqdm'
+    ],
+)
