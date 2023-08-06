1cd88b00eb4a3fc79ef39d6fbffb30fcc17ded08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:20 +0000

    Add package upbge-stubs/0.3.1.27.dev4624659974/upbge-stubs-0.3.1.27.dev4624659974.tar.gz

---
+++ packages/upbge-stubs/upbge-stubs-0.3.1.27.dev4624659974.tar.gz/upbge-stubs-0.3.1.27.dev4624659974/bpy/ops/camera.pyi
@@ -0,0 +1,37 @@
+"""
+
+
+Camera Operators
+****************
+
+:func:`preset_add`
+
+:func:`safe_areas_preset_add`
+
+"""
+
+import typing
+
+def preset_add(name: str = '', remove_name: bool = False, remove_active: bool = False, use_focal_length: bool = False) -> None:
+
+  """
+
+  Add or remove a Camera Preset
+
+  :file:`startup/bl_operators/presets.py\:73 <https://projects.blender.org/blender/blender/src/branch/main/scripts/startup/bl_operators/presets.py#L73>`_
+
+  """
+
+  ...
+
+def safe_areas_preset_add(name: str = '', remove_name: bool = False, remove_active: bool = False) -> None:
+
+  """
+
+  Add or remove a Safe Areas Preset
+
+  :file:`startup/bl_operators/presets.py\:73 <https://projects.blender.org/blender/blender/src/branch/main/scripts/startup/bl_operators/presets.py#L73>`_
+
+  """
+
+  ...
