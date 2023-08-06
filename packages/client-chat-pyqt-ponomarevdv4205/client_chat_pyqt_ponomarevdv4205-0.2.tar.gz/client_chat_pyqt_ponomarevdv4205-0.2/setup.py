e0792339f33e282adf786975cf00d74329b7dfe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:59 +0000

    Add package client-chat-pyqt-ponomarevdv4205/0.2/client_chat_pyqt_ponomarevdv4205-0.2.tar.gz

---
+++ packages/client-chat-pyqt-ponomarevdv4205/client_chat_pyqt_ponomarevdv4205-0.2.tar.gz/client_chat_pyqt_ponomarevdv4205-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='client_chat_pyqt_ponomarevdv4205',
+      version='0.2',
+      description='Client packet',
+      packages=find_packages(),
+      author_email='ponomarevdv4205@yandex.ru',
+      author='Dima Ponomarev',
+      install_requeres=['PyQt5', 'sqlalchemy', 'pycruptodome', 'pycryptodomex']
+      )
