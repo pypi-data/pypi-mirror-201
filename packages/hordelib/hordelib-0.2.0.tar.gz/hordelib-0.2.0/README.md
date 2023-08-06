254a0de4b31ee626ad9c095111254b6437423016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:39 +0000

    Add package hordelib/0.2.0/hordelib-0.2.0.tar.gz

---
+++ packages/hordelib/hordelib-0.2.0.tar.gz/hordelib-0.2.0/README.md
@@ -0,0 +1,164 @@
+# hordelib
+
+`hordelib` is a thin wrapper around [ComfyUI](https://github.com/comfyanonymous/ComfyUI) primarily to enable the [AI Horde](https://aihorde.net/) to run inference pipelines designed in ComfyUI. `hordelib` and ComfyUI are licensed under the terms of the GNU General Public License.
+
+The developers of this project can be found in the AI Horde Discord server: https://discord.gg/3DxrhksKznb
+
+## Purpose
+
+The goal here is to be able to design inference pipelines in the excellent ComfyUI, and then call those inference pipelines programmatically, in a manner ultimately suitable for use in stable horde.
+
+## Installation
+
+If being installed from pypi, use a requirements file of the form:
+```
+--extra-index-url https://download.pytorch.org/whl/cu117
+hordelib
+
+...your other dependencies...
+```
+
+## Usage
+
+Horde payloads can be processed simply with (for example):
+
+```python
+import os
+import hordelib
+hordelib.initialise()
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+
+# Wherever your models are
+os.environ["AIWORKER_CACHE_HOME"] = "f:/ai/models"
+
+generate = HordeLib()
+SharedModelManager.loadModelManagers(compvis=True)
+SharedModelManager.manager.load("Deliberate")
+
+data = {
+    "sampler_name": "k_dpmpp_2m",
+    "cfg_scale": 7.5,
+    "denoising_strength": 1.0,
+    "seed": 123456789,
+    "height": 512,
+    "width": 512,
+    "karras": True,
+    "tiling": False,
+    "hires_fix": False,
+    "clip_skip": 1,
+    "control_type": "canny",
+    "image_is_control": False,
+    "return_control_map": False,
+    "prompt": "an ancient llamia monster",
+    "ddim_steps": 25,
+    "n_iter": 1,
+    "model": "Deliberate",
+}
+pil_image = generate.basic_inference(data)
+pil_image.save("test.png")
+```
+
+## Development
+
+Requirements:
+- `git` (install git)
+- `tox` (`pip install tox`)
+- Set the environmental variable `AIWORKER_CACHE_HOME` to point to a model directory.
+
+Note the model directory must currently be in the original AI Horde directory structure:
+```
+<AIWORKER_CACHE_HOME>\
+   nataili\
+        clip\
+        codeformer\
+        compvis\
+            Deliberate.ckpt
+            ...etc...
+        controlnet\
+        embeds\
+        esrgan\
+        gfpgan\
+        safety_checker\
+```
+
+### Running the Tests
+
+Simply execute: `tox` (or `tox -q` for less noisy output)
+
+This will take a while the first time as it installs all the dependencies.
+
+If the tests run successfully images will be produced in the project with the name of each pipeline, for example, `pipeline_stable_diffusion.png` and likely some other .png files too.
+
+#### Running a specific test file
+
+`tox -- -k <filename>` for example `tox -- -k test_initialisation`
+
+### Directory Structure
+
+`hordelib/pipeline_designs/`
+Contains ComfyUI pipelines in a format that can be opened by the ComfyUI web app. These saved directly from the web app.
+
+`hordelib/pipelines/`
+Contains the above pipeline JSON files converted to the format required by the backend pipeline processor. These are converted from the web app, see _Converting ComfyUI pipelines_ below.
+
+`hordelib/nodes/` These are the custom ComfyUI nodes we use for `hordelib` specific processing.
+
+### Running ComfyUI Web Application
+
+`tox -e comfyui`
+
+Then open a browser at: http://127.0.0.1:8188/
+
+### Designing ComfyUI Pipelines
+
+Use the standard ComfyUI web app. Use the "title" attribute to name the nodes, these names become parameter names in the `hordelib`. For example, a KSampler with the "title" of "sampler2" would become a parameter `sampler2.seed`, `sampler2.cfg`, etc. Load the pipeline `hordelib/pipeline_designs/pipeline_stable_diffusion.json` in the ComfyUI web app for an example.
+
+Save any new pipeline in `hordelib/pipeline_designs` using the naming convention "pipeline_\<name\>.json".
+
+Convert the JSON for the model (see _Converting ComfyUI pipelines_ below) and save the resulting JSON in `hordelib/pipelines` using the same filename as the previous JSON file.
+
+That is all. This can then be called from `hordelib` using the `run_image_pipeline()` method in `hordelib.comfy.Comfy()`
+
+### Converting ComfyUI pipelines
+
+In addition to the design file saved from the UI, we need to save the pipeline file in the backend format. This file is created in the `hordelib` project root named `comfy-prompt.json` automatically if you run a pipeline through the ComfyUI version embedded in `hordelib`. Running ComfyUI with `tox -e comfyui` automatically patches ComfyUI so this JSON file is saved.
+
+### Build Configuration
+
+The main config files for the project are: `pyproject.toml`, `tox.ini` and `requirements.txt`
+
+### PyPi Publishing
+
+Three steps:
+1. Bump the version in `hordelib/cosnts.py`
+1. `tox` _make sure everything works_
+1. `python build_helper.py` _builds the dist files_
+1. `twine upload -r pypi dist/*`
+
+### Updating the embedded version of ComfyUI
+
+We use a ComfyUI version pinned to a specific commit, see `hordelib/consts.py:COMFYUI_VERSION`
+
+To test if the latest version works and upgrade to it, from the project root simply:
+
+1. `cd ComfyUI` _Change CWD to the embedded comfy_
+1. `git checkout master` _Switch to master branch_
+1. `git pull` _Get the latest comfyui code_
+1. `git rev-parse HEAD` _Update the hash in `hordelib.consts:COMFYUI_VERSION`_
+1. `cd ..` _Get back to the hordelib project root_
+1. `tox` _See if everything still works_
+
+Now ComfyUI is pinned to a new version.
+
+### ComfyUI Patching
+
+It may be that we need to patch the ComfyUI source code. Currently this optional and only used for development helpers. However, the mechanisms to patch ComfyUI source code at runtime are also already in place.
+
+To create a patch file:
+- Make the required changes to a clean install of ComfyUI and then run `git diff > yourfile.patch` then move the patch file to wherever you want to save it.
+
+Note that the patch file _really_ needs to be in UTF-8 format and some common terminals, e.g. Powershell, won't do this by default. In Powershell to create a patch file use: `git diff | Set-Content -Encoding utf8 -Path yourfile.patch`
+
+Patches can be applied with the `hordelib.install_comfyui.Installer` classes `apply_patch()` method.
---
+++ packages/hordelib/hordelib-0.2.0.tar.gz/hordelib-0.2.0/hordelib/horde.py
@@ -0,0 +1,213 @@
+# horde.py
+# Main interface for the horde to this library.
+import contextlib
+
+from PIL import Image
+
+from hordelib.comfy_horde import Comfy_Horde
+from hordelib.shared_model_manager import SharedModelManager
+
+
+class HordeLib:
+    # Horde to comfy sampler mapping
+    SAMPLERS_MAP = {
+        "k_euler": "euler",
+        "k_euler_a": "euler_ancestral",
+        "k_heun": "heun",
+        "k_dpm_2": "dpm_2",
+        "k_dpm_2_a": "dpm_2_ancestral",
+        "k_lms": "lms",
+        "k_dpm_fast": "dpm_fast",
+        "k_dpm_adaptive": "dpm_adaptive",
+        "k_dpmpp_2s_a": "dpmpp_2s_ancestral",
+        "k_dpmpp_sde": "dpmpp_sde",
+        "k_dpmpp_2m": "dpmpp_2m",
+        "ddim": "ddim",
+        "uni_pc": "uni_pc",
+        "uni_pc_bh2": "uni_pc_bh2",
+        "plms": "<not supported>",
+    }
+
+    # Horde to tex2img parameter mapping
+    # XXX Items mapped to None are ignored for now
+    BASIC_INFERENCE_PARAMS = {
+        "sampler_name": "sampler.sampler_name",
+        "cfg_scale": "sampler.cfg",
+        "denoising_strength": "sampler.denoise",
+        "seed": "sampler.seed",
+        "height": "empty_latent_image.height",
+        "width": "empty_latent_image.width",
+        # "karras": Handled below
+        "tiling": None,
+        # "hires_fix": Handled below
+        "clip_skip": "clip_skip.stop_at_clip_layer",
+        "control_type": None,
+        "image_is_control": None,
+        "return_control_map": None,
+        # "prompt": Handled below
+        "ddim_steps": "sampler.steps",
+        "n_iter": "empty_latent_image.batch_size",
+        "model": "model_loader.model_name",
+        "source_image": "image_loader.image",
+        "source_mask": None,
+        "source_processing": "source_processing",
+    }
+
+    SOURCE_IMAGE_PROCESSING_OPTIONS = ["img2img", "inpainting", "outpainting"]
+
+    def _parameter_remap(self, payload: dict[str, str | None]) -> dict[str, str | None]:
+        params = {}
+        # Extract from the payload things we understand
+        for key, value in payload.items():
+            newkey = HordeLib.BASIC_INFERENCE_PARAMS.get(key, None)
+            if newkey:
+                params[newkey] = value
+
+        # Inject model manager if needed
+        if "model_loader.model_manager" not in params:
+            params["model_loader.model_manager"] = SharedModelManager
+
+        return params
+
+    def _parameter_remap_basic_inference(
+        self,
+        payload: dict[str, str | None],
+    ) -> dict[str, str | None]:
+        params = self._parameter_remap(payload)
+
+        # XXX I think we need seed as an integer
+        with contextlib.suppress(ValueError):
+            params["sampler.seed"] = int(params["sampler.seed"])
+
+        # karras flag determines which scheduler we use
+        if payload.get("karras", False):
+            params["sampler.scheduler"] = "karras"
+        else:
+            params["sampler.scheduler"] = "normal"
+
+        # We break prompt up on horde's "###"
+        promptsCombined = payload.get("prompt", "")
+
+        if promptsCombined is None:  # XXX
+            raise TypeError("`None` value encountered!")
+
+        promptsSplit = [x.strip() for x in promptsCombined.split("###")][:2]
+        if len(promptsSplit) == 1:
+            params["prompt.text"] = promptsSplit[0]
+            params["negative_prompt.text"] = ""
+        elif len(promptsSplit) == 2:
+            params["prompt.text"] = promptsSplit[0]
+            params["negative_prompt.text"] = promptsSplit[1]
+
+        # Sampler remap
+        sampler = HordeLib.SAMPLERS_MAP.get(params["sampler.sampler_name"], "euler")
+        params["sampler.sampler_name"] = sampler
+
+        # Clip skip inversion, comfy uses -1, -2, etc
+        clip_skip_key = "clip_skip.stop_at_clip_layer"
+        if params.get(clip_skip_key, 0) > 0:
+            params[clip_skip_key] = -params[clip_skip_key]
+
+        # If hires fix is enabled, use the same parameters as the main
+        # sampler in our upscale sampler.
+        if payload.get("hires_fix"):
+            params["upscale_sampler.seed"] = params["sampler.seed"]
+            params["upscale_sampler.scheduler"] = params["sampler.scheduler"]
+            params["upscale_sampler.cfg"] = params["sampler.cfg"]
+            params["upscale_sampler.steps"] = params["sampler.steps"]
+            params["upscale_sampler.sampler_name"] = params["sampler.sampler_name"]
+            params[
+                "upscale_sampler.denoise"
+            ] = 0.6  # XXX is this ok for latent upscale denoise?
+            # Adjust image sizes
+            width = params.get("empty_latent_image.width", 0)
+            height = params.get("empty_latent_image.height", 0)
+            if width > 512 and height > 512:
+                final_width = width
+                final_height = height
+                params["latent_upscale.width"] = final_width
+                params["latent_upscale.height"] = final_height
+                first_pass_ratio = min(final_height / 512, final_width / 512)
+                width = (int(final_width / first_pass_ratio) // 64) * 64
+                height = (int(final_height / first_pass_ratio) // 64) * 64
+                params["empty_latent_image.width"] = width
+                params["empty_latent_image.height"] = height
+                # Finally mark that we are using hires fix
+                params["hires_fix"] = True
+
+        return params
+
+    # Fix any nonsensical requests
+    def _validate_BASIC_INFERENCE_PARAMS(self, payload):
+        # Turn off hires fix if we're not generating a hires image
+        if "hires_fix" in payload and (
+            payload["width"] <= 512 or payload["height"] <= 512
+        ):
+            payload["hires_fix"] = False
+
+        # Remove source_processing if it's not valid
+        img_proc = payload.get("source_processing")
+        if img_proc and img_proc not in HordeLib.SOURCE_IMAGE_PROCESSING_OPTIONS:
+            del payload["source_processing"]
+
+        # Remove source image if we don't need it
+        if payload.get("source_image"):
+            if not img_proc or img_proc not in HordeLib.SOURCE_IMAGE_PROCESSING_OPTIONS:
+                del payload["source_image"]
+
+        # Turn off hires fix if we're painting as the dimensions are from the image
+        if "hires_fix" in payload and (
+            img_proc == "inpainting" or img_proc == "outpainting"
+        ):
+            payload["hires_fix"] = False
+
+    def _get_appropriate_pipeline(self, params):
+        # Determine the correct pipeline based on the parameters we have
+
+        # Hires fix
+        if "hires_fix" in params:
+            del params["hires_fix"]
+            pipeline = "stable_diffusion_hires_fix"
+        else:
+            pipeline = "stable_diffusion"
+
+        # Source processing modes
+        source_proc = params.get("source_processing")
+        if source_proc:
+            del params["source_processing"]
+        if source_proc == "img2img":
+            pass  # doesn't impact pipeline
+        elif source_proc == "inpainting":
+            pipeline = "stable_diffusion_paint"
+        elif source_proc == "outpainting":
+            pipeline = "stable_diffusion_paint"
+
+        return pipeline
+
+    def basic_inference(self, payload: dict[str, str | None]) -> Image.Image | None:
+        generator = Comfy_Horde()
+        # Validate our payload parameters
+        params = self._validate_BASIC_INFERENCE_PARAMS(payload)
+        # Determine our parameters
+        params = self._parameter_remap_basic_inference(payload)
+        # Determine the correct pipeline
+        pipeline = self._get_appropriate_pipeline(params)
+        # Run the pipeline
+        images = generator.run_image_pipeline(pipeline, params)
+        if images is None:
+            return None  # XXX Log error and/or raise Exception here
+        # XXX Assumes the horde only asks for and wants 1 image
+        return Image.open(images[0]["imagedata"])
+
+    def image_upscale(self, payload: dict[str, str | None]) -> Image.Image | None:
+        generator = Comfy_Horde()
+        # Determine our parameters
+        params = self._parameter_remap(payload)
+        # Determine the correct pipeline
+        pipeline = "image_upscale"
+        # Run the pipeline
+        images = generator.run_image_pipeline(pipeline, params)
+        if images is None:
+            return None  # XXX Log error and/or raise Exception here
+        # XXX Assumes the horde only asks for and wants 1 image
+        return Image.open(images[0]["imagedata"])
