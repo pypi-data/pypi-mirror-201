36b9d124a10ce02629b7b869b703b387f7eaa95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:30 +0000

    Add package collection-framework-by-Dima/0.0.1/collection_framework_by_dima-0.0.1-py3-none-any.whl

---
+++ packages/collection-framework-by-Dima/collection_framework_by_dima-0.0.1-py3-none-any.whl/collection_framework_by_Dima/__init__.py
@@ -0,0 +1,3 @@
+from .collect_framework import get_args, get_result
+from .my_exceptions import NoArgumentsError, UniqueTypeError
+from .unique_collection import unique_symbol, unique_symbol_in_list
