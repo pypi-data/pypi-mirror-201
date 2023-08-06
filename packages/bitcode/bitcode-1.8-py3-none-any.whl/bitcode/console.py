6e5b03256fde645120ce08e46b082019eaa5d9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:24:43 +0000

    Add package bitcode/1.8/bitcode-1.8-py3-none-any.whl

---
+++ packages/bitcode/bitcode-1.8-py3-none-any.whl/bitcode/console.py
@@ -0,0 +1,39 @@
+"""\
+Reference: 123web.uk/bitpy/docs/console.html
+"""
+
+import os
+
+def write(*text, end="", split="", flush=True):
+	"""Print the given text to stdout"""
+	print(*text, end=end, sep=split, flush=flush)
+
+def writeln(*text, split="", flush=True):
+	"""Print the given text to stdout, and appends a new line"""
+	write(*text, end="\n", split=split, flush=flush)
+
+def beep():
+	"""Play the \\a 'beep' sound in the console"""
+	write("\a")
+
+def clear():
+	"""Clear the console"""
+	if os.name == "nt":
+		os.system("cls")
+	else:
+		os.system("clear")
+
+def prompt(return_type=str):
+	error = None
+	try:
+		return return_type(input())
+	except:
+		error = TypeError(f"console.prompt: cannot convert user input to {return_type}")
+	if error:
+		raise error
+
+def get_width():
+	return os.get_terminal_size().columns
+
+def colour(ansi_colour):
+	write(f"\u001b[{ansi_colour}m")
