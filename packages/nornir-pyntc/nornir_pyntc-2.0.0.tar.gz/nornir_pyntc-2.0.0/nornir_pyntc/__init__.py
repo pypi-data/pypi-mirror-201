60fff973a60f0604b5c8c3f14b4168f7ad1aea11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:08 +0000

    Add package nornir-pyntc/2.0.0/nornir_pyntc-2.0.0.tar.gz

---
+++ packages/nornir-pyntc/nornir_pyntc-2.0.0.tar.gz/nornir_pyntc-2.0.0/nornir_pyntc/__init__.py
@@ -0,0 +1,23 @@
+"""Initialize Connection and Tasks."""
+
+try:
+    from importlib import metadata  # type: ignore[attr-defined]
+except ImportError:
+    # Python version < 3.8
+    import importlib_metadata as metadata  # type: ignore[no-redef]
+
+from nornir_pyntc.connections import CONNECTION_NAME, Pyntc
+from nornir_pyntc.tasks import pyntc_config, pyntc_file_copy, pyntc_install_os, pyntc_reboot, pyntc_save, pyntc_show
+
+__version__ = metadata.version(__name__)
+
+__all__ = (
+    "Pyntc",
+    "CONNECTION_NAME",
+    "pyntc_show",
+    "pyntc_config",
+    "pyntc_file_copy",
+    "pyntc_install_os",
+    "pyntc_reboot",
+    "pyntc_save",
+)
