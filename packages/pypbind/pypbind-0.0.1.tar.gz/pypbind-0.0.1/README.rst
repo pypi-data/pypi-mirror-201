b811e1d166728cf00072cc81ff9410e9d93ca94e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:23 +0000

    Add package pypbind/0.0.1/pypbind-0.0.1.tar.gz

---
+++ packages/pypbind/pypbind-0.0.1.tar.gz/pypbind-0.0.1/README.rst
@@ -0,0 +1,5 @@
+pypbind
+=======
+
+Partial port of supercollider stream-pattern-event system to python.
+
---
+++ packages/pypbind/pypbind-0.0.1.tar.gz/pypbind-0.0.1/pbind/__init__.py
@@ -0,0 +1,6 @@
+from .patterns import *
+from .warps import Pwarp, Warp
+from .pbind import inf, wrap, clip, fold, choose, choosefun, rrand, rrandfun, Pmin, Pmax, Pdifferentiate, Pintegrate
+from .pbind import windex, wnindex, wchoice, wnchoice, Pclip, Pfold, Pwrap, Pbind, Pfin, Pfindur
+from .event import Event
+from .markov import MSM, Pmsm
