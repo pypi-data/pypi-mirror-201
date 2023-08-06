a06d0327c05526aeef65b28ecb889da92708a73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:21 +0000

    Add package oaklib/0.4.1/oaklib-0.4.1-py3-none-any.whl

---
+++ packages/oaklib/oaklib-0.4.1-py3-none-any.whl/oaklib/parsers/association_parser_factory.py
@@ -0,0 +1,18 @@
+from typing import Type
+
+from oaklib.parsers import AssociationParser
+
+
+def get_association_parser(syntax: str, *args, **kwargs) -> Type[AssociationParser]:
+    """
+    Get an association parser for a given syntax
+
+    :param syntax:
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    from oaklib.parsers import get_association_parser_resolver
+
+    cls = get_association_parser_resolver().lookup(syntax)
+    return cls(*args, **kwargs)
