30f92bcc7f3f49ab4fb0973b66fed392a0244594
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:45 +0000

    Add package cyrillic-tolatin/0.0.2/cyrillic_tolatin-0.0.2-py3-none-any.whl

---
+++ packages/cyrillic-tolatin/cyrillic_tolatin-0.0.2-py3-none-any.whl/cyrillictolatin/__init__.py
@@ -0,0 +1,1 @@
+from .main import latin_to_cyrillic
---
+++ packages/cyrillic-tolatin/cyrillic_tolatin-0.0.2-py3-none-any.whl/cyrillictolatin/main.py
@@ -0,0 +1,45 @@
+def cyrillic_to_latin(text):
+    cyrillic_map = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'yo', 'ж': 'zh',
+        'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o',
+        'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts',
+        'ч': 'ch', 'ш': 'sh', 'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu',
+        'я': 'ya',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'Yo', 'Ж': 'Zh',
+        'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O',
+        'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts',
+        'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch', 'Ъ': '', 'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu',
+        'Я': 'Ya'
+    }
+    latin_text = ''
+    for char in text:
+        latin_char = cyrillic_map.get(char, char)
+        latin_text += latin_char
+    return latin_text
+
+
+def latin_to_cyrillic(text):
+    latin_map = {
+        'a': 'а', 'b': 'б', 'c': 'ц', 'd': 'д', 'e': 'е', 'f': 'ф', 'g': 'г', 'h': 'х', 'i': 'и', 'j': 'й',
+        'k': 'к', 'l': 'л', 'm': 'м', 'n': 'н', 'o': 'о', 'p': 'п', 'q': 'к', 'r': 'р', 's': 'с', 't': 'т',
+        'u': 'у', 'v': 'в', 'w': 'в', 'x': 'кс', 'y': 'й', 'z': 'з',
+        'A': 'А', 'B': 'Б', 'C': 'Ц', 'D': 'Д', 'E': 'Е', 'F': 'Ф', 'G': 'Г', 'H': 'Х', 'I': 'И', 'J': 'Й',
+        'K': 'К', 'L': 'Л', 'M': 'М', 'N': 'Н', 'O': 'О', 'P': 'П', 'Q': 'К', 'R': 'Р', 'S': 'С', 'T': 'Т',
+        'U': 'У', 'V': 'В', 'W': 'В', 'X': 'КС', 'Y': 'Й', 'Z': 'З',
+        'ya': 'я', 'Ya': 'Я', 'yu': 'ю', 'Yu': 'Ю', 'ye': 'е', 'Ye': 'Е', 'yo': 'ё', 'Yo': 'Ё', 'zh': 'ж', 'Zh': 'Ж',
+        'ch': 'ч', 'Ch': 'Ч', 'sh': 'ш', 'Sh': 'Ш', 'sch': 'щ', 'Sch': 'Щ'
+    }
+    cyrillic_text = ''
+    i = 0
+    while i < len(text):
+        if text[i:i + 2] in latin_map:
+            cyrillic_text += latin_map[text[i:i + 2]]
+            i += 2
+        else:
+            cyrillic_char = latin_map.get(text[i], text[i])
+            cyrillic_text += cyrillic_char
+            i += 1
+    return cyrillic_text
+
+
+print(latin_to_cyrillic("Q"))
