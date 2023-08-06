fc50b87c0cf1ccdb18b2e3947b7cc2ea88268422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:25 +0000

    Add package flaskez/0.3.0a1/flaskez-0.3.0a1.tar.gz

---
+++ packages/flaskez/flaskez-0.3.0a1.tar.gz/flaskez-0.3.0a1/flaskez/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 IHasBone
+"""
+Flaskez is a multipurpose flask extension. It exists both to make creating websites easier, but also to add extra functionality to flask.
+"""
+
+from ._models.models import config
+from ._basic.main_functions import create_app, create_blueprint
+from ._basic.instant_setup import instant_setup
