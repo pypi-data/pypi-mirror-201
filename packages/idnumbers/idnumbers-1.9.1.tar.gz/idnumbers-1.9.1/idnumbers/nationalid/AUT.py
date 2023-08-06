041ace94f47af440a399af023bf9982b7f908bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:14 +0000

    Add package idnumbers/1.9.1/idnumbers-1.9.1.tar.gz

---
+++ packages/idnumbers/idnumbers-1.9.1.tar.gz/idnumbers-1.9.1/idnumbers/nationalid/AUT.py
@@ -0,0 +1,5 @@
+from .aut.tax_id import TaxIDNumber
+from .util import alias_of
+
+NationalID = alias_of(TaxIDNumber)
+"""alias of TaxIDNumber"""
---
+++ packages/idnumbers/idnumbers-1.9.1.tar.gz/idnumbers-1.9.1/idnumbers/nationalid/BEL.py
@@ -0,0 +1,5 @@
+from .bel.national_registration import NationalRegistrationNumber
+from .util import alias_of
+
+NationalID = alias_of(NationalRegistrationNumber)
+"""alias of national registration number"""
