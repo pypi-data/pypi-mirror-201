8dc39a7c5500c1547796b1cd37a1859efafb58c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:43 +0000

    Add package design.plone.policy/4.0.3/design.plone.policy-4.0.3.tar.gz

---
+++ packages/design.plone.policy/design.plone.policy-4.0.3.tar.gz/design.plone.policy-4.0.3/src/design/plone/policy/interfaces.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""Module where all interfaces, events and exceptions live."""
+
+from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface
+from zope import schema
+
+
+class IDesignPlonePolicyLayer(IDefaultBrowserLayer):
+    """Marker interface that defines a browser layer."""
+
+
+class IDesignPlonePolicySettings(Interface):
+    twitter_token = schema.TextLine(title="Twitter Bearer token")
