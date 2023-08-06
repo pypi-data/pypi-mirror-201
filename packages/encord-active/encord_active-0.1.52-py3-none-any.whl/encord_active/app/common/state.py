97c8e57f57d756cd69044518152eb461993f27f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:39 +0000

    Add package encord-active/0.1.52/encord_active-0.1.52-py3-none-any.whl

---
+++ packages/encord-active/encord_active-0.1.52-py3-none-any.whl/encord_active/app/common/state.py
@@ -0,0 +1,138 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from pathlib import Path
+from typing import Any, Callable, Dict, List, Optional
+
+import pandas as pd
+import streamlit as st
+from pandera.typing import DataFrame
+
+from encord_active.lib.common.image_utils import ObjectDrawingConfigurations
+from encord_active.lib.dataset.outliers import MetricsSeverity
+from encord_active.lib.dataset.summary_utils import AnnotationStatistics
+from encord_active.lib.db.connection import DBConnection
+from encord_active.lib.db.merged_metrics import MergedMetrics
+from encord_active.lib.db.tags import Tag, Tags
+from encord_active.lib.embeddings.utils import Embedding2DSchema
+from encord_active.lib.metrics.metric import EmbeddingType
+from encord_active.lib.metrics.utils import MetricData, MetricSchema
+from encord_active.lib.model_predictions.reader import LabelSchema, OntologyObjectJSON
+from encord_active.lib.model_predictions.writer import OntologyClassificationJSON
+from encord_active.lib.project import ProjectFileStructure
+
+
+class StateKey(str, Enum):
+    GLOBAL = "GLOBAL"
+    SCOPED = "SCOPED"
+    MEMO = "MEMO"
+
+
+@dataclass
+class MetricNames:
+    predictions: Dict[str, MetricData] = field(default_factory=dict)
+    selected_prediction: Optional[str] = None
+    labels: Dict[str, MetricData] = field(default_factory=dict)
+    selected_label: Optional[str] = None
+
+
+@dataclass
+class PredictionsState:
+    decompose_classes = False
+    metric_datas: MetricNames = field(default_factory=MetricNames)
+    metric_datas_classification: MetricNames = field(default_factory=MetricNames)
+    all_classes_objects: Dict[str, OntologyObjectJSON] = field(default_factory=dict)
+    all_classes_classifications: Dict[str, OntologyClassificationJSON] = field(default_factory=dict)
+    selected_classes_objects: Dict[str, OntologyObjectJSON] = field(default_factory=dict)
+    selected_classes_classifications: Dict[str, OntologyClassificationJSON] = field(default_factory=dict)
+    labels: Optional[DataFrame[LabelSchema]] = None
+    nbins: int = 50
+
+
+@dataclass
+class PageGridSettings:
+    columns: int = 4
+    rows: int = 5
+
+
+@dataclass
+class FilteringState:
+    merged_metrics: Optional[pd.DataFrame] = None
+    selected_annotators: List[str] = field(default_factory=list)
+    selected_classes: List[str] = field(default_factory=list)
+    sort_by_metric: Optional[MetricData] = None
+    sorted_items: Optional[DataFrame[MetricSchema]] = None
+
+
+@dataclass
+class State:
+    """
+    Use this only for getting default values, otherwise use `get_state()`
+    To get the default `iou_threshold` we would call `State.iou_threshold`
+    and to get/set the current value we would call `get_state().iou_threshold.`
+    """
+
+    project_paths: ProjectFileStructure
+    refresh_projects: Callable[[], Any]
+    all_tags: List[Tag]
+    merged_metrics: pd.DataFrame
+    filtering_state: FilteringState = field(default_factory=FilteringState)
+    ignore_frames_without_predictions = False
+    iou_threshold = 0.5
+    page_grid_settings: PageGridSettings = field(default_factory=PageGridSettings)
+    predictions: PredictionsState = field(default_factory=PredictionsState)
+    similarities_count = 8
+    annotation_sizes: Optional[AnnotationStatistics] = None
+    metrics_data_summary: Optional[MetricsSeverity] = None
+    metrics_label_summary: Optional[MetricsSeverity] = None
+    object_drawing_configurations: ObjectDrawingConfigurations = field(default_factory=ObjectDrawingConfigurations)
+    reduced_embeddings: dict[EmbeddingType, Optional[DataFrame[Embedding2DSchema]]] = field(default_factory=dict)
+
+    @classmethod
+    def init(cls, project_dir: Path, refresh_projects: Callable[[], Any]):
+        if (
+            st.session_state.get(StateKey.GLOBAL) is None
+            or project_dir != st.session_state[StateKey.GLOBAL].project_paths.project_dir
+        ):
+            DBConnection.set_project_path(project_dir)
+            st.session_state[StateKey.GLOBAL] = State(
+                project_paths=ProjectFileStructure(project_dir),
+                refresh_projects=refresh_projects,
+                merged_metrics=MergedMetrics().all(),
+                all_tags=Tags().all(),
+            )
+
+
+def has_state():
+    return StateKey.GLOBAL in st.session_state
+
+
+def get_state() -> State:
+    if not has_state():
+        st.stop()
+
+    return st.session_state[StateKey.GLOBAL]
+
+
+def refresh(
+    *,
+    clear_global: bool = False,
+    clear_scoped: bool = False,
+    clear_memo: bool = False,
+    clear_component: bool = False,
+    nuke: bool = False,
+):
+    if nuke:
+        st.session_state.clear()
+    else:
+        if clear_global:
+            st.session_state.pop(StateKey.GLOBAL, None)
+        if clear_scoped:
+            st.session_state.get(StateKey.SCOPED, {}).clear()
+        if clear_memo:
+            st.session_state.get(StateKey.MEMO, {}).clear()
+        if clear_component:
+            keys = {key for key in st.session_state.keys() if key not in StateKey.__members__}
+            for key in keys:
+                st.session_state.pop(key, None)
+
+    st.experimental_rerun()
---
+++ packages/encord-active/encord_active-0.1.52-py3-none-any.whl/encord_active/lib/project/project_file_structure.py
@@ -0,0 +1,99 @@
+import json
+from pathlib import Path
+from typing import Iterator, NamedTuple, Optional
+
+
+class DataUnitStructure(NamedTuple):
+    hash: str
+    path: Path
+
+
+class LabelRowStructure:
+    def __init__(self, path: Path, mappings: dict[str, str]):
+        self.path: Path = path
+        self._mappings: dict[str, str] = mappings
+        self._rev_mappings: dict[str, str] = {v: k for k, v in mappings.items()}
+
+    @property
+    def label_row_file(self) -> Path:
+        return self.path / "label_row.json"
+
+    @property
+    def images_dir(self) -> Path:
+        return self.path / "images"
+
+    def iter_data_unit(
+        self, data_unit_hash: Optional[str] = None, frame: Optional[int] = None
+    ) -> Iterator[DataUnitStructure]:
+        if data_unit_hash:
+            glob_string = self._mappings.get(data_unit_hash, data_unit_hash)
+        else:
+            glob_string = "*"
+        if frame:
+            glob_string += f"_{frame}"
+        glob_string += ".*"
+        for du_path in self.images_dir.glob(glob_string):
+            du_hash = du_path.name.split(".")[0].split("_")[0]
+            yield DataUnitStructure(self._rev_mappings.get(du_hash, du_hash), du_path)
+
+    def is_present(self):
+        return self.path.is_dir()
+
+
+class ProjectFileStructure:
+    def __init__(self, project_dir: Path):
+        self.project_dir: Path = project_dir.expanduser().resolve()
+        self._mappings = json.loads(self.mappings.read_text()) if self.mappings.exists() else {}
+
+    @property
+    def data(self) -> Path:
+        return self.project_dir / "data"
+
+    @property
+    def metrics(self) -> Path:
+        return self.project_dir / "metrics"
+
+    @property
+    def metrics_meta(self) -> Path:
+        return self.metrics / "metrics_meta.json"
+
+    @property
+    def embeddings(self) -> Path:
+        return self.project_dir / "embeddings"
+
+    @property
+    def predictions(self) -> Path:
+        return self.project_dir / "predictions"
+
+    @property
+    def db(self) -> Path:
+        return self.project_dir / "sqlite.db"
+
+    @property
+    def label_row_meta(self) -> Path:
+        return self.project_dir / "label_row_meta.json"
+
+    @property
+    def image_data_unit(self) -> Path:
+        return self.project_dir / "image_data_unit.json"
+
+    @property
+    def ontology(self) -> Path:
+        return self.project_dir / "ontology.json"
+
+    @property
+    def project_meta(self) -> Path:
+        return self.project_dir / "project_meta.yaml"
+
+    def label_row_structure(self, label_hash: str) -> LabelRowStructure:
+        path = self.data / self._mappings.get(label_hash, label_hash)
+        return LabelRowStructure(path=path, mappings=self._mappings)
+
+    def iter_labels(self) -> Iterator[LabelRowStructure]:
+        for label_hash in self.data.iterdir():
+            path = self.data / label_hash
+            yield LabelRowStructure(path=path, mappings=self._mappings)
+
+    @property
+    def mappings(self) -> Path:
+        return self.project_dir / "hash_mappings.json"
