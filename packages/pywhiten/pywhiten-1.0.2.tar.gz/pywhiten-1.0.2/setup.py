b68131ae34bf421ba213d20d71812cd89d69abb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:33 +0000

    Add package pywhiten/1.0.2/pywhiten-1.0.2.tar.gz

---
+++ packages/pywhiten/pywhiten-1.0.2.tar.gz/pywhiten-1.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name="pywhiten",
+      version="1.0.2",
+      description="A python package for conducting Lomb-Scargle-based prewhitening of stellar time series. ",
+      url="https://www.github.com/erikstacey/pywhiten",
+      author="Erik William Stacey",
+      author_email = "erik@erikstacey.com",
+      license="MIT",
+      packages=["pywhiten"],
+      install_requires=[
+            "numpy",
+            "matplotlib",
+            "scipy",
+            "astropy",
+            "lmfit",
+            "tomli"
+      ],
+      tests_require=["pytest"],
+      zip_safe = False,
+      include_package_data=True,
+      package_data={'': ['/cfg/default.toml']},
+      )
