c2452a1dcd02eef536275465efe265ede385ea93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:21:55 +0000

    Add package proxy6api/0.1.0/proxy6api-0.1.0-py3-none-any.whl

---
+++ packages/proxy6api/proxy6api-0.1.0-py3-none-any.whl/proxy6api/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ["Proxy_6_API_URL", "Proxy_6_Client", "COUNTRIES_HUMAN_NAME_KEYS", "COUNTRIES_ISO2_KEYS", "CODES_OF_ERRORS"]
+
+from proxy6api.client import Proxy_6_API_URL, Proxy_6_Client
+from proxy6api.settings.bases import COUNTRIES_HUMAN_NAME_KEYS, COUNTRIES_ISO2_KEYS
+from proxy6api.settings.errors import CODES_OF_ERRORS
