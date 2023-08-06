24927c32d263d5efc8f7494b95c7b5a246d874b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:00 +0000

    Add package plone.app.contentrules/5.0.1/plone.app.contentrules-5.0.1.tar.gz

---
+++ packages/plone.app.contentrules/plone.app.contentrules-5.0.1.tar.gz/plone.app.contentrules-5.0.1/setup.py
@@ -0,0 +1,72 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "5.0.1"
+
+setup(
+    name="plone.app.contentrules",
+    version=version,
+    description="Plone integration for plone.contentrules",
+    long_description=(open("README.rst").read() + "\n\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    keywords="plone automatic content rules",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://pypi.org/project/plone.app.contentrules",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    python_requires=">=3.8",
+    extras_require={
+        "test": [
+            "plone.app.testing",
+            "plone.app.contenttypes[test]",
+            "plone.dexterity",
+            "plone.testing",
+        ]
+    },
+    install_requires=[
+        "BTrees",
+        "Products.MailHost",
+        "Products.PluggableAuthService",
+        "lxml",
+        "plone.app.contenttypes",
+        "plone.app.discussion",
+        "plone.app.uuid",
+        "plone.registry",
+        "plone.supermodel",
+        "plone.z3cform",
+        "z3c.form",
+        "setuptools",
+        "plone.base",
+        "plone.contentrules",
+        "plone.memoize",
+        "plone.stringinterp",
+        "plone.uuid",
+        "plone.autoform",
+        "plone.app.vocabularies",
+        "zope.annotation",
+        "zope.browser",
+        "Products.CMFPlone",
+        "Products.GenericSetup >= 2.0",
+        "Products.statusmessages",
+        "Zope",
+    ],
+)
