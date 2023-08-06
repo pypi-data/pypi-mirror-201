baf94effa1375da5ddb0365a5122f1bde9dc660e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:46 +0000

    Add package asciicolor/4.2/asciicolor-4.2.tar.gz

---
+++ packages/asciicolor/asciicolor-4.2.tar.gz/asciicolor-4.2/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+setup(
+    name="asciicolor",
+    version='4.2',
+    license='Eclipse Public License 2.0',
+    authors=["ascidev"],
+    author_email="rootcli53@gmail.com",
+    description="python3 package to colors your output",
+    long_description='Documentation: https://github.com/ascidev/asciicolor',
+    packages=['asciicolor']
+)
+
