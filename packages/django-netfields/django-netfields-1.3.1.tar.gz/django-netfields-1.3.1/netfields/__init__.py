9c3541a2c2eed4b3c6f550aec0c11f7886ec69c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:48 +0000

    Add package django-netfields/1.3.1/django-netfields-1.3.1.tar.gz

---
+++ packages/django-netfields/django-netfields-1.3.1.tar.gz/django-netfields-1.3.1/netfields/__init__.py
@@ -0,0 +1,5 @@
+from netfields.managers import NetManager
+from netfields.fields import (InetAddressField, CidrAddressField,
+                              MACAddressField, MACAddress8Field)
+
+default_app_config = 'netfields.apps.NetfieldsConfig'
