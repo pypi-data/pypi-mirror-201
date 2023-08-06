43e91a02ee26c910ed80212fc756f8610909459e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:28 +0000

    Add package backend-sqlalchemy/0.1.1/backend_sqlalchemy-0.1.1.tar.gz

---
+++ packages/backend-sqlalchemy/backend_sqlalchemy-0.1.1.tar.gz/backend_sqlalchemy-0.1.1/backend_sqlalchemy/main.py
@@ -0,0 +1,10 @@
+def sum_two(a, b):
+    return a + b
+
+
+def concat_name(first_name: str, last_name: str):
+    return f"{first_name} {last_name}"
+
+
+if __name__ == '__main__':
+    print("Hello my package!!")
