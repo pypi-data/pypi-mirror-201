2ee3cf41ecbc9e0ac4c88e14dfe680f30888ee9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:41 +0000

    Add package jupyterlite-xeus-python/0.8.0/jupyterlite-xeus-python-0.8.0.tar.gz

---
+++ packages/jupyterlite-xeus-python/jupyterlite-xeus-python-0.8.0.tar.gz/jupyterlite-xeus-python-0.8.0/docs/conf.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+extensions = [
+    'jupyterlite_sphinx'
+]
+
+master_doc = 'index'
+source_suffix = '.rst'
+
+project = 'jupyterlite-xeus-python'
+copyright = 'JupyterLite Team'
+author = 'JupyterLite Team'
+
+exclude_patterns = []
+
+html_theme = "pydata_sphinx_theme"
+
+jupyterlite_dir = "."
+
+html_theme_options = {
+   "logo": {
+      "image_light": "xeus-python.svg",
+      "image_dark": "xeus-python.svg",
+   }
+}
