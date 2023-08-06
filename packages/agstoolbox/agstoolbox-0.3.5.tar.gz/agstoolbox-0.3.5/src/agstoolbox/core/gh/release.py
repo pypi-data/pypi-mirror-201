fe6bf10bc64aa95ff330ac2352f0dfdf4d14a1d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:13 +0000

    Add package agstoolbox/0.3.5/agstoolbox-0.3.5.tar.gz

---
+++ packages/agstoolbox/agstoolbox-0.3.5.tar.gz/agstoolbox-0.3.5/src/agstoolbox/core/gh/release.py
@@ -0,0 +1,24 @@
+from __future__ import annotations  # for python 3.8
+from datetime import datetime
+
+from agstoolbox.core.ags.ags_editor import AgsEditor
+
+
+class Release(AgsEditor):
+    """a GitHub release object"""
+    url: str = None
+    id: str = None
+    tag: str = None
+
+    html_url: str = None
+
+    is_pre_release: bool = False
+    text_details: str = None
+    published_at: str = None
+    published_at_timestamp: datetime = None
+    archive_name: str = None
+    archive_url: str = None
+    archive_size = None
+    archive_id: str = None
+
+
