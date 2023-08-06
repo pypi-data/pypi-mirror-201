a58bd8cbdf47ee51f7b1b15bdc95257fb5ce20e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:45 +0000

    Add package python-helper/0.3.63/python_helper-0.3.63.tar.gz

---
+++ packages/python-helper/python_helper-0.3.63.tar.gz/python_helper-0.3.63/Tests.py
@@ -0,0 +1,189 @@
+from python_helper import TestHelper
+TestHelper.run(__file__, inspectGlobals=False)
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         'ModuleImportsTest.allImportedSuccesfuly'
+#     ],
+#     ignoreModules = [
+#         'ModuleImportsTest'
+#     ]
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         # 'ReflectionHelperTest.instanciateItWithNoArgsConstructor_withSuccess',
+#         'ReflectionHelperTest.getAttributeOrMethodByNamePath'
+#     ]
+# )
+# TestHelper.run(__file__,
+#     testStatus = True,
+#     logStatus = True,
+#     logResult = True,
+#     inspectGlobals=False
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         'DateTimeHelperTest.plusMonths',
+#         'DateTimeHelperTest.minusMonths'
+#     ]
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         # 'ObjectHelperTest.equal_whenListOfDictionaries',
+#         'ObjectHelperTest.equal_whenSets'
+#     ]
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         # 'SettingHelperTest.mustHandleLateValue',
+#         # 'SettingHelperTest.updateActiveEnvironment_withSuccess',
+#         # 'SettingHelperTest.mustReadSettingFile',
+#         # 'SettingHelperTest.mustNotReadSettingFile',
+#         # 'SettingHelperTest.mustPrintSettingTree',
+#         # 'SettingHelperTest.mustHandleSettingValueInFallbackSettingTree',
+#         # 'SettingHelperTest.mustHandleSettingValueInFallbackSettingTree_whenFallbackSettingFilePathIsPassedInsted',
+#         # 'SettingHelperTest.getSettingTree_whenIsSettingKeyActuallyContainsSettingKey',
+#         # 'SettingHelperTest.getSettingTree_fallbackPriority',
+#         # 'SettingHelperTest.getSettingTree_otherApplication',
+#         # 'SettingHelperTest.getSettingTree_whenThereAreNoneValuesAllOverThePlace',
+#         # 'SettingHelperTest.getBooleanSetting'
+#     ],
+#     # testStatus = False,
+#     # logStatus = False,
+#     # logResult = True,
+#     # inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 2,
+#     testStatus = False,
+#     logStatus = True,
+#     logResult = True,
+#     inspectGlobals = True
+# )
+# TestHelper.run(
+#     __file__,
+#     # times = 10,
+#     runOnly = [
+#         'LogHelperTest.mustLogWithColors',
+#         'LogHelperTest.mustLogWithoutColors',
+#         'LogHelperTest.mustLogWithoutColorsAsWell',
+#         'LogHelperTest.mustLogEnvironmentSettings',
+#         'LogHelperTest.mustLogPretyPythonWithoutColors',
+#         'LogHelperTest.mustLogPretyPythonWithColors',
+#         'LogHelperTest.mustLogPretyJsonWithColors',
+#         'LogHelperTest.mustPrintMessageLog_withColors'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = False,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 100,
+#     runOnly = [
+#         'StringHelperTest.prettyJson_withSucces'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = True,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 100,
+#     runOnly = [
+#         'StringHelperTest.prettyPython_withSucces'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = True,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 10,
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = True,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 1,
+#     runOnly = [
+#         'StringHelperTest.prettifyPerformance'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = True,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 1,
+#     runOnly = [
+#         'ModuleImportsTest.allImportedSuccesfuly'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = False,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 1,
+#     runOnly = [
+#         'SettingHelperTest.getSettingTree_fallbackPriority'
+#     ],
+#     settingStatus = False,
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = False,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 1,
+#     runOnly = [
+#         'MethodAnnotationTest.Function_withSuccess'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = False,
+#     inspectGlobals = False
+# )
+# TestHelper.run(
+#     __file__,
+#     times = 1,
+#     runOnly = [
+#         'LogHelperTest.mustLogWithColors'
+#     ],
+#     testStatus = False,
+#     logStatus = False,
+#     logResult = False,
+#     inspectGlobals = True
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         'ReflectionHelperTest.getName_manyCases'
+#     ]
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         'EnvironmentHelperTest.osIdentifierTest'
+#     ]
+# )
+# TestHelper.run(
+#     __file__,
+#     runOnly = [
+#         'SettingHelperTest.getSettingTree_whenThereAreNoneValuesAllOverThePlace'
+#     ]
+# )
