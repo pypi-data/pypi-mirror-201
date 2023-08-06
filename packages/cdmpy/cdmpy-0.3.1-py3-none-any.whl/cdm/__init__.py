2377dbd0ae28d451caf53c9aa96911b2d0ebf562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:22 +0000

    Add package cdmpy/0.3.1/cdmpy-0.3.1-py3-none-any.whl

---
+++ packages/cdmpy/cdmpy-0.3.1-py3-none-any.whl/cdm/__init__.py
@@ -0,0 +1,10 @@
+# Author: Lu Xu <oliver_lew@outlook.com">
+# License: MIT
+# Original Repo: https://github.com/OliverLew/fio-cdm
+# Packaging: https://github.com/Pythoniasm/cdmpy
+
+
+from cdm.__main__ import Job, get_parser, entrypoint
+
+
+__all__ = ["get_parser", "entrypoint", "Job"]
