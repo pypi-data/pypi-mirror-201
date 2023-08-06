4fc9585c00ad6f124a305a7d7d89a462078d7de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:20 +0000

    Add package rdpywheel/0.1/rdpywheel-0.1.tar.gz

---
+++ packages/rdpywheel/rdpywheel-0.1.tar.gz/rdpywheel-0.1/README.md
@@ -0,0 +1,18 @@
+### pywheel
+
+Python项目的公共代码。
+
+
+### 安装
+
+```bash
+# 本地安装此库
+/usr/local/bin/python3.10 setup.py install
+```
+
+```bash
+# 打包
+/usr/local/bin/python3.10 setup.py sdist bdist_wheel
+# 上传
+twine upload dist/*              
+```
---
+++ packages/rdpywheel/rdpywheel-0.1.tar.gz/rdpywheel-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='rdpywheel',
+    version='0.1',
+    description='My library for common code',
+    author='jiangtingqiang',
+    url='https://github.com/jiangxiaoqiang/rdpywheel.git',
+    author_email='jiangtingqiang@gmail.com',
+    packages=['rdpywheel'],
+)
