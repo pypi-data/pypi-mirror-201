110fe54eb44a675fd1c56cc76f75bb2865106556
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package openai-game-translator/0.0.5/openai-game-translator-0.0.5.tar.gz

---
+++ packages/openai-game-translator/openai-game-translator-0.0.5.tar.gz/openai-game-translator-0.0.5/README.md
@@ -0,0 +1,28 @@
+# openai-game-translator
+ChatGPT API based video game audio translator application and web service
+
+[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+![GitHub issues](https://img.shields.io/github/issues/Erisae/openai-game-translator)
+![build and test](https://github.com/Erisae/openai-game-translator/actions/workflows/test_and_coverage.yml/badge.svg)
+![coverage](./test/coverage.svg)
+
+
+## Overview
+A game translation app that uses the ChatGPT API to recognize in-game speech (and even game visuals) and provide smooth text translations on platforms like Switch and PS5, thanks to the powerful language abilities of GPT.
+
+## Installation
+```shell
+pip install -i https://test.pypi.org/simple/ openai-game-translator==0.0.5
+```
+
+## Quick Start
+In terminal, run
+```shell
+translate --xunfei_appid <appid> --xunfei_apikey  <apikey> --xunfei_apisecret <apisecret> --openai_key <key> -t <model> -o <language> --pre_recorded <use_prerecored:0|1> --file <audio_path>
+```
+- `<model>`: transcription model, choose from `aws_pre`, `aws_live` and `xunfei`.
+- `<language>`: translation target language, for example "English"
+- `<use_prerecored>`: whether to use prerecorded audio, 0 no, 1 yes
+- `<audio_path>`: prerecorded or transcription related audio file path
+
+
---
+++ packages/openai-game-translator/openai-game-translator-0.0.5.tar.gz/openai-game-translator-0.0.5/tests/test_all.py
@@ -0,0 +1,72 @@
+import unittest
+import argparse
+import openai
+
+from test_xunfei import TestXFTranscriptor
+from test_aws_live import TestLiveStream
+from test_aws_pre import TestPrerecorded
+from test_openai import TestOpenai
+from test_record import TestRecord
+from test_game_translator import TranslatorTest
+
+parser_test = argparse.ArgumentParser(description="Description of your program")
+parser_test.add_argument(
+    "--xunfei_appid", required=True, type=str, help="xunfei transcription appid"
+)
+parser_test.add_argument(
+    "--xunfei_apikey", required=True, type=str, help="xunfei transcription apikey"
+)
+parser_test.add_argument(
+    "--xunfei_apisecret",
+    required=True,
+    type=str,
+    help="xunfei transccription api secret",
+)
+parser_test.add_argument(
+    "--openai_key", required=True, type=str, help="open ai translatio api key"
+)
+args_test = parser_test.parse_args()
+openai.api_key = args_test.openai_key
+
+if __name__ == "__main__":
+    # define overall testsuite
+    test_suite = unittest.TestSuite()
+
+    # add testsuites
+    TestXFTranscriptor.appid = args_test.xunfei_appid
+    TestXFTranscriptor.apikey = args_test.xunfei_apikey
+    TestXFTranscriptor.apisecret = args_test.xunfei_apisecret
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestXFTranscriptor))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestPrerecorded))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestLiveStream))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestOpenai))
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestRecord))
+
+    # add integration test
+    TranslatorTest.appid = args_test.xunfei_appid
+    TranslatorTest.apikey = args_test.xunfei_apikey
+    TranslatorTest.apisecret = args_test.xunfei_apisecret
+    test_suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TranslatorTest))
+
+    # run all tests and generate coverage report
+    # cov = coverage.Coverage(
+    #     include=[
+    #         "../aws_streaming_transcription/*",
+    #         "../openai_translation/*",
+    #         "../xunfei_speed_transcription/*",
+    #         "../game_translator.py",
+    #         "../audio/record.py",
+    #     ],
+    #     omit=["../test/*"],
+    # )
+    # cov.start()
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(test_suite)
+
+    # cov.stop()
+    # cov.report()
+    # cov.html_report(directory="covhtml")
+
+
+# should comment the argparser and the main part in game_translator.py to run this script
