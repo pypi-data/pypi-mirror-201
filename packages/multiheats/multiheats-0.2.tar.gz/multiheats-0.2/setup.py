5b4397b17ebc4530597957e4f108f99359cde32f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:47 +0000

    Add package multiheats/0.2/multiheats-0.2.tar.gz

---
+++ packages/multiheats/multiheats-0.2.tar.gz/multiheats-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="multiheats",
+    version="0.2",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    description="MultIHeaTS is a Multi-layered Implicit Heat Transfer Solver. It uses finite differences to  simulates and predicts the surface temperature in 1D multi-layered planetary surfaces exposed to solar radiation.",
+    install_requires=[
+        "wheel",
+        "ipython",
+        "numpy",
+        "matplotlib",
+        "scipy",
+        "pandas",
+        "tqdm",
+        "xlrd",
+    ],
+)
