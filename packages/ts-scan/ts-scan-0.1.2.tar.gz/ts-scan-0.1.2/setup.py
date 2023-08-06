ac4f0166b4620d7de1d5a99f2de9e2548ab03f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:55:10 +0000

    Add package ts-scan/0.1.2/ts-scan-0.1.2.tar.gz

---
+++ packages/ts-scan/ts-scan-0.1.2.tar.gz/ts-scan-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='ts-scan',
+    packages=[
+        'ts_scan',
+        'ts_scan.pm'
+    ],
+    version='0.1.2',
+    description='TrustSource PM scanner',
+    author='EACG GmbH',
+    license='Apache-2.0',
+    url='https://github.com/trustsource/ts-scan.git',
+    download_url='',
+    keywords=['scanning', 'dependencies', 'modules', 'compliance', 'TrustSource'],
+    classifiers=[],
+    install_requires=[
+        'wheel',
+        'build',
+        'importlib-metadata',
+        'alive-progress',
+        'ts-python-client==2.0.2',
+        'ts-deepscan==2.0.2'
+    ],
+    scripts=['ts-scan'],
+    entry_points={
+        'console_scripts': ['ts-scan=ts_scan.cli:main'],
+    }
+)
