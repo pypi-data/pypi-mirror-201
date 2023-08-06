da034b76ec0aa39e3003efd8f3ed63bbceddf4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:13 +0000

    Add package protein-variant-nomenclature-parser/0.5.0/protein-variant-nomenclature-parser-0.5.0.tar.gz

---
+++ packages/protein-variant-nomenclature-parser/protein-variant-nomenclature-parser-0.5.0.tar.gz/protein-variant-nomenclature-parser-0.5.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="protein-variant-nomenclature-parser",
+    version="0.5.0",
+    packages=find_packages(),
+    url="https://github.com/tansey-lab/protein-variant-nomenclature-parser",
+    author="Jeff Quinn",
+    install_requires=[],
+)
