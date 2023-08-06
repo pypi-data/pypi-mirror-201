6c7c0d7e8e724660e1b5dc538d25d6136fdaf2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:47 +0000

    Add package strkit-rust-ext/0.1.0/strkit_rust_ext-0.1.0-cp39-none-win_amd64.whl

---
+++ packages/strkit-rust-ext/strkit_rust_ext-0.1.0-cp39-none-win_amd64.whl/strkit_rust_ext/__init__.pyi
@@ -0,0 +1,18 @@
+def mk_snvs_dict(
+    query_sequence: str,
+    pairs: list[tuple[int, int]],
+    ref_seq: str,
+    ref_coord_start: int,
+    tr_start_pos: int,
+    tr_end_pos: int,
+) -> dict[int, str]: ...
+
+
+def mk_snvs_hash(
+    query_sequence: str,
+    pairs: list[tuple[int, int]],
+    ref_seq: str,
+    ref_coord_start: int,
+    tr_start_pos: int,
+    tr_end_pos: int,
+) -> dict[int, str]: ...
