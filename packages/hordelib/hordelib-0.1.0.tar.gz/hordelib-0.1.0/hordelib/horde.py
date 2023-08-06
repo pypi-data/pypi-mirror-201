0420ddd743d4d9512c76646c79b33fc34bf04033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:11 +0000

    Add package hordelib/0.1.0/hordelib-0.1.0.tar.gz

---
+++ packages/hordelib/hordelib-0.1.0.tar.gz/hordelib-0.1.0/hordelib/horde.py
@@ -0,0 +1,110 @@
+# horde.py
+# Main interface for the horde to this library.
+import contextlib
+
+from PIL import Image
+
+from hordelib.comfy_horde import Comfy_Horde
+from hordelib.model_manager.hyper import ModelManager
+from hordelib import install_comfy
+from hordelib.consts import COMFYUI_VERSION
+from hordelib.shared_model_manager import SharedModelManager
+
+
+class HordeLib:
+    # Horde to comfy sampler mapping
+    SAMPLERS_MAP = {
+        "k_euler": "euler",
+        "k_euler_a": "euler_ancestral",
+        "k_heun": "heun",
+        "k_dpm_2": "dpm_2",
+        "k_dpm_2_a": "dpm_2_ancestral",
+        "k_lms": "lms",
+        "k_dpm_fast": "dpm_fast",
+        "k_dpm_adaptive": "dpm_adaptive",
+        "k_dpmpp_2s_a": "dpmpp_2s_ancestral",
+        "k_dpmpp_sde": "dpmpp_sde",
+        "k_dpmpp_2m": "dpmpp_2m",
+        "ddim": "ddim",
+        "uni_pc": "uni_pc",
+        "uni_pc_bh2": "uni_pc_bh2",
+        "plms": "<not supported>",
+    }
+
+    # Horde to tex2img parameter mapping
+    # XXX Items mapped to None are ignored for now
+    TEXT_TO_IMAGE_PARAMS = {
+        "sampler_name": "sampler.sampler_name",
+        "cfg_scale": "sampler.cfg",
+        "denoising_strength": "sampler.denoise",
+        "seed": "sampler.seed",
+        "height": "empty_latent_image.height",
+        "width": "empty_latent_image.width",
+        # "karras": false,
+        "tiling": None,
+        "hires_fix": None,
+        "clip_skip": "clip_skip.stop_at_clip_layer",
+        "control_type": None,
+        "image_is_control": None,
+        "return_control_map": None,
+        # "prompt": "string",
+        "ddim_steps": "sampler.steps",
+        "n_iter": "empty_latent_image.batch_size",
+        "model": "model_loader.ckpt_name",
+    }
+
+    def _parameter_remap(self, payload: dict[str, str | None]) -> dict[str, str | None]:
+        params = {}
+        # Extract from the payload things we understand
+        for key, value in payload.items():
+            newkey = HordeLib.TEXT_TO_IMAGE_PARAMS.get(key, None)
+            if newkey:
+                params[newkey] = value
+
+        # XXX I think we need seed as an integer
+        with contextlib.suppress(ValueError):
+            params["sampler.seed"] = int(params["sampler.seed"])
+
+        # karras flag determines which scheduler we use
+        if payload.get("karras", False):
+            params["sampler.scheduler"] = "karras"
+        else:
+            params["sampler.scheduler"] = "normal"
+
+        # We break prompt up on horde's "###"
+        promptsCombined = payload.get("prompt", "")
+
+        if promptsCombined is None:  # XXX
+            raise TypeError("`None` value encountered!")
+
+        promptsSplit = [x.strip() for x in promptsCombined.split("###")][:2]
+        if len(promptsSplit) == 1:
+            params["prompt.text"] = promptsSplit[0]
+            params["negative_prompt.text"] = ""
+        elif len(promptsSplit) == 2:
+            params["prompt.text"] = promptsSplit[0]
+            params["negative_prompt.text"] = promptsSplit[1]
+
+        # Sampler remap
+        sampler = HordeLib.SAMPLERS_MAP.get(params["sampler.sampler_name"], "euler")
+        params["sampler.sampler_name"] = sampler
+
+        # Clip skip inversion, comfy uses -1, -2, etc
+        clip_skip_key = "clip_skip.stop_at_clip_layer"
+        if params.get(clip_skip_key, 0) > 0:
+            params[clip_skip_key] = -params[clip_skip_key]
+
+        # Inject model manager
+        params["model_loader.model_manager"] = SharedModelManager
+
+        return params
+
+    def text_to_image(self, payload: dict[str, str | None]) -> Image.Image | None:
+        generator = Comfy_Horde()
+        images = generator.run_image_pipeline(
+            "stable_diffusion", self._parameter_remap(payload)
+        )
+        if images is None:
+            return None  # XXX Log error and/or raise Exception here
+        # XXX Assumes the horde only asks for and wants 1 image
+        return Image.open(images[0]["imagedata"])
---
+++ packages/hordelib/hordelib-0.1.0.tar.gz/hordelib-0.1.0/tests/test_horde.py
@@ -0,0 +1,237 @@
+# test_horde.py
+import pytest
+
+from hordelib.horde import HordeLib
+from hordelib.shared_model_manager import SharedModelManager
+
+
+class TestSharedModelManager:
+    horde = HordeLib()
+    default_model_manager_args: dict
+
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        self.horde = HordeLib()
+
+        self.default_model_manager_args = {
+            # aitemplate
+            "blip": True,
+            "clip": True,
+            "codeformer": True,
+            "compvis": True,
+            "controlnet": True,
+            "diffusers": True,
+            # "esrgan": True,
+            # "gfpgan": True,
+            "safety_checker": True,
+        }
+        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
+        assert SharedModelManager.manager is not None
+        yield
+        del self.horde
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_compvis(self):
+        from hordelib.model_manager.compvis import CompVisModelManager
+
+        CompVisModelManager()
+
+    def test_horde_model_manager_init(self):
+        assert SharedModelManager.manager is not None
+        # assert SharedModelManager.manager.aitemplate is not None
+        assert SharedModelManager.manager.blip is not None
+        assert SharedModelManager.manager.clip is not None
+        assert SharedModelManager.manager.codeformer is not None
+        assert SharedModelManager.manager.compvis is not None
+        assert SharedModelManager.manager.controlnet is not None
+        assert SharedModelManager.manager.diffusers is not None
+        assert SharedModelManager.manager.safety_checker is not None
+
+    def test_horde_model_manager_reload_db(self):
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.reload_database()
+
+    def test_horde_model_manager_download_model(self):
+        assert SharedModelManager.manager is not None
+        result: bool | None = SharedModelManager.manager.download_model("Deliberate")
+        assert result is True
+
+    def test_horde_model_manager_validate(self):
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate")
+        result: bool | None = SharedModelManager.manager.validate_model("Deliberate")
+        assert result is True
+
+    def test_taint_models(self):
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.taint_models(["Deliberate"])
+        assert "Deliberate" not in SharedModelManager.manager.get_available_models()
+        assert "Deliberate" not in SharedModelManager.manager.get_loaded_models_names()
+
+    # XXX add a test for model missing?
+    def test_horde_model_manager_unload_model(self):
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate")
+        assert "Deliberate" in SharedModelManager.manager.get_loaded_models_names()
+        result = SharedModelManager.manager.unload_model("Deliberate")
+        assert result is True
+        assert "Deliberate" not in SharedModelManager.manager.get_loaded_models_names()
+
+
+class TestHordeInference:
+    @pytest.fixture(autouse=True)
+    def setup_and_teardown(self):
+        self.horde = HordeLib()
+
+        self.default_model_manager_args = {
+            # aitemplate
+            "blip": True,
+            "clip": True,
+            "codeformer": True,
+            "compvis": True,
+            "controlnet": True,
+            "diffusers": True,
+            # "esrgan": True,
+            # "gfpgan": True,
+            "safety_checker": True,
+        }
+        SharedModelManager.loadModelManagers(**self.default_model_manager_args)
+        assert SharedModelManager.manager is not None
+        SharedModelManager.manager.load("Deliberate")
+        yield
+        del self.horde
+        SharedModelManager._instance = None
+        SharedModelManager.manager = None
+
+    def test_parameter_remap_simple(self):
+        data = {
+            "sampler_name": "k_lms",
+            "cfg_scale": 5,
+            "denoising_strength": 0.75,
+            "seed": "23113",
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": "canny",
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dog ### cat, mouse, lion",
+            "ddim_steps": 30,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+
+        expected = {
+            "sampler.sampler_name": "lms",
+            "sampler.cfg": 5,
+            "sampler.denoise": 0.75,
+            "sampler.seed": 23113,
+            "empty_latent_image.height": 512,
+            "empty_latent_image.width": 512,
+            "sampler.steps": 30,
+            "empty_latent_image.batch_size": 1,
+            "model_loader.ckpt_name": "Deliberate",
+            "sampler.scheduler": "karras",
+            "prompt.text": "a dog",
+            "negative_prompt.text": "cat, mouse, lion",
+            "clip_skip.stop_at_clip_layer": -1,
+            "model_loader.model_manager": SharedModelManager,
+        }
+        assert self.horde is not None
+        result = self.horde._parameter_remap(data)
+        assert result == expected, f"Dictionaries don't match: {result} != {expected}"
+
+    def test_parameter_remap_variation(self):
+        data = {
+            "sampler_name": "k_lms",
+            "cfg_scale": 5,
+            "denoising_strength": 0.75,
+            "seed": "23113",
+            "height": 512,
+            "width": 512,
+            "karras": False,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": "canny",
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "a dog",
+            "ddim_steps": 30,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+
+        expected = {
+            "sampler.sampler_name": "lms",
+            "sampler.cfg": 5,
+            "sampler.denoise": 0.75,
+            "sampler.seed": 23113,
+            "empty_latent_image.height": 512,
+            "empty_latent_image.width": 512,
+            "sampler.steps": 30,
+            "empty_latent_image.batch_size": 1,
+            "model_loader.ckpt_name": "Deliberate",
+            "sampler.scheduler": "normal",
+            "prompt.text": "a dog",
+            "negative_prompt.text": "",
+            "clip_skip.stop_at_clip_layer": -1,
+            "model_loader.model_manager": SharedModelManager,
+        }
+        assert self.horde is not None
+        result = self.horde._parameter_remap(data)
+        assert result == expected, f"Dictionaries don't match: {result} != {expected}"
+
+    def test_text_to_image(self):
+        data = {
+            "sampler_name": "k_dpmpp_2m",
+            "cfg_scale": 7.5,
+            "denoising_strength": 1.0,
+            "seed": 123456789,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 1,
+            "control_type": "canny",
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "an ancient llamia monster",
+            "ddim_steps": 25,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.text_to_image(data)
+        assert pil_image is not None
+        pil_image.save("horde_text_to_image.png")
+
+    def test_text_to_image_clip_skip_2(self):
+        data = {
+            "sampler_name": "k_dpmpp_2m",
+            "cfg_scale": 7.5,
+            "denoising_strength": 1.0,
+            "seed": 123456789,
+            "height": 512,
+            "width": 512,
+            "karras": True,
+            "tiling": False,
+            "hires_fix": False,
+            "clip_skip": 2,
+            "control_type": "canny",
+            "image_is_control": False,
+            "return_control_map": False,
+            "prompt": "an ancient llamia monster",
+            "ddim_steps": 25,
+            "n_iter": 1,
+            "model": "Deliberate",
+        }
+        assert self.horde is not None
+        pil_image = self.horde.text_to_image(data)
+        assert pil_image is not None
+        pil_image.save("horde_text_to_image_clip_skip_2.png")
