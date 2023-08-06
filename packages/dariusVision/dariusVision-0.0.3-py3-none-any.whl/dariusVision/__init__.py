19b99ee235333bd05cac80cc31714de7bd69bb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:35 +0000

    Add package dariusVision/0.0.3/dariusVision-0.0.3-py3-none-any.whl

---
+++ packages/dariusVision/dariusVision-0.0.3-py3-none-any.whl/dariusVision/__init__.py
@@ -0,0 +1,9 @@
+# from .cam import lastest_Cam
+from .cam import lastest_Cam2 
+from .realsense2 import rs2_AsyncCam
+from .utils import *
+
+version='0.0.3'
+
+def get_version():
+    return version
