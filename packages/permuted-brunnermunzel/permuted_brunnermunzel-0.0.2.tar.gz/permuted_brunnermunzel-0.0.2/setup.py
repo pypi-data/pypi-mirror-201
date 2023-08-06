0017d010f693afd555af23a9988d42178d491760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:02 +0000

    Add package permuted-brunnermunzel/0.0.2/permuted_brunnermunzel-0.0.2.tar.gz

---
+++ packages/permuted-brunnermunzel/permuted_brunnermunzel-0.0.2.tar.gz/permuted_brunnermunzel-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(name='permuted_brunnermunzel',
+      version='0.0.2',
+      description='Python implementation of the permuted brunner munzel',
+      url='https://github.com/MatthewCorney',
+      author='Matthew Corney',
+      author_email='matthew_corney@yahoo.co.uk',
+      license='MIT',
+      package_dir={'':"src"},
+      packages=find_packages("src"),
+      install_requires=[
+          'numpy', 'scipy',
+      ],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      zip_safe=False)
