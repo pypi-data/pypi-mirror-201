a148bd9b6c825289bd6602d4b01982a75f32c60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:55 +0000

    Add package inspectnn/0.2.1/inspectnn-0.2.1.tar.gz

---
+++ packages/inspectnn/inspectnn-0.2.1.tar.gz/inspectnn-0.2.1/inspectnn/__init__.py
@@ -0,0 +1,11 @@
+from inspectnn.Conv.ConvLayer import ConvLayer
+from inspectnn.Dense.DenseLayer import DenseLayer
+from inspectnn import Dataset
+from inspectnn.Pooling.PoolingLayer import PoolingLayer
+from inspectnn.Flatten.FlattenLayer import FlattenLayer
+from inspectnn.Model.NetworkModel import NetworkModel
+from inspectnn.Model.GenericModel_tflite import GenericModelTflite
+name = "inspectnn"
+__version__ = "0.2.1"
+__author__ = "Sdeaalvatore Barone, Filippo Ferrandino"
+__credits__ = "Department of Electrical Engineering and Information Technologies, University of Naples Federico II, Via Claudio 21, Naples, Italy"
