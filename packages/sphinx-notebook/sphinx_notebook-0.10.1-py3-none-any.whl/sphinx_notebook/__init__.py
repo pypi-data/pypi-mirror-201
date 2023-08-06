4cc5ca485dc00ff948013a00ae6e0c5a4787a760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:57:22 +0000

    Add package sphinx-notebook/0.10.1/sphinx_notebook-0.10.1-py3-none-any.whl

---
+++ packages/sphinx-notebook/sphinx_notebook-0.10.1-py3-none-any.whl/sphinx_notebook/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for Sphinx Notebook."""
+
+import re
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import anytree
+
+from . import notebook
+
+__author__ = """Justin Stout"""
+__email__ = 'midwatch@jstout.us'
+__version__ = '0.10.1'
