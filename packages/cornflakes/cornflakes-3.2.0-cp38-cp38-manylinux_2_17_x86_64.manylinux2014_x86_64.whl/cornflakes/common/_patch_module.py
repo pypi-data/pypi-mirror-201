6f9ba0d89e24d62d61a119c84ea4b6f996210f1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:12 +0000

    Add package cornflakes/3.2.0/cornflakes-3.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/cornflakes/cornflakes-3.2.0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/cornflakes/common/_patch_module.py
@@ -0,0 +1,42 @@
+import inspect
+import logging
+
+
+def isclassmethod(method):
+    """Check if a classmethod."""
+    bound_to = getattr(method, "__self__", None)
+    if not isinstance(bound_to, type):
+        # must be bound to a class
+        return False
+    name = method.__name__
+    for cls in bound_to.__mro__:
+        descriptor = vars(cls).get(name)
+        if descriptor is not None:
+            return isinstance(descriptor, classmethod)
+    return False
+
+
+def patch_module(m):
+    """Method to overwrite module variables in a generic way.
+
+    1. Overwrite names from submodules declared in __all__ to parent module.
+    2. Overwrite doc_string and adds auto summary with objects defined in __all__.
+    """
+    for obj in [m[x] for x in m["__all__"]]:
+        if not inspect.ismodule(obj) and not isclassmethod(obj):
+            try:
+                obj.__module__ = m["__name__"]
+            except Exception as e:
+                logging.debug(e)
+
+    m[
+        "__doc__"
+    ] = f"""{m["__doc__"]}
+.. currentmodule:: {m["__name__"]}
+
+.. autosummary::
+   :toctree: _generate
+
+    {'''
+    '''.join(m["__all__"])}
+"""
