2ddc4b65b1532fcca2a0539ca4bcc758244f23d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:16 +0000

    Add package ISCV/2.1.1/ISCV-2.1.1.tar.gz

---
+++ packages/ISCV/ISCV-2.1.1.tar.gz/ISCV-2.1.1/README.md
@@ -0,0 +1,15 @@
+# ISCV
+ISCV (Interactive Simulations using Computer Vision) is an python library that lets you use machine learning models in an easy and productive way for your project.
+
+![ISCV 'Interactive Simulations Using Computer Vision Logo'](https://raw.githubusercontent.com/looph0le/ISCV/main/logo.png)
+
+## AIM
+Our aim through this project is to provide a better development experience of developing machine learning applications which require machine learning models and configurations.
+This project lets you use our pre configured modules still with ability to configure important parameters.
+
+## Modules
+- [X] Hand Tracking Unit
+- [X] Face Mesh Unit
+- [X] Full Body Motion Capture 
+- [X] Mouse Controls
+- [X] Mouse Click Actions
---
+++ packages/ISCV/ISCV-2.1.1.tar.gz/ISCV-2.1.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '2.1.1'
+DESCRIPTION = 'ISCV is a computer vision library.'
+LONG_DESCRIPTION = 'ISCV (Interactive Simulations using Computer Vision) is an python library that lets you use machine learning models in an easy and productive way for your project.'
+
+# Setting up
+setup(
+    name="ISCV",
+    version=VERSION,
+    author="Mitansh Disha Meet Parnika",
+    author_email="<mitanshpanchal@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['opencv-python', 'pyautogui', 'numpy', 'mediapipe'],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets', 'computer vision', 'machine learning', 'AI'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
