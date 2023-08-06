730a41b475a61dd1bda8601a4b49b039556c139d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:30 +0000

    Add package e2e-client/1.2.9/e2e_client-1.2.9.tar.gz

---
+++ packages/e2e-client/e2e_client-1.2.9.tar.gz/e2e_client-1.2.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='e2e_client',
+    version='1.2.9',
+    description="This a E2E Client tool for Certbot Plugin",
+    author="Abhay Bhati",
+    author_email="abhaybhati987@gmail.com",
+    packages=find_packages(include=["e2e_client"]),
+    install_requires=['requests', 'setuptools', 'python-whois'],
+    include_package_data=True,
+)
