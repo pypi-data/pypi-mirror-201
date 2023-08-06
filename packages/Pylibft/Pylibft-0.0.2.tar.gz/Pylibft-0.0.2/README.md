cfacefd6b18ee1284a16855e7c325a443e07028c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:52 +0000

    Add package Pylibft/0.0.2/Pylibft-0.0.2.tar.gz

---
+++ packages/Pylibft/Pylibft-0.0.2.tar.gz/Pylibft-0.0.2/README.md
@@ -0,0 +1,17 @@
+# Pylibft
+Personal python module for my daily usage on CTF/Pentest/Coding
+
+```python
+base_convert(number: str, fromBase: int, toBase: int)
+```
+Same as the `PHP` function, return a string containing `number` represented in base `toBase`.
+
+```python
+bin2hex(string: str)
+```
+Same as the `PHP` function, return string containing the hexadecimal representation of string.
+
+```python
+hex2bin(string: str)
+```
+Same as the `PHP` function, decode a hexadecimally encoded binary string.
---
+++ packages/Pylibft/Pylibft-0.0.2.tar.gz/Pylibft-0.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.2'
+DESCRIPTION = 'You can find here some functions that can help you during the automation in CTF/Pentest.'
+
+# Setting up
+setup(
+    name="Pylibft",
+    version=VERSION,
+    author="Wiloti",
+    author_email="contact@wiloti.fr",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['termcolor', 'typing_extensions'],
+    keywords=['python', 'CTF', 'Pentest', 'automation'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
