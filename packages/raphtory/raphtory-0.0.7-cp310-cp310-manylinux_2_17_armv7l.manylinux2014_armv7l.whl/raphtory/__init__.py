a7708e762908c6fe39e73dd0291f2a459412128b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:39 +0000

    Add package raphtory/0.0.7/raphtory-0.0.7-cp310-cp310-manylinux_2_17_armv7l.manylinux2014_armv7l.whl

---
+++ packages/raphtory/raphtory-0.0.7-cp310-cp310-manylinux_2_17_armv7l.manylinux2014_armv7l.whl/raphtory/__init__.py
@@ -0,0 +1,13 @@
+import sys
+from .raphtory import *
+sys.modules["raphtory.algorithms"] = algorithms
+sys.modules["raphtory.graph_gen"] = graph_gen
+sys.modules["raphtory.graph_loader"] = graph_loader
+
+
+from .plot import draw
+from .nullmodels import *
+
+__doc__ = raphtory.__doc__
+if hasattr(raphtory, "__all__"):
+    __all__ = raphtory.__all__
