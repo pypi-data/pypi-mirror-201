500d4b498ab2a9ab976099486935dd6b855ad417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:24 +0000

    Add package lndb/0.40.2/lndb-0.40.2.tar.gz

---
+++ packages/lndb/lndb-0.40.2.tar.gz/lndb-0.40.2/lndb/dev/__init__.py
@@ -0,0 +1,19 @@
+"""Developer API.
+
+.. autosummary::
+   :toctree:
+
+   UserSettings
+   InstanceSettings
+   StorageSettings
+
+"""
+from ._deprecated import deprecated
+from ._docs import doc_args
+from ._settings_instance import InstanceSettings
+from ._settings_user import UserSettings
+from ._storage import StorageSettings
+from ._testdb import setup_local_test_postgres, setup_local_test_sqlite_file
+
+# below is for backward compat
+Storage = StorageSettings
