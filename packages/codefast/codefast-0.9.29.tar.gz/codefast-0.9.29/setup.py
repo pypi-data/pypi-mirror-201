5ff47e2fbbf9dcf0370d0409dddda5774ea47e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:08 +0000

    Add package codefast/0.9.29/codefast-0.9.29.tar.gz

---
+++ packages/codefast/codefast-0.9.29.tar.gz/codefast-0.9.29/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="codefast",
+    version="0.9.29",  # Latest version .
+    author="none",
+    author_email="i@pm.me",
+    description="A package for faster coding.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'colorlog', 'lxml', 'requests', 'tqdm',
+        'smart-open', 'bs4', 'arrow', 'termcolor',
+        'pydub', 'pycryptodome', 'requests-toolbelt'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
