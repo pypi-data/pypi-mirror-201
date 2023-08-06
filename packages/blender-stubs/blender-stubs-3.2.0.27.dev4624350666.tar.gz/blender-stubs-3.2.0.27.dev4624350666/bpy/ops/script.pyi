7f27d3bded10889a11e719b938b2405ab26c00ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:19 +0000

    Add package blender-stubs/3.2.0.27.dev4624350666/blender-stubs-3.2.0.27.dev4624350666.tar.gz

---
+++ packages/blender-stubs/blender-stubs-3.2.0.27.dev4624350666.tar.gz/blender-stubs-3.2.0.27.dev4624350666/bpy/ops/script.pyi
@@ -0,0 +1,47 @@
+"""
+
+
+Script Operators
+****************
+
+:func:`execute_preset`
+
+:func:`python_file_run`
+
+:func:`reload`
+
+"""
+
+import typing
+
+def execute_preset(filepath: str = '', menu_idname: str = '') -> None:
+
+  """
+
+  Load a preset
+
+  :file:`startup/bl_operators/presets.py\:231 <https://projects.blender.org/blender/blender/src/branch/main/scripts/startup/bl_operators/presets.py#L231>`_
+
+  """
+
+  ...
+
+def python_file_run(filepath: str = '') -> None:
+
+  """
+
+  Run Python file
+
+  """
+
+  ...
+
+def reload() -> None:
+
+  """
+
+  Reload scripts
+
+  """
+
+  ...
