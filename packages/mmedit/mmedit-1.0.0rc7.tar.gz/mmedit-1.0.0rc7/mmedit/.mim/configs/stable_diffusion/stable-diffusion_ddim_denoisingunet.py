06349b9e1a62dfb3cdabe86fad97ea1cba8491fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:48 +0000

    Add package mmedit/1.0.0rc7/mmedit-1.0.0rc7.tar.gz

---
+++ packages/mmedit/mmedit-1.0.0rc7.tar.gz/mmedit-1.0.0rc7/mmedit/.mim/configs/stable_diffusion/stable-diffusion_ddim_denoisingunet.py
@@ -0,0 +1,62 @@
+unet = dict(
+    type='DenoisingUnet',
+    image_size=512,
+    base_channels=320,
+    channels_cfg=[1, 2, 4, 4],
+    unet_type='stable',
+    act_cfg=dict(type='silu'),
+    cross_attention_dim=768,
+    num_heads=8,
+    in_channels=4,
+    layers_per_block=2,
+    down_block_types=[
+        'CrossAttnDownBlock2D', 'CrossAttnDownBlock2D', 'CrossAttnDownBlock2D',
+        'DownBlock2D'
+    ],
+    up_block_types=[
+        'UpBlock2D', 'CrossAttnUpBlock2D', 'CrossAttnUpBlock2D',
+        'CrossAttnUpBlock2D'
+    ],
+    output_cfg=dict(var='fixed'))
+
+vae = dict(
+    type='EditAutoencoderKL',
+    act_fn='silu',
+    block_out_channels=[128, 256, 512, 512],
+    down_block_types=[
+        'DownEncoderBlock2D', 'DownEncoderBlock2D', 'DownEncoderBlock2D',
+        'DownEncoderBlock2D'
+    ],
+    in_channels=3,
+    latent_channels=4,
+    layers_per_block=2,
+    norm_num_groups=32,
+    out_channels=3,
+    sample_size=512,
+    up_block_types=[
+        'UpDecoderBlock2D', 'UpDecoderBlock2D', 'UpDecoderBlock2D',
+        'UpDecoderBlock2D'
+    ])
+
+diffusion_scheduler = dict(
+    type='EditDDIMScheduler',
+    variance_type='learned_range',
+    beta_end=0.012,
+    beta_schedule='scaled_linear',
+    beta_start=0.00085,
+    num_train_timesteps=1000,
+    set_alpha_to_one=False,
+    clip_sample=False)
+
+model = dict(
+    type='StableDiffusion',
+    unet=unet,
+    vae=vae,
+    text_encoder=dict(
+        type='ClipWrapper',
+        clip_type='huggingface',
+        pretrained_model_name_or_path='runwayml/stable-diffusion-v1-5',
+        subfolder='text_encoder'),
+    tokenizer='runwayml/stable-diffusion-v1-5',
+    scheduler=diffusion_scheduler,
+    test_scheduler=diffusion_scheduler)
---
+++ packages/mmedit/mmedit-1.0.0rc7.tar.gz/mmedit-1.0.0rc7/mmedit/models/data_preprocessors/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .edit_data_preprocessor import EditDataPreprocessor
+from .mattor_preprocessor import MattorPreprocessor
+
+__all__ = ['EditDataPreprocessor', 'MattorPreprocessor']
