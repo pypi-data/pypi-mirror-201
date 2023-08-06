2b13a47105e1a52b2e4486f61728ef299cc633bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:34 +0000

    Add package aioscryfall/0.4.1.dev1/aioscryfall-0.4.1.dev1.tar.gz

---
+++ packages/aioscryfall/aioscryfall-0.4.1.dev1.tar.gz/aioscryfall-0.4.1.dev1/tests/utils.py
@@ -0,0 +1,51 @@
+"""Test utility functions."""
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
+import aiofiles
+
+if TYPE_CHECKING:
+    from aioresponses import aioresponses
+
+TEST_DATA_DIR = Path(__file__).parent / "data"
+
+
+async def load_get_payload(
+    mock_aioresponses: "aioresponses", url: str, filename: str, *, status_code: int = 200
+) -> None:
+    """Load a mock payload from a file."""
+    async with aiofiles.open(TEST_DATA_DIR / filename, "rb") as file:
+        mock_aioresponses.get(url, status=status_code, body=await file.read())
+
+
+def sync_load_get_payload(
+    mock_aioresponses: "aioresponses", url: str, filename: str, *, status_code: int = 200
+) -> None:
+    """Load a mock payload from a file."""
+    with open(TEST_DATA_DIR / filename, "rb") as file:
+        mock_aioresponses.get(url, status=status_code, body=file.read())
+
+
+async def load_post_payload(
+    mock_aioresponses: "aioresponses",
+    url: str,
+    filename: str,
+    *,
+    status_code: int = 200,
+) -> None:
+    """Load a mock payload from a file."""
+    async with aiofiles.open(TEST_DATA_DIR / filename, "rb") as file:
+        mock_aioresponses.post(url, status=status_code, body=await file.read())
+
+
+def sync_load_post_payload(
+    mock_aioresponses: "aioresponses",
+    url: str,
+    filename: str,
+    *,
+    status_code: int = 200,
+) -> None:
+    """Load a mock payload from a file."""
+    with open(TEST_DATA_DIR / filename, "rb") as file:
+        mock_aioresponses.post(url, status=status_code, body=file.read())
