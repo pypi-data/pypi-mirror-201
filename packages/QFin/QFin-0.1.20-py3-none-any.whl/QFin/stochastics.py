d7ced42138c1f8b8033c0c791ddb2ab64dc643d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:21 +0000

    Add package QFin/0.1.20/QFin-0.1.20-py3-none-any.whl

---
+++ packages/QFin/QFin-0.1.20-py3-none-any.whl/QFin/stochastics.py
@@ -0,0 +1,78 @@
+from abc import ABC, abstractmethod
+from scipy.stats import norm
+import numpy as np
+
+# Abstract class framework for a stochastic process
+class StochasticModel:
+
+    @abstractmethod
+    # Risk neutral pricing kernel
+    def vanilla_pricing(self, F0, X, T, op_type="CALL"):
+        pass
+
+    # Calibration is conducted via an inputted vol surface and the vanilla pricing function
+    def calibrate(self, impl_vol, T, op_type="CALL"):
+        pass
+
+    # simulation is conducted after calibration of params to price exotics
+    @abstractmethod
+    def simulate(self):
+        pass
+
+    def __init__(self, params) -> None:
+        self.params = params
+
+
+class ArithmeticBrownianMotion(StochasticModel):
+
+    # closed form vanilla euro option pricing
+    def vanilla_pricing(self, F0, X, T, op_type="CALL"):
+        # return closed-form Bachelier call
+        if op_type == "CALL":
+            return (F0 - X)*(norm.cdf((F0 - X)/(self.params[0]*np.sqrt(T)))) + self.params[0]*np.sqrt(T)*(norm.pdf((F0 - X)/(self.params[0]*np.sqrt(T))))
+        # use call-put parity for put price
+        elif op_type == "PUT":
+            return self.vanilla_pricing(F0, X, T) - F0 + X
+        else:
+            return "Option type must be CALL/PUT"
+        
+    # simulating paths of arithmetic Brownian motion
+    def simulate(self, F0, n, dt, T):
+        paths = []
+        for i in range(n):
+            # n simulations
+            ttm = T
+            path = [F0]
+            # while the step is greator than zero diffuse the process
+            while ttm-dt > 0:
+                path.append(path[-1] + self.params[0]*np.random.randn()*np.sqrt(dt))
+                ttm -= dt
+            # final time increment
+            if dt > 0:
+                path.append(path[-1] + self.params[0]*np.random.randn()*np.sqrt(dt))
+            # append the path
+            paths.append(path)
+
+        # store paths and path characteristics
+        self.path_characteristics = (paths, n, dt, T)
+
+        # return paths and path characteristics
+        return (paths, n, dt, T)
+
+    # Calibration cannot be conducted due to flat volatility surface
+    def __init__(self, params) -> None:
+        super().__init__(params)
+
+abm = ArithmeticBrownianMotion([.3])
+
+print(abm.vanilla_pricing(100, 99, 1, "CALL"))
+
+abm.simulate(100, 10000, 1/252, 1)
+
+payoffs = []
+X = 99
+
+for path in abm.path_characteristics[0]:
+    payoffs.append(max((path[-1] - X), 0))
+
+print(np.average(payoffs))
