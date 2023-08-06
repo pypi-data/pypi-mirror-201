fe748fb93627230883ad415c43a679840e0288ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:58 +0000

    Add package fw-http-parser/0.3.0/fw-http-parser-0.3.0.tar.gz

---
+++ packages/fw-http-parser/fw-http-parser-0.3.0.tar.gz/fw-http-parser-0.3.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as md:
+	long_description = md.read()
+
+setuptools.setup(
+	name="fw-http-parser",
+	version="0.3.0",
+	author="fwRelik",
+	author_email="relik.fw@gmail.com",
+	description="Parsing data from a socket connection",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/fwRelik/fw-http-parser",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
