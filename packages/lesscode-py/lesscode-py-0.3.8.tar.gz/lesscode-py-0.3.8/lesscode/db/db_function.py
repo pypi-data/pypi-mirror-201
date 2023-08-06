45ff5b986a1fb636e47e333d000decea84401b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:15 +0000

    Add package lesscode-py/0.3.8/lesscode-py-0.3.8.tar.gz

---
+++ packages/lesscode-py/lesscode-py-0.3.8.tar.gz/lesscode-py-0.3.8/lesscode/db/db_function.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# author:chao.yy
+# email:yuyc@ishangqi.com
+# date:2021/12/14 3:44 下午
+
+class DBFunction:
+    def __init__(self, function_body, value):
+        self.function_body = function_body
+        self.value = value
