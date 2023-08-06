2b69e79757f90d7a709f60df30c51581788eef8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:09 +0000

    Add package paraphase/2.1.0/paraphase-2.1.0.tar.gz

---
+++ packages/paraphase/paraphase-2.1.0.tar.gz/paraphase-2.1.0/paraphase/genome_depth.py
@@ -0,0 +1,35 @@
+# paraphase
+# Author: Xiao Chen <xchen@pacificbiosciences.com>
+
+
+import numpy as np
+import pysam
+
+
+class GenomeDepth:
+    def __init__(self, bam, genome_depth_region_file):
+        self.bam = bam
+        self.genome_depth_region_file = genome_depth_region_file
+        self._bamh = pysam.AlignmentFile(bam, "rb")
+        self.mdepth = None
+        self.mad = None
+
+    def get_genome_depth(self):
+        depth = []
+        with open(self.genome_depth_region_file) as f:
+            for line in f:
+                at = line.split()
+                nchr = at[0]
+                pos1 = int(at[1])
+                for pos in [pos1, pos1 + 1600]:
+                    site_depth = self._bamh.count(
+                        nchr, pos - 1, pos, read_callback="all"
+                    )
+                    depth.append(site_depth)
+        self.mdepth = np.median(depth)
+        self.mad = np.median([abs(a - self.mdepth) for a in depth]) / self.mdepth
+
+    def call(self):
+        self.get_genome_depth()
+        self._bamh.close()
+        return self.mdepth, self.mad
