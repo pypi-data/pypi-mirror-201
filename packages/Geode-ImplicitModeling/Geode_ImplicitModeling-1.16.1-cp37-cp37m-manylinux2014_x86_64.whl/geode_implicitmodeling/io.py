3c169eec8004aec0cf35c53ea08fce73a780db43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:29 +0000

    Add package Geode-ImplicitModeling/1.16.1/Geode_ImplicitModeling-1.16.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.16.1-cp37-cp37m-manylinux2014_x86_64.whl/geode_implicitmodeling/io.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode_geosciences
+import opengeode_geosciencesio
+
+from .geode_implicitmodeling_py_model_io import *
+ImplicitModelingIO.initialize()
