7826f065cd1ab0ed6698ae87a25661a05a19f7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:37 +0000

    Add package render-engine-rss/0.0.0/render_engine_rss-0.0.0.tar.gz

---
+++ packages/render-engine-rss/render_engine_rss-0.0.0.tar.gz/render_engine_rss-0.0.0/src/render_engine_rss/parsers.py
@@ -0,0 +1,40 @@
+import pathlib
+import feedparser
+from typing import Any
+
+from render_engine.parsers.base_parsers import BasePageParser
+
+
+def parse_rss_feed_entry(entry: dict) -> dict[str, Any]:
+    if image := entry.get("image", None):
+        if isinstance(image, dict):
+            entry["image"] = image.get("href", entry["image"])
+
+    if content := entry.pop("content", None):
+        if isinstance(content, list):
+            for item in content:
+                if item["type"] == "text/html":
+                    return entry, item["value"]
+        elif isinstance(content, str):
+            return entry, content
+
+    elif content := entry.pop("summary", None):
+        if isinstance(content, str):
+            return entry, content
+
+    return entry, ""
+
+
+class RSSFeedPageParser(BasePageParser):
+    @staticmethod
+    def parse_content_path(content: dict) -> tuple[dict[str, Any], str]:
+        """Fething content and atttributes from a content_path"""
+        return parse_rss_feed_entry(content)
+
+    @staticmethod
+    def parse_content(content: str) -> tuple[dict[str, Any], str]:
+        return parse_rss_feed_entry(content)
+
+
+class PodcastPageParser(RSSFeedPageParser):
+    pass
