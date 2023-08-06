39c4e4665f75fc532afe3c38ced697626cc77d65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:01 +0000

    Add package testformyclass1/0.1.0/testformyclass1-0.1.0-py3-none-any.whl

---
+++ packages/testformyclass1/testformyclass1-0.1.0-py3-none-any.whl/testformyclassmodules1/testformyclassfile1.py
@@ -0,0 +1,10 @@
+__author__='timenet2300'
+__author_email__='timenet2300@gmail.com'
+
+#Define your code here.
+class myclass1:
+    def myfun1(self,x):
+        return(x**2)
+class myclass2:
+    def myfun2(self,x):
+        return(x**3)
