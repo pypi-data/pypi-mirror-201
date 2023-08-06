3576460ec1c81e3cb3d0f4bf8b6f93b9e70c3b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:17 +0000

    Add package zliPkg/11.1/zliPkg-11.1.tar.gz

---
+++ packages/zliPkg/zliPkg-11.1.tar.gz/zliPkg-11.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='zliPkg',
+    version='11.1',
+    license='MIT',
+    author='zli',
+    packages=['zliPkg'],
+    package_dir={'zliPkg': 'zliPkg'}
+)
