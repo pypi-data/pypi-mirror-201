be1bdd46393da7c2ab3895a75a6c12e5d050e66c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:51:48 +0000

    Add package gptcache/0.1.5/gptcache-0.1.5-py3-none-any.whl

---
+++ packages/gptcache/gptcache-0.1.5-py3-none-any.whl/gptcache/embedding/string.py
@@ -0,0 +1,2 @@
+def to_embeddings(data, **kwargs):
+    return data
---
+++ packages/gptcache/gptcache-0.1.5-py3-none-any.whl/gptcache/similarity_evaluation/similarity_evaluation.py
@@ -0,0 +1,11 @@
+from abc import ABCMeta, abstractmethod
+
+
+class SimilarityEvaluation(metaclass=ABCMeta):
+    @abstractmethod
+    def evaluation(self, src_dict, cache_dict, **kwargs):
+        pass
+
+    @abstractmethod
+    def range(self):
+        pass
