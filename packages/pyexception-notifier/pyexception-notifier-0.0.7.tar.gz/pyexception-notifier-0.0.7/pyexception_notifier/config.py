045371e6e25438a986b13d11773afe72d3207ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:23 +0000

    Add package pyexception-notifier/0.0.7/pyexception-notifier-0.0.7.tar.gz

---
+++ packages/pyexception-notifier/pyexception-notifier-0.0.7.tar.gz/pyexception-notifier-0.0.7/pyexception_notifier/config.py
@@ -0,0 +1,23 @@
+from pydantic import BaseSettings, root_validator
+
+from pyexception_notifier.utils.enums import TargetNotifySources
+
+
+class Settings(BaseSettings):
+    NOTIFY_TO: TargetNotifySources = None
+    BOT_TOKEN: str = None
+    CHAT_ID: str = None
+    SERVICE_NAME: str = None
+
+    class Config:
+        env_file = '.env'
+        env_prefix = 'EXCEPTION_NOTIFY_'
+
+    @root_validator
+    def root_validator_(cls, values):
+        if not all((values['BOT_TOKEN'], values['CHAT_ID'], values['SERVICE_NAME'], values['NOTIFY_TO'])):
+            raise ValueError(f"please set env vars for exception notifier package {values}")
+        return values
+
+
+settings = Settings()
---
+++ packages/pyexception-notifier/pyexception-notifier-0.0.7.tar.gz/pyexception-notifier-0.0.7/setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+if os.getenv('readme') == 'true':
+    with open('Readme.md') as f:
+        long_description = f.read()
+else:
+    long_description = ''
+
+
+setup(
+    name='pyexception-notifier',
+    packages=find_packages(),
+    version='0.0.7',
+    description='Exception notifier',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='r.islyamgali',
+    license='MIT',
+    install_requires=[
+        "pydantic",
+        "uvicorn",
+        "requests(>=2.27,<3.0)",
+        "python-dotenv",
+        "gunicorn[standard]",
+    ],
+    python_requires=">=3.8"
+)
