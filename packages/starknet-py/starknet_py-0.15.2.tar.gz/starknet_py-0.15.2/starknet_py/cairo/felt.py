e63172a8cc8df1e80b6786adf868a6578fecbf9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:52 +0000

    Add package starknet-py/0.15.2/starknet_py-0.15.2.tar.gz

---
+++ packages/starknet-py/starknet_py-0.15.2.tar.gz/starknet_py-0.15.2/starknet_py/cairo/felt.py
@@ -0,0 +1,99 @@
+import warnings
+from typing import List
+
+from starkware.cairo.lang.compiler.ast.cairo_types import (
+    CairoType,
+    TypeFelt,
+    TypePointer,
+)
+from starkware.cairo.lang.compiler.identifier_definition import StructDefinition
+
+from starknet_py.constants import FIELD_PRIME
+
+CairoData = List[int]
+
+
+def is_felt_pointer(cairo_type: CairoType) -> bool:
+    warnings.warn(
+        "Function is_felt_pointer is deprecated. Use starknet_py.cairo.type_parser module instead.",
+        category=DeprecationWarning,
+    )
+    return isinstance(cairo_type, TypePointer) and isinstance(
+        cairo_type.pointee, TypeFelt
+    )
+
+
+def is_uint256(definition: StructDefinition) -> bool:
+    warnings.warn(
+        "Function is_uint256 is deprecated. Use starknet_py.cairo.type_parser module instead.",
+        category=DeprecationWarning,
+    )
+    (struct_name, *_) = definition.full_name.path
+
+    return (
+        struct_name == "Uint256"
+        and len(definition.members.items()) == 2
+        and definition.members.get("low")
+        and definition.members.get("high")
+        and isinstance(definition.members["low"].cairo_type, TypeFelt)
+        and isinstance(definition.members["high"].cairo_type, TypeFelt)
+    )
+
+
+MAX_UINT256 = (1 << 256) - 1
+MIN_UINT256 = 0
+
+
+def uint256_range_check(value: int):
+    if not MIN_UINT256 <= value <= MAX_UINT256:
+        raise ValueError(
+            f"Uint256 is expected to be in range [0;2**256), got: {value}."
+        )
+
+
+MIN_FELT = -FIELD_PRIME // 2
+MAX_FELT = FIELD_PRIME // 2
+
+
+def is_in_felt_range(value: int) -> bool:
+    return 0 <= value < FIELD_PRIME
+
+
+def cairo_vm_range_check(value: int):
+    if not is_in_felt_range(value):
+        raise ValueError(
+            f"Felt is expected to be in range [0; {FIELD_PRIME}), got: {value}."
+        )
+
+
+def encode_shortstring(text: str) -> int:
+    """
+    A function which encodes short string value (at most 31 characters) into cairo felt (MSB as first character)
+
+    :param text: A short string value in python
+    :return: Short string value encoded into felt
+    """
+    if len(text) > 31:
+        raise ValueError(
+            f"Shortstring cannot be longer than 31 characters, got: {len(text)}."
+        )
+
+    try:
+        text_bytes = text.encode("ascii")
+    except UnicodeEncodeError as u_err:
+        raise ValueError(f"Expected an ascii string. Found: {repr(text)}.") from u_err
+    value = int.from_bytes(text_bytes, "big")
+
+    cairo_vm_range_check(value)
+    return value
+
+
+def decode_shortstring(value: int) -> str:
+    """
+    A function which decodes a felt value to short string (31 characters)
+
+    :param value: A felt value
+    :return: Decoded string which is corresponds to that felt
+    """
+    cairo_vm_range_check(value)
+    return "".join([chr(i) for i in value.to_bytes(31, byteorder="big")])
---
+++ packages/starknet-py/starknet_py-0.15.2.tar.gz/starknet_py-0.15.2/starknet_py/common.py
@@ -0,0 +1,115 @@
+import warnings
+from typing import List, Literal, Optional, Union, cast
+
+from marshmallow import EXCLUDE
+
+from starknet_py.compile.compiler import Compiler, StarknetCompilationSource
+from starknet_py.net.client_models import (
+    CasmClass,
+    CompiledContract,
+    ContractClass,
+    SierraCompiledContract,
+)
+from starknet_py.net.schemas.gateway import (
+    CasmClassSchema,
+    CompiledContractSchema,
+    ContractClassSchema,
+    SierraCompiledContractSchema,
+)
+
+
+def create_compiled_contract(
+    compilation_source: Optional[StarknetCompilationSource] = None,
+    compiled_contract: Optional[str] = None,
+    search_paths: Optional[List[str]] = None,
+) -> CompiledContract:
+    """
+    Creates CompiledContract instance.
+
+    :param compilation_source:
+        source of the contract to be compiled.
+
+         .. deprecated:: 0.15.0
+            Argument compilation_source has been deprecated. Use compiled_contract instead.
+    :param compiled_contract: compiled contract string.
+    :param search_paths:
+        paths used to resolve compilation dependencies.
+
+         .. deprecated:: 0.15.0
+            Argument search_paths has been deprecated. Use compiled_contract instead.
+    :return: CompiledContract instance.
+    """
+    warnings.warn(
+        "Argument compilation_source is deprecated and will be removed in the future. "
+        "Consider using already compiled contracts instead.",
+        category=DeprecationWarning,
+    )
+
+    if not compiled_contract:
+        if not compilation_source:
+            raise ValueError(
+                "One of compiled_contract or compilation_source is required."
+            )
+
+        compiled_contract = Compiler(
+            contract_source=compilation_source, cairo_path=search_paths
+        ).compile_contract()
+    return cast(CompiledContract, CompiledContractSchema().loads(compiled_contract))
+
+
+def create_sierra_compiled_contract(compiled_contract: str) -> SierraCompiledContract:
+    """
+    Creates SierraCompiledContract instance.
+
+    :param compiled_contract: compiled contract string.
+    :return: SierraCompiledContract instance.
+    """
+    return cast(
+        SierraCompiledContract,
+        SierraCompiledContractSchema().loads(compiled_contract, unknown=EXCLUDE),
+    )
+
+
+def create_contract_class(
+    compiled_contract: str,
+) -> ContractClass:
+    """
+    Creates ContractClass from already compiled contract.
+
+     .. deprecated:: 0.15.0
+        Function create_contract_class is deprecated and will be removed in the future.
+        Use :meth:`create_compiled_contract` instead.
+
+    :return: a ContractClass.
+    """
+    warnings.warn(
+        "Function create_contract_class is deprecated and will be removed in the future. "
+        "Consider using create_compiled_contract instead.",
+        category=DeprecationWarning,
+    )
+    return cast(ContractClass, ContractClassSchema().loads(compiled_contract))
+
+
+def create_casm_class(compiled_contract: str) -> CasmClass:
+    """
+    Creates CasmClass instance.
+
+    :param compiled_contract: contract compiled using starknet-sierra-compile.
+    :return: CasmClass instance.
+    """
+    return cast(CasmClass, CasmClassSchema().loads(compiled_contract))
+
+
+def int_from_hex(number: Union[str, int]) -> int:
+    return number if isinstance(number, int) else int(number, 16)
+
+
+def int_from_bytes(
+    value: bytes,
+    byte_order: Literal["big", "little"] = "big",
+    signed: bool = False,
+) -> int:
+    """
+    Converts the given bytes object (parsed according to the given byte order) to an integer.
+    """
+    return int.from_bytes(value, byteorder=byte_order, signed=signed)
