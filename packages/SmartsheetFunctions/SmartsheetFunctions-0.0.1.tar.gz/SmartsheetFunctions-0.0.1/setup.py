cde03489370f4945a7b1ec2eeb54f428c3dc7b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:13 +0000

    Add package SmartsheetFunctions/0.0.1/SmartsheetFunctions-0.0.1.tar.gz

---
+++ packages/SmartsheetFunctions/SmartsheetFunctions-0.0.1.tar.gz/SmartsheetFunctions-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="SmartsheetFunctions",
+    version="0.0.1",
+    author="Derek Bantel",
+    author_email="derekbantel@outlook.com",
+    description="SmartSheet Functions for development",
+    license="MIT",
+    packages=["SmartsheetFunctions"],
+    install_requires=["requests"],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
