2c24f9c3f70054a0ecdd50a9ed2d22f1cad6da71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:53 +0000

    Add package bitcode/1.3.0/bitcode-1.3.0-py3-none-any.whl

---
+++ packages/bitcode/bitcode-1.3.0-py3-none-any.whl/bitcode/console.py
@@ -0,0 +1,14 @@
+"""\
+Reference: 123web.uk/bitpy/docs/console.html
+"""
+
+def write(*text, end="", split="", flush=True):
+	"""Print the given text to stdout"""
+	print(*text, end=end, sep=split, flush=flush)
+
+def writeln(*text, split="", flush=True):
+	"""Print the given text to stdout, and appends a new line"""
+	write(*text, end="\n", split=split, flush=flush)
+
+def beep():
+	write("\a")
