62b6431da59387e568f2b1d7251e9adff6ff7b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:23 +0000

    Add package django-moncash/2.3.3/django_moncash-2.3.3-py3-none-any.whl

---
+++ packages/django-moncash/django_moncash-2.3.3-py3-none-any.whl/django_moncash/migrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# makemigrations.py
+
+from django.core.management import call_command
+from boot_django import boot_django
+
+import sys
+
+
+def main():
+
+
+    boot_django()
+    call_command("migrate")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
