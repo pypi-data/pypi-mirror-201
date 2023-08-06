b641499d13f4362839531f5aaebb625fd0d85e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:18 +0000

    Add package tcalbyjiyatus/0.0.4/tcalbyjiyatus-0.0.4-py3-none-any.whl

---
+++ packages/tcalbyjiyatus/tcalbyjiyatus-0.0.4-py3-none-any.whl/cal/a2.py
@@ -0,0 +1,8 @@
+def mul(num1,num2):
+    con = num1*num2
+    print(f"The result by Multiplying {num1} and {num2} is: ",con)
+
+def div(num1,num2):
+    con = num1/num2
+    print(f"The result by Dividing {num1} from {num2} is: ",con)
+
---
+++ packages/tcalbyjiyatus/tcalbyjiyatus-0.0.4-py3-none-any.whl/cal/a3.py
@@ -0,0 +1,7 @@
+def square(num):
+    num**=2
+    print("Here is the result of square of your number is: ",num)
+
+def cube(num):
+    num**=3
+    print("Here is the result of cube of your number is: ",num)
