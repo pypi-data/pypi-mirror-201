4bb502a18ad3185ddebca789c1a28da8142198e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:44:21 +0000

    Add package sarus-data-spec-public/2.12.0.dev1/sarus_data_spec_public-2.12.0.dev1.tar.gz

---
+++ packages/sarus-data-spec-public/sarus_data_spec_public-2.12.0.dev1.tar.gz/sarus_data_spec_public-2.12.0.dev1/sarus_data_spec/manager/asyncio/worker/caching_computation.py
@@ -0,0 +1,6 @@
+from sarus_data_spec.manager.asyncio.worker.parquet_computation import (  # noqa : F401
+    ToParquetComputation,
+)
+from sarus_data_spec.manager.asyncio.worker.to_sql_computation import (  # noqa : F401
+    ToSQLComputation,
+)
