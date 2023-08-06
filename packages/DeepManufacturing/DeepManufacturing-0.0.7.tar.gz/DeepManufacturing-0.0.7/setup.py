7e836de670ecef5dbf08c85acb4aa3751526b50c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:55 +0000

    Add package DeepManufacturing/0.0.7/DeepManufacturing-0.0.7.tar.gz

---
+++ packages/DeepManufacturing/DeepManufacturing-0.0.7.tar.gz/DeepManufacturing-0.0.7/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup,find_packages
+
+package_name ='DeepManufacturing'
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+classifiers=[
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3 :: Only',
+    'Intended Audience :: Education']
+
+setup(name=package_name,\
+      version='0.0.7',\
+      description='AI and Machine Learning for manufacturing related datasets',
+      long_description=LONG_DESCRIPTION,
+      author='Amir Barati Farimani',
+      author_email='barati@andrew.cmu.edu',
+      license='MIT',
+      classifiers=classifiers,
+      keywords=' ',
+      python_requires='>=3.5, <4',
+      install_requires=['sklearn', 'matplotlib', 
+                        'xgboost', 'scikit-learn', 
+                        'timm', 'Pillow'],
+      packages=find_packages(),
+      zip_safe=False)
