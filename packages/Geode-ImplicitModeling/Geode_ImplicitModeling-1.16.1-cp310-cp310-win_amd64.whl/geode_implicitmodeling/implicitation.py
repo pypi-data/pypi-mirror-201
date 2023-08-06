46cebd9c83225a4d7dcabdc12ffad48674db4a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:31 +0000

    Add package Geode-ImplicitModeling/1.16.1/Geode_ImplicitModeling-1.16.1-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-ImplicitModeling/Geode_ImplicitModeling-1.16.1-cp310-cp310-win_amd64.whl/geode_implicitmodeling/implicitation.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+from geode_numerics import scalar_function
+
+from .geode_implicitmodeling_py_implicitation import *
+ImplicitModelingImplicitation.initialize()
