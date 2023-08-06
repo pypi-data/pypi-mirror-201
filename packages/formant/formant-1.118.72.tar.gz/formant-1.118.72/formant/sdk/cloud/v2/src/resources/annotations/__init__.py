8eb1e9f763e9aaf9ca568d33b7df5df860fa923e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:15 +0000

    Add package formant/1.118.72/formant-1.118.72.tar.gz

---
+++ packages/formant/formant-1.118.72.tar.gz/formant-1.118.72/formant/sdk/cloud/v2/src/resources/annotations/__init__.py
@@ -0,0 +1,5 @@
+
+from formant.sdk.cloud.v2.formant_admin_api_client.models import AnnotationTemplateListResponse, AnnotationFieldValuesResponse, AnnotationFieldValuesRequest, AnnotationFieldType, SheetParameters, UserParametersRolesItem, AnnotationTags, TagParameters, AnnotationTemplate, AnnotationField, AnnotationFieldValuesRequestTags, AnnotationTemplateTags, Annotation, AnnotationStreamType, AnnotationType, AnnotationFieldValue, UserParameters
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation import annotation_controller_post
+from formant.sdk.cloud.v2.formant_admin_api_client.api.annotation_template import annotation_template_controller_list, annotation_template_controller_get_one
+from formant.sdk.cloud.v2.formant_admin_api_client.types import *
---
+++ packages/formant/formant-1.118.72.tar.gz/formant-1.118.72/formant/sdk/cloud/v2/src/resources/count/__init__.py
@@ -0,0 +1,4 @@
+
+from formant.sdk.cloud.v2.formant_query_api_client.models import CountHistoryQuery, UuidListResponse, CountHistoryEntry, CountHistory, ActiveDevicesQueryType, CountHistoryQueryType, CountHistoryEntryCount, ActiveDevicesQuery
+from formant.sdk.cloud.v2.formant_query_api_client.api.count import count_controller_history, count_controller_active_devices
+from formant.sdk.cloud.v2.formant_query_api_client.types import *
