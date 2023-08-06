9454a26899b7f1a857b65d00b08297fb0c9cf73a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:23 +0000

    Add package users-db/0.0.11/users_db-0.0.11.tar.gz

---
+++ packages/users-db/users_db-0.0.11.tar.gz/users_db-0.0.11/users_db/db.py
@@ -0,0 +1,93 @@
+import logging
+from typing import Union
+from enum import Enum
+
+from functools import wraps
+
+from sqlalchemy import create_engine
+from sqlalchemy.sql.dml import Insert, Update, Delete
+from sqlalchemy.exc import SQLAlchemyError
+
+from users_db import config
+
+log = logging.getLogger(__name__)
+
+engine = create_engine(url=config.get_postgres_uri())
+
+
+REMOVE_KEYS = ["password"]
+
+
+def secure_result(result: Union[dict, list]):
+    """
+    secure_result removes sensitive rows from the result
+    """
+
+    if isinstance(result, dict):
+        for key in REMOVE_KEYS:
+            if key in result:
+                del result[key]
+    elif isinstance(result, list):
+        for item in result:
+            secure_result(item)
+
+    return result
+
+
+def serialize_enums(result: Union[dict, list]):
+    """
+    serialize_enums converts enums to strings
+    """
+    if isinstance(result, dict):
+        for key, value in result.items():
+            if isinstance(value, Enum):
+                result[key] = value.name
+    elif isinstance(result, list):
+        for item in result:
+            serialize_enums(item)
+
+    return result
+
+
+def db_connection(f):
+    """
+    db_connection decorator supports executing select, insert, update, delete
+    operations with a single transaction; post-process data in a user friendly manner
+    """
+
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        with engine.connect() as connection:
+            try:
+                r = f(*args, **kwargs, db_conn=connection)
+
+                if isinstance(r, (dict, list)):
+                    # post-process data here, for example excluding sensitive rows
+                    result = r
+                elif isinstance(r, Insert):
+                    result = connection.execute(r.returning(r.table.c.id)).all()
+                    result = (
+                        result[0][0] if len(result) == 1 else [r[0] for r in result]
+                    )
+                    connection.commit()
+                elif isinstance(r, Update):
+                    # update returns the whole object
+                    result = connection.execute(r.returning(r.table)).all()
+                    result = (
+                        dict(result[0]._mapping)
+                        if len(result) == 1
+                        else [dict(r._mapping) for r in result]
+                    )
+                    connection.commit()
+                elif isinstance(r, Delete):
+                    result = connection.execute(r)
+                    result = result.rowcount
+                    connection.commit()
+
+                result = secure_result(serialize_enums(result))
+                return result
+            except SQLAlchemyError as err:
+                log.error(f"Error: {err}")
+                connection.rollback()
+
+    return wrapped
---
+++ packages/users-db/users_db-0.0.11.tar.gz/users_db-0.0.11/users_db/users.py
@@ -0,0 +1,98 @@
+from sqlalchemy import insert, select, update, delete
+
+from users_db.db import db_connection
+from users_db.schema import Role, users
+from users_db.role_permissions import (
+    ROLE_SUPER_ADMIN,
+    ROLE_ADMIN,
+    ROLE_USER,
+)
+
+
+@db_connection
+def create_user(first_name, middle_name, last_name, email, password, role, db_conn):
+    return insert(users).values(
+        first_name=first_name,
+        middle_name=middle_name,
+        last_name=last_name,
+        email=email,
+        password=password,
+        role=role,
+    )
+
+
+@db_connection
+def get_user(user_id, db_conn=None):
+    stmt = select(users).where(users.c.id == user_id)
+    rows = db_conn.execute(stmt)
+    rows = rows.mappings().one()
+    return dict(rows)
+
+
+@db_connection
+def get_hashed_password_by_email(email, db_conn=None):
+    stmt = select(
+        users.c.id, users.c.email, users.c.password.label("hashed_password")
+    ).where(users.c.email == email)
+    rows = db_conn.execute(stmt)
+    rows = rows.mappings().one()
+
+    return dict(rows)
+
+
+@db_connection
+def get_users(
+    user_id=None,
+    user_ids=None,
+    first_name=None,
+    middle_name=None,
+    last_name=None,
+    email=None,
+    role=None,
+    db_conn=None,
+):
+    stmt = select(users)
+
+    if user_id:
+        stmt = stmt.where(users.c.id == user_id)
+    if user_ids:
+        stmt = stmt.where(users.c.id.in_(user_ids))
+    if first_name:
+        stmt = stmt.where(users.c.first_name == first_name)
+    if middle_name:
+        stmt = stmt.where(users.c.middle_name == middle_name)
+    if last_name:
+        stmt = stmt.where(users.c.last_name == last_name)
+    if email:
+        stmt = stmt.where(users.c.email == email)
+    if role:
+        stmt = stmt.where(users.c.role == role)
+
+    rows = db_conn.execute(stmt)
+    rows = [dict(row) for row in rows.mappings().all()]
+    return rows
+
+
+@db_connection
+def update_user(
+    user_id,
+    db_conn=None,
+    **values,
+):
+    return update(users).where(users.c.id == user_id).values(**values)
+
+
+@db_connection
+def delete_user(
+    user_id,
+    db_conn=None,
+):
+    return delete(users).where(users.c.id == user_id)
+
+
+@db_connection
+def bulk_delete_users(
+    user_ids,
+    db_conn=None,
+):
+    return delete(users).where(users.c.id.in_(user_ids))
