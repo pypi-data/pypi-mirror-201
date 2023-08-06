81fa923f87fba04c6121f4e62dd055c7c2278312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:24 +0000

    Add package chemaphy/2.12.2/chemaphy-2.12.2.tar.gz

---
+++ packages/chemaphy/chemaphy-2.12.2.tar.gz/chemaphy-2.12.2/chemaphy/test.py
@@ -0,0 +1,16 @@
+from chemaphy import Statistics as stats
+from chemaphy import Sort
+from chemaphy import Sets
+
+array = [9,8,7,6,5,4,3,2,1,0]
+
+print(stats.mean(array))
+print(stats.median(array))
+print(stats.geometric_mean(array))
+print(stats.harmonic_mean(array))
+print(Sort.merge_sort(array))
+
+
+x = [1,2,3,1,5]
+y = [1,3,5]
+print(Sets.union(Sets.set(x),Sets.set(y)))
