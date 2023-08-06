4723f57e6e9f9361c2786d2b66c9ab607f2f614f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:37 +0000

    Add package flake8-iw/0.0.17/flake8_iw-0.0.17-py2.py3-none-any.whl

---
+++ packages/flake8-iw/flake8_iw-0.0.17-py2.py3-none-any.whl/flake8_iw/plugin.py
@@ -0,0 +1,41 @@
+import sys
+
+from .bulk_batch_update import BulkUpdateFinder
+from .datetime_now import DatetimeNowFinder
+from .datetime_replace_tzinfo import DatetimeReplaceTzinfoFinder
+from .freeze_time import ForbiddenTimePatchFinder
+from .logging import ErrorLoggerFinder
+from .patch_call import ForbiddenPatchCallFinder
+from .shared_task import SharedTaskFinder
+from .timezone_activate import TimezoneActivateNowFinder
+
+if sys.version_info < (3, 8):
+    import importlib_metadata
+else:
+    import importlib.metadata as importlib_metadata
+
+
+class Plugin(object):
+    name = "flake8_iw"
+    version = importlib_metadata.version("flake8_iw")
+    rules = (
+        ForbiddenPatchCallFinder,
+        ForbiddenTimePatchFinder,
+        ErrorLoggerFinder,
+        DatetimeNowFinder,
+        DatetimeReplaceTzinfoFinder,
+        BulkUpdateFinder,
+        SharedTaskFinder,
+        TimezoneActivateNowFinder
+    )
+
+    def __init__(self, tree) -> None:
+        self.tree = tree
+
+    def run(self):
+        for rule_class in self.rules:
+            parser = rule_class(self.tree)
+            parser.visit(self.tree)
+
+            for lineno, column, msg in parser.issues:
+                yield (lineno, column, msg, Plugin)
