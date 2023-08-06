6b845629a1a0d5948268875f51328e024aad359c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:32 +0000

    Add package zef/0.16.7/zef-0.16.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/zef/zef-0.16.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/zef/core/fx/local_file.py
@@ -0,0 +1,273 @@
+# Copyright 2022 Synchronous Technologies Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .fx_types import Effect
+# from ..error import Error
+from .. import internals
+import yaml 
+import json 
+import toml
+import pandas as pd
+import io
+from .._image import Image
+
+#TODO Docstring!
+
+def read_localfile_handler(eff: Effect):
+    """
+    Reads the file as a string. Returns a str object.
+    To read as a binary file, use FX.LocalFile.ReadBinary.
+    >>> FX.LocalFile.Read(filename='my_file.txt')
+
+    Response example:
+    {
+        'content': some_dict,
+        'filename': 'my_file.txt',
+    }
+    """
+    try:
+        filename  = eff["filename"]
+        with open(filename, "r") as f:
+            f = f.read()
+        return {"content": f, "filename": filename}
+    except Exception as e:
+        raise RuntimeError(f"Error reading file in FX.LocalFile.Read for effect={eff}:\n {repr(e)}")
+
+def readbinary_localfile_handler(eff: Effect):
+    """
+    Reads a localfile in binary mode and returns the content as bytes.
+    >>> FX.LocalFile.ReadBinary(filename='my_file.txt')
+
+    Response example:
+    {
+        'content': some_dict,
+        'filename': 'my_file.txt',
+    }
+    """
+    try:
+        filename  = eff["filename"]
+        with open(filename, "rb") as f:
+            f = f.read()
+        return {"content": f, "filename": filename}
+    except Exception as e:
+        raise RuntimeError(f"Error reading file in FX.LocalFile.Read for effect={eff}:\n {repr(e)}")
+
+
+
+def load_localfile_handler(eff: Effect):
+    """
+    Opinionated loading of file: it parses based on the file extension.
+    If you want plain reading of a file, use 
+    >>> FX.LocalFile.Load(filename='my_file.json')
+
+    Supported formats:
+    - json
+    - csv
+    - toml
+    - yaml / yml
+    - svg
+    - png
+    - jpg
+    - jpeg
+    - gif
+
+    Response example:
+    {
+        'content': some_dict,
+        'format': 'json',
+        'filename': 'my_file.json',
+    }
+    """
+    try:
+        filename  = eff["filename"]
+        settings  = eff.get("settings", {})
+        format    = eff.get("format", None)
+
+        if not format:
+            if "." not in filename: raise ValueError("filename is missing an extension and a format wasn't provided!", filename)
+            else: format = filename[filename.rindex(".") + 1:]
+        elif "." not in filename: filename = filename + f".{format}"
+
+
+        if format in {"svg", "png", "jpg", "jpeg", "gif"}:
+            with open(filename, "rb") as f:
+                content = f.read()
+            content = Image(content, format)
+        else:
+            with open(filename, "rb") as f:
+                content = f.read()  
+            if format in {"yaml", "yml"}:
+                content = yaml.safe_load(content)
+            elif format == "toml":
+                content = toml.loads(content)
+            elif format == "csv":
+                content = pd.read_csv(io.StringIO(content), **settings)
+            elif format == "json":
+                content = json.loads(content)
+
+        return {"content": content, "format": format, "filename": filename}
+    except Exception as e:
+        raise Exception(f'executing FX.LocalFile.Load for effect {eff}:\n{repr(e)}')
+
+
+
+def save_localfile_handler(eff: Effect):
+    """
+    Example Effect
+
+    {
+        'type':     FX.LocalFile.Save,
+        'filename': fname,
+        'content':  content,
+        'settings': settings,
+    }
+    """
+    try:
+        content   = eff["content"]
+        filename  = eff["filename"]
+        settings  = eff.get("settings", {})
+        formats = {".svg", ".png", ".jpg", ".jpeg", ".gif"}
+        
+        if isinstance(content, Image):
+            format = f'.{content.format}'
+            if format not in formats: raise Exception(f'Image format needs to be one of these types {formats} got {format} instead.')        
+            filename = filename if format == (filename[-4:] or filename[-5:])  else filename + format
+            with open(filename, 'wb') as f: f.write(internals.decompress_zstd(content.buffer))
+            
+        elif "." in filename:
+            format = filename[filename.rindex(".") + 1:]
+            if format in {"yaml", "yml"}:
+                with open(filename, 'w') as f: f.write(yaml.safe_dump(content))
+            elif format == "toml":
+                with open(filename, 'w') as f: f.write(toml.dumps(content))
+            elif format == "csv":
+                with open(filename, 'w') as f: f.write(content.to_csv(**settings))
+            elif format == "json":
+                with open(filename, 'w') as f: f.write(json.dumps(content))
+            else:
+                raise Exception(f'Unsupported writing to file type {format}!')
+        else:
+            filename += ".txt"
+            with open(filename, 'w') as f: f.write(content)
+
+        return {"filename": filename}
+    except Exception as e:
+        raise Exception(f'executing FX.LocalFile.Save for effect {eff}: {repr(e)}')
+
+
+def write_localfile_handler(eff: Effect):
+    """
+    Example Effect
+
+    {
+        'type':     FX.LocalFile.Write,
+        'filename': fname,
+        'content':  content,
+    }
+    """
+    content   = eff["content"]
+    filename  = eff["filename"]
+
+    mode = "wb"  if isinstance(content, bytes) else "w"
+    with open(filename, mode) as f: f.write(content)
+    return {"filename": filename}
+
+
+
+def system_open_with_handler(eff: Effect):
+    """
+    This will open the file using the system's default handler application.
+
+    Example
+    =======
+
+    {
+            'type': FX.LocalFile.SystemOpenWith,
+            'filepath': fname,
+    }
+    """
+    try:
+        import subprocess, os, platform
+        if platform.system() == 'Darwin':       # macOS
+            subprocess.Popen(('open', eff['filepath']))
+        elif platform.system() == 'Windows':    # Windows
+            os.startfile(eff['filepath'])
+        else:                                   # linux variants
+            subprocess.Popen(('xdg-open', eff['filepath']))
+
+    except Exception as e:
+        raise Exception(f'executing FX.LocalFile.SystemOpenWith for effect {eff}:\n{repr(e)}')
+
+
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+class ZefEventHandler(FileSystemEventHandler):
+    def __init__(self, created = None, modified = None, moved = None, deleted = None):
+        super().__init__()
+        self.created = created
+        self.modified = modified
+        self.moved = moved
+        self.deleted = deleted
+    def on_created(self, event):
+        super().on_created(event)
+        if self.created: self.created(event)
+        
+    def on_modified(self, event):
+        super().on_modified(event)
+        if self.modified: self.modified(event)
+    def on_moved(self, event):
+        super().on_moved(event)
+        if self.moved: self.moved(event)
+    
+    def on_deleted(self, event):
+        super().on_deleted(event)
+        if self.deleted: self.deleted(event)
+
+def monitor_path_handler(eff: Effect):
+    """
+    Watches for changes in files and nested directories at given path for changes.
+    These changes include, file creation, modification, deletion and moving.
+
+    If a change handler is set, once an event of the type is triggered the handler is called.
+
+    Example
+    =======
+
+    {
+            'type': FX.LocalFile.MonitorPath,
+            'path': fpath,
+            'recursive': True,           # default is False
+            'created_handler': f1,       # default None
+            'modified_handler': f2,      # default None
+            'moved_handler': f3,         # default None
+            'deleted_handler': f4,       # default None
+    }
+    """
+    assert 'path' in eff, "path is required for FX.LocalFile.MonitorPath"
+    path = eff['path']
+    recursive = eff.get('recursive', False)
+
+    created_handler = eff.get("created_handler", None)
+    modified_handler = eff.get("modified_handler", None)
+    moved_handler = eff.get("moved_handler", None)
+    deleted_handler = eff.get("deleted_handler", None)
+
+    event_handler = ZefEventHandler(created = created_handler, modified = modified_handler, moved = moved_handler, deleted = deleted_handler)
+
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=recursive)
+    observer.start()
+
+    return {"observer_object": observer}
---
+++ packages/zef/zef-0.16.7-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/zef/graphql/simplegql/schema_file_parser.py
@@ -0,0 +1,379 @@
+# Copyright 2022 Synchronous Technologies Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from ... import *
+from ...ops import *
+
+from ...core.logger import log
+
+import json
+import graphql
+
+
+def parse_partial_graphql(schema):
+    """Goes through the schema line by line and creates a json file that represents
+    the schema one-to-one. This should be an invertible process.
+
+    """
+    doc = graphql.parse(schema)
+
+    output = {
+        "types": {},
+        "enums": {}
+    }
+
+    # Find all Zef directives first, which are single-line comments that begin with # Zef.<name>:
+    lines = (schema
+             | split["\n"]
+             | filter[lambda x: x.startswith("# Zef.")]
+             | map[lambda x: x[len("# Zef."):]]
+             | collect)
+
+    for line in lines:
+        parts = line.split(':', 1)
+        assert len(parts) == 2, "Zef directive line contains no ':'."
+        name, details = parts
+        name = name.strip()
+
+        assert name in ["SchemaVersion", "DataTag", "Authentication", "Route", "GraphQLRoute"], f"Don't understand Zef directive of name {name}"
+
+        if name == "SchemaVersion":
+            assert "schema_version" not in output, "Not allowed to have multiple Zef.SchemaVersion directives."
+            output["schema_version"] = details.strip()
+            continue
+        elif name == "DataTag":
+            assert "data_tag" not in output, "Not allowed to have multiple Zef.DataTag directives."
+            output["data_tag"] = details.strip()
+            continue
+
+        # Anything else requires json parsing
+        try:
+            details = json.loads(details)
+        except Exception as exc:
+            raise Exception(f"JSON parsing of Zef directive '{name}' failed") from exc
+
+        if name == "Authentication":
+            assert "auth" not in output, "Not allowed to have multiple Zef.Authentication directives."
+            for key in details:
+                assert key in ["Algo", "JWKURL", "Audience", "Header", "Namespace",
+                               "VerificationKey", "VerificationKeyEnv", "Public"], f"Unknown auth key '{key}'"
+            output["auth"] = details
+        elif name == "Route":
+            assert set(keys(details)) == {"route", "hook"}
+            if "route" in ["/", "/gql"]:
+                raise Exception("Custom route should not alias '/' or '/gql'")
+            routes = output.setdefault("routes", [])
+            routes.append((details["route"], details["hook"]))
+        elif name == "GraphQLRoute":
+            assert set(keys(details)) == {"route"}
+            routes = output.setdefault("graphql_routes", [])
+            routes.append(details["route"])
+        else:
+            raise Exception(f"Unsupported Zef.{name} directive")
+
+    if output.get("schema_version", None) != "v1":
+        raise Exception(
+            "Only support schemas with an explicit version of 'v1'. Please include:\n# Zef.SchemaVersion: v1\ninto your schema file.")
+
+    for definition in doc.definitions:
+        if definition.kind == "object_type_definition":
+            t_def = output["types"][definition.name.value] = {}
+
+            for directive in definition.directives:
+                if directive.name.value == "auth":
+                    if "auth" not in output:
+                        raise Exception(
+                            "Disallowing @auth directives when no # Zef.Authentication... line is present to prevent accidentally security holes.")
+                    for arg in directive.arguments:
+                        if arg.name.value == "query":
+                            t_def["_AllowQuery"] = arg.value.value
+                        elif arg.name.value == "add":
+                            t_def["_AllowAdd"] = arg.value.value
+                        elif arg.name.value == "update":
+                            t_def["_AllowUpdate"] = arg.value.value
+                        elif arg.name.value == "updatePost":
+                            t_def["_AllowUpdatePost"] = arg.value.value
+                        elif arg.name.value == "delete":
+                            t_def["_AllowDelete"] = arg.value.value
+                        else:
+                            raise Exception(f"Don't know how to handle auth kind of {arg.name.value}")
+                elif directive.name.value == "upfetch":
+                    assert len(directive.arguments) == 1, "upfetch directive needs exactly one argument, 'field'"
+                    arg = directive.arguments[0]
+                    if arg.name.value != "field":
+                        raise Exception("upfetch directive needs exactly one argument, 'field'")
+                    t_def["_Upfetch"] = arg.value.value
+                elif directive.name.value == "RAE":
+                    assert len(directive.arguments) == 1, "RAE directive needs exactly one argument, 'et' or 'rt'"
+                    arg = directive.arguments[0]
+                    if arg.name.value not in ["et", "rt"]:
+                        raise Exception("ET directive needs exactly one argument, 'et'")
+                    if arg.name.value == "et":
+                        t_def["_RAE"] = ET(arg.value.value)
+                    if arg.name.value == "rt":
+                        t_def["_RAE"] = RT(arg.value.value)
+                elif directive.name.value == "hook":
+                    for arg in directive.arguments:
+                        for hook_name in ["Create", "Remove", "Update"]:
+                            if arg.name.value == f"on{hook_name}":
+                                t_def[f"_On{hook_name}"] = arg.value.value
+                                break
+                        else:
+                            raise Exception(f"Unknown hook name {arg.name.value}")
+                else:
+                    raise Exception(f"Don't know how to handle type directive @{directive.name.value}")
+
+            for field in definition.fields:
+                f_def = t_def[field.name.value] = {}
+
+                f_type = field.type
+                if f_type.kind == "non_null_type":
+                    f_def["required"] = True
+                    f_type = f_type.type
+                if f_type.kind == "list_type":
+                    f_def["list"] = True
+                    f_type = f_type.type
+                if f_type.kind == "non_null_type":
+                    f_def["listNonNullItem"] = True
+                    f_type = f_type.type
+                assert f_type.kind == "named_type", f"Don't know what this kind of field type is {f_type.kind}"
+                f_def["type"] = f_type.name.value
+
+                assert len(field.arguments) == 0, "Currently not allowing custom arguments to a field"
+                for directive in field.directives:
+                    v = directive.name.value
+                    for bool_key in ["search", "unique", "incoming"]:
+                        if v == bool_key:
+                            f_def[bool_key] = True
+                            assert len(directive.arguments) == 0, f"@{bool_key} cannot take arguments"
+                            break
+                    else:
+                        if v == "relation":
+                            args = parse_arguments_as_dict(directive.arguments)
+                            assert args.keys() == {"rt"} or args.keys() == {"source", "rt", "target"}, "@relation must take either 'rt' or 'source', 'rt', 'target' arguments. Got {set(args.keys())}"
+                            if "source" in args:
+                                f_def["relation"] = (ET(args["source"]), RT(args["rt"]), ET(args["target"]))
+                            else:
+                                f_def["relation"] = RT(args["rt"])
+                        elif v == "dynamic":
+                            args = parse_arguments_as_dict(directive.arguments)
+                            assert args.keys() == {"hook"}, "@dynamic must take a single argument, 'hook'"
+                            f_def["dynamic"] = args["hook"]
+                        else:
+                            raise Exception(f"Don't know how to handle directive @{v}")
+        elif definition.kind == "enum_type_definition":
+            e_def = output["enums"][definition.name.value] = []
+            assert len(definition.directives) == 0, "Don't accept directives on enums"
+
+            for value in definition.values:
+                e_def += [value.name.value]
+
+                assert len(value.directives) == 0, "Don't accept directives on enum values"
+        else:
+            raise Exception(f"Don't know how to handle a top-level graphql schema object of kind {definition.kind}")
+
+    return output
+
+
+def string_to_RAET(s):
+    if s.startswith("ET."):
+        return ET(s[len("ET."):])
+    elif s.startswith("RT."):
+        return RT(s[len("RT."):])
+    elif s.startswith("AET."):
+        return AET(s[len("AET."):])
+    elif s.startswith("EN."):
+        return AET(s[len("EN."):])
+    else:
+        raise Exception(f"Can't parse string as a RAE type: {s}")
+
+
+def parse_arguments_as_dict(args):
+    d = {}
+    for arg in args:
+        d[arg.name.value] = arg.value.value
+    return d
+
+
+def simple_capitalize(s):
+    # This is here to make things like firebaseID go to FirebaseID rather than
+    # FirebaseId. This is more predicatable for users.
+    assert len(s) > 0
+    return s[0].upper() + s[1:]
+
+
+def json_to_minimal_nodes(json, g):
+    """This takes a json schema, such as one produced by the `parse_schema`
+        function, and puts the minimal GQL schema nodes onto the graph.
+        "Minimal" is the key word here, as the generated schema graph nodes will
+        not define any extra resolvers, these will be done at server run-time.
+
+    """
+
+    core_types = {
+        "String": "String",
+        "Float": "Float",
+        "DateTime": "Time",
+        "Int": "Int",
+        "Boolean": "Bool"
+    }
+
+    actions = []
+
+    # The root node meta data
+    actions += [ET.GQL_Root["root"]]
+    if "auth" in json:
+        # A bit of sanity checking here - we should have one algo, which selects
+        # symmetric/asymmetric keys, which allows only particular details to be
+        # given.
+        for key in ["Algo", "Header", "Audience"]:
+            assert key in json["auth"], f"Auth requires key '{key}'"
+        algo = json["auth"]["Algo"]
+        assert algo in ["RS256", "HS256"], f"Auth algo is '{algo}', must be one of 'RS256' or 'HS256'"
+
+        actions += [(Z["root"], RT.AuthAlgo, algo)]
+        actions += [(Z["root"], RT.AuthHeader, json["auth"]["Header"])]
+        actions += [(Z["root"], RT.AuthAudience, json["auth"]["Audience"])]
+        actions += [(Z["root"], RT.AuthPublic, json["auth"].get("Public", True))]
+        if "Namespace" in json["auth"]:
+            actions += [(Z["root"], RT.AuthNamespace, json["auth"]["Namespace"])]
+
+        assert sum(x in json["auth"] for x in ["JWKURL", "VerificationKey", "VerificationKeyEnv"]) == 1, "Need exactly one of ['JWKURL', 'VerificationKey', 'VerificationKeyEnv'] in auth"
+        if algo == "RS256":
+            assert "JWKURL" in json["auth"], "RS256 needs JWKURL specified."
+            actions += [(Z["root"], RT.AuthJWKURL, json["auth"]["JWKURL"])]
+        if algo == "HS256":
+            vkey = json["auth"].get("VerificationKey", None)
+            if vkey is None:
+                env = json["auth"].get("VerificationKeyEnv", None)
+                if env is None:
+                    raise Exception("HS256 needs either VerificationKey or VerificationKeyEnv specified.")
+                import os
+                if env not in os.environ:
+                    raise Exception(f"VerificationKeyEnv asked for environment variable '{env}' which was not present.")
+                vkey = os.environ[env]
+            vkey = vkey.replace("\\n", '\n')
+            actions += [(Z["root"], RT.AuthPresharedKey, vkey)]
+    if "routes" in json:
+        for route,hook in json["routes"]:
+            if hook not in now(g):
+                raise Exception(f"Hook named {hook} not found on schema graph")
+            actions += [(Z["root"], RT.Route,
+                         {ET.Route: {RT.Route: route,
+                                     RT.Hook: g | now | get[hook] | collect}})]
+    if "graphql_routes" in json:
+        for route in json["graphql_routes"]:
+            actions += [(Z["root"], RT.GraphQLRoute, route)]
+
+    if "data_tag" in json:
+        actions += [(Z["root"], RT.DataTag, json["data_tag"])]
+                
+
+    for gql_name,typ in core_types.items():
+        actions += [
+            getattr(AET, typ)[gql_name],
+            # Helpful hack to make the resolvers easier
+            (Z[gql_name], RT.Name, gql_name),
+            (Z["root"], RT.GQL_CoreScalarType, Z[gql_name])
+        ]
+
+    def name_to_raet(name):
+        if name in core_types:
+            return getattr(internals.VRT, core_types[name])
+        if name in json["enums"]:
+            return getattr(internals.VRT.Enum, name)
+        rae = json["types"][name].get("_RAE", ET(name))
+        return rae
+
+    for type_name,fields in json["types"].items():
+
+        actions += [(ET.GQL_Type[type_name], RT.Name, type_name)]
+        actions += [(Z["root"], RT.GQL_Type, Z[type_name])]
+
+        rae = name_to_raet(type_name)
+        actions += [(Z[type_name], RT.GQL_Delegate, delegate_of(rae))]
+
+        for field_name,field in fields.items():
+            assert field_name != "id", "id is an automatically generated field, do not explicitly include"
+
+            if field_name.startswith("_"):
+                # Special handling here
+                if field_name == "_RAE":
+                    continue
+                elif field_name in ["_AllowQuery", "_AllowAdd", "_AllowUpdate", "_AllowUpdatePost", "_AllowDelete"]:
+                    # TODO: Turn into a zef function later on
+                    actions += [(Z[type_name], RT(field_name[1:]), field)]
+                elif field_name in ["_OnCreate", "_OnRemove", "_OnUpdate"]:
+                    # Find the zef function that this corresponds to
+                    if field not in now(g):
+                        raise Exception(f"Hook named {field} not found on schema graph")
+                    z_func = g | now | get[field] | collect
+                    actions += [(Z[type_name], RT(field_name[1:]), z_func)]
+
+                elif field_name == "_Upfetch":
+                    assert field in fields and "unique" in fields[field] and "required" in fields[field], f"Upfetch field '{field}' must exist, be unique and be required."
+                    # We can figure out which relation we can attach the upfetch bool to.
+                    qual_name = type_name + "__" + field
+                    actions += [(Z[qual_name], RT.Upfetch, True)]
+                else:
+                    raise Exception(f"Don't understand special field name '{field_name}'")
+            else:
+                field = {**field}
+
+                qual_name = type_name + "__" + field_name
+                actions += [
+                    (Z[type_name], RT.GQL_Field[qual_name], Z[field["type"]]),
+                    (Z[qual_name], RT.Name, field_name)
+                ]
+                for bool_key in ["search", "unique", "incoming", "list", "required", "listNonNullItem"]:
+                    if bool_key in field:
+                        actions += [(Z[qual_name], RT(simple_capitalize(bool_key)), field[bool_key])]
+                        del field[bool_key]
+
+                if "dynamic" in field:
+                    assert "relation" not in field, f"Can't give a relation with a dynamically resolved field: {field}"
+                    if field["dynamic"] not in now(g):
+                        raise Exception(f"Hook named {field['dynamic']} not found on schema graph")
+                    z_func = g | now | get[field["dynamic"]] | collect
+                    actions += [(Z[qual_name], RT.GQL_FunctionResolver, z_func)]
+                    del field["dynamic"]
+                else:
+                    if "relation" in field:
+                        resolve_with = delegate_of(field["relation"])
+                        del field["relation"]
+                    else:
+                        this = rae
+                        rt = RT(simple_capitalize(field_name))
+                        other = name_to_raet(field["type"])
+                        if field.get("incoming", False):
+                            resolve_with = delegate_of((other, rt, this))
+                        else:
+                            resolve_with = delegate_of((this, rt, other))
+
+                    actions += [(Z[qual_name], RT.GQL_Resolve_With, resolve_with)]
+
+                del field["type"]
+                assert len(field) == 0, f"There are unhandled keys left in field object {field}"
+
+    for enum_name,opts in json["enums"].items():
+        actions += [(ET.GQL_Enum[enum_name], RT.Name, enum_name)]
+        actions += [(Z["root"], RT.GQL_Enum, Z[enum_name])]
+
+        for opt in opts:
+            actions += [(Z[enum_name], RT.GQL_Field, EN(enum_name, opt))]
+
+    return actions
+
+
+
