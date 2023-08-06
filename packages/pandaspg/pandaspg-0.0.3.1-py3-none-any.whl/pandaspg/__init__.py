82c9984ce9c9116fcdf5412b380619f0fe471982
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:25 +0000

    Add package pandaspg/0.0.3.1/pandaspg-0.0.3.1-py3-none-any.whl

---
+++ packages/pandaspg/pandaspg-0.0.3.1-py3-none-any.whl/pandaspg/__init__.py
@@ -0,0 +1,11 @@
+from .main import *
+
+
+__all__ = (
+    "csv_to_dataframe",
+    "get_dataframe_column_dtypes_dict",
+    "map_pandas_to_postgresql_datatypes",
+    "connect_to_postgresql",
+    "create_postgresql_table",
+    "insert_dataframe_into_postgresql",
+)
