982f6671a89dde79be09166d0a1914da889fc133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:31 +0000

    Add package gl-webhooks/1.1.12/gl_webhooks-1.1.12-py2.py3-none-any.whl

---
+++ packages/gl-webhooks/gl_webhooks-1.1.12-py2.py3-none-any.whl/gl_webhooks/tasks/jobs_monitor.py
@@ -0,0 +1,20 @@
+import os
+
+from celery import shared_task
+
+from ..controllers import jobs_monitor as ctrl
+
+
+@shared_task(ignore_result=True)
+def insert_jobs_from_all_projects(settings):
+    ctrl.insert_jobs_from_all_projects(settings)
+
+
+@shared_task(ignore_result=True)
+def update(settings):
+    ctrl.update(settings)
+
+
+@shared_task(ignore_result=True)
+def purge():
+    ctrl.atomic_file_action(ctrl.purge)
