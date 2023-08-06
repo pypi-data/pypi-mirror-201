aceea64076aa6b233c237995be7e3e74b0a07b12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:37:48 +0000

    Add package proms/1.0.12/proms-1.0.12-py3-none-any.whl

---
+++ packages/proms/proms-1.0.12-py3-none-any.whl/proms/__init__.py
@@ -0,0 +1,13 @@
+from .fs_methods import ProMS, ProMS_mo
+from .fs_methods import fs_methods, FeatureSelector
+
+from .dataset import Dataset, Data
+__version__ = '1.0.12'
+__all__ = [
+           'Data',
+           'Dataset',
+           'ProMS',
+           'ProMS_mo',
+           'fs_methods',
+           'FeatureSelector'
+           ]
