0b5a70256c26daae0a741c09635f3e27d51d9974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:49 +0000

    Add package csv2graph/2.0/csv2graph-2.0.tar.gz

---
+++ packages/csv2graph/csv2graph-2.0.tar.gz/csv2graph-2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+#
+# Nom du fichier:       setup.py
+# Date de création:     08/04/2023
+# Version:              0.0.1
+# Auteur:               Simon STEPHAN <simon.stephan@u-bourgogne.fr>
+#
+# Copyright (c) 2023,
+# Tous droits réservés. 
+#
+
+from setuptools import setup, find_packages
+
+setup(
+    name="csv2graph",
+    version="2.0",
+    packages=find_packages(),
+    install_requires=[
+        "networkx",
+        "pandas"
+    ],
+    entry_points={
+        "console_scripts": [
+            "csv2graph=csv2graph.csv2graph:main"
+        ]
+    },
+    python_requires=">=3.6",
+    license="GNU",
+    description="A Python library to convert CSV files to graph objects",
+    readme="readme.md",
+    author="Simon STEPHAN",
+    author_email="simon.stephan@u-bourgogne.fr",
+    url="https://github.com/TheCodingHornet/csv2graph"
+)
