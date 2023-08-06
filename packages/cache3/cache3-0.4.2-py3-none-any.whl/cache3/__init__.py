644c3407ac3f065d405a637d79f4650485eb1eb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:21 +0000

    Add package cache3/0.4.2/cache3-0.4.2-py3-none-any.whl

---
+++ packages/cache3/cache3-0.4.2-py3-none-any.whl/cache3/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# DATE: 2021/7/24
+# Author: clarkmonkey@163.com
+
+"""
+Cache3 is a MIT licensed  safe and lightweight cache library, written in pure-Python.
+"""
+
+from cache3.utils import lazy, LazyObject
+from cache3.disk import DiskCache, LazyDiskCache
+from cache3.memory import Cache, MiniCache, LazyCache
+
+__author__: str = 'St·Kali <clarkmonkey@163.com>'
+__name__: str = 'cache3'
+__email__: str = 'clarkmonkey@163.com'
+__version__: str = '0.4.2'
+
+__all__: list = [
+    'DiskCache', 'LazyDiskCache',
+    'Cache', 'MiniCache', 'LazyCache',
+    'LazyObject', 'lazy',
+]
+
