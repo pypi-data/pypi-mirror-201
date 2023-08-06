6e9511482a3975e643ce67be2d912f11274b364f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:15 +0000

    Add package kili/2.131.0/kili-2.131.0.tar.gz

---
+++ packages/kili/kili-2.131.0.tar.gz/kili-2.131.0/setup.py
@@ -0,0 +1,101 @@
+"""This script permits to setup the python package."""
+
+from setuptools import find_packages, setup
+
+from src.kili import __version__
+
+install_requires = [
+    "pandas<2.0.0",
+    "click",
+    "requests",
+    "tabulate",
+    "tenacity>=8.0.0,<9.0.0",
+    "tqdm",
+    "typeguard<3.0.0",
+    "typing_extensions>=4.1.0",
+    "pyparsing",
+    "websocket-client",
+    "pyyaml",
+    "Pillow",
+    "cuid",
+    "pydantic",
+    "urllib3>=1.26",
+    "ffmpeg-python",
+    "gql[requests,websockets]",
+]
+
+image_requires = [
+    "opencv-python",
+]
+
+dev_extra = [
+    # release
+    "bump2version",
+    # tests
+    "pytest",
+    "pytest-mock",
+    "pytest-cov",
+    "pytest-xdist[psutil]",
+    # documentation
+    "mkdocs",
+    "mkdocs-material",
+    "mkdocstrings",
+    "mkdocstrings-python-legacy",
+    "mkdocs-click",
+    "mike",
+    "pymdown-extensions",
+    # linting
+    "black",
+    "pre-commit",
+    "pylint",
+    "flake8-unused-arguments",
+    "pyright",
+    # notebooks tests
+    "nbformat",
+    "nbconvert",
+    "ipykernel",
+    # image utils
+    "opencv-python",
+]
+
+setup(
+    name="kili",
+    version=__version__,
+    package_dir={"": "src"},
+    packages=find_packages(where="src", exclude=["tests"]),
+    author="Kili Technology",
+    author_email="contact@kili-technology.com",
+    description="Python client for Kili Technology labeling tool",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    install_requires=install_requires,
+    extras_require={
+        "dev": dev_extra,
+        "image-utils": image_requires,
+    },
+    include_package_data=True,
+    entry_points={
+        "console_scripts": ["kili=kili.entrypoints.cli:main"],
+    },
+    url="https://github.com/kili-technology/kili-python-sdk",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Topic :: Software Development",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Operating System :: Unix",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
---
+++ packages/kili/kili-2.131.0.tar.gz/kili-2.131.0/src/kili/entrypoints/queries/asset/exceptions.py
@@ -0,0 +1,5 @@
+"""Asset queries related exceptions."""
+
+
+class MissingPropertyError(ValueError):
+    """Raised when trying to download an asset media with a missing field."""
