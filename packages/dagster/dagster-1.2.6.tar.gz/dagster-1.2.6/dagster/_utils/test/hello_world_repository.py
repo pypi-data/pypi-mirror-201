d8beaeec29ac336180934c4e23c816cddb604f22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:23 +0000

    Add package dagster/1.2.6/dagster-1.2.6.tar.gz

---
+++ packages/dagster/dagster-1.2.6.tar.gz/dagster-1.2.6/dagster/_utils/test/hello_world_repository.py
@@ -0,0 +1,16 @@
+from dagster._core.definitions import op, pipeline, repository
+
+
+@op
+def hello_world(_):
+    pass
+
+
+@pipeline
+def hello_world_pipeline():
+    hello_world()
+
+
+@repository
+def hello_world_repository():
+    return [hello_world_pipeline]
