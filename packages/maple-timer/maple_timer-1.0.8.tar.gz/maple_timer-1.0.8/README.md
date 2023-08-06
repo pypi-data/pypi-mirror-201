0be1271ac742427ee1a616cd908174705f1f426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:43 +0000

    Add package maple-timer/1.0.8/maple_timer-1.0.8.tar.gz

---
+++ packages/maple-timer/maple_timer-1.0.8.tar.gz/maple_timer-1.0.8/README.md
@@ -0,0 +1,7 @@
+# maple_timer
+
+analysis maple endpoint's performance.
+
+python2, python3 supported.
+
+通过每个命令字的总消耗时间，占总时间的比例 等等参数来判断是哪个命令字出了问题
---
+++ packages/maple-timer/maple_timer-1.0.8.tar.gz/maple_timer-1.0.8/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+setup(
+    name="maple_timer",
+    version="1.0.8",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=[],
+    scripts=[],
+    url="https://github.com/dantezhu/maple_timer",
+    license="MIT",
+    author="dantezhu",
+    author_email="dantezhu@qq.com",
+    description="analysis maple endpoint's performance.",
+)
