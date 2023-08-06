427d87d39a4e18a138fffd101916c38d401f9bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:53 +0000

    Add package tendril-iotedge/0.1.5/tendril-iotedge-0.1.5.tar.gz

---
+++ packages/tendril-iotedge/tendril-iotedge-0.1.5.tar.gz/tendril-iotedge-0.1.5/src/tendril/common/iotedge/formats.py
@@ -0,0 +1,33 @@
+
+
+from typing import Literal
+from typing import Optional
+from tendril.utils.pydantic import TendrilTBaseModel
+from tendril.common.interests.states import InterestLifecycleStatus
+
+
+IoTDeviceIDTModel = str
+IoTDeviceAppnameTModel = str
+
+
+class IoTDeviceMessageTModel(TendrilTBaseModel):
+    id: IoTDeviceIDTModel
+
+
+class IoTDeviceAnnounceTModel(IoTDeviceMessageTModel):
+    appname: IoTDeviceAppnameTModel
+    have_credentials: bool
+
+
+class IoTDevicePingTModel(IoTDeviceMessageTModel):
+    status: Optional[dict]
+
+
+class IoTDeviceAnnounceResponseTModel(IoTDeviceMessageTModel):
+    status: InterestLifecycleStatus
+    interest_id: int
+    password: Optional[str]
+
+
+class IoTDeviceSettingRequestTModel(IoTDeviceMessageTModel):
+    appname: IoTDeviceAppnameTModel
