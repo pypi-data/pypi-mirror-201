da3d28dc2d68dbb2e56a2d31b4c0389c99dd5b89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:06 +0000

    Add package electron/0.0.4/electron-0.0.4.tar.gz

---
+++ packages/electron/electron-0.0.4.tar.gz/electron-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="electron",
+    version="0.0.4",
+    author="CrowXeo",
+    author_email="crowxeo@gmail.com",
+    description="A web framework for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pornhub.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.11',
+)
+
