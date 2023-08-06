ae1c1ff1b2cfcfddb29b37a473c63ee4510245a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:22 +0000

    Add package cdmpy/0.3.1/cdmpy-0.3.1.tar.gz

---
+++ packages/cdmpy/cdmpy-0.3.1.tar.gz/cdmpy-0.3.1/tests/test_cdm.py
@@ -0,0 +1,23 @@
+# Author: Lu Xu <oliver_lew@outlook.com">
+# License: MIT
+# Original Repo: https://github.com/OliverLew/fio-cdm
+# Packaging: https://github.com/Pythoniasm/cdmpy
+
+import tests
+
+from subprocess import Popen, PIPE
+from cdm import entrypoint
+
+
+__all__ = ["tests"]
+
+
+def test_entrypoint():
+    entrypoint()
+
+
+def test_script():
+    command = "cdm -s 1m"
+    res = Popen(command.split(" "), stdout=PIPE)
+    output, _ = res.communicate()
+    assert output != ""
