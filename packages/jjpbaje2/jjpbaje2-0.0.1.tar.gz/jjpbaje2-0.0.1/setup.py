b8b03e293a3c08f9578866c528063ec051fe65b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:30 +0000

    Add package jjpbaje2/0.0.1/jjpbaje2-0.0.1.tar.gz

---
+++ packages/jjpbaje2/jjpbaje2-0.0.1.tar.gz/jjpbaje2-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="jjpbaje2",
+      version="0.0.1",
+      url="",
+      maintainer='R Praneeth',
+      maintainer_email='praneeth.renduchinthala210@gmail.com',
+      py_modules=['jjpbaje2'],
+      description="initial")
