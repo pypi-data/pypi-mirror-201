d80cdcd5f5630f35377754569de69cbef1709d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:41 +0000

    Add package riscv-config/3.7.2/riscv_config-3.7.2-py2.py3-none-any.whl

---
+++ packages/riscv-config/riscv_config-3.7.2-py2.py3-none-any.whl/riscv_config/constants.py
@@ -0,0 +1,39 @@
+import os
+import re
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+isa_schema = os.path.join(root, 'schemas/schema_isa.yaml')
+debug_schema = os.path.join(root, 'schemas/schema_debug.yaml')
+platform_schema = os.path.join(root, 'schemas/schema_platform.yaml')
+Zvl_extensions = [
+        "Zvl32b", "Zvl64b", "Zvl128b", "Zvl256b", "Zve512b", "Zvl1024b"
+]
+Zvef_extensions = [
+        "Zve32f", "Zve64f"
+]
+Zved_extensions = [
+        "Zve64d"
+]
+Zve_extensions = [
+        "Zve32x",  "Zve64x"
+] + Zvef_extensions + Zved_extensions
+
+Z_extensions = [
+        "Zicbom", "Zicbop", "Zicboz", "Zicntr", "Zicsr", "Zicond", "Zifencei", "Zihintpause", "Zihpm",
+        "Zmmul",
+        "Zam",
+        "Zfh",
+        "Zfinx", "Zdinx", "Zhinx", "Zhinxmin",
+        "Ztso",
+        "Zba", "Zbb", "Zbc", "Zbe", "Zbf", "Zbkb", "Zbkc", "Zbkx", "Zbm", "Zbp", "Zbpbo", "Zbr", "Zbs", "Zbt",
+        "Zk", "Zkn", "Zknd", "Zkne", "Zknh", "Zkr", "Zks", "Zksed", "Zksh", "Zkt",
+        "Zpn", "Zpsf",
+        "Svnapot"
+] + Zve_extensions + Zvl_extensions
+
+Zb_extensions = ["Zba", "Zbb", "Zbc", "Zbe", "Zbf", "Zbm", "Zbp", "Zbr", "Zbs", "Zbt"]
+Zp_extensions = ["Zbpbo", "Zpn", "Zpsf"]
+
+isa_regex = \
+        re.compile("^RV(32|64|128)[IE][ACDFGHJLMNPQSTUV]*(("+'|'.join(Z_extensions)+")(_("+'|'.join(Z_extensions)+"))*){,1}(X[a-z0-9]*)*(_X[a-z0-9]*)*$")
