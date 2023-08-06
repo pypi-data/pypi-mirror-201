cb0263f667dc1fae97f4b7721dac602203d548a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:32 +0000

    Add package zmxtools/0.1.4/zmxtools-0.1.4-py3-none-any.whl

---
+++ packages/zmxtools/zmxtools-0.1.4-py3-none-any.whl/zmxtools/__init__.py
@@ -0,0 +1,28 @@
+import logging
+from importlib.metadata import PackageNotFoundError, version
+
+import coloredlogs
+
+formatter_class = coloredlogs.ColoredFormatter
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    __version__ = '0.0.0'
+
+__all__ = ['__version__', 'zar']
+
+# create logger
+log = logging.getLogger(__name__)
+log.level = logging.INFO
+
+# create formatter and add it to the handlers
+_formatter = formatter_class('%(asctime)s|%(name)s-%(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S.%f')
+_debug_formatter = logging.Formatter(  # Don't use colored logs
+    '%(asctime)s|%(name)s-%(levelname)5s %(threadName)s:%(filename)s:%(lineno)s:%(funcName)s| %(message)s',
+)
+
+# create console handler
+_ch = logging.StreamHandler()
+_ch.setFormatter(_formatter)
+log.addHandler(_ch)  # add the handler to the logger
